24f0a6008bb7b115baf085d8330384752cb44b89
a-kozhanov <a.kozhanov@outlook.com>
2019-07-02 11:26:24 +0300

    Added solutinon "Palindrome Strings"

---
+++ CodeWars/PalindromeStrings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+//https://www.codewars.com/kata/57a5015d72292ddeb8000b31
+//https://www.codewars.com/kata/57a5015d72292ddeb8000b31/solutions/csharp
+
+//Palindrome Strings
+
+//A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+//This includes capital letters, punctuation, and word dividers.
+//Implement a function that checks if something is a palindrome.
+
+//Examples
+//isPalindrome("anna")   ==> true
+//isPalindrome("walter") ==> false
+//isPalindrome(12321)    ==> true
+//isPalindrome(123456)   ==> false
+
+namespace CodeWars
+{
+    public class PalindromeStrings
+    {
+        public static bool IsPalindrome(object line)
+        {
+            return line.ToString() == string.Concat(line.ToString().Reverse());
+        }
+
+
+        //public static bool IsPalindrome(object line) => line.ToString() == String.Join("", line.ToString().Reverse());
+
+
+        //public static bool IsPalindrome(object o) => o.ToString().SequenceEqual(o.ToString().Reverse());
+
+
+        //public static bool IsPalindrome(object line)
+        //{
+        //    string str = line.ToString();
+        //    string reverseStr = new string(str.Reverse().ToArray());
+        //    return str == reverseStr;
+        //}
+
+
+        //public static bool IsPalindrome(object line)
+        //{
+        //    char[] str = line.ToString().ToCharArray();
+        //    char[] reverseStr = str.Reverse<char>().ToArray();
+        //    return new string(str).Equals(new string(reverseStr));
+        //}
+
+        //public static bool IsPalindrome(object line)
+        //{
+        //    return line.ToString() == new String(line.ToString().ToCharArray().Reverse().ToArray());
+        //}
+
+
+
+    }
+
+}
---
+++ CodeWarsTests/PalindromeStringsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class PalindromeStringsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(true, PalindromeStrings.IsPalindrome("anna"));
+            Assert.AreEqual(false, PalindromeStrings.IsPalindrome("walter"));
+            Assert.AreEqual(true, PalindromeStrings.IsPalindrome(12321));
+            Assert.AreEqual(false, PalindromeStrings.IsPalindrome(123456));
+        }
+    }
+}
