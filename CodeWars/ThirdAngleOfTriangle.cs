ca78557b5e947b8ce1657492010f4abe2775a53f
a-kozhanov <a.kozhanov@outlook.com>
2019-07-03 19:27:34 +0300

    Added solution "Third Angle of a Triangle"

---
+++ CodeWars/ThirdAngleOfTriangle.cs
@@ -0,0 +1,22 @@
+﻿
+//https://www.codewars.com/kata/third-angle-of-a-triangle/train/csharp
+//https://www.codewars.com/kata/5a023c426975981341000014/solutions/csharp
+
+//Third Angle of a Triangle
+
+//You are given two angles (in degrees) of a triangle.
+//Write a function to return the 3rd.
+//Note: only positive integers will be tested.
+
+using System;
+
+namespace CodeWars
+{
+    public class ThirdAngleOfTriangle
+    {
+        public static int OtherAngle(int a, int b)
+        {
+            return 180 - (a + b);
+        }
+    }
+}
---
+++ CodeWarsTests/ThirdAngleOfTriangleTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThirdAngleOfTriangleTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(ThirdAngleOfTriangle.OtherAngle(30, 60), Is.EqualTo(90));
+            Assert.That(ThirdAngleOfTriangle.OtherAngle(60, 60), Is.EqualTo(60));
+            Assert.That(ThirdAngleOfTriangle.OtherAngle(43, 78), Is.EqualTo(59));
+            Assert.That(ThirdAngleOfTriangle.OtherAngle(10, 20), Is.EqualTo(150));
+        }
+    }
+}
