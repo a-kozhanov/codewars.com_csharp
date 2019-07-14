dad1a8105ac0fe09362e56c2224e13155735cc50
a-kozhanov <a.kozhanov@outlook.com>
2019-07-14 08:47:36 +0300

    Added solution "Array plus array"

---
+++ CodeWars/ArrayPlusArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+//https://www.codewars.com/kata/array-plus-array/train/csharp
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/solutions/csharp
+
+//Array plus array
+
+//I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+//P.S.Each array includes only integer numbers.Output is a number too.
+
+namespace CodeWars
+{
+    public class KataArrayPlusArray
+    {
+        public static int ArrayPlusArray(int[] arr1, int[] arr2)
+        {
+            return arr1.Sum() + arr2.Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/ArrayPlusArrayTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    class ArrayPlusArrayTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(21, KataArrayPlusArray.ArrayPlusArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+            Assert.AreEqual(-21, KataArrayPlusArray.ArrayPlusArray(new int[] { -1, -2, -3 }, new int[] { -4, -5, -6 }));
+            Assert.AreEqual(15, KataArrayPlusArray.ArrayPlusArray(new int[] { 0, 0, 0 }, new int[] { 4, 5, 6 }));
+            Assert.AreEqual(2100, KataArrayPlusArray.ArrayPlusArray(new int[] { 100, 200, 300 }, new int[] { 400, 500, 600 }));
+        }
+    }
+}
