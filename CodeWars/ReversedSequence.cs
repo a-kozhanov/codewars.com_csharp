ca5ceebe8f63ad3b36a36154734cbf3ec691a220
a-kozhanov <a.kozhanov@outlook.com>
2019-07-15 21:23:14 +0300

    Added solution "Reversed sequence"

---
+++ CodeWars/ReversedSequence.cs
@@ -0,0 +1,48 @@
+﻿
+//https://www.codewars.com/kata/reversed-sequence/train/csharp
+//https://www.codewars.com/kata/5a00e05cc374cb34d100000d/solutions/csharp
+
+//Reversed sequence
+
+//Get the number n (n>0) to return the reversed sequence from n to 1.
+//Example : n=5 >> [5,4,3,2,1]
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ReversedSequence
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            return Enumerable.Range(1, n).Reverse().ToArray();
+        }
+
+        //public static int[] ReverseSeq(int n)
+        //{
+        //    int[] reversed = new int[n];
+        //    for(int i=0;i<n;i++){
+        //        reversed[i]=n-i;
+        //    }
+        //    return reversed;
+        //}
+
+
+        //public static int[] ReverseSeq(int n) => Enumerable.Range(0, n).Select(x => n - x).ToArray();
+
+
+        //public static int[] ReverseSeq(int n)
+        //{
+        //    return new int[n].Select(el => el = n--).ToArray();
+        //}
+
+
+        //public static int[] ReverseSeq(int n)
+        //{
+        //    return (from number in Enumerable.Range(1, n) select number).Reverse().ToArray();
+        //}
+
+
+
+    }
+}
---
+++ CodeWarsTests/ReversedSequenceTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReversedSequenceTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(ReversedSequence.ReverseSeq(5), Is.EqualTo(new int[] { 5, 4, 3, 2, 1 }));
+        }
+    }
+}
