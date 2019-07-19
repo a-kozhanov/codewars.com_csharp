8fb2861ef6911152b3f18279d542ebbf49d40f4a
a-kozhanov <a.kozhanov@outlook.com>
2019-07-19 17:13:11 +0300

    Added solution "Basic variable assignment" 8 kyu

---
+++ CodeWars/8kyu/BasicVariableAssignment.cs
@@ -0,0 +1,19 @@
+﻿
+//https://www.codewars.com/kata/basic-variable-assignment/train/csharp
+//https://www.codewars.com/kata/50ee6b0bdeab583673000025/solutions/csharp
+
+//Basic variable assignment
+//8 kyu
+
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+namespace CodeWars
+{
+    public class BasicVariableAssignment
+    {
+        public static string A = "code";
+        public static string B = "wa.rs";
+        public static string Name = A + B;
+    }
+}
--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -10,15 +10,9 @@ namespace ConsoleTestApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            //Console.WriteLine(SumMultiples.SumMul(2, 9));
-
-            Console.WriteLine(IsDivisibleByXAndY.isDivisible(8, 3, 4));
-            Console.WriteLine(IsDivisibleByXAndY.isDivisible(3, 3, 4));
-            Console.WriteLine(IsDivisibleByXAndY.isDivisible(8, 3, 4));
-
-
 
         }
     }
