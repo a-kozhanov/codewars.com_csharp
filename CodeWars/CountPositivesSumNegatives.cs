36746937ced8c0dc1821b343d156250ef5505672
a-kozhanov <a.kozhanov@outlook.com>
2019-07-24 23:19:19 +0300

    Added solution "Count of positives / sum of negatives"

---
+++ CodeWars/CountPositivesSumNegatives.cs
@@ -0,0 +1,62 @@
+﻿
+//https://www.codewars.com/kata/count-of-positives-slash-sum-of-negatives/train/csharp
+//https://www.codewars.com/kata/576bb71bbbcf0951d5000044/solutions/csharp
+
+//Count of positives / sum of negatives
+
+//Given an array of integers.
+//Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+//If the input array is empty or null, return an empty array.
+//Example
+//For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataCountPositivesSumNegatives
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            if (input == null || input.Length == 0) return new int[] { };
+            return new[] { input.Count(x => x > 0), input.Where(i => i < 0).Sum() };
+        }
+
+        //public static int[] CountPositivesSumNegatives(int[] input)
+        //{
+        //    if(input == null || !input.Any())
+        //    {
+        //        return new int[] {};
+        //    }
+
+        //    int countPositives = input.Count(n => n > 0);
+        //    int sumNegatives = input.Where(n => n < 0).Sum();
+
+        //    return new int[] { countPositives, sumNegatives };
+        //}
+
+        //public static int[] CountPositivesSumNegatives(int[] input)    
+        //{   
+        //    return (input == null || input.Length ==0) ? new int[0] : new int[] { input.Count(o => o > 0), input.Where(o => o < 0).Sum() };
+        //}
+
+        //public static int[] CountPositivesSumNegatives(int[] input)
+        //{
+        //    return (input == null || !input.Any()) ? new int[]{} :
+        //        new int[] {input.Where(x => x > 0).Count(), input.Where(x => x < 0).Sum()};
+        //}
+
+        //public static int[] CountPositivesSumNegatives(int[] input)
+        //{
+        //    return input == null || !input.Any() 
+        //        ? new int[] {} 
+        //        : new int[] { input.Count(x => x > 0) , input.Where(x => x < 0).Sum() };
+        //}
+
+        //public static int[] CountPositivesSumNegatives(int[] input)
+        //{
+        //    return input == null || input.Length == 0 ? new int[0] : new int[] {input.Where(x => x > 0).Count(), input.Where(x => x < 0).Sum() }; 
+        //}
+    }
+}
---
+++ CodeWarsTests/CountPositivesSumNegativesTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CountPositivesSumNegativesTests
+    {
+        [Test]
+        public void CountPositivesSumNegatives_BasicTest()
+        {
+            int[] expectedResult = new int[] { 10, -65 };
+
+            Assert.AreEqual(expectedResult, KataCountPositivesSumNegatives.CountPositivesSumNegatives(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }));
+        }
+
+        [Test]
+        public void CountPositivesSumNegatives_InputWithZeroes()
+        {
+            int[] expectedResult = new int[] { 8, -50 };
+
+            Assert.AreEqual(expectedResult, KataCountPositivesSumNegatives.CountPositivesSumNegatives(new[] { 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 }));
+        }
+    }
+}
