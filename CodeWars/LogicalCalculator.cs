5027205d5c47d5c36585d76637433ba9ffe6b990
a-kozhanov <a.kozhanov@outlook.com>
2019-07-24 09:41:18 +0300

    Added solution "Logical calculator"

---
+++ CodeWars/LogicalCalculator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+//https://www.codewars.com/kata/logical-calculator/train/csharp
+//https://www.codewars.com/kata/57096af70dad013aa200007b/solutions/csharp
+
+//Logical calculator
+
+//Your task is to calculate logical value of boolean array.Test arrays are one-dimensional and their size is in the range 1-50.
+//Links referring to logical operations: AND, OR and XOR.
+//You should begin at the first value, and repeatedly apply the logical operation across the remaining elements in the array sequentially.
+
+//First Example:
+//Input: true, true, false, operator: AND
+//Steps: true AND true -> true, true AND false -> false
+//Output: false
+
+//Second Example:
+//Input: true, true, false, operator: OR
+//Steps: true OR true -> true, true OR false -> true
+//Output: true
+
+//Third Example:
+//Input: true, true, false, operator: XOR
+//Steps: true XOR true -> false, false XOR false -> false
+//Output: false
+//Input:
+//boolean array, string with operator' s name: 'AND', 'OR', 'XOR'.
+//Output:
+//calculated boolean
+
+
+namespace CodeWars
+{
+    public class LogicalCalculator
+    {
+        public static bool LogicalCalc(bool[] array, string op)
+        {
+            switch (op)
+            {
+                case "AND": return array.Aggregate((x,y) => x & y);
+                case "OR": return array.Aggregate((x, y) => x | y);
+                case "XOR": return array.Aggregate((x, y) => x ^ y);
+                default: throw new System.InvalidOperationException();
+            }
+        }
+
+        //public static bool LogicalCalc(bool[] array, string op)
+        //{
+        //    if (op == "AND")
+        //        return array.All(x => x);
+        //    else if (op == "OR")
+        //        return array.Any(x => x);
+        //    else
+        //        return array.Aggregate((i, j) => i ^ j);
+        //}
+
+
+        //public static bool LogicalCalc(bool[] array, string op)
+        //{
+        //    switch (op)
+        //    {
+        //        case "AND":
+        //            return !array.Contains(false);
+        //        case "OR":
+        //            return array.Contains(true);
+        //        default:
+        //            return array.Aggregate((r, b) => r ^ b);
+        //    }
+        //}
+
+
+        //public static bool LogicalCalc(bool[] array, string op)
+        //{
+        //    return op == "AND" ? array.All(x => x) : op == "OR" ? array.Any(x => x) : array.Aggregate((x, y) => x ^ y);
+        //}
+
+        //public static bool LogicalCalc(bool[] array, string op)
+        //{
+        //    return array.Aggregate((a, b) => op == "AND" ? a && b : op == "OR" ? a || b : a != b);
+        //}
+
+
+        //public static bool LogicalCalc(bool[] array, string op)
+        //{
+        //    Func<bool, bool, bool> logicalFunc;
+        //    switch (op)
+        //    {
+        //        case "AND":
+        //            logicalFunc = (x, y) => x && y;
+        //            break;
+        //        case "OR":
+        //            logicalFunc = (x, y) => x || y;
+        //            break;
+        //        default:
+        //            logicalFunc = (x, y) => x ^ y;
+        //            break;
+        //    }
+        //    return array.ToList().Aggregate(logicalFunc);
+        //}
+
+
+    }
+}
---
+++ CodeWarsTests/LogicalCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class LogicalCalculatorTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { true, true, true, false }, "AND"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, true, true, false }, "OR"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, true, true, false }, "XOR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { true, true, false, false }, "AND"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, true, false, false }, "OR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { true, true, false, false }, "XOR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { true, false, false, false }, "AND"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, false, false, false }, "OR"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, false, false, false }, "XOR"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, true }, "AND"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true, true }, "OR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { true, true }, "XOR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { false, false }, "AND"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { false, false }, "OR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { false, false }, "XOR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { false }, "AND"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { false }, "OR"));
+            Assert.AreEqual(false, LogicalCalculator.LogicalCalc(new bool[] { false }, "XOR"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true }, "AND"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true }, "OR"));
+            Assert.AreEqual(true, LogicalCalculator.LogicalCalc(new bool[] { true }, "XOR"));
+        }
+    }
+}
