d53476cddbc6c5ec719aa37f54a418f4efe17f5a
a-kozhanov <a.kozhanov@outlook.com>
2019-07-26 15:11:59 +0300

    Added solution "Reversed Words"

---
+++ CodeWars/ReversedWords.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+//https://www.codewars.com/kata/reversed-words/train/csharp
+//https://www.codewars.com/kata/51c8991dee245d7ddf00000e/solutions/csharp
+
+//Reversed Words
+
+//Complete the solution so that it reverses all of the words within the string passed in.
+//Example:
+//Kata.ReverseWords("The greatest victory is that which requires no battle");
+//// should return "battle no requires which that is victory greatest The"
+
+namespace CodeWars
+{
+    public class KataReversedWords
+    {
+        public static string ReverseWords(string str)
+        {
+            return string.Join(" ", str.Split().Reverse());
+        }
+    }
+}
---
+++ CodeWarsTests/ReversedWordsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class ReversedWordsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("world! hello", KataReversedWords.ReverseWords("hello world!"));
+            Assert.AreEqual("this like speak doesn't yoda", KataReversedWords.ReverseWords("yoda doesn't speak like this"));
+            Assert.AreEqual("foobar", KataReversedWords.ReverseWords("foobar"));
+            Assert.AreEqual("kata editor", KataReversedWords.ReverseWords("editor kata"));
+            Assert.AreEqual("boat your row row row", KataReversedWords.ReverseWords("row row row your boat"));
+        }
+    }
+}
