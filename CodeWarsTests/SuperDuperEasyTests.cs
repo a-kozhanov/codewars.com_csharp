63e0cec4a004d672e5dae49362c09b418a64f8e2
a-kozhanov <a.kozhanov@outlook.com>
2019-07-27 19:14:41 +0300

    Added solution "Super Duper Easy" 8 kyu

---
+++ CodeWarsTests/SuperDuperEasyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SuperDuperEasyTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("Error", SuperDuperEasy.Problem("hello"));
+            Assert.AreEqual("56", SuperDuperEasy.Problem("1"));
+            Assert.AreEqual("256", SuperDuperEasy.Problem("5"));
+            Assert.AreEqual("6", SuperDuperEasy.Problem("0"));
+            Assert.AreEqual("66", SuperDuperEasy.Problem("1,2"));
+        }
+    }
+}
