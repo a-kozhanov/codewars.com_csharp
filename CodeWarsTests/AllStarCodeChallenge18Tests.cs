e90f2dbe315b624589833fa384553cbc04e9d85a
a-kozhanov <a.kozhanov@outlook.com>
2019-07-30 22:42:39 +0300

    Added solution "All Star Code Challenge #18" 8 kyu

---
+++ CodeWarsTests/AllStarCodeChallenge18Tests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AllStarCodeChallenge18Tests
+    {
+        [TestCase("Hello", "o", 1)]
+        [TestCase("Hello", "l", 2)]
+        [TestCase("", "z", 0)]
+        public void BasicTetst(string a, string b, int expected)
+        {
+            Assert.That(AllStarCodeChallenge18.StrCount(a, b), Is.EqualTo(expected));
+        }
+    }
+}
