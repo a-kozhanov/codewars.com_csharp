412ea1a760cef194ce2054411d0a59b0a129dd29
a-kozhanov <a.kozhanov@outlook.com>
2019-08-04 09:35:27 +0300

    Added solution "Simple multiplication"

---
+++ CodeWars/SimpleMultiplication.cs
@@ -0,0 +1,37 @@
+﻿
+//https://www.codewars.com/kata/simple-multiplication/train/csharp
+//https://www.codewars.com/kata/583710ccaa6717322c000105/solutions/csharp
+
+//Simple multiplication
+
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+namespace CodeWars
+{
+    public class SimpleMultiplication
+    {
+        public static int Multiply(int x)
+        {
+            return x % 2 == 1 ? x * 9 : x * 8;
+        }
+
+        ////public static int Multiply(int x)
+        ////{
+        ////    return x * (8 + x % 2);
+        ////}
+
+        ////public static int Multiply(int x) => x * ((x % 2) + 8);
+
+        ////public static int Multiply(int x) 
+        ////{
+        ////    return x * (x % 2 == 1 ? 9 : 8);
+        ////}
+
+        ////public static int Multiply(int x) 
+        ////{
+        ////    return x * ((x & 1) == 0 ? 8 : 9);
+        ////}
+
+    }
+}
---
+++ CodeWarsTests/SimpleMultiplicationTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleMultiplicationTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(9, SimpleMultiplication.Multiply(1), "Should return given argument times nine");
+            Assert.AreEqual(16, SimpleMultiplication.Multiply(2), "Should return given argument times eight");
+            Assert.AreEqual(64, SimpleMultiplication.Multiply(8), "Should return given argument times eight");
+            Assert.AreEqual(32, SimpleMultiplication.Multiply(4), "Should return given argument times eight");
+            Assert.AreEqual(45, SimpleMultiplication.Multiply(5), "Should return given argument times nine");
+        }
+    }
+}
