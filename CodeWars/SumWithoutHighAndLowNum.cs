6fc92f3ed587d69a2987f25edf65c88bc187d9a3
a-kozhanov <a.kozhanov@outlook.com>
2019-08-07 19:43:45 +0300

    Added new solutions "SumWithoutHighestAndLowestNumber"

---
+++ CodeWars/SumWithoutHighAndLowNum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://www.codewars.com/kata/sum-without-highest-and-lowest-number/train/csharp
+//https://www.codewars.com/kata/576b93db1129fcf2200001e6/solutions/csharp
+
+//Sum all the numbers of the array(in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+//(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+//Example:
+
+//{ 6, 2, 1, 8, 10 } => 16
+//{ 1, 1, 11, 2, 3 } => 6
+
+//If array is empty, null or None, or if only 1 Element exists, return 0.
+//Note:In C++ instead null an empty vector is used.In C there is no null. ;-) 
+
+
+//-- There's no null in Haskell, therefore Maybe [Int] is used. Nothing represents null.
+//Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+//I have created other katas.Have a look if you like coding and challenges.
+
+
+namespace CodeWars
+{
+    public static class SumWithoutHighestAndLowestNumber
+    {
+        public static int Sum(int[] numbers)
+        {
+            if (numbers != null && numbers.Length > 1)
+                return numbers.Sum() - numbers.Max() - numbers.Min();
+
+            return 0;
+        }
+
+        //Best Practices
+        //public static int Sum(int[] numbers)
+        //{
+        //    return numbers == null || numbers.Length < 2
+        //        ? 0
+        //        : numbers.Sum() - numbers.Max() - numbers.Min();
+        //}
+
+    }
+}
--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -8,8 +8,17 @@ namespace ConsoleTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new string("world".Reverse().ToArray()));
-            Console.WriteLine(ReversedStrings.Solution("world"));
+            //Console.WriteLine(new string("world".Reverse().ToArray()));
+            //Console.WriteLine(ReversedStrings.Solution("world"));
+
+            Console.WriteLine(SumWithoutHighestAndLowestNumber.Sum(new[] { 1, 1, 11, 2, 3 })); //6
+            Console.WriteLine(SumWithoutHighestAndLowestNumber.Sum(new[] { 6, 2, 1, 8, 10 })); //16
+            Console.WriteLine(SumWithoutHighestAndLowestNumber.Sum(new[] { 1, 2, 3 })); 
+            Console.WriteLine(SumWithoutHighestAndLowestNumber.Sum(new[] { 1, 2})); 
+            Console.WriteLine(SumWithoutHighestAndLowestNumber.Sum(new[] { 3 }));
+            Console.WriteLine(SumWithoutHighestAndLowestNumber.Sum(null));
+
+
         }
     }
 }
