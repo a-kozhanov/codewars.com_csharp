d6df2b997b748e2d1bc727950aa4631d8f0da617
a-kozhanov <a.kozhanov@outlook.com>
2019-08-15 23:30:41 +0300

    Added new solution "Thinkful - String Drills: Repeater"

---
+++ CodeWars/StringDrillsRepeater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://www.codewars.com/kata/thinkful-string-drills-repeater/train/csharp
+//https://www.codewars.com/kata/thinkful-string-drills-repeater/solutions/csharp
+
+//Write a function named repeater() that takes two arguments(a string and a number), and returns a new string where the input string is repeated that many times.
+
+//Example:
+//Repeater.repeat("a", 5)
+//should return
+
+//"aaaaa"
+
+
+namespace CodeWars
+{
+    public class StringDrillsRepeater
+    {
+        public static string Repeater(string s, int n)
+        {
+            //return new string(char.Parse(s), n);
+            return string.Concat(Enumerable.Repeat(s, n));
+        }
+    }
+}
---
+++ CodeWarsTests/StringDrillsRepeaterTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StringDrillsRepeaterTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("aaaaa", StringDrillsRepeater.Repeater("a", 5));
+            Assert.AreEqual("NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa", StringDrillsRepeater.Repeater("Na", 16));
+            Assert.AreEqual("Wub Wub Wub Wub Wub Wub ", StringDrillsRepeater.Repeater("Wub ", 6));
+        }
+    }
+}
