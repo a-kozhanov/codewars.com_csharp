0cc31dae7757a396969d1eb5f7ef9a8d08fbcc5f
a-kozhanov <a.kozhanov@outlook.com>
2019-08-20 22:58:24 +0300

    Added new solution "Basic Mathematical Operations"

---
+++ CodeWars/BasicMathematicalOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/csharp
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/solutions/csharp
+
+//Basic Mathematical Operations
+
+//Your task is to create a function that does four basic mathematical operations.
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+
+//Examples:
+//basicOp('+', 4, 7)         // Output: 11
+//basicOp('-', 15, 18)       // Output: -3
+//basicOp('*', 5, 5)         // Output: 25
+//basicOp('/', 49, 7)        // Output: 7
+
+
+namespace CodeWars
+{
+    public class BasicMathematicalOperations
+    {
+        public static double basicOp(char operation, double value1, double value2)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return value1 + value2;
+                case '-':
+                    return value1 - value2;
+                case '*':
+                    return value1 * value2;
+                case '/':
+                    return value1 / value2;
+            }
+            return 0;
+        }
+    }
+
+}
---
+++ CodeWarsTests/BasicMathematicalOperationsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using CodeWars;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BasicMathematicalOperationsTests
+    {
+        [Test]
+        public void StaticTests()
+        {
+            Assert.AreEqual(BasicMathematicalOperations.basicOp('+', 4, 7), 11);
+            Assert.AreEqual(BasicMathematicalOperations.basicOp('-', 15, 18), -3);
+            Assert.AreEqual(BasicMathematicalOperations.basicOp('*', 5, 5), 25);
+            Assert.AreEqual(BasicMathematicalOperations.basicOp('/', 49, 7), 7);
+        }
+    }
+}
