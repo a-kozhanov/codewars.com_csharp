d0a75ee310144704fdd3514b1ee4b6c2c9dbb4d6
a-kozhanov <a.kozhanov@outlook.com>
2019-08-22 10:48:45 +0300

    Added solution "Extra Perfect Numbers (Special Numbers Series #7)"

---
+++ CodeWars/ExtraPerfectNumbers.cs
@@ -0,0 +1,86 @@
+﻿
+//https://www.codewars.com/kata/extra-perfect-numbers-special-numbers-series-number-7/train/csharp
+//https://www.codewars.com/kata/5a662a02e626c54e87000123/solutions/csharp
+
+//Extra Perfect Numbers (Special Numbers Series #7)
+
+//Definition
+//Extra perfect number is the number that first and last bits are set bits.
+
+//Task
+//Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
+
+//Warm-up (Highly recommended)
+//Playing With Numbers Series
+//Notes
+//Number passed is always Positive .
+
+//Returned array/list should contain the extra perfect numbers in ascending order from lowest to highest
+
+//Input >> Output Examples
+//extraPerfect(3)  ==>  return {1,3}
+//Explanation:
+//(1)10 =(1)2
+//First and last bits as set bits.
+
+//(3)10 = (11)2
+//First and last bits as set bits.
+
+//extraPerfect(7)  ==>  return {1,3,5,7}
+//Explanation:
+//(5)10 = (101)2
+//First and last bits as set bits.
+
+//(7)10 = (111)2
+//First and last bits as set bits.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ExtraPerfectNumbers
+    {
+        public static int[] ExtraPerfect(int n)
+        {
+            return Enumerable.Range(1, n).Where(x => x % 2 == 1).ToArray();
+        }
+
+
+        //public static IEnumerable<int> ExtraPerfect(int n)
+        //{
+        //    return Enumerable.Range(1, n).Where(x => x % 2 != 0);
+        //}
+
+
+        //public static int[] ExtraPerfect(int n)
+        //{
+        //    List<int> list = new List<int>();
+
+        //    for (int i = 0; i <= n; i++)
+        //    {
+        //        if (i % 2 != 0)
+        //            list.Add(i);
+        //    }
+        //    return list.ToArray();
+        //}
+
+
+        //public static int[] ExtraPerfect(int n) => Enumerable.Range(1, (n + 1) / 2).Select((i => i * 2 - 1)).ToArray(
+
+
+        //public static int[] ExtraPerfect(int n)
+        //{
+        //    List<int> result = new List<int>();
+        //    for (int i = 1; i <= n; i += 2)
+        //    {
+        //        result.Add(i);
+        //    }
+        //    return result.ToArray();
+        //}
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/ExtraPerfectNumbersTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExtraPerfectNumbersTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { 1, 3 }, ExtraPerfectNumbers.ExtraPerfect(3));
+            Assert.AreEqual(new int[] { 1, 3, 5 }, ExtraPerfectNumbers.ExtraPerfect(5));
+            Assert.AreEqual(new int[] { 1, 3, 5, 7 }, ExtraPerfectNumbers.ExtraPerfect(7));
+            Assert.AreEqual(new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27 }, ExtraPerfectNumbers.ExtraPerfect(28));
+            Assert.AreEqual(new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39 }, ExtraPerfectNumbers.ExtraPerfect(39));
+        }
+    }
+}
