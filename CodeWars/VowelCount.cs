30bf1d55401206a2486ee30f97aa48c88a4c93d6
a-kozhanov <a.kozhanov@outlook.com>
2019-08-23 20:18:56 +0300

    Addded new solution "Vowel Count"

---
+++ CodeWars/VowelCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://www.codewars.com/kata/vowel-count/train/csharp
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/solutions/csharp
+
+//Vowel Count
+//Return the number(count) of vowels in the given string.
+//We will consider a, e, i, o, and u as vowels for this Kata.
+//The input string will only consist of lower case letters and/or spaces.
+
+
+namespace CodeWars
+{
+    public class VowelCount
+    {
+        public static int GetVowelCount(string str)
+        {
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            return str.Count(c => vowels.Contains(c));
+        }
+
+        //All Solutions
+        //public static int GetVowelCount(string str)
+        //{
+        //    return str.Count(i => "aeiou".Contains(i));
+        //}
+
+    }
+}
---
+++ CodeWarsTests/VowelCountTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class VowelCountTests
+    {
+        [Test]
+        public void GetVowelCountTest()
+        {
+            Assert.AreEqual(5, VowelCount.GetVowelCount("abracadabra"), "Nope!");
+        }
+
+        [Test]
+        public void TestNull()
+        {
+            Assert.AreEqual(0, VowelCount.GetVowelCount(""), "Nope!");
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            Assert.AreEqual(4, VowelCount.GetVowelCount("pear tree"), "Nope!");
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            Assert.AreEqual(13, VowelCount.GetVowelCount("o a kak ushakov lil vo kashu kakao"), "Nope!");
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            Assert.AreEqual(168, VowelCount.GetVowelCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), "Nope!");
+        }
+    }
+}
