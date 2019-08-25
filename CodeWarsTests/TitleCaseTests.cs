015e69b95d873e9e841f10d625b4440168e7518c
a-kozhanov <a.kozhanov@outlook.com>
2019-08-25 17:15:09 +0300

    Files moved

---
+++ CodeWarsTests/TitleCaseTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TitleCaseTests
+    {
+        [TestCase("a clash of KINGS", "a an the of", "A Clash of Kings")]
+        [TestCase("THE WIND IN THE WILLOWS", "The In", "The Wind in the Willows")]
+        public void MyTest(string sampleTitle, string sampleMinorWords, string expected)
+        {
+            Assert.AreEqual(expected, TitleCase.titleCase(sampleTitle, sampleMinorWords));
+        }
+        [Test]
+        public void MyTest2()
+        {
+            Assert.AreEqual("", TitleCase.titleCase(""));
+        }
+        [Test]
+        public void MyTest3()
+        {
+            Assert.AreEqual("The Quick Brown Fox", TitleCase.titleCase("the quick brown fox"));
+        }
+    }
+}
