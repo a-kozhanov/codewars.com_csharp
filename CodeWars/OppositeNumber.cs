1e37d347c9fa038806de90a52f08a0d1e8465540
a-kozhanov <a.kozhanov@outlook.com>
2019-08-28 08:59:44 +0300

    Added solution "Opposite number"

---
+++ CodeWars/OppositeNumber.cs
@@ -0,0 +1,48 @@
+﻿
+//https://www.codewars.com/kata/opposite-number/train/csharp
+//https://www.codewars.com/kata/56dec885c54a926dcd001095/solutions/csharp
+
+//Opposite number
+
+//Very simple, given a number, find its opposite.
+
+//Examples:
+//1: -1
+//14: -14
+//-34: 34
+
+using System;
+
+namespace CodeWars
+{
+    public class OppositeNumber
+    {
+        public static int Opposite(int number)
+        {
+            return -number;
+        }
+
+        //public static int Opposite(int n)
+        //{
+        //    return n * -1;
+        //}
+
+        //public static int Opposite(int number)
+        //{
+        //    return number < 0 ? number + ((number * 2) * (-1)) : number - (number * 2);
+        //}
+
+
+        //public static int Opposite(int number)
+        //{
+        //    // Happy Coding
+        //    return number - (number * 2);
+        //}
+
+
+        //public static int Opposite(int number)
+        //{
+        //    return number - number - number;
+        //}
+    }
+}
---
+++ CodeWarsTests/OppositeNumberTests.cs
@@ -0,0 +1,30 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class OppositeNumberTests
+    {
+        [Test]
+        public void Test_1()
+        {
+            Assert.AreEqual(-1, OppositeNumber.Opposite(1));
+        }
+        [Test]
+        public void Test_2()
+        {
+            Assert.AreEqual(0, OppositeNumber.Opposite(0));
+        }
+        [Test]
+        public void Test_3()
+        {
+            Assert.AreEqual(-3000, OppositeNumber.Opposite(3000));
+        }
+        [Test]
+        public void Test_4()
+        {
+            Assert.AreEqual(58000, OppositeNumber.Opposite(-58000));
+        }
+    }
+}
