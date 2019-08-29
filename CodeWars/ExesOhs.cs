540324f441d8c2b8064e58c8bf6a3989d9e8ea4f
a-kozhanov <a.kozhanov@outlook.com>
2019-08-29 22:42:05 +0300

    Added new solution "Exes and Ohs"

---
+++ CodeWars/ExesOhs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://www.codewars.com/kata/exes-and-ohs/train/csharp
+//https://www.codewars.com/kata/55908aad6620c066bc00002a/solutions/csharp
+
+//Exes and Ohs
+
+//Check to see if a string has the same amount of 'x's and 'o's.The method must return a boolean and be case insensitive.The string can contain any char.
+//Examples input/output:
+//XO("ooxx") => true
+//XO("xooxx") => false
+//XO("ooxXm") => true
+//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+//XO("zzoo") => false
+
+namespace CodeWars
+{
+    public class ExesOhs
+    {
+        public static bool XO(string input)
+        {
+            var xCount = 0;
+            var oCount = 0;
+
+            foreach (var c in input.ToUpper())
+            {
+                if (c == 'X') xCount++;
+                if (c == 'O') oCount++;
+            }
+
+            return xCount == oCount;
+        }
+
+        //Best Practices
+        //public static bool XO(string input)
+        //{
+        //    return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
+        //}
+
+        //public static bool XO(string input) => input.Count("xX".Contains) == input.Count("oO".Contains);
+
+        //public static bool XO(string input)
+        //{
+        //    int xCounter = 0;
+        //    int oCounter = 0;
+
+        //    foreach (Char c in input)
+        //    {
+        //        if (Char.ToLower(c) == 'x') xCounter++;
+        //        else if (Char.ToLower(c) == 'o') oCounter++;
+        //    }
+
+        //    return xCounter == oCounter;
+        //}
+
+
+    }
+}
---
+++ CodeWarsTests/ExesOhsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class ExesOhsTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(true, ExesOhs.XO("xo"));
+            Assert.AreEqual(true, ExesOhs.XO("xxOo"));
+            Assert.AreEqual(false, ExesOhs.XO("xxxm"));
+            Assert.AreEqual(false, ExesOhs.XO("Oo"));
+            Assert.AreEqual(false, ExesOhs.XO("ooom"));
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(true, ExesOhs.XO("xo"));
+            Assert.AreEqual(true, ExesOhs.XO("XO"));
+            Assert.AreEqual(true, ExesOhs.XO("xo0"));
+            Assert.AreEqual(false, ExesOhs.XO("xxxoo"));
+            Assert.AreEqual(true, ExesOhs.XO("xxOo"));
+            Assert.AreEqual(true, ExesOhs.XO(""), "Empty string contains equal amount of x and o");
+            Assert.AreEqual(true, ExesOhs.XO("xxxxxoooxooo"));
+            Assert.AreEqual(false, ExesOhs.XO("xxxm"));
+            Assert.AreEqual(false, ExesOhs.XO("ooom"));
+            Assert.AreEqual(false, ExesOhs.XO("Oo"));
+            Assert.AreEqual(true, ExesOhs.XO("abcdefghijklmnopqrstuvwxyz"), "Alphabet contains equal amount of x and o");
+        }
+
+    }
+}
