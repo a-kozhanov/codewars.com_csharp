9b32250d048dfaebcf24a7e78ed21bee68eb5e82
a-kozhanov <a.kozhanov@outlook.com>
2019-09-02 19:56:01 +0300

    Added new solution "String repeat"

---
+++ CodeWars/StringRepeat.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://www.codewars.com/kata/string-repeat/train/csharp
+//
+
+//String repeat
+
+//repeatStr(6, "I") // "IIIIII"
+//repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+
+namespace CodeWars
+{
+    public class StringRepeat
+    {
+        public static string repeatStr(int n, string s)
+        {
+            return string.Concat(Enumerable.Repeat(s, n));
+        }
+
+        //Best Practices
+        //public static string repeatStr(int n, string s)
+        //{
+        //    return String.Concat(Enumerable.Repeat(s, n));
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    return new StringBuilder(s.Length * n).Insert(0, s, n).ToString();
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    StringBuilder builder = new StringBuilder();
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        builder.Append(s);
+        //    }
+        //    return builder.ToString();
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    var res = "";
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        res += s;
+        //    }
+        //    return res;
+        //}
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    return string.Join("", Enumerable.Repeat(s, n));
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    string t = "";
+
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        t = s + t;
+        //    }
+        //    return t;
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    return new string(s[0], n).Replace(s[0] + "", s);
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    if (n == 1) return s;
+        //    return repeatStr(n - 1, s) + s;
+        //}
+
+
+        //public static string repeatStr(int n, string s)
+        //{
+        //    return string.Join(s, new string[n + 1]);
+        //}
+
+
+
+    }
+}
---
+++ CodeWarsTests/StringRepeatTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StringRepeatTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual("***", StringRepeat.repeatStr(3, "*"));
+        }
+    }
+}
