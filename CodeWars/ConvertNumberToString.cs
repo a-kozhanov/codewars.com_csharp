1932fcd8710df3ec43edbda22eeb784316852fec
a-kozhanov <a.kozhanov@outlook.com>
2019-09-02 21:32:32 +0300

    Added new solution "Convert a Number to a String!"

---
+++ CodeWars/ConvertNumberToString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.codewars.com/kata/convert-a-number-to-a-string/train/csharp
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/csharp
+
+//Convert a Number to a String!
+
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+
+//Examples:
+//Kata.NumberToString(123) => "123";
+//Kata.NumberToString(999) => "999";
+
+namespace CodeWars
+{
+    public class ConvertNumberToString
+    {
+        public static string NumberToString(int num)
+        {
+            return num.ToString();
+        }
+        //Best Practices
+
+    }
+}
---
+++ CodeWarsTests/ConvertNumberToStringTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertNumberToStringTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(67).Returns("67");
+                yield return new TestCaseData(79585).Returns("79585");
+                yield return new TestCaseData(1 + 2).Returns("3");
+                yield return new TestCaseData(1 - 2).Returns("-1");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(int num) => ConvertNumberToString.NumberToString(num);
+
+        private static Random rnd = new Random();
+
+        [Test]
+        public void RandomTests()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                int num = rnd.Next(-1000, 1001);
+                Assert.AreEqual("" + num, ConvertNumberToString.NumberToString(num));
+            }
+        }
+    }
+}
