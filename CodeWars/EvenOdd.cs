ff100660cf1a01da491735166bd1e14f159b4f14
a-kozhanov <a.kozhanov@outlook.com>
2019-09-03 19:27:47 +0300

    Added new solution "Even or Odd"

---
+++ CodeWars/EvenOdd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.codewars.com/kata/even-or-odd/train/csharp
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/solutions/csharp
+
+//Even or Odd
+
+//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+namespace CodeWars
+{
+    public class EvenOdd
+    {
+        public static string EvenOrOdd(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+
+        //public static string EvenOrOdd(int number)
+        //{
+        //    return Convert.ToBoolean(number & 1) ? "Odd" : "Even";
+        //}
+
+        //public static string EvenOrOdd(int number)
+        //{
+        //    return Convert.ToBoolean(number & 1) ? "Odd" : "Even";
+        //}
+    }
+}
---
+++ CodeWarsTests/EvenOddTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EvenOddTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual("Even", EvenOdd.EvenOrOdd(2));
+            Assert.AreEqual("Odd", EvenOdd.EvenOrOdd(1));
+            Assert.AreEqual("Even", EvenOdd.EvenOrOdd(0));
+            Assert.AreEqual("Odd", EvenOdd.EvenOrOdd(7));
+            Assert.AreEqual("Odd", EvenOdd.EvenOrOdd(-1));
+        }
+    }
+}
