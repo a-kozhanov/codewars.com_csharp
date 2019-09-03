89bb1370e5eb6929e0b83bdce050f581eca4f9f2
a-kozhanov <a.kozhanov@outlook.com>
2019-09-03 16:56:57 +0300

    Added new solution "Century From Year"

---
+++ CodeWars/CenturyFromYear.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars;
+
+//https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/solutions/csharp
+//https://www.codewars.com/kata/century-from-year/train/csharp
+
+//Century From Year
+
+//Description:
+//Introduction
+//The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+//Task :
+//Given a year, return the century it is in.
+
+//Input , Output Examples ::
+//centuryFromYear(1705)  returns(18)
+//centuryFromYear(1900)  returns(19)
+//centuryFromYear(1601)  returns(17)
+//centuryFromYear(2000)  returns(20)
+//Hope you enjoy it .. Awaiting for Best Practice Codes
+
+//Enjoy Learning !!!
+
+namespace CodeWars
+{
+    public class CenturyFromYear
+    {
+        public static int centuryFromYear(int year)
+        {
+            int result = year / 100;
+            if (year % 100 > 0) result++;
+            return result;
+        }
+
+        //Best Practices
+        //public static int СenturyFromYear(int year) => (year - 1) / 100 + 1;
+
+
+        //public static int СenturyFromYear(int year)
+        //{
+        //    return (year - 1) / 100 + 1;
+        //}
+
+        //public static int СenturyFromYear(int year) => year % 100 == 0 ? year / 100 : year / 100 + 1;
+
+
+        //public static int СenturyFromYear(int year)
+        //{
+        //    return (int)Math.Ceiling(year * 0.01);
+        //}
+
+
+
+        //public static int СenturyFromYear(int year) => (year + 99) / 100;
+
+
+
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/CenturyFromYearTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+        [TestFixture]
+        class CenturyFromYearTests
+        {
+            private int check(int year)
+            {
+                return year % 100 == 0 ? year / 100 : year / 100 + 1;
+            }
+
+            [Test]
+            public void BasicTest()
+            {
+                Assert.AreEqual(21, CenturyFromYear.centuryFromYear(2018));
+                Assert.AreEqual(15, CenturyFromYear.centuryFromYear(1455));
+                Assert.AreEqual(18, CenturyFromYear.centuryFromYear(1703));
+                Assert.AreEqual(18, CenturyFromYear.centuryFromYear(1800));
+            }
+
+            [Test]
+            public void RandomTest()
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 100; i++)
+                {
+                    int n = rnd.Next(100, 1000000);
+                    Assert.That(CenturyFromYear.centuryFromYear(n), Is.EqualTo(check(n)));
+                }
+
+            }
+        }
+}
