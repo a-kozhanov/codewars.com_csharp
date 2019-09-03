04494bb1de7f05d2308163d2644323af3717704e
a-kozhanov <a.kozhanov@outlook.com>
2019-09-03 19:22:12 +0300

    Added new solution "Convert a Number to a String!"

---
+++ CodeWars/ConvertStringToNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.codewars.com/kata/544675c6f971f7399a000e79
+//
+
+//Convert a String to a Number!
+
+//Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+//Description
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+//Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+//Examples
+//StringToNumber("1234") == 1234
+//StringToNumber("605") == 605
+//StringToNumber("1405") == 1405
+//StringToNumber("-7") == -7
+
+namespace CodeWars
+{
+    public class ConvertStringToNumber
+    {
+        public static int StringToNumber(String str)
+        {
+            return int.Parse(str);
+        }
+
+        //public static int StringToNumber(String str)
+        //{
+        //    return Convert.ToInt32(str);
+        //}
+
+        //public static int StringToNumber(String str)
+        //{
+        //    var ok = int.TryParse(str, out var number);
+        //    return number;
+        //}
+    }
+}
---
+++ CodeWarsTests/ConvertStringToNumberTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertStringToNumberTests
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual(1234, ConvertStringToNumber.StringToNumber("1234"));
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual(605, ConvertStringToNumber.StringToNumber("605"));
+        }
+
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual(1405, ConvertStringToNumber.StringToNumber("1405"));
+        }
+
+        [Test]
+        public void test4()
+        {
+            Assert.AreEqual(-7, ConvertStringToNumber.StringToNumber("-7"));
+        }
+    }
+}
