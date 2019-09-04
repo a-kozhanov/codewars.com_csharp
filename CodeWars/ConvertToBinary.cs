e42e1ee4de3f8385b6f5a0a1fea29d3ff7fadf1f
a-kozhanov <a.kozhanov@outlook.com>
2019-09-04 19:32:34 +0300

    Added new solution "Convert to Binary"

---
+++ CodeWars/ConvertToBinary.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.codewars.com/kata/convert-to-binary/train/csharp
+//https://www.codewars.com/kata/59fca81a5712f9fa4700159a/solutions/csharp
+
+//Convert to Binary
+
+//Task Overview
+//Given a non-negative integer n, write a function toBinary/ToBinary which returns that number in a binary format.
+
+//Documentation:
+//Kata.ToBinary Method(Int32)
+//Returns the binary representation of a non-negative integer as an integer.
+
+//Syntax
+//public static int ToBinary(
+//int n
+//)
+
+//Parameters
+//n
+//Type: System.Int32
+//The integer to convert.
+
+//Return Value
+//Type: System.Int32
+//The binary representation of the argument as an integer.
+//to_binary(1)  /* should return 1 */
+//to_binary(5)  /* should return 101 */
+//to_binary(11) /* should return 1011 */
+
+namespace CodeWars
+{
+    public class ConvertToBinary
+    {
+        public static int ToBinary(int n)
+        {
+            return int.Parse(Convert.ToString(n, 2));
+        }
+
+        //Best Practices
+        //public static int ToBinary(int n)
+        //{
+        //    return Convert.ToInt32(Convert.ToString(n, 2));
+        //}
+
+        //public static int ToBinary(int n)
+        //{
+        //    return int.Parse(Convert.ToString(n, 2));
+        //}
+
+        //public static int ToBinary(int n)
+        //{
+        //    int q = n, tmp = 0, m = 1;
+        //    while (q != 0)
+        //    {
+        //        tmp += q % 2 * m;
+        //        q = q / 2;
+        //        m *= 10;
+        //    }
+        //    return tmp;
+        //}
+
+        //public static int ToBinary(int n)
+        //{
+        //    int result = 0;
+        //    for (int i = 0; n > 0; i++, n /= 2)
+        //    {
+        //        int pow = 1;
+        //        int exp = i;
+        //        while (exp-- != 0) { pow *= 10; }
+        //        result = n % 2 * pow + result;
+        //    }
+        //    return result;
+        //}
+
+        //public static int ToBinary(int num)
+        //{
+        //    string stringBinary = Convert.ToString(num, 2);
+        //    return Convert.ToInt32(stringBinary);
+        //}
+    }
+}
---
+++ CodeWarsTests/ConvertToBinaryTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertToBinaryTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(1).Returns(1).SetDescription("Arguments: (n: 1)\n      Expected: 1");
+                yield return new TestCaseData(2).Returns(10).SetDescription("Arguments: (n: 2)\n      Expected: 10");
+                yield return new TestCaseData(3).Returns(11).SetDescription("Arguments: (n: 3)\n      Expected: 11");
+                yield return new TestCaseData(5).Returns(101).SetDescription("Arguments: (n: 5)\n      Expected: 101");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(int n) =>
+            ConvertToBinary.ToBinary(n);
+    }
+}
