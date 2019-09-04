06c64f6bee16036267cd8e42a3fd27fd6fc8ca05
a-kozhanov <a.kozhanov@outlook.com>
2019-09-04 19:44:08 +0300

    Added new solution "Bin to Decimal"

---
+++ CodeWars/BinToDecimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//
+//https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/solutions/csharp
+
+//Bin to Decimal
+
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+namespace CodeWars
+{
+    public class BinToDecimal
+    {
+        public static int binToDec(string s)
+        {
+            return Convert.ToInt32(s, 2);
+        }
+    }
+}
---
+++ CodeWarsTests/BinToDecimalTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BinToDecimalTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(0, BinToDecimal.binToDec("0"));
+            Assert.AreEqual(1, BinToDecimal.binToDec("1"));
+            Assert.AreEqual(2, BinToDecimal.binToDec("10"));
+            Assert.AreEqual(3, BinToDecimal.binToDec("11"));
+            Assert.AreEqual(6, BinToDecimal.binToDec("110"));
+            Assert.AreEqual(170, BinToDecimal.binToDec("10101010"));
+            Assert.AreEqual(255, BinToDecimal.binToDec("11111111"));
+        }
+    }
+}
