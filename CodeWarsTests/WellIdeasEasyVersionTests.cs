729b681bc9128846ad4308b5ec0657adbe5b528e
a-kozhanov <a.kozhanov@outlook.com>
2019-09-05 23:27:54 +0300

    Added solution "Well of Ideas - Easy Version"

---
+++ CodeWarsTests/WellIdeasEasyVersionTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WellIdeasEasyVersionTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Fail!", WellIdeasEasyVersion.Well(new string[] { "bad", "bad", "bad" }));
+            Assert.AreEqual("Publish!", WellIdeasEasyVersion.Well(new string[] { "good", "bad", "bad", "bad", "bad" }));
+            Assert.AreEqual("I smell a series!", WellIdeasEasyVersion.Well(new string[] { "good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good" }));
+        }
+    }
+}
