f4b83ad5be84421634ca96b4b0ffe884f45d21c6
a-kozhanov <a.kozhanov@outlook.com>
2019-09-05 23:42:45 +0300

    Complited solution "Well of Ideas - Easy Version"

--- CodeWars/WellIdeasEasyVersion.cs
+++ CodeWars/WellIdeasEasyVersion.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 //https://www.codewars.com/kata/well-of-ideas-easy-version/train/csharp
-//
+//https://www.codewars.com/kata/57f222ce69e09c3630000212/solutions/csharp
 
 //Well of Ideas - Easy Version
 
@@ -19,7 +20,29 @@ namespace CodeWars
     {
         public static string Well(string[] x)
         {
+            var good = x.Count(s => s == "good");
 
+            if (good == 1 || good == 2) return "Publish!";
+            return good > 2 ? "I smell a series!" : "Fail!";
         }
+
+
+        //Best Practices
+        //public static string Well(string[] x)
+        //{
+        //    int count = x.Count(v => v == "good");
+        //    return (count > 2) ? "I smell a series!" : (count >= 1) ? "Publish!" : "Fail!";
+        //}
+
+
+        //public static string Well(string[] x)
+        //{
+        //    var good = x.Count(y => y == "good");
+        //    return good == 0 ? "Fail!" : good > 2 ? "I smell a series!" : "Publish!";
+        //}
+
+        //public static string Well(string[] x)
+        //    => new string[] { "Fail!", "Publish!", "Publish!", "I smell a series!" }[Math.Min(3, x.Count(e => e == "good"))];
+
     }
 }
