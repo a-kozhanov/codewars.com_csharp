0df0c07f19e52e1403d32c215b33c4ceea6466ca
a-kozhanov <a.kozhanov@outlook.com>
2019-09-06 19:54:30 +0300

    Added new solution "Find numbers which are divisible by given number"

---
+++ CodeWars/FindNumbersDivisible.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+//https://www.codewars.com/kata/55edaba99da3a9c84000003b
+//https://www.codewars.com/kata/55edaba99da3a9c84000003b/solutions/csharp
+
+//Find numbers which are divisible by given number
+
+//Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.First argument is an array of numbers and the second is the divisor.
+
+//Example
+//DivisibleBy(new int[]{ 1, 2, 3, 4, 5, 6}, 2) == new int[] {2, 4, 6}
+
+namespace CodeWars
+{
+    public class FindNumbersDivisible
+    {
+        public static int[] DivisibleBy(int[] numbers, int divisor)
+        {
+            return numbers.Where(x => x % divisor == 0).ToArray();
+        }
+
+        //public static int[] DivisibleBy(int[] numbers, int divisor)
+        //{
+        //    List<int> result = new List<int>();
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        if (numbers[i] % divisor == 0)
+        //        {
+        //            result.Add(numbers[i]);
+        //        }
+        //    }
+        //    return result.ToArray();
+        //}
+
+
+        //public static int[] DivisibleBy(int[] numbers, int divisor)
+        //{
+        //    List<int> divisibles = new List<int>();
+
+        //    for (int i = 0; i < numbers.Length; i++)
+        //        if (numbers[i] % divisor == 0) divisibles.Add(numbers[i]);
+
+        //    return divisibles.ToArray();
+        //}
+
+    }
+}
---
+++ CodeWarsTests/FindNumbersDivisibleTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindNumbersDivisibleTests
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, 2, ExpectedResult = new int[] { 2, 4, 6 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, 3, ExpectedResult = new int[] { 3, 6 })]
+        [TestCase(new int[] { 0, 1, 2, 3, 4, 5, 6 }, 4, ExpectedResult = new int[] { 0, 4 })]
+        public static int[] FixedTest(int[] numbers, int divisor)
+        {
+            return FindNumbersDivisible.DivisibleBy(numbers, divisor);
+        }
+
+        private static Random rnd = new Random();
+
+        [Test, Description("Random Tests")]
+        public static void RandomTest()
+        {
+            const int Tests = 1000;
+
+            for (int i = 0; i < Tests; ++i)
+            {
+                int divisor = rnd.Next(1, 11);
+                int[] numbers = new int[rnd.Next(10, 101)].Select(_ => rnd.Next(1, 1001)).ToArray();
+
+                int[] expected = Solution(numbers, divisor);
+                int[] actual = FindNumbersDivisible.DivisibleBy(numbers, divisor);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        private static int[] Solution(int[] numbers, int divisor)
+        {
+            return new List<int>(numbers).Where(x => x % divisor == 0).ToArray();
+        }
+    }
+}
