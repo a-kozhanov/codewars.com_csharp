1ce7bedd5c3f628e8b26407808b504e7f65948ac
a-kozhanov <a.kozhanov@outlook.com>
2019-09-06 22:51:39 +0300

    Added solution "Grasshopper - Personalized Message"

---
+++ CodeWars/GrasshopperPersonalizedMessage.cs
@@ -0,0 +1,38 @@
+﻿
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/csharp
+//https://www.codewars.com/kata/5772da22b89313a4d50012f7/solutions/csharp
+
+//Grasshopper - Personalized Message
+
+//Personalized greeting
+//Create a function that gives a personalized greeting.This function takes two parameters: name and owner.
+
+//Use conditionals to return the proper message:
+//case	return
+//name equals owner   'Hello boss'
+//otherwise   'Hello guest'
+
+namespace CodeWars
+{
+    public class GrasshopperPersonalizedMessage
+    {
+        public static string Greet(string name, string owner)
+        {
+            return name == owner ? "Hello boss" : "Hello guest";
+        }
+
+        //public static string Greet(string name, string owner)
+        //{
+        //    return $"Hello {name == owner ? "boss" : "guest"}";
+        //}
+
+        //public static string Greet(string name, string owner) => $"Hello {(name.Equals(owner) ? "boss" : "guest")}";
+
+        //public static string Greet(string name, string owner)
+        //    => string.Format("Hello {0}", name == owner ? "boss" : "guest");
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/GrasshopperPersonalizedMessageTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperPersonalizedMessageTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual("Hello boss", GrasshopperPersonalizedMessage.Greet("Daniel", "Daniel"));
+            Assert.AreEqual("Hello guest", GrasshopperPersonalizedMessage.Greet("Greg", "Daniel"));
+        }
+    }
+}
