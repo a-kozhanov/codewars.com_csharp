f97a85b119aef16b0b9976b0f110e2f0f3c8332b
a-kozhanov <a.kozhanov@outlook.com>
2019-09-07 13:20:45 +0300

    Added solution "Greatest common divisor"

---
+++ CodeWars/GreatestCommonDivisor.cs
@@ -0,0 +1,21 @@
+﻿
+//https://www.codewars.com/kata/greatest-common-divisor/train/csharp
+//https://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd/solutions/csharp
+
+//Greatest common divisor
+
+//Find the greatest common divisor of two positive integers.The integers can be large, so you need to find a clever solution.
+//The inputs x and y are always greater or equal to 1, so the the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+
+namespace CodeWars
+{
+    public class GreatestCommonDivisor
+    {
+        public static int Gcd(int a, int b)
+        {
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+
+    }
+}
---
+++ CodeWarsTests/GreatestCommonDivisorTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GreatestCommonDivisorTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(30, 12).Returns(6);
+                yield return new TestCaseData(8, 9).Returns(1);
+                yield return new TestCaseData(1, 1).Returns(1);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(int a, int b) => GreatestCommonDivisor.Gcd(a, b);
+    }
+}
