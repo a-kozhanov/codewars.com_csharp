6c82c77de1470407325ec6622d366031f0cad220
a-kozhanov <a.kozhanov@outlook.com>
2019-09-09 21:02:04 +0300

    Added solution "Beginner - Reduce but Grow"

---
+++ CodeWars/ReduceButGrow.cs
@@ -0,0 +1,52 @@
+﻿
+
+//https://www.codewars.com/kata/beginner-reduce-but-grow/train/csharp
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/csharp
+
+//Beginner - Reduce but Grow
+
+//Given a non-empty array of integers, return the result of multiplying the values together in order.Example:
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ReduceButGrow
+    {
+        public static int Grow(int[] x)
+        {
+
+            return x.Aggregate((a, b) => a * b);
+        }
+
+        //public static int Grow(int[] x)
+        //{
+        //    var sum = 1;
+        //    for (var i = 0; i < x.Length; i++)
+        //    {
+        //        sum *= x[i];
+        //    }
+        //    return sum;
+        //}
+
+
+        //public static int Grow(int[] x)
+        //{
+        //    int e = 1;
+        //    foreach (int c in x) { e *= c; }
+        //    return e;
+        //}
+
+        //public static int Grow(int[] x)
+        //{
+        //    int value = 1;
+        //    x.OrderBy(y => y).ToList().ForEach(z => value = value * z);
+        //    return value;
+        //}
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/ReduceButGrowTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReduceButGrowTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(6, ReduceButGrow.Grow(new[] { 1, 2, 3 }));
+            Assert.AreEqual(16, ReduceButGrow.Grow(new[] { 4, 1, 1, 1, 4 }));
+            Assert.AreEqual(64, ReduceButGrow.Grow(new[] { 2, 2, 2, 2, 2, 2 }));
+        }
+
+
+        [Test]
+        public void RandomTests()
+        {
+            var rand = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                var len = rand.Next(2, 8);
+                var x = Enumerable.Range(0, len).Select(a => rand.Next(1, 10)).ToArray();
+
+                var expected = x.Aggregate((a, b) => a * b);
+
+                Assert.AreEqual(expected, ReduceButGrow.Grow(x), "It should work for random inputs too");
+            }
+        }
+    }
