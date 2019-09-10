6165194853dffed553032269f86b583179dcdfbc
a-kozhanov <a.kozhanov@outlook.com>
2019-09-10 19:41:43 +0300

    Added solution "Get the mean of an array"

---
+++ CodeWars/GetMeanArray.cs
@@ -0,0 +1,106 @@
+﻿
+//https://www.codewars.com/kata/get-the-mean-of-an-array/train/csharp
+//https://www.codewars.com/kata/563e320cee5dddcf77000158/solutions/csharp
+
+//Get the mean of an array
+
+//It's the academic year's end, fateful moment of your school report.The averages must be calculated.All the students come to you and entreat you to calculate their average for them.Easy ! You just need to write a script.
+//Return the average of the given array rounded down to its nearest integer.
+//The array will never be empty.
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class GetMeanArray
+    {
+        public static int GetAverage(int[] marks)
+        {
+            return (int)marks.Average();
+        }
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    return marks.Sum() / marks.Length;
+        //}
+
+
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    long marksSum = 0;
+        //    foreach (int mark in marks)
+        //    {
+        //        marksSum += mark;
+        //    }
+        //    long result = marksSum / (long)marks.Length;
+
+        //    return (int)result;
+        //}
+
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    var result = 0;
+
+        //    foreach (var val in marks)
+        //    {
+        //        result += val;
+        //    }
+
+        //    return result / marks.Length;
+        //}
+
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    double now = marks.Average();
+        //    int then = (int)Math.Floor(now);
+        //    return then;
+
+        //}
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < marks.Length; i++)
+        //    {
+        //        sum += marks[i];
+        //    }
+
+        //    return marks.Length == 0 ? 0 : sum / marks.Length;
+        //}
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    int sum = 0, count = 0;
+        //    foreach (int elem in marks)
+        //    {
+        //        sum += elem;
+        //        count++;
+        //    }
+
+        //    return sum / count;
+        //}
+
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    int total = marks.Sum();
+        //    int length = marks.Length;
+        //    return total / length;
+        //}
+
+        //public static int GetAverage(int[] marks)
+        //{
+        //    int average = 0;
+        //    for (int i = 0; i <= marks.Length - 1; i++)
+        //    {
+        //        average += marks[i];
+        //    }
+        //    return average / marks.Length;
+        //}
+
+    }
+}
---
+++ CodeWarsTests/GetMeanArrayTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GetMeanArrayTests
+    {
+        [Test]
+        [TestCase(new int[] { 2, 2, 2, 2 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 5, 10 }, ExpectedResult = 7)]
+        public static int FixedTest(int[] marks)
+        {
+            return GetMeanArray.GetAverage(marks);
+        }
+
+
+        private static int Solution(int[] marks)
+        {
+            return (int)Math.Floor(new List<int>(marks).Average());
+        }
+
+        [Test]
+        public static void RandomTest([Random(1, 10, 98)]int x)
+        {
+            List<int> l = new List<int>();
+            Random r = new Random();
+            for (int i = 0; i < x; i++)
+            {
+                l.Add(r.Next(100) + 1);
+            }
+            int[] arr = l.ToArray();
+            Assert.AreEqual(Solution(arr), GetMeanArray.GetAverage(arr), string.Format("Should work for {0}", string.Join(", ", l.ToArray())));
+        }
+
+
+    }
+}
