4f16ec461d0eae8cd5603d5818f7bac3d20fbe99
a-kozhanov <a.kozhanov@outlook.com>
2019-09-12 22:10:52 +0300

    Added solution "get ascii value of character"

---
+++ CodeWars/GetAsciiValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+//https://www.codewars.com/kata/get-ascii-value-of-character/train/csharp
+//https://www.codewars.com/kata/55acfc59c3c23d230f00006d/solutions/csharp
+
+//get ascii value of character
+
+//for ASCII table, you can refer to http://www.asciitable.com/
+
+namespace CodeWars
+{
+    public class GetAsciiValue
+    {
+        public static int GetASCII(char c)
+        {
+            return c;
+        }
+    }
+}
---
+++ CodeWarsTests/GetAsciiValueTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GetAsciiValueTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(GetAsciiValue.GetASCII('A'), Is.EqualTo(65));
+            Assert.That(GetAsciiValue.GetASCII(' '), Is.EqualTo(32));
+            Assert.That(GetAsciiValue.GetASCII('!'), Is.EqualTo(33));
+        }
+    }
+}
