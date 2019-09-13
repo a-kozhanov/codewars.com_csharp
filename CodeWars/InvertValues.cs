cb6f48a7a226c1fd21b29be10bad9cd7b1bf7820
a-kozhanov <a.kozhanov@outlook.com>
2019-09-13 21:54:18 +0300

    Added solution "Invert values"

---
+++ CodeWars/InvertValues.cs
@@ -0,0 +1,24 @@
+﻿
+//https://www.codewars.com/kata/invert-values/train/csharp
+//https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/solutions/csharp
+
+//Invert values
+
+//Given a set of numbers, return the additive inverse of each.Each positive becomes negatives, and the negatives become positives.
+//invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+//invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+//invert([]) == []
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class InvertNumbers
+    {
+        public static int[] InvertValues(int[] input)
+        {
+            return input.Select(x => -x).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/InvertValuesTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class InvertValuesTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { -1, -2, -3, -4, -5 }, InvertNumbers.InvertValues(new int[] { 1, 2, 3, 4, 5 }));
+            Assert.AreEqual(new int[] { -1, 2, -3, 4, -5 }, InvertNumbers.InvertValues(new int[] { 1, -2, 3, -4, 5 }));
+            Assert.AreEqual(new int[] { }, InvertNumbers.InvertValues(new int[] { }));
+            Assert.AreEqual(new int[] { 0 }, InvertNumbers.InvertValues(new int[] { 0 }));
+        }
+    }
+}
