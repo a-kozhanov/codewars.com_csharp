526b872582219487045742df8805cdb52bd18ab8
a-kozhanov <a.kozhanov@outlook.com>
2019-09-19 19:55:55 +0300

    Added solution "Grasshopper - Messi goals function"

---
+++ CodeWars/GrasshopperMessiGoals.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/csharp
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4/solutions/csharp
+
+//Grasshopper - Messi goals function
+
+//Messi goals function
+//Messi is a soccer player with goals in three leagues:
+//LaLiga
+//Copa del Rey
+//Champions.
+//Complete the goals function to return his total goals (the sum) for all three leagues.
+//Note: the parameter for this function will always be a valid number.
+//Ex :
+//goals(5,10,2) == 5+10+2 = 17
+
+
+namespace CodeWars
+{
+    public class GrasshopperMessiGoals
+    {
+        public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+        {
+            return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+        }
+
+        //        public static int GetGoals(params int[] goalsPerLeague) => goalsPerLeague.Sum();
+        //
+        //        public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+
+    }
+}
---
+++ CodeWarsTests/GrasshopperMessiGoalsTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperMessiGoalsTests
+    {
+        [Test]
+        public void NoGoals()
+        {
+            Assert.AreEqual(GrasshopperMessiGoals.GetGoals(0, 0, 0), 0);
+        }
+        [Test]
+        public void FiftyEightGoals()
+        {
+            Assert.AreEqual(GrasshopperMessiGoals.GetGoals(43, 10, 5), 58);
+        }
+    }
+}
