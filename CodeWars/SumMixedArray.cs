f9d8db314cd0837bb8179bb40435ee4ede0ee6ce
a-kozhanov <a.kozhanov@outlook.com>
2019-09-20 11:53:29 +0300

    Added solution "Sum Mixed Array"

---
+++ CodeWars/SumMixedArray.cs
@@ -0,0 +1,66 @@
+﻿
+//https://www.codewars.com/kata/sum-mixed-array/train/csharp
+//https://www.codewars.com/kata/57eaeb9578748ff92a000009/solutions/csharp
+
+//Sum Mixed Array
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//Return your answer as a number.
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SumMixedArray
+    {
+        public static int SumMix(object[] x)
+        {
+            return x.Sum(n => int.Parse(n.ToString()));
+        }
+
+        //public static int SumMix(object[] x) => x.Sum(ToInt32);
+
+        //public static int SumMix(object[] x)
+        //{
+        //    var number = 0;
+
+        //    foreach (var val in x)
+        //    {
+        //        number += int.Parse(val.ToString());
+        //    }
+        //    return number;
+        //}
+
+
+        //public static int SumMix(object[] x) =>
+        //    x.Sum(i => Convert.ToInt32(i));
+
+        //public static int SumMix(object[] x) => x.Where(w => w is IConvertible).Sum(Convert.ToInt32);
+
+
+        //public static int SumMix(object[] x)
+        //{
+        //    return x.Sum(Convert.ToInt32);
+        //}
+
+
+        //public static int SumMix(object[] x)
+        //{
+        //    return x.Select(n => Convert.ToInt32(n)).Sum();
+
+        //}
+
+        //public static int SumMix(object[] x)
+        //{
+        //    int result = 0;
+        //    foreach(object value in x)
+        //    {
+        //        result += Convert.ToInt32(value);
+        //    }
+        //    return result;
+        //}
+
+
+    }
+}
---
+++ CodeWarsTests/SumMixedArrayTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SumMixedArrayTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(22, SumMixedArray.SumMix(new object[] { 9, 3, "7", "3" }));
+            Assert.AreEqual(42, SumMixedArray.SumMix(new object[] { "5", "0", 9, 3, 2, 1, "9", 6, 7 }));
+            Assert.AreEqual(41, SumMixedArray.SumMix(new object[] { "3", 6, 6, 0, "5", 8, 5, "6", 2, "0" }));
+        }
+
+        [Test, Description("Basic Tests")]
+        public void MyTest()
+        {
+            Assert.AreEqual(22, SumMixedArray.SumMix(new object[] { 9, 3, "7", "3" }));
+            Assert.AreEqual(42, SumMixedArray.SumMix(new object[] { "5", "0", 9, 3, 2, 1, "9", 6, 7 }));
+            Assert.AreEqual(41, SumMixedArray.SumMix(new object[] { "3", 6, 6, 0, "5", 8, 5, "6", 2, "0" }));
+            Assert.AreEqual(45, SumMixedArray.SumMix(new object[] { "1", "5", "8", 8, 9, 9, 2, "3" }));
+            Assert.AreEqual(61, SumMixedArray.SumMix(new object[] { 8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7 }));
+        }
+
+        private static Random rnd = new Random();
+
+        private static object[] names = new object[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        private static int Solution(object[] x) =>
+            x.Sum(v => (v as string == null) ? (int)v : Int32.Parse((string)v));
+
+        [Test, Description("Random Tests")]
+        public void RandomTest()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                object[] test = new object[rnd.Next(1, 31)];
+                for (int j = 0; j < test.Length; ++j)
+                {
+                    test[j] = names[rnd.Next(0, names.Length)];
+                }
+                int expected = Solution(test);
+                int actual = SumMixedArray.SumMix(test);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
