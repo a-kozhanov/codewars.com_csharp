0f1629be06b1a000cd6514a03e95ee36b2842d81
a-kozhanov <a.kozhanov@outlook.com>
2019-09-21 22:20:22 +0300

    Added solution "Student's Final Grade"

---
+++ CodeWars/StudentFinalGrade.cs
@@ -0,0 +1,89 @@
+﻿
+
+//https://www.codewars.com/kata/students-final-grade/train/csharp
+//https://www.codewars.com/kata/5ad0d8356165e63c140014d4/solutions/csharp
+
+//Student's Final Grade
+
+//Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+//This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+//This function should return a number (final grade). There are four types of final grades:
+//100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+//90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+//75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+//0, in other cases
+//Examples:
+//FinalGrade(100, 12);  // 100
+//FinalGrade(99, 0);    // 100
+//FinalGrade(10, 15);   // 100
+//FinalGrade(85, 5);    // 90
+//FinalGrade(55, 3);    // 75
+
+//FinalGrade(55, 0);    // 0
+//FinalGrade(20, 2);    // 0
+//*Use Comparison and Logical Operators.
+
+using System;
+
+namespace CodeWars
+{
+    public class StudentFinalGrade
+    {
+        public static int FinalGrade(int exam, int projects)
+        {
+            if (exam > 90 || projects > 10) return 100;
+            if (exam > 75 && projects >= 5) return 90;
+            if (exam > 50 && projects >= 2) return 75;
+            return 0;
+        }
+
+
+        //public static int FinalGrade(int exam, int projects)
+        //{
+        //    if (exam > 90 || projects > 10) return 100;
+        //    if (exam > 75 && projects > 4) return 90;
+        //    if (exam > 50 && projects > 1) return 75;
+        //    return 0;
+        //}
+
+
+        //public static int FinalGrade(int exam, int projects)
+        //{
+        //    return
+        //        exam > 90 | projects > 10 ? 100 :
+        //        exam > 75 & projects >= 5 ? 90 :
+        //        exam > 50 & projects >= 2 ? 75 :
+        //        0;
+        //}
+
+
+        //public static int FinalGrade(int exam, int projects)
+        //{
+        //    int grade = 0;
+
+        //    if (exam > 90 || projects > 10)
+        //        grade = 100;
+        //    else if (exam > 75 && projects >= 5)
+        //        grade = 90;
+        //    else if (exam > 50 && projects >= 2)
+        //        grade = 75;
+
+        //    return grade;
+        //}
+
+
+        //public static int FinalGrade(int exam, int projects)
+        //{
+        //    return (exam > 90 || projects > 10) ? 100 : ((exam > 75 && exam <= 90) && projects >= 5) ? 90 : ((exam > 50) && (projects >= 2)) ? 75 : 0;
+        //}
+
+        //public static int FinalGrade(int exam, int projects)
+        //{
+        //    return exam > 90 ? 100 : projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+        //}
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/StudentFinalGradeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StudentFinalGradeTests
+    {
+        [Test]
+        public void Basic()
+        {
+            Assert.AreEqual(100, StudentFinalGrade.FinalGrade(100, 12));
+            Assert.AreEqual(90, StudentFinalGrade.FinalGrade(85, 5));
+        }
+
+
+        private int solution(int a, int b)
+        {
+            if (a > 90 || b > 10) return 100;
+            if (a > 75 && b >= 5) return 90;
+            if (a > 50 && b >= 2) return 75;
+            return 0;
+        }
+
+        [Test]
+        public void FinalGradeTest()
+        {
+            //       Assert.AreEqual(100, Kata.FinalGrade(100, 12));
+            //       Assert.AreEqual(90, Kata.FinalGrade(85, 5));
+            //       Assert.AreEqual(100, Kata.FinalGrade(99, 0));
+            //       Assert.AreEqual(100, Kata.FinalGrade(10, 15));
+            //       Assert.AreEqual(90, Kata.FinalGrade(85, 5));
+            //       Assert.AreEqual(75, Kata.FinalGrade(55, 3));
+            //       Assert.AreEqual(0, Kata.FinalGrade(55, 0));
+            //       Assert.AreEqual(0, Kata.FinalGrade(20, 2));
+            for (int e = 0; e <= 100; e++)
+                for (int p = 0; p <= 11; p++)
+                    Assert.AreEqual(solution(e, p), StudentFinalGrade.FinalGrade(e, p),
+                        $"exam = {e}, projects = {p}");
+
+            Random rnd = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                var a = rnd.Next(0, 100);
+                var b = rnd.Next(0, 20);
+                Assert.AreEqual(solution(a, b), StudentFinalGrade.FinalGrade(a, b));
+            }
+        }
+
+    }
+}
