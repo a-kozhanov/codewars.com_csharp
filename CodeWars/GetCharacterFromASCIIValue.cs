ae625c7502fd8ffd3b30e7bc2d9d8f2d87a993e0
a-kozhanov <a.kozhanov@outlook.com>
2019-09-23 22:01:56 +0300

    Added solution "get character from ASCII Value"

---
+++ CodeWars/GetCharacterFromASCIIValue.cs
@@ -0,0 +1,30 @@
+﻿
+//https://www.codewars.com/kata/get-character-from-ascii-value/train/csharp
+//https://www.codewars.com/kata/55ad04714f0b468e8200001c/solutions/csharp
+
+//get character from ASCII Value
+
+//Write a function which takes a number and returns the corresponding ASCII char for that value.
+//Example:
+//get_char(65) # => 'A'
+//For ASCII table, you can refer to http://www.asciitable.com/
+
+
+using System;
+
+namespace CodeWars
+{
+    public class GetCharacterFromASCIIValue
+    {
+        public static char GetChar(int charcode)
+        {
+            return (char)charcode;
+        }
+
+        //public static char GetChar(int charcode)
+        //{
+        //    return Convert.ToChar(charcode);   
+        //}
+
+    }
+}
---
+++ CodeWarsTests/GetCharacterFromASCIIValueTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GetCharacterFromASCIIValueTests
+    {
+        [Test]
+        [TestCase(55, ExpectedResult = '7')]
+        [TestCase(55, ExpectedResult = '7')]
+        [TestCase(56, ExpectedResult = '8')]
+        [TestCase(57, ExpectedResult = '9')]
+        [TestCase(58, ExpectedResult = ':')]
+        [TestCase(59, ExpectedResult = ';')]
+        [TestCase(60, ExpectedResult = '<')]
+        [TestCase(61, ExpectedResult = '=')]
+        [TestCase(62, ExpectedResult = '>')]
+        [TestCase(63, ExpectedResult = '?')]
+        [TestCase(64, ExpectedResult = '@')]
+        [TestCase(65, ExpectedResult = 'A')]
+        public static char FixedTest(int charCode)
+        {
+            return GetCharacterFromASCIIValue.GetChar(charCode);
+        }
+    }
+}
