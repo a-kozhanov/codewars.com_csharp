3ea7ca6ce744bb75b318b0e644595215f36614b8
a-kozhanov <a.kozhanov@outlook.com>
2019-09-24 21:50:25 +0300

    Added solution "How many lightsabers do you own?"

---
+++ CodeWars/HowManyLightsabersDoYouOwn.cs
@@ -0,0 +1,26 @@
+﻿
+//https://www.codewars.com/kata/how-many-lightsabers-do-you-own/train/csharp
+//https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/solutions/csharp
+
+//How many lightsabers do you own?
+
+//Description:
+//Inspired by the development team at Vooza, write the function howManyLightsabersDoYouOwn/how_many_light_sabers_do_you_own that
+
+//accepts the name of a programmer, and
+//returns the number of lightsabers owned by that person.
+//The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+//howManyLightsabersDoYouOwn("anyone else") == 0
+//howManyLightsabersDoYouOwn("Zach") == 18
+
+namespace CodeWars
+{
+    public class KataHowManyLightsabersDoYouOwn
+    {
+        public static int HowManyLightsabersDoYouOwn(string name)
+        {
+            return name == "Zach" ? 18 : 0;
+        }
+    }
+}
---
+++ CodeWarsTests/HowManyLightsabersDoYouOwnTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HowManyLightsabersDoYouOwnTests
+    {
+        [Test]
+        public static void Adam()
+        {
+            Assert.AreEqual(0, KataHowManyLightsabersDoYouOwn.HowManyLightsabersDoYouOwn("Adam"));
+        }
+
+        [Test]
+        public static void Zach()
+        {
+            Assert.AreEqual(18, KataHowManyLightsabersDoYouOwn.HowManyLightsabersDoYouOwn("Zach"));
+        }
+    }
+}
