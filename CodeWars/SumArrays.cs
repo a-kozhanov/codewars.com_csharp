89b48e6733052ede3fae2bbe2dcae3045edf0de7
a-kozhanov <a.kozhanov@outlook.com>
2019-09-24 16:35:23 +0300

    Added solution "Sum Arrays"

---
+++ CodeWars/SumArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+//https://www.codewars.com/kata/sum-arrays/train/csharp
+//https://www.codewars.com/kata/53dc54212259ed3d4f00071c/solutions/csharp
+
+//Sum Arrays
+
+//Write a method sum(sum_array in python, sumarray in julia, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. 
+//These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. 
+//If the array does not contain any numbers then you should return 0.
+//Examples
+//Kata.SumArray(new int[] {1, 2, 3}) => 6
+//Assumptions
+//You can assume that you are only given numbers.
+//You cannot assume the size of the array.
+//You can assume that you do get an array and if the array is empty, return 0.
+
+namespace CodeWars
+{
+    public class KataSumArrays
+    {
+        public static double SumArray(double[] array)
+        {
+            return array.Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/SumArraysTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class SumArraysTests
+    {
+        private static object[] testCases = new object[]
+{
+      new object[] {new double[] {1, 5.2, 4, 0, -1}, 9.2},
+      new object[] {new double[] {}, 0},
+};
+
+        [Test, TestCaseSource("testCases")]
+        public void Test(double[] array, double expected) => Assert.AreEqual(expected, KataSumArrays.SumArray(array));
+    }
+}
