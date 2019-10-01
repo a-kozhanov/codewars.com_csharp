74949502ae0ee97b1a3c2357cfe7592dcb1658c9
a-kozhanov <a.kozhanov@outlook.com>
2019-10-01 12:54:40 +0300

    Added solution "Find Maximum and Minimum Values of a List" 8 kyu

---
+++ CodeWars/FindMaximumAndMinimumValuesList.cs
@@ -0,0 +1,37 @@
+﻿
+//https://www.codewars.com/kata/find-maximum-and-minimum-values-of-a-list/train/csharp
+//https://www.codewars.com/kata/577a98a6ae28071780000989/solutions/csharp
+
+//Find Maximum and Minimum Values of a List
+//8 kyu
+
+
+//Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs,
+//respectively, the largest and lowest number in that array/vector.
+
+//#Examples
+//max({4,6,2,1,9,63,-134,566}) returns 566
+//min({-52, 56, 30, 29, -54, 0, -110}) returns -110
+//max({5}) returns 5
+//min({42, 54, 65, 87, 0}) returns 0
+
+//#Notes
+//You may consider that there will not be any empty arrays/vectors.
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindMaximumAndMinimumValuesList
+    {
+        public int Min(int[] list)
+        {
+            return list.Min();
+        }
+
+        public int Max(int[] list)
+        {
+            return list.Max();
+        }
+    }
+}
---
+++ CodeWarsTests/FindMaximumAndMinimumValuesListTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindMaximumAndMinimumValuesListTests
+    {
+        [Test]
+        public void TestExamples()
+        {
+            Assert.AreEqual(-110, new FindMaximumAndMinimumValuesList().Min(new[] { -52, 56, 30, 29, -54, 0, -110 }));
+            Assert.AreEqual(0, new FindMaximumAndMinimumValuesList().Min(new[] { 42, 54, 65, 87, 0 }));
+            Assert.AreEqual(566, new FindMaximumAndMinimumValuesList().Max(new[] { 4, 6, 2, 1, 9, 63, -134, 566 }));
+            Assert.AreEqual(5, new FindMaximumAndMinimumValuesList().Max(new[] { 5 }));
+        }
+    }
+}
