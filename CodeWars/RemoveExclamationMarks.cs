d81d693caf604d9822e132aa120be0cfe0cfbe43
a-kozhanov <a.kozhanov@outlook.com>
2019-10-01 13:51:44 +0300

    Added solution "Find Maximum and Minimum Values of a List" 8 kyu

---
+++ CodeWars/RemoveExclamationMarks.cs
@@ -0,0 +1,31 @@
+﻿
+
+//https://www.codewars.com/kata/remove-exclamation-marks/train/csharp
+//https://www.codewars.com/kata/57a0885cbb9944e24c00008e/solutions/csharp
+
+//Remove exclamation marks
+//8 kyu
+
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+using System;
+
+namespace CodeWars
+{
+    public static class KataRemoveExclamationMarks
+    {
+        public static string RemoveExclamationMarks(string s)
+        {
+            return s.Replace("!", "");
+        }
+
+
+        //public static string RemoveExclamationMarks(string s)
+        //{
+        //    return string.IsNullOrEmpty(s) ? s : s.Replace("!", "");
+        //}
+
+
+
+    }
+}
---
+++ CodeWarsTests/RemoveExclamationMarksTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RemoveExclamationMarksTests
+    {
+        [Test]
+        public static void MyTests()
+        {
+            Assert.AreEqual("", KataRemoveExclamationMarks.RemoveExclamationMarks(""), "Input: Empty string");
+            Assert.AreEqual("", KataRemoveExclamationMarks.RemoveExclamationMarks("!"), "Input: " + "!");
+            Assert.AreEqual("", KataRemoveExclamationMarks.RemoveExclamationMarks("!!"), "Input: " + "!!");
+            Assert.AreEqual("Hi", KataRemoveExclamationMarks.RemoveExclamationMarks("Hi!"), "Input: " + "Hi!");
+            Assert.AreEqual("?", KataRemoveExclamationMarks.RemoveExclamationMarks("!?!"), "Input: " + "!?!");
+        }
+    }
+}
