4a9a7ce8d25519082c36e1d0a68821934b39e443
a-kozhanov <a.kozhanov@outlook.com>
2019-10-01 23:28:29 +0300

    Added solution "Jenny's secret message" 8 kyu

---
+++ CodeWars/8kyu/JennySecretMessage.cs
@@ -0,0 +1,45 @@
+﻿
+//https://www.codewars.com/kata/jennys-secret-message/train/csharp
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/csharp
+
+//Jenny's secret message
+//8 kyu
+
+//Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny,
+//and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+//Can you help her?
+
+using System;
+
+namespace CodeWars
+{
+    public class JennySecretMessage
+    {
+        public static string greet(string name)
+        {
+            return name == "Johnny" ? "Hello, my love!" : $"Hello, {name}!";
+        }
+
+
+        //public static string greet(string name)
+        //{
+        //    return $"Hello, {name == "Johnny" ? "my love" : name}!";
+        //}
+
+
+        //public static string greet(string name)
+        //{
+        //    return String.Format("Hello, {0}!", name.Equals("Johnny") ? "my love" : name);
+        //}
+
+
+        //public static string greet(string name)
+        //{
+        //    return string.Format("Hello, {0}!", name == "Johnny" ? "my love" : name);
+        //}
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/8kyu/JennySecretMessageTests.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class JennySecretMessageTests
+    {
+        [Test]
+        public static void ShouldGreetJimNormally()
+        {
+            Assert.AreEqual("Hello, Jim!", JennySecretMessage.greet("Jim"));
+        }
+        [Test]
+        public static void ShouldGreetJaneNormally()
+        {
+            Assert.AreEqual("Hello, Jane!", JennySecretMessage.greet("Jane"));
+        }
+        [Test]
+        public static void ShouldGreetSimonNormally()
+        {
+            Assert.AreEqual("Hello, Simon!", JennySecretMessage.greet("Simon"));
+        }
+
+        [Test]
+        public static void ShouldGreetJohnnySpecially()
+        {
+            Assert.AreEqual("Hello, my love!", JennySecretMessage.greet("Johnny"));
+        }
+    }
+}
