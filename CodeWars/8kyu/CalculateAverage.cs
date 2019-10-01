6b76c330e7ba4321ab8e2ceac5c213b0084a47fb
a-kozhanov <a.kozhanov@outlook.com>
2019-10-01 12:36:02 +0300

    Added solution "Calculate average" 8 kyu

---
+++ CodeWars/8kyu/CalculateAverage.cs
@@ -0,0 +1,38 @@
+﻿
+//https://www.codewars.com/kata/calculate-average/train/csharp
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/solutions/csharp
+
+//Calculate average
+//8 kyu
+
+//Write function avg which calculates average of numbers in given list.
+
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class CalculateAverage
+    {
+        public static double FindAverage(double[] array)
+        {
+            return array.Length == 0 ? 0 : array.Average();
+        }
+
+
+        //public static Double FindAverage( Double[] array )
+        //{
+        //    if ( array == null )
+        //        throw new ArgumentNullException( nameof( array ), "Array can not be null" );
+
+        //    return array.Any() ? array.Average() : 0;
+        //}
+
+
+        //public static double FindAverage(double[] array) => array.DefaultIfEmpty().Average();
+
+
+
+    }
+}
---
+++ CodeWarsTests/8kyu/CalculateAverageTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CalculateAverageTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            double[] array = new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 };
+            Assert.AreEqual(200.0 / 13.0, CalculateAverage.FindAverage(array));
+
+            // Should return 0 on empty array
+            Assert.AreEqual(0, CalculateAverage.FindAverage(new double[] { }));
+        }
+    }
+}
