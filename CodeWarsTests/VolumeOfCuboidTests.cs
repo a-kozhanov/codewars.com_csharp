bcf978dd8e160e4cc776319485896d3ff932c296
a-kozhanov <a.kozhanov@outlook.com>
2019-10-07 18:17:31 +0300

    Added solution "Volume of a Cuboid" 8 kyu

---
+++ CodeWarsTests/VolumeOfCuboidTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class VolumeOfCuboidTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(4, VolumeOfCuboid.getVolumeOfCubiod(1, 2, 2));
+            Assert.AreEqual(63, VolumeOfCuboid.getVolumeOfCubiod(6.3, 2, 5));
+        }
+    }
+}
