36457921a83241006d5130c142b537ae85eaec6d
a-kozhanov <a.kozhanov@outlook.com>
2019-10-08 18:34:50 +0300

    Added solution "Function 2 - squaring an argument" 8 kyu

---
+++ CodeWarsTests/SquaringArgumentTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SquaringArgumentTests
+    {
+        [Test, Description("Sample Tests")]
+        public void Test()
+        {
+            Assert.AreEqual(4, Kata.Square(2));
+            Assert.AreEqual(100, Kata.Square(10));
+        }
+    }
+}
