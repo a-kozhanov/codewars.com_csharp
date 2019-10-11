116f22f64c3de01673f54b8651d4a9b7bc606416
a-kozhanov <a.kozhanov@outlook.com>
2019-10-11 15:19:49 +0300

    Added solutiuon "Beginner Series #4 Cockroach" 8 kyu

---
+++ CodeWarsTests/BeginnerSeriesCockroachTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BeginnerSeriesCockroachTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(30, BeginnerSeriesCockroach.CockroachSpeed(1.08));
+            Assert.AreEqual(30, BeginnerSeriesCockroach.CockroachSpeed(1.09));
+            Assert.AreEqual(0, BeginnerSeriesCockroach.CockroachSpeed(0));
+        }
+    }
+}
