e58ba8aeb0e256db18adc0f0c58344500fc9c5a8
a-kozhanov <a.kozhanov@outlook.com>
2019-10-11 15:06:54 +0300

    Added solution "My head is at the wrong end!" 8 kyu

---
+++ CodeWarsTests/MyHeadIsAtWrongEndTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MyHeadIsAtWrongEndTests
+    {
+        private static Random rnd = new Random();
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new string[] { "head", "body", "tail" }, MyHeadIsAtWrongEnd.FixTheMeerkat(new string[] { "tail", "body", "head" }));
+            Assert.AreEqual(new string[] { "heads", "body", "tails" }, MyHeadIsAtWrongEnd.FixTheMeerkat(new string[] { "tails", "body", "heads" }));
+            Assert.AreEqual(new string[] { "top", "middle", "bottom" }, MyHeadIsAtWrongEnd.FixTheMeerkat(new string[] { "bottom", "middle", "top" }));
+            Assert.AreEqual(new string[] { "upper legs", "torso", "lower legs" }, MyHeadIsAtWrongEnd.FixTheMeerkat(new string[] { "lower legs", "torso", "upper legs" }));
+            Assert.AreEqual(new string[] { "ground", "rainbow", "sky" }, MyHeadIsAtWrongEnd.FixTheMeerkat(new string[] { "sky", "rainbow", "ground" }));
+        }
+    }
+}
---
+++ CodeWarsTests/OppositesAttractTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class OppositesAttractTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, OppositesAttract.lovefunc(1, 4));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(false, OppositesAttract.lovefunc(2, 2));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(true, OppositesAttract.lovefunc(0, 1));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(false, OppositesAttract.lovefunc(0, 0));
+        }
+    }
+}
