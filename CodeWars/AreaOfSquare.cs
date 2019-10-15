48c08793fe3546394a1f8b19f45baae1bf86c295
a-kozhanov <a.kozhanov@outlook.com>
2019-10-15 18:38:14 +0300

    Added solution "Area of a Square" 8 kyu

---
+++ CodeWars/AreaOfSquare.cs
@@ -0,0 +1,48 @@
+﻿
+
+//https://www.codewars.com/kata/area-of-a-square/train/csharp
+//https://www.codewars.com/kata/5748838ce2fab90b86001b1a/solutions/csharp
+
+//Area of a Square
+//8 kyu
+
+
+//Write the function squareArea ( or square_area ) that finds the area of the red square when you have the length of the circular arc A.
+//Use π = Math.PI ( M_PI in PHP )
+//Round to two decimals
+
+using System;
+
+namespace CodeWars
+{
+    public class AreaOfSquare
+    {
+        public static double SquareArea(double A)
+        {
+            return Math.Round(Math.Pow(A / Math.PI, 2) * 4, 2);
+        }
+
+
+        //public static double SquareArea(double A) => Math.Round((4 * A / (2 * Math.PI)) * (4 * A / (2 * Math.PI)), 2);
+
+
+        //public static double SquareArea(double A)
+        //{
+        //    return Math.Round(Math.Pow(4 * A / (2 * Math.PI), 2), 2);
+        //}
+
+        //public static double SquareArea(double A)
+        //{
+        //    return Math.Round((A / Math.PI * 2) * (A / Math.PI * 2), 2);
+        //}
+
+
+        //public static double SquareArea(double A)
+        //{
+        //    return Math.Round(Math.Pow(2 * A / System.Math.PI, 2), 2);
+        //}
+
+
+
+    }
+}
