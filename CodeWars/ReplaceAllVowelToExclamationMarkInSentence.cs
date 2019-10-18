c972b55dc870746267b21b46a0d852a4f5033248
a-kozhanov <a.kozhanov@outlook.com>
2019-10-18 21:38:48 +0300

    Added solution "Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence" 8 kyu

---
+++ CodeWars/ReplaceAllVowelToExclamationMarkInSentence.cs
@@ -0,0 +1,52 @@
+﻿
+
+//https://www.codewars.com/kata/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/train/csharp
+//https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/solutions/csharp
+
+//8 kyu
+//Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+
+
+//Description:
+//Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+//Examples
+//replace("Hi!") === "H!!"
+//replace("!Hi! Hi!") === "!H!! H!!"
+//replace("aeiou") === "!!!!!"
+//replace("ABCDE") === "!BCD!"
+
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ReplaceAllVowelToExclamationMarkInSentence
+    {
+        public static string Replace(string s)
+        {
+            return string.Concat(s.Select(x => "aeiouAEIOU".Contains(x) ? '!' : x));
+        }
+
+
+        //public static string Replace(string s) => Regex.Replace(s, @"[aeiou]", "!", RegexOptions.IgnoreCase);
+
+
+        //public static string Replace(string s) => Regex.Replace(s, @"[aeiouAEIOU]", "!");
+
+        //public static string Replace(string s)
+        //{
+        //    return Regex.Replace(s, "(?i)[aeiou]", "!");
+        //}
+
+        //public static string Replace(string s)
+        //{
+        //    return string.Concat(s.ToCharArray().Select(x => "aeiouAEIOU".Contains(x) ? '!' : x));
+        //}
+
+
+
+    }
+}
---
+++ CodeWarsTests/ReplaceAllVowelToExclamationMarkInSentenceTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReplaceAllVowelToExclamationMarkInSentenceTests
+    {
+        [Test, Description("Basic Tests")]
+        public void BasicTest()
+        {
+            Assert.AreEqual("H!!", ReplaceAllVowelToExclamationMarkInSentence.Replace("Hi!"));
+            Assert.AreEqual("!H!! H!!", ReplaceAllVowelToExclamationMarkInSentence.Replace("!Hi! Hi!"));
+            Assert.AreEqual("!!!!!", ReplaceAllVowelToExclamationMarkInSentence.Replace("aeiou"));
+            Assert.AreEqual("!BCD!", ReplaceAllVowelToExclamationMarkInSentence.Replace("ABCDE"));
+        }
+    }
+}
