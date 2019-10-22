9ee5fb84d73cea1feee8a0d7a578edb062875ad7
a-kozhanov <a.kozhanov@outlook.com>
2019-10-22 19:36:24 +0300

    Added solution "Will you make it?" 8 kyu

---
+++ CodeWarsTests/WillYouMakeItTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WillYouMakeItTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, WillYouMakeIt.ZeroFuel(50, 25, 2));
+            Assert.AreEqual(false, WillYouMakeIt.ZeroFuel(100, 50, 1));
+        }
+    }
+}
