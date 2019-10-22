be21ae73fb59a62a5ac82b6fc664bf1f3b417637
a-kozhanov <a.kozhanov@outlook.com>
2019-10-22 13:47:07 +0300

    Added solution "SpeedCode #2 - Array Madness" 8 kyu

---
+++ CodeWars/ArrayMadness.cs
@@ -0,0 +1,49 @@
+﻿
+//https://www.codewars.com/kata/speedcode-number-2-array-madness/train/csharp
+//https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1/solutions/csharp
+
+//8 kyu
+//SpeedCode #2 - Array Madness
+
+//SpeedCode #2 - Array Madness
+//Objective
+//Given two integer arrays a, b, both of length >= 1,
+//create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+//E.g.
+
+//Kata.ArrayMadness(new int[] {4, 5, 6}, new int[] {1, 2, 3}) => true // because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+//Get your timer out. Are you ready? Ready, get set, GO!!!
+
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataArrayMadness
+    {
+        public static bool ArrayMadness(int[] a, int[] b)
+        {
+            return a.Sum(x => Math.Pow(x, 2)) > b.Sum(x => Math.Pow(x, 3));
+        }
+
+
+        //public static bool ArrayMadness(int[] a, int[] b) => a.Sum(n => n * n) > b.Sum(n => n * n * n);
+
+
+        //public static bool ArrayMadness(int[] a, int[] b)
+        //{
+        //    return a.Sum(x => x * x) > b.Sum(x => x * x * x);
+        //}
+
+
+        //public static bool ArrayMadness(int[] a, int[] b)
+        //{
+        //    return a.Sum(t => Math.Pow(t, 2)) > b.Sum(t => Math.Pow(t, 3));
+        //}
+
+
+
+    }
+}
---
+++ CodeWarsTests/ArrayMadnessTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ArrayMadnessTests
+    {
+        [Test, Description("Should work for sample tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, KataArrayMadness.ArrayMadness(new int[] { 4, 5, 6 }, new int[] { 1, 2, 3 }));
+            Assert.AreEqual(false, KataArrayMadness.ArrayMadness(new int[] { 5, 6, 7 }, new int[] { 4, 5, 6 }));
+        }
+    }
+}
