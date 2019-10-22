1db64a83a42adef030da948824ab3ff562d24cf6
a-kozhanov <a.kozhanov@outlook.com>
2019-10-22 21:58:11 +0300

    Added solution "simple calculator" 8 kyu

---
+++ CodeWars/SimpleCalculator.cs
@@ -0,0 +1,82 @@
+﻿
+
+//https://www.codewars.com/kata/simple-calculator/train/csharp
+//https://www.codewars.com/kata/5810085c533d69f4980001cf/solutions/csharp
+
+//8 kyu
+//simple calculator
+
+
+//You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+//Your function will accept three arguments:
+//The first and second argument will be numbers.
+//The third argument will represent a sign indicating the operation to perform on these two numbers.
+
+//Example:
+//Kata.Calculator(1, 2, '+') => 3
+//If the sign is not a valid sign, throw an ArgumentException.
+
+//Example:
+//Kata.Calculator(1, 2, '$') // throws ArgumentException
+//Good luck!
+
+
+using System;
+
+namespace CodeWars
+{
+    public class SimpleCalculator
+    {
+        public static double Calculator(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+': return a + b;
+                case '-': return a - b;
+                case '*': return a * b;
+                case '/': return a / b;
+                default: throw new ArgumentException();
+            }
+        }
+
+        //public static double Calculator(double a, double b, char op)
+        //{
+        //    return (op == '+') ? a + b : (op == '-') ? a - b : (op == '*') ? a * b : (op == '/') ? a / b : throw new System.ArgumentException();
+        //}
+
+
+
+        //public static double Calculator(double a, double b, char op)
+        //{
+
+        //    switch (op)
+        //    {
+        //        case '+':
+        //            return a + b;
+        //        case '-':
+        //            return a - b;
+        //        case '*':
+        //            return a * b;
+        //        case '/':
+        //            return a / b;
+        //        default:
+        //            throw new System.ArgumentException("Parameter cannot be null", "original");
+        //    }
+
+        //}
+
+
+
+        //public static double Calculator(double a, double b, char op)
+        //{
+        //    return op == '+' ? a + b : op == '-' ? a - b : op == '*' ? a * b : op == '/' ? a / b : throw new ArgumentException();
+        //}
+
+
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/SimpleCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        [Test, Description("should calculate the result of valid inputs")]
+        public void Test()
+        {
+            Assert.AreEqual(8, SimpleCalculator.Calculator(6, 2, '+'));
+            Assert.AreEqual(1, SimpleCalculator.Calculator(4, 3, '-'));
+            Assert.AreEqual(25, SimpleCalculator.Calculator(5, 5, '*'));
+            Assert.AreEqual(1.25, SimpleCalculator.Calculator(5, 4, '/'));
+        }
+
+        [Test, Description("should throw ArgumentException with invalid inputs")]
+        public void ErrorTest()
+        {
+            Assert.Throws<ArgumentException>(() => SimpleCalculator.Calculator(6, 2, '&'));
+        }
+    }
+}
