c703dd8b59d24d67c4a82b568803da217dfc9997
a-kozhanov <a.kozhanov@outlook.com>
2019-10-22 12:28:36 +0300

    Added solution "Sleigh Authentication" 8 kyu

---
+++ CodeWarsTests/SleighAuthenticationTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SleighAuthenticationTests
+    {
+        [Test, Description("The Sleigh must authenticate with correct credentials")]
+        public void CorrectTest()
+        {
+            Assert.That(SleighAuthentication.Authenticate("Santa Claus", "Ho Ho Ho!"));
+        }
+
+        [Test, Description("The Sleigh must not authenticate with incorrect credentials")]
+        public void IncorrectTest()
+        {
+            Assert.That(!SleighAuthentication.Authenticate("Santa", "Ho Ho Ho!"));
+            Assert.That(!SleighAuthentication.Authenticate("Santa Claus", "Ho Ho!"));
+            Assert.That(!SleighAuthentication.Authenticate("jhoffner", "CodeWars"));
+        }
+    }
+}
