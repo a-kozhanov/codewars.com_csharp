793cc22e3bfe81dedea8da3c1e2c342d34f34133
a-kozhanov <a.kozhanov@outlook.com>
2019-10-22 12:50:43 +0300

    Added xsolution "Unicode Total" 8 kyu

---
+++ CodeWars/UnicodeTotal.cs
@@ -0,0 +1,43 @@
+﻿
+
+//https://www.codewars.com/kata/unicode-total/train/csharp
+//https://www.codewars.com/kata/572b6b2772a38bc1e700007a/solutions/csharp
+
+//8 kyu
+//Unicode Total
+
+
+//You'll be given a string, and have to return the total of all the unicode characters as an int. Should be able to handle any characters sent at it.
+//examples:
+//uniTotal("a") == 97 uniTotal("aaa") == 291
+
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class UnicodeTotal
+    {
+        public static int UniTotal(string str)
+        {
+            return str.Sum(c => c);
+        }
+
+
+        //public static int UniTotal(string str) => str.Sum(x => (int)x);
+
+
+        //public static int UniTotal(string s) => s.Sum(Convert.ToInt32);
+
+        //public static int UniTotal(string str)
+        //{
+        //    return str.Sum(c => (int)c);
+        //}
+
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/UnicodeTotalTests.cs
@@ -0,0 +1,37 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class UnicodeTotalTests
+    {
+        [Test, Description("An empty string should return zero")]
+        public void ZeroTest()
+        {
+            Assert.AreEqual(0, UnicodeTotal.UniTotal(""));
+        }
+
+        [Test, Description("Should work with single letters")]
+        public void SingleTest()
+        {
+            Assert.AreEqual(97, UnicodeTotal.UniTotal("a"));
+            Assert.AreEqual(98, UnicodeTotal.UniTotal("b"));
+            Assert.AreEqual(99, UnicodeTotal.UniTotal("c"));
+            Assert.AreEqual(100, UnicodeTotal.UniTotal("d"));
+            Assert.AreEqual(101, UnicodeTotal.UniTotal("e"));
+        }
+
+        [Test, Description("Should work with multiple letters")]
+        public void MultipleTest()
+        {
+            Assert.AreEqual(291, UnicodeTotal.UniTotal("aaa"));
+        }
+
+        [Test, Description("Should work with sentence and spaces")]
+        public void SentenceTest()
+        {
+            Assert.AreEqual(1873, UnicodeTotal.UniTotal("Mary Had A Little Lamb"));
+        }
+    }
+}
