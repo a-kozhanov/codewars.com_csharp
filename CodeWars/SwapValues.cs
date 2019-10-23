c114107e28c14106f783da4f457292ae043d9af2
a-kozhanov <a.kozhanov@outlook.com>
2019-10-23 13:23:43 +0300

    Added solution "Swap Values" 8 kyu

---
+++ CodeWars/SwapValues.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+
+
+//https://www.codewars.com/kata/swap-values/train/csharp
+//https://www.codewars.com/kata/5388f0e00b24c5635e000fc6/solutions/csharp
+
+//8 kyu
+//Swap Values
+
+//I would like to be able to pass an array with two elements to my swapValues function to swap the values.
+//However it appears that the values aren't changing.
+//Can you figure out what's wrong here?
+
+
+namespace CodeWars
+{
+    public class KataSwapValues
+    {
+        public object[] Arguments { get; private set; }
+
+        public KataSwapValues(object[] args)
+        {
+            Arguments = args;
+        }
+
+        public void SwapValues()
+        {
+            //object[] args = new[] { Arguments[0], Arguments[1] };
+
+            //object temp = args[0];
+            //args[0] = args[1];
+            //args[1] = temp;
+
+            Array.Reverse(Arguments);
+        }
+    }
+}
+
+
+//public class Swapper
+//{
+//    public object[] Arguments { get; private set; }
+
+//    public Swapper(object[] args)
+//    {
+//        Arguments = args;
+//    }
+
+//    public void SwapValues()
+//    {
+//        object temp = Arguments[0];
+//        Arguments[0] = Arguments[1];
+//        Arguments[1] = temp;
+//    }
+//}
+
+
+//public class Swapper
+//{
+//    public object[] Arguments { get; private set; }
+
+//    public Swapper(object[] args)
+//    {
+//        Arguments = args;
+//    }
+
+//    public void SwapValues()
+//    {
+//        object[] args = new[] { Arguments[0], Arguments[1] };
+
+//        Arguments[0] = args[1];
+//        Arguments[1] = args[0];
+//    }
+//}
+
+
+
+//public class Swapper
+//{
+//    public object[] Arguments { get; private set; }
+
+//    public Swapper(object[] args)
+//    {
+//        Arguments = args;
+//    }
+
+//    public void SwapValues()
+//    {
+//        System.Array.Reverse(Arguments);
+//    }
+//}
+
+
+//public class Swapper
+//{
+//    public object[] Arguments { get; private set; }
+
+//    public Swapper(object[] args)
+//    {
+//        Arguments = args;
+//    }
+
+//    public void SwapValues()
+//    {
+//        object[] args = new[] { Arguments[0], Arguments[1] };
+
+//        object temp = args[0];
+//        temp = args[0];
+//        Arguments[0] = args[1];
+//        Arguments[1] = temp;
+//    }
+//}
---
+++ CodeWarsTests/SwapValuesTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SwapValuesTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            int[] args = new[] { 1, 2 };
+
+            KataSwapValues swapper = new KataSwapValues(args.Cast<object>().ToArray());
+            swapper.SwapValues();
+
+            Assert.AreEqual(2, swapper.Arguments[0], "Failed swapping numbers");
+            Assert.AreEqual(1, swapper.Arguments[1], "Failed swapping numbers");
+        }
+    }
+}
