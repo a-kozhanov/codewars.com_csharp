c078f32be68170198b3c6f7faf4c0d290be81500
a-kozhanov <a.kozhanov@outlook.com>
2019-10-25 16:28:17 +0300

    Added solution "BASIC: Making Six Toast" 8 kyu

---
+++ CodeWarsTests/MakingSixToastTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MakingSixToastTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(0, MakingSixToast.SixToast(6));
+            Assert.AreEqual(11, MakingSixToast.SixToast(17));
+            Assert.AreEqual(3, MakingSixToast.SixToast(3));
+        }
+    }
+}
