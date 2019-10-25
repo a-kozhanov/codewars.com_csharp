e2f681783c2f5c5127e69b55eb66f4e9f9c6bbbf
a-kozhanov <a.kozhanov@outlook.com>
2019-10-25 15:09:16 +0300

    Added solution "Heads and Legs" 8 kyu

---
+++ CodeWars/HeadsAndLegs.cs
@@ -0,0 +1,77 @@
+﻿
+//https://www.codewars.com/kata/heads-and-legs/train/csharp
+//https://www.codewars.com/kata/574c5075d27783851800169e/solutions/csharp
+
+//8 kyu
+//Heads and Legs
+
+//#Description
+//Everybody has probably heard of the animal heads and legs problem from the earlier years at school. It goes:
+//“A farm contains chickens and cows. There are x heads and y legs. How many chickens and cows are there?”
+//Where x <= 1000 and y <=1000
+
+//#Task
+//Assuming there are no other types of animals, work out how many of each animal are there.
+//Return a tuple in Python - (chickens, cows) and an array list - [chickens, cows]/{chickens, cows} in all other languages
+//If either the heads & legs is negative, the result of your calculation is negative or the calculation is a float return "No solutions" (no valid cases).
+//In the form:
+//(Heads, Legs) = (72, 200)
+//VALID - (72, 200) =>             (44 , 28) 
+//(Chickens, Cows)
+//INVALID - (72, 201) => "No solutions"
+//However, if 0 heads and 0 legs are given always return [0, 0] since zero heads must give zero animals.
+//There are many different ways to solve this, but they all give the same answer.
+//You will only be given integers types - however negative values (edge cases) will be given.
+
+//Happy coding!
+
+using System;
+
+namespace CodeWars
+{
+    public class HeadsAndLegs
+    {
+        public static object Animals(int heads, int legs)
+        {
+            for (var i = 0; i <= heads; i++)
+            {
+                if (i * 4 + (heads - i) * 2 == legs) return new[] { heads - i, i };
+            }
+            return "No solutions";
+        }
+
+
+        //public static object Animals(int heads, int legs)
+        //{
+        //    if (legs % 2 != 0)
+        //        return "No solutions";
+
+        //    int cows = legs/2 - heads;
+        //    int chickens = heads - cows;
+
+        //    return (cows < 0 || chickens < 0) ? (object)"No solutions" : new int[2] { chickens, cows };
+        //}
+
+
+
+        //public static object Animals(int heads, int legs)
+        //{
+        //    var cows = (legs - 2 * heads) / 2;
+        //    var chickens = heads - cows;
+        //    if (cows < 0 || chickens < 0 || cows + chickens != heads || 2 * chickens + 4 * cows != legs)
+        //        return "No solutions";
+
+        //    return new[] { chickens, cows };
+        //}
+
+
+        //public static object Animals(int heads, int legs)
+        //{if( heads<0 || legs<0 || legs%2 == 1 || 2*heads-legs/2 <0 ||legs/2-heads <0 ) return "No solutions"; 
+        //    return new int[] {2*heads-legs/2 ,legs/2-heads }; 
+
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/HeadsAndLegsTests.cs
@@ -0,0 +1,40 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HeadsAndLegsTests
+    {
+        [Test]
+        public void ValidNumberTests()
+        {
+            Assert.AreEqual(new int[] { 44, 28 }, HeadsAndLegs.Animals(72, 200));
+            Assert.AreEqual(new int[] { 91, 25 }, HeadsAndLegs.Animals(116, 282));
+            Assert.AreEqual(new int[] { 12, 0 }, HeadsAndLegs.Animals(12, 24));
+            Assert.AreEqual(new int[] { 0, 6 }, HeadsAndLegs.Animals(6, 24));
+            Assert.AreEqual(new int[] { 252, 92 }, HeadsAndLegs.Animals(344, 872));
+            Assert.AreEqual(new int[] { 8, 150 }, HeadsAndLegs.Animals(158, 616));
+        }
+
+        [Test]
+        public void InvalidNumberTests()
+        {
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(25, 255));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(12, 25));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(54, 956));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(5455, 54956));
+        }
+
+        [Test]
+        public void EdgeCasesTests()
+        {
+            Assert.AreEqual(new int[] { 0, 0 }, HeadsAndLegs.Animals(0, 0));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(-1, -1));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(-45, 5));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(500, 0));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(0, 500));
+            Assert.AreEqual("No solutions", HeadsAndLegs.Animals(5, -55));
+        }
+    }
+}
