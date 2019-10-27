cd5205d4072f4395993f34f1ac3ff534133df7a6
a-kozhanov <a.kozhanov@outlook.com>
2019-10-27 13:33:56 +0300

    Added solution "Broken Counter" 8 kyu

---
+++ CodeWars/BrokenCounter.cs
@@ -0,0 +1,101 @@
+﻿
+/*
+https://www.codewars.com/kata/broken-counter/train/csharp
+https://www.codewars.com/kata/526471539d52735c620000c6/solutions/csharp
+
+8 kyu
+Broken Counter
+
+Our counter prototype is broken. Can you spot, what's wrong here?
+Counter.Value must have manually defined getter/setter methods, according to our company's style guide.
+
+*/
+
+namespace CodeWars
+{
+    public class BrokenCounter
+    {
+        private int _value;
+        public int Value
+        {
+            get => _value;
+            private set => _value = value;
+        }
+
+        public void Increase() => Value++;
+        public void Reset() => Value = 0;
+    }
+}
+
+
+//public class Counter
+//{
+//    private int _Value;
+//    public int Value
+//    {
+//        get
+//        {
+//            return _Value;
+//        }
+//        private set
+//        {
+//            _Value = value;
+//        }
+//    }
+
+//    public Counter()
+//    {
+//        Value = 0;
+//    }
+
+//    public void Increase()
+//    {
+//        Value++;
+//    }
+
+//    public void Reset()
+//    {
+//        Value = 0;
+//    }
+//}
+
+
+
+
+//public class Counter
+//{
+//    private int val = 0;
+
+//    public int Value
+//    {
+//        get
+//        {
+//            return val;
+//        }
+//        private set
+//        {
+//            val = value;
+//        }
+//    }
+
+//    public Counter()
+//    {
+//        Value = 0;
+//    }
+
+//    public void Increase()
+//    {
+//        ++Value;
+//    }
+
+//    public void Reset()
+//    {
+//        Value = 0;
+//    }
+//}
+
+
+
+
+
+
---
+++ CodeWarsTests/BrokenCounterTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BrokenCounterTests
+    {
+        [Test]
+        public void Test()
+        {
+            BrokenCounter counter = new BrokenCounter();
+            Assert.AreEqual(0, counter.Value, "Initial counter value must be 0");
+            counter.Increase();
+            Assert.AreEqual(1, counter.Value, "Counter value must be incremented.");
+            counter.Reset();
+            Assert.AreEqual(0, counter.Value, "Counter value must be reset.");
+        }
+    }
+}
