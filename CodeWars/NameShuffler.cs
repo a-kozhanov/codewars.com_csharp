2bdf7ed41307655f0177061a521a38cc28d85c11
a-kozhanov <a.kozhanov@outlook.com>
2019-10-31 23:31:38 +0300

    Added solution "Name Shuffler" 8 kyu

---
+++ CodeWars/NameShuffler.cs
@@ -0,0 +1,65 @@
+﻿
+/*
+https://www.codewars.com/kata/name-shuffler/train/csharp
+https://www.codewars.com/kata/559ac78160f0be07c200005a/solutions/csharp
+
+8 kyu
+Name Shuffler
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+Kata.NameShuffler("john McClane") => "McClane john"
+
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataNameShuffler
+    {
+        public static string NameShuffler(string str)
+        {
+            var result = str.Split();
+            Array.Reverse(result);
+            return string.Join(" ", result);
+        }
+
+        //public static string NameShuffler(string str) => string.Join(" ", str.Split(' ').Reverse());
+
+
+        //public static string NameShuffler(string str)
+        //{
+        //    return string.Join(' ', str.Split(' ').Reverse());
+        //}
+
+
+        //public static string NameShuffler(string str)
+        //{
+        //    var s = str.Split(' ');
+        //    return $"{s[1]} {s[0]}";
+        //}
+
+        //public static string NameShuffler(string str) => string.Join(" ", str.Split(' ').Reverse());
+
+        //public static string NameShuffler(string str) => str.Substring(str.IndexOf(' ') + 1) + " " + str.Substring(0, str.IndexOf(' '));
+
+        //public static string NameShuffler(string str)
+        //{
+        //    return string.Join(" ",Enumerable.Reverse(str.Split(" ")));
+        //}
+
+
+        //public static string NameShuffler(string str)
+        //{
+        //    string[] x = str.Split(" ");
+        //    return x[1] + " " + x[0];
+        //}
+
+
+
+
+
+    }
+}
---
+++ CodeWarsTests/NameShufflerTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NameShufflerTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("McClane john", KataNameShuffler.NameShuffler("john McClane"));
+            Assert.AreEqual("jeggins Mary", KataNameShuffler.NameShuffler("Mary jeggins"));
+            Assert.AreEqual("jerry tom", KataNameShuffler.NameShuffler("tom jerry"));
+        }
+    }
+}
