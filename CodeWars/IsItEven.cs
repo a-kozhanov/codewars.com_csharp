9ba05d84594edd4b545be82a4e729d6248898e0f
a-kozhanov <a.kozhanov@outlook.com>
2019-11-01 12:00:01 +0300

    Added solution "Is it even?" 8 kyu

---
+++ CodeWars/IsItEven.cs
@@ -0,0 +1,53 @@
+﻿
+/*
+https://www.codewars.com/kata/is-it-even/train/csharp
+https://www.codewars.com/kata/555a67db74814aa4ee0001b5/solutions/csharp
+
+8 kyu
+Is it even?
+
+In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats are considered UNeven for this kata.
+*/
+
+
+using System;
+
+namespace CodeWars
+{
+    public class IsItEven
+    {
+        public bool IsEven(double n)
+        {
+            return n % 2 == 0;
+        }
+
+        //public bool IsEven(double n) => n % 2 == 0;
+
+
+        //public bool IsEven(double n)
+        //{
+        //    return (n % 2 == 0) ? true : false;
+        //}
+
+
+        //public bool IsEven(double n) => n % 2 == 0;
+
+
+        //public bool IsEven(double n)
+        //{
+        //    return (n is float) ? false : n % 2 == 0 ? true : false;
+        //}
+
+
+        //public bool IsEven(double n)
+        //{
+        //    return n % 1 == 0 && n % 2 == 0;
+        //}
+
+    }
+
+}
---
+++ CodeWarsTests/IsItEvenTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IsItEvenTests
+    {
+        private IsItEven num;
+
+        [SetUp]
+        public void SetUp()
+        {
+            num = new IsItEven();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            num = null;
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(true, num.IsEven(0));
+            Assert.AreEqual(false, num.IsEven(0.5));
+            Assert.AreEqual(false, num.IsEven(1));
+            Assert.AreEqual(true, num.IsEven(2));
+            Assert.AreEqual(true, num.IsEven(-4));
+        }
+    }
+}
