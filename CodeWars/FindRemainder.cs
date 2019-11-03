30535eecb974c2b3a090993ea4ec4f4cbfd6e16d
a-kozhanov <a.kozhanov@outlook.com>
2019-11-03 19:14:54 +0300

    Added solution "Find the Remainder" 8 kyu

---
+++ CodeWars/FindRemainder.cs
@@ -0,0 +1,56 @@
+﻿
+/*
+https://www.codewars.com/kata/find-the-remainder/train/csharp
+https://www.codewars.com/kata/524f5125ad9c12894e00003f/solutions/csharp
+
+8 kyu
+Find the Remainder
+
+Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number. 
+Division by zero should return NaN (in C#, throw a new DivideByZeroException instead). Arguments will both be integers.
+*/
+
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindRemainder
+    {
+        public static int Remainder(params int[] n)
+        {
+            return n.Max() % n.Min();
+        }
+
+
+        //public static int Remainder(int a, int b) => Math.Max(a, b) % Math.Min(a, b);
+
+
+        //public static int Remainder(int a, int b)
+        //{
+        //    if(Math.Min(a, b) == 0) throw new DivideByZeroException();
+        //    return Math.Max(a, b) % Math.Min(a, b);
+        //}
+
+        //public static int Remainder(int a, int b) => a > b ? a % b : b % a;
+
+
+        //public static int Remainder(int a, int b) => a >= b? a % b: b % a;
+
+
+        //public static int Remainder(int a, int b)
+        //{
+        //    if (a <= b && a != 0) return b%a;
+        //    else if (b <= a && b != 0) return a%b;
+        //    else throw new DivideByZeroException();
+        //}
+
+
+        //public static int Remainder(int a, int b)
+        //{
+        //    return a >= b ? a % b : b % a;
+        //}
+
+    }
+}
---
+++ CodeWarsTests/FindRemainderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindRemainderTests
+    {
+        [Test, Description("Your solution should handle arguments and math as defined in specifications")]
+        public void FixedTest()
+        {
+            Assert.AreEqual(2, FindRemainder.Remainder(17, 5), "Returned value should be the value left over after dividing as much as possible.");
+            Assert.AreEqual(FindRemainder.Remainder(72, 13), FindRemainder.Remainder(13, 72), "The order the arguments are passed should not matter.");
+            Assert.Throws<DivideByZeroException>(() => FindRemainder.Remainder(0, 1));
+            Assert.Throws<DivideByZeroException>(() => FindRemainder.Remainder(0, 0));
+        }
+    }
+}
