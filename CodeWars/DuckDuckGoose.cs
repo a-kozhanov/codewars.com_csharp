6c649e7d1abe32c2a1f95140e739585af4471588
a-kozhanov <a.kozhanov@outlook.com>
2019-11-03 14:25:55 +0300

    Added solution "Duck Duck Goose" 8 kyu

---
+++ CodeWars/DuckDuckGoose.cs
@@ -0,0 +1,78 @@
+﻿/*
+https://www.codewars.com/kata/duck-duck-goose/train/csharp
+
+
+8 kyu
+Duck Duck Goose
+
+
+The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+Example:
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+// PHP only
+duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"]
+*/
+
+
+using System;
+
+namespace CodeWars
+{
+    public class KataDuckDuckGoose
+    {
+        public static string DuckDuckGoose(Player[] players, int goose)
+        {
+            var count = 0;
+            for (var i = 1; i < goose; i++)
+            {
+                if (count > players.Length - 1) count = 1;
+                else count++;
+            }
+            return players[count].Name;
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+
+    //public static string DuckDuckGoose(Player[] players, int goose)
+    //{
+    //    return players[(goose - 1) % players.Length].Name;
+    //}
+
+    //public static string DuckDuckGoose(Player[] players, int goose) => players[(goose - 1) % players.Length].Name;
+
+
+    //public static string DuckDuckGoose(Player[] players, int goose)
+    //{
+    //    while (goose > players.Length)
+    //    {
+    //        goose = goose - players.Length;
+    //    }
+    //    return players[goose - 1].Name;
+    //}
+
+
+    //public static string DuckDuckGoose(Player[] players, int goose)
+    //{
+    //    var index = goose % players.Length;
+    //    return players[(index == 0 ? players.Length : index) - 1].Name;
+    //}
+
+
+
+
+}
---
+++ CodeWarsTests/DuckDuckGooseTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataDuckDuckGooseTests
+    {
+        [TestCase(1, "a")]
+        [TestCase(3, "c")]
+        [TestCase(10, "z")]
+        [TestCase(20, "z")]
+        [TestCase(30, "z")]
+        [TestCase(18, "g")]
+        [TestCase(28, "g")]
+        [TestCase(12, "b")]
+        [TestCase(2, "b")]
+        [TestCase(7, "f")]
+
+        public void DuckDuckGooseTests(int goose, string expectedName)
+        {
+            var exampleNames = new string[] { "a", "b", "c", "d", "c", "e", "f", "g", "h", "z" };
+            var players = exampleNames.Select(x => new Player(x)).ToArray();
+            string userAns = KataDuckDuckGoose.DuckDuckGoose(players, goose);
+            Assert.AreEqual(expectedName, userAns);
+        }
+    }
+}
