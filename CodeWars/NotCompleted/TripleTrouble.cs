494a0fadc81a638b6d541977a339ab93ec752536
a-kozhanov <a.kozhanov@outlook.com>
2019-11-04 12:27:53 +0300

    Added solution "Triple trouble" 6 kyu

--- CodeWars/NotCompleted/TripleTrouble.cs
+++ CodeWars/NotCompleted/TripleTrouble.cs
@@ -1,6 +1,6 @@
 ﻿
 //https://www.codewars.com/kata/triple-trouble-1/train/csharp
-//
+//https://www.codewars.com/kata/55d5434f269c0c3f1b000058/solutions/csharp
 
 //6 kyu
 //Triple trouble
@@ -17,14 +17,75 @@
 //TripleDouble(12345, 12345) == 0
 //TripleDouble(666789, 12345667) == 1
 
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace CodeWars
 {
-    public static class TripleTrouble
+    public static class TripleTrouble6
     {
         public static int TripleDouble(long num1, long num2)
         {
-            //code me ^^
+            for (var i = 0; i <= 9; i++)
+            {
+                if (num1.ToString().Contains($"{i}{i}{i}") && num2.ToString().Contains($"{i}{i}")) return 1;
+            }
             return 0;
         }
     }
+
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    return "0123456789".Count(number => num1.ToString().Contains(new string(number, 3)) && num2.ToString().Contains(new string(number, 2)));
+    //}
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    var a = num1.ToString().Distinct();
+    //    foreach (var item in a)
+    //    {
+    //        if (
+    //            num1.ToString().Contains(String.Format("{0}{0}{0}", item))
+    //            && num2.ToString().Contains(String.Format("{0}{0}", item))
+    //        ) return 1;
+
+    //    }
+    //    return 0;
+    //}
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    return Convert.ToInt32(Regex.IsMatch($"{num1} {num2}", "^.*(.)\\1{2}.* .*\\1{2}.*$"));
+    //}
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    return new Regex(@"^\d*(\d)\1\1\d*\s\d*\1\1\d*$").IsMatch($"{num1} {num2}") ? 1 : 0;
+    //}
+
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    //code me ^^
+    //    return "0123456789".ToList().Any(x => num1.ToString().Contains(new String(x, 3)) && num2.ToString().Contains(new String(x, 2))) ? 1 : 0;
+    //}
+
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    return Enumerable.Range(0, 10)
+    //    .Select(d => num1.ToString().Contains("" + d + d + d) ?
+    //    num2.ToString().Contains("" + d + d) ? 1 : 0
+    //    : 0).Sum();
+    //}
+
+    //public static int TripleDouble(long num1, long num2)
+    //{
+    //    return (Regex.IsMatch($"{num1}#{num2}", @"(\d)\1\1.+#.+\1\1")) ? 1 : 0;
+    //}
+
+
+
 }
--- CodeWarsTests/NotCompleted/TripleTroubleTest.cs
+++ CodeWarsTests/NotCompleted/TripleTroubleTest.cs
@@ -4,18 +4,19 @@ using CodeWars;
 namespace CodeWarsTests
 {
     [TestFixture]
-    public class TripleTroubleTests
+    public class TripleTrouble6Tests
     {
-        //[Test]
-        //[TestCase(451999277, 41177722899, ExpectedResult = 1)]
-        //[TestCase(1222345, 12345, ExpectedResult = 0)]
-        //[TestCase(12345, 12345, ExpectedResult = 0)]
-        //[TestCase(666789, 12345667, ExpectedResult = 1)]
-        //[TestCase(10560002, 100, ExpectedResult = 1)]
-        //[TestCase(1112, 122, ExpectedResult = 0)]
-        //public static int FixedTest(long s1, long s2)
-        //{
-        //    return TripleTrouble.TripleDouble(s1, s2);
-        //}
+        [Test]
+        [TestCase(451999277, 41177722899, ExpectedResult = 1)]
+        [TestCase(1222345, 12345, ExpectedResult = 0)]
+        [TestCase(12345, 12345, ExpectedResult = 0)]
+        [TestCase(666789, 12345667, ExpectedResult = 1)]
+        [TestCase(10560002, 100, ExpectedResult = 1)]
+        [TestCase(1112, 122, ExpectedResult = 0)]
+
+        public static int FixedTest(long s1, long s2)
+        {
+            return TripleTrouble6.TripleDouble(s1, s2);
+        }
     }
 }
