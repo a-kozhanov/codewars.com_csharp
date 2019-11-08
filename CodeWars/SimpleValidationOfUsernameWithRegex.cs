c78ac3a2209c150a178e37dadf920e83d9934166
a-kozhanov <a.kozhanov@outlook.com>
2019-11-08 14:35:42 +0300

    Added solution "Simple validation of a username with regex" 8 kyu

---
+++ CodeWars/SimpleValidationOfUsernameWithRegex.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/simple-validation-of-a-username-with-regex/train/csharp
+https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/solutions/csharp
+
+8 kyu
+Simple validation of a username with regex
+
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class SimpleValidationOfUsernameWithRegex
+    {
+        public static bool ValidateUsr(string username)
+        {
+            return new Regex("^[a-z0-9_]{4,16}$").Match(username).Success;
+        }
+
+
+        //public static bool ValidateUsr(string username)
+        //{
+        //    return Regex.IsMatch(username, "^[a-z0-9_]{4,16}$");
+        //}
+
+
+        //public static bool ValidateUsr(string username)
+        //{
+        //    return Regex.IsMatch(username, @"\A[a-z0-9_]{4,16}\z");
+        //}
+
+
+        //public static bool ValidateUsr(string username)
+        //{
+        //    return Regex.IsMatch(username, @"^[a-z0-9_]{4,16}$");
+        //}
+
+        //public static bool ValidateUsr(string Username) => System.Text.RegularExpressions.Regex.IsMatch(Username, "^[a-z0-9_]{4,16}$");
+
+        //public static bool ValidateUsr(string username)
+        //{
+        //    return Regex.Matches(username, @"^[\da-z_]{4,16}$").Count > 0;
+        //}
+
+    }
+}
---
+++ CodeWarsTests/SimpleValidationOfUsernameWithRegexTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleValidationOfUsernameWithRegexTests
+    {
+        [Test]
+        [TestCase("asddsa", ExpectedResult = true)]
+        [TestCase("a", ExpectedResult = false)]
+        [TestCase("Hass", ExpectedResult = false)]
+        [TestCase("Hasd_12ssssssssssssssasasasasasssasassss", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("____", ExpectedResult = true)]
+        [TestCase("012", ExpectedResult = false)]
+        [TestCase("p1pp1", ExpectedResult = true)]
+        [TestCase("asd43 34", ExpectedResult = false)]
+        [TestCase("asd43_34", ExpectedResult = true)]
+
+        public static bool FixedTest(string username)
+        {
+            return SimpleValidationOfUsernameWithRegex.ValidateUsr(username);
+        }
+    }
+}
