ca4ed76a4bc3c6b6af06939fb7c555c0e39c78dc
a-kozhanov <a.kozhanov@outlook.com>
2019-11-09 10:56:11 +0300

    Added solution "Filling an array (part 1)" 8 kyu

---
+++ CodeWars/FillingAnArrayPart1.cs
@@ -0,0 +1,38 @@
+﻿
+/*
+https://www.codewars.com/kata/filling-an-array-part-1/train/csharp
+https://www.codewars.com/kata/571d42206414b103dc0006a1/solutions/csharp
+
+8 kyu
+Filling an array (part 1)
+
+We want an array, but not just any old array, an array with contents!
+Write a function that produces an array with the numbers 0 to N-1 in it.
+For example, the following code will result in an array containing the numbers 0 to 4:
+arr(5) // => [0,1,2,3,4]
+
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FillingAnArrayPart1
+    {
+        public static int[] Arr(int N)
+        {
+            return Enumerable.Range(0, N).ToArray();
+        }
+
+        //public static int[] Arr(int N)
+        //{
+        //    List<int> newInt = new List<int>();
+        //    for(int i = 0; i <= N-1; i++)
+        //    {
+        //        newInt.Add(i);
+        //    }
+        //    return newInt.ToArray();
+        //}
+    }
+}
---
+++ CodeWarsTests/FillingAnArrayPart1Tests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FillingAnArrayPart1Tests
+    {
+        [Test, Description("Kata.Arr creates a new array with the numbers 0 to N-1")]
+        public void ExampleTest()
+        {
+            Assert.IsTrue(FillingAnArrayPart1.Arr(0).GetType().IsArray, "FillingAnArrayPart1.Arr creates a new array with the numbers 0 to N-1");
+            Assert.AreEqual(new int[0], FillingAnArrayPart1.Arr(0), "should return a blank array when called with no argument");
+            Assert.AreEqual(new int[] { 0, 1, 2, 3 }, FillingAnArrayPart1.Arr(4), "should return 0 to 3 when called with 4");
+        }
+    }
+}
