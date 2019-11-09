1f3180244b3e83691cc4bc756666415c2353d1e7
a-kozhanov <a.kozhanov@outlook.com>
2019-11-09 19:40:18 +0300

    Added solution "Are arrow functions odd?" 8 kyu

---
+++ CodeWars/AreArrowFunctionsOdd.cs
@@ -0,0 +1,47 @@
+﻿
+/*
+https://www.codewars.com/kata/are-arrow-functions-odd/train/javascript
+https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/solutions/csharp
+
+8 kyu
+Are arrow functions odd?
+
+Time to test your basic knowledge in functions! Return the odds from a list:
+Kata.Odds(new List<int> {1,2,3,4,5}) => new List<int> {1,3,5};
+*/
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class AreArrowFunctionsOdd
+    {
+        public static List<int> Odds(List<int> values) => values.Where(x => x % 2 == 1).ToList();
+    }
+
+
+    //public static List<int> Odds(List<int> values) =>
+    //values.Where(q => q % 2 != 0)
+    //.ToList();
+
+
+    //public static List<int> Odds(List<int> values) =>
+    //// arrow it
+    //values.Where(x => x % 2 == 1)
+    //.ToList();
+
+
+    //public static List<int> Odds(List<int> values) =>
+    //// arrow it
+    //values.Where(s => s % 2 == 1)
+    //.ToList();
+
+
+    //public static List<int> Odds(List<int> values)
+    //{
+    //    return values.Where(x => x % 2 == 1).ToList();
+    //}
+
+}
---
+++ CodeWarsTests/AreArrowFunctionsOddTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AreArrowFunctionsOddTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new List<int>()).Returns(new List<int>()).SetDescription("Should handle empty list");
+                yield return new TestCaseData(new List<int> { 2, 4, 6 }).Returns(new List<int>()).SetDescription("Should handle list with even numbers only");
+                yield return new TestCaseData(new List<int> { 1, 3, 5 }).Returns(new List<int> { 1, 3, 5 }).SetDescription("Should handle list with odd numbers only");
+                yield return new TestCaseData(new List<int> { 1, 2, 3, 4, 5, 6 }).Returns(new List<int> { 1, 3, 5 }).SetDescription("Should handle mixed list");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public List<int> Test(List<int> values) =>
+            AreArrowFunctionsOdd.Odds(values);
+    }
+
+}
