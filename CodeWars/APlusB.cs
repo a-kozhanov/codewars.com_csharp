7fd7b116a6044034958bd3814ae721df16a61f86
a-kozhanov <a.kozhanov@outlook.com>
2019-11-10 19:29:20 +0300

    Added solution "A + B" 8 kyu

---
+++ CodeWars/APlusB.cs
@@ -0,0 +1,22 @@
+﻿
+/*
+https://www.codewars.com/kata/a-plus-b/discuss/csharp
+https://www.codewars.com/kata/5512a0b0509063e57d0003f5/solutions/csharp
+
+8 kyu
+A + B
+
+Vasya Pupkin just started learning Java and C#. 
+At first, he decided to write simple program that was supposed to sum up two small numbers (numbers and their sum fit in a byte), 
+but it didn't work. Vasya was too sad to find out what is wrong. 
+Help him to correct the mistake.
+
+*/
+
+namespace CodeWars
+{
+    public class APlusB
+    {
+        public static byte sum(byte a, byte b) => (byte)(a + b);
+    }
+}
---
+++ CodeWarsTests/APlusBTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class APlusBTests
+    {
+        [Test]
+        public void testSum()
+        {
+            byte a = 1;
+            byte b = 2;
+            Assert.AreEqual(3, APlusB.sum(a, b));
+        }
+    }
+}
