5b33266f83d6417216479a6730946f7c3ddc4f7d
a-kozhanov <a.kozhanov@outlook.com>
2019-11-11 19:34:42 +0300

    Added solution "Thinkful - Number Drills: Pixelart planning" 8 kyu

---
+++ CodeWars/ThinkfulNumberDrillsPixelartPlanning.cs
@@ -0,0 +1,24 @@
+﻿/*
+https://www.codewars.com/kata/thinkful-number-drills-pixelart-planning/train/csharp
+https://www.codewars.com/kata/58630e2ae88af44d2b0000ea/solutions/csharp
+
+8 kyu
+Thinkful - Number Drills: Pixelart planning
+
+Your function should take two arguments: the size of the wall in millimeters and the size of a pixel in millimeters. 
+It should return True if you can fit an exact number of pixels on the wall, otherwise it should return False. 
+For example is_divisible(4050, 27) should return True, but is_divisible(4066, 27) should return False.
+
+Note: you don't need to use an if statement here. Remember that in Python an expression using the == comparison operator will evaluate to either True or False:
+*/
+
+namespace CodeWars
+{
+    public class ThinkfulNumberDrillsPixelartPlanning
+    {
+        public static bool IsDivisible(int wallLength, int pixelSize)
+        {
+            return wallLength % pixelSize == 0;
+        }
+    }
+}
---
+++ CodeWarsTests/ThinkfulNumberDrillsPixelartPlanningTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkfulNumberDrillsPixelartPlanningTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, ThinkfulNumberDrillsPixelartPlanning.IsDivisible(4050, 27));
+            Assert.AreEqual(false, ThinkfulNumberDrillsPixelartPlanning.IsDivisible(4066, 27));
+            Assert.AreEqual(true, ThinkfulNumberDrillsPixelartPlanning.IsDivisible(10000, 20));
+            Assert.AreEqual(false, ThinkfulNumberDrillsPixelartPlanning.IsDivisible(10005, 20));
+            Assert.AreEqual(true, ThinkfulNumberDrillsPixelartPlanning.IsDivisible(10005, 1));
+        }
+    }
+}
