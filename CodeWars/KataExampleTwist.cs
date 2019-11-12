0739311a8547259bb8cd9d661dd0709381997987
a-kozhanov <a.kozhanov@outlook.com>
2019-11-12 18:38:50 +0300

    Added solution "Kata Example Twist" 8 kyu

---
+++ CodeWars/KataExampleTwist.cs
@@ -0,0 +1,47 @@
+﻿/*
+https://www.codewars.com/kata/kata-example-twist/train/csharp
+https://www.codewars.com/kata/525c1a07bb6dda6944000031/solutions/csharp
+
+8 kyu
+Kata Example Twist
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataExampleTwist
+    {
+        public static string[] Websites => Enumerable.Repeat("codewars", 1000).ToArray();
+
+        //public static string[] Websites = new string[1000];
+        //static Kata()
+        //{
+        //    for(int i = 0; i < 1000; i++)
+        //        Websites[i] = "codewars";
+        //}
+
+
+
+        //public static string[] Websites
+        //{
+        //    get
+        //    {
+        //        var sites = new string[1000];
+
+        //        for (var i = 0; i < 1000; i++)
+        //        { 
+        //            sites[i] = "codewars";
+        //        }
+        //        return sites;
+        //    }
+        //}
+
+
+
+    }
+}
---
+++ CodeWarsTests/KataExampleTwistTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataExampleTwistTests
+    {
+        [Test]
+        public static void Tests()
+        {
+            Assert.AreEqual(KataExampleTwist.Websites.Length, 1000);
+            Assert.AreEqual(KataExampleTwist.Websites.GetType().GetElementType(), typeof(System.String));
+            Assert.That(Array.TrueForAll(KataExampleTwist.Websites, (v) => v == "codewars"));
+        }
+    }
+}
