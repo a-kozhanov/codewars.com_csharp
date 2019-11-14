1bfc93ab45037c650a6895b465102da2eb27d883
a-kozhanov <a.kozhanov@outlook.com>
2019-11-14 21:02:35 +0300

    Added solution "Filtering even numbers (Bug Fixes)" 8 kyu

---
+++ CodeWars/FilteringEvenNumbers.cs
@@ -0,0 +1,49 @@
+﻿/*
+https://www.codewars.com/kata/filtering-even-numbers-bug-fixes/train/csharp
+https://www.codewars.com/kata/566dc566f6ea9a14b500007b/solutions/csharp
+
+8 kyu
+Filtering even numbers (Bug Fixes)
+
+Fix the bug in Filtering method
+The method is supposed to remove even numbers from the list and return a list that contains odd number.
+
+However, there is a bug in the method that needs to be resolved.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FilteringEvenNumbers
+    {
+        public static List<int> FilterOddNumber(List<int> listOfNumbers)
+        {
+            return listOfNumbers.Where(x => x % 2 == 1).ToList();
+
+            //for (int i = 0; i < listOfNumbers.Count; i++)
+            //{
+            //    if (listOfNumbers[i] % 2 == 1)
+            //    {
+            //        listOfNumbers.RemoveAt(i);
+            //    }
+            //}
+            //return listOfNumbers;
+        }
+
+        //public static List<int> FilterOddNumber(List<int> listOfNumbers)
+        //{
+        //    for (int i = 0; i < listOfNumbers.Count; i++)
+        //    {
+        //        if (listOfNumbers[i] % 2 == 0)
+        //        {
+        //            listOfNumbers.RemoveAt(i--);
+        //        }
+        //    }
+        //    return listOfNumbers;
+        //}
+
+
+    }
+}
---
+++ CodeWarsTests/FilteringEvenNumbersTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FilteringEvenNumbersTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            List<int> listOfNumbers = new List<int>() { 1, 2, 2, 2, 4, 3, 4, 5, 6, 7 };
+            List<int> expectedResult = new List<int>() { 1, 3, 5, 7 };
+            StringAssert.AreEqualIgnoringCase(string.Join(",", expectedResult), string.Join(",", FilteringEvenNumbers.FilterOddNumber(listOfNumbers)));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            List<int> listOfNumbers = new List<int>() { 1, 2, 2, 2, 4, 3, 4 };
+            List<int> expectedResult = new List<int>() { 1, 3 };
+            StringAssert.AreEqualIgnoringCase(string.Join(",", expectedResult), string.Join(",", FilteringEvenNumbers.FilterOddNumber(listOfNumbers)));
+        }
+    }
+}
