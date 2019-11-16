02c921108a56b5b904a1e8dbb40f6774b97b3e65
a-kozhanov <a.kozhanov@outlook.com>
2019-11-17 00:06:53 +0300

    Added solution "String Templates - Bug Fixing #5" 8 kyu

---
+++ CodeWars/FormattingDecimalPlaces0.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public class FormattingDecimalPlaces0
+    {
+        public static double TwoDecimalPlaces(double number)
+        {
+            return Math.Round(number, 2);
+        }
+    }
+}
---
+++ CodeWarsTests/FormattingDecimalPlaces0Tests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FormattingDecimalPlaces0Tests
+    {
+        [Test]
+        public void Test_01()
+        {
+            Assert.AreEqual(4.66, FormattingDecimalPlaces0.TwoDecimalPlaces(4.659725356));
+        }
+
+        [Test]
+        public void Test_02()
+        {
+            Assert.AreEqual(173735326.38, FormattingDecimalPlaces0.TwoDecimalPlaces(173735326.3783732637948948));
+        }
+    }
+}
