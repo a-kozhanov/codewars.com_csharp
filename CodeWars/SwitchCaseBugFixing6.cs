c0a863c0c7def644621b3c0d971f31976d0e945d
a-kozhanov <a.kozhanov@outlook.com>
2019-11-19 09:57:09 +0300

    Added solution "Switch/Case - Bug Fixing #6" 8 kyu

---
+++ CodeWars/SwitchCaseBugFixing6.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+https://www.codewars.com/kata/switch-slash-case-bug-fixing-number-6/train/csharp
+https://www.codewars.com/kata/55c933c115a8c426ac000082/solutions/csharp
+
+8 kyu
+Switch/Case - Bug Fixing #6
+
+Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+*/
+
+
+using System;
+
+namespace CodeWars
+{
+    public class SwitchCaseBugFixing6
+    {
+        public static double EvalObject(double value1, double value2, char operation)
+        {
+            switch (operation)
+            {
+                case '+': return value1 + value2;
+                case '-': return value1 - value2;
+                case '/': return value1 / value2;
+                case '*': return value1 * value2;
+                case '%': return value1 % value2;
+                case '^': return Math.Pow(value1, value2);
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ CodeWarsTests/SwitchCaseBugFixing6Tests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SwitchCaseBugFixing6Tests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            Assert.AreEqual(2, SwitchCaseBugFixing6.EvalObject(1, 1, '+'));
+            Assert.AreEqual(0, SwitchCaseBugFixing6.EvalObject(1, 1, '-'));
+            Assert.AreEqual(1, SwitchCaseBugFixing6.EvalObject(1, 1, '/'));
+            Assert.AreEqual(1, SwitchCaseBugFixing6.EvalObject(1, 1, '*'));
+            Assert.AreEqual(0, SwitchCaseBugFixing6.EvalObject(1, 1, '%'));
+            Assert.AreEqual(1, SwitchCaseBugFixing6.EvalObject(1, 1, '^'));
+        }
+    }
+}
