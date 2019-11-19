74a452aa9ae47406b723f036445aeb9112d0f9e5
a-kozhanov <a.kozhanov@outlook.com>
2019-11-19 20:17:51 +0300

    Added solution "Determine offspring sex based on genes XX and XY chromosomes" 8 kyu

---
+++ CodeWarsTests/DetermineOffspringSexBasedTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DetermineOffspringSexBasedTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Congratulations! You're going to have a son.", DetermineOffspringSexBased.ChromosomeCheck("XY"));
+            Assert.AreEqual("Congratulations! You're going to have a daughter.", DetermineOffspringSexBased.ChromosomeCheck("XX"));
+        }
+    }
+}
