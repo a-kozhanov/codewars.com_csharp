337ab07fc44ed0f55cdbf57999002a52017b539a
a-kozhanov <a.kozhanov@outlook.com>
2019-11-19 21:33:30 +0300

    Added solution "Basic subclasses - Adam and Eve" 8 kyu

---
+++ CodeWarsTests/NotCompleted/BasicSubclassesAdamAndEveTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BasicSubclassesAdamAndEveTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            BasicSubclassesAdamAndEve.Human[] humans = BasicSubclassesAdamAndEve.Create();
+            Assert.That(humans[0] is BasicSubclassesAdamAndEve.Man, "The first object in the array should be a Man");
+        }
+    }
+}
