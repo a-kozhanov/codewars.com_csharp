06bd409296f396909fb144b79d034a9ef2507c6b
a-kozhanov <a.kozhanov@outlook.com>
2019-11-19 20:02:16 +0300

    Added solution "Exclusive "or" (xor) Logical Operator" 8 kyu

---
+++ CodeWarsTests/ExclusiveOrXorLogicalOperatorTests.cs
@@ -0,0 +1,38 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExclusiveOrXorLogicalOperatorTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            testing(ExclusiveOrXorLogicalOperator.Xor(false, false), false);
+            testing(ExclusiveOrXorLogicalOperator.Xor(true, false), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(false, true), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(true, true), false);
+        }
+
+        [Test]
+        public void NestedTests()
+        {
+            testing(ExclusiveOrXorLogicalOperator.Xor(false, ExclusiveOrXorLogicalOperator.Xor(false, false)), false);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, false), false), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, true), false), false);
+            testing(ExclusiveOrXorLogicalOperator.Xor(true, ExclusiveOrXorLogicalOperator.Xor(true, true)), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(false, false), ExclusiveOrXorLogicalOperator.Xor(false, false)), false);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(false, false), ExclusiveOrXorLogicalOperator.Xor(false, true)), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, false), ExclusiveOrXorLogicalOperator.Xor(false, false)), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, false), ExclusiveOrXorLogicalOperator.Xor(true, false)), false);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, true), ExclusiveOrXorLogicalOperator.Xor(true, false)), true);
+            testing(ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, ExclusiveOrXorLogicalOperator.Xor(true, true)), ExclusiveOrXorLogicalOperator.Xor(ExclusiveOrXorLogicalOperator.Xor(true, true), false)), true);
+        }
+
+        private static void testing(bool actual, bool expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
