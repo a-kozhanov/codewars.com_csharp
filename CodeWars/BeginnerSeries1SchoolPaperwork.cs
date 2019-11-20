11f6703ddce6ae1a94616d8dca51e7a099dea9ad
a-kozhanov <a.kozhanov@outlook.com>
2019-11-20 21:42:13 +0300

    Added solution "Beginner Series #1 School Paperwork" 8 kyu

---
+++ CodeWars/BeginnerSeries1SchoolPaperwork.cs
@@ -0,0 +1,56 @@
+﻿/*
+https://www.codewars.com/kata/beginner-series-number-1-school-paperwork/train/csharp
+https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/solutions/csharp
+
+8 kyu
+Beginner Series #1 School Paperwork
+
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+Your task is to calculate how many blank pages do you need.
+
+Example:
+Paperwork(5, 5) == 25
+
+Note! if n or m < 0 return 0! Waiting for translations and Feedback! Thanks!
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class BeginnerSeries1SchoolPaperwork
+    {
+        public static int Paperwork(int n, int m)
+        {
+            return n < 0 || m < 0 ? 0 : n * m;
+        }
+
+        //public static int Paperwork(int n, int m) => (n > 0 && m > 0) ? m*n : 0;
+
+
+        //public static int Paperwork(int n, int m)
+        //{
+        //    //#Happy Coding! ^_^
+        //    return (n < 0 | m < 0) ? 0 : n * m;
+        //}
+
+        //public static int Paperwork(int n, int m)
+        //{
+        //    if (n < 0) n = 0;
+        //    if (m < 0) m = 0;
+
+        //    return n*m;
+        //}
+
+
+        //public static int Paperwork(int n, int m) => Math.Max(n,0) * Math.Max(m, 0);
+
+
+        //public static int Paperwork(int n, int m)
+        //{
+        //    return (n < 0) ? 0 : (m < 0) ? 0 : n * m;
+        //}
+
+
+    }
+}
---
+++ CodeWarsTests/BeginnerSeries1SchoolPaperworkTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BeginnerSeries1SchoolPaperworkTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(25, BeginnerSeries1SchoolPaperwork.Paperwork(5, 5));
+        }
+    }
+}
