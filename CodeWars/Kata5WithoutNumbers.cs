fd9e4ce18e830d63f540633f487acdd6fb641c35
a-kozhanov <a.kozhanov@outlook.com>
2019-11-20 22:25:47 +0300

    Added solution "5 without numbers !!" 8 kyu

---
+++ CodeWars/Kata5WithoutNumbers.cs
@@ -0,0 +1,22 @@
+﻿/*
+https://www.codewars.com/kata/5-without-numbers/train/csharp
+https://www.codewars.com/kata/59441520102eaa25260000bf/solutions/csharp
+
+8 kyu
+5 without numbers !!
+
+Write a function that always returns 5
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+Good luck :)
+*/
+
+namespace CodeWars
+{
+    public class Kata5WithoutNumbers
+    {
+        public static int UnusualFive()
+        {
+            return "abcde".Length;
+        }
+    }
+}
---
+++ CodeWarsTests/Kata5WithoutNumbersTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class Kata5WithoutNumbersTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(Kata5WithoutNumbers.UnusualFive(), Is.EqualTo(5));
+        }
+    }
+}
