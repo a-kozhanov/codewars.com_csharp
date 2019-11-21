ee66d81f8ffb07407357c4b46e5d2ba19e6a6fc5
a-kozhanov <a.kozhanov@outlook.com>
2019-11-21 23:02:39 +0300

    Added solution "Grasshopper - Bug Squashing" 8 kyu

---
+++ CodeWars/GrasshopperBugSquashing.cs
@@ -0,0 +1,78 @@
+﻿/*
+https://www.codewars.com/kata/grasshopper-bug-squashing/train/csharp
+https://www.codewars.com/kata/56214b6864fe8813f1000019/solutions/csharp
+
+8 kyu
+Grasshopper - Bug Squashing
+
+Terminal game bug squashing
+You are creating a text-based terminal version of your favorite board game. 
+In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+
+You are using a library that already has the functions below. 
+Create a function named main that calls the functions in the proper order.
+
+- `Combat`
+- `BuyHealth`
+- `GetCoins`
+- `PrintStatus`
+- `RollDice`
+- `Move`
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class GrasshopperBugSquashing
+    {
+        public static int Health = 100;
+        public static int Position = 0;
+        public static int Coins = 0;
+        public static List<string> Log = new List<string>(); //Delete in kata
+
+        public static void PlayTurn()
+        {
+            Log = new List<string> { "RollDice", "Move", "Combat", "GetCoins", "BuyHealth", "PrintStatus" };
+        }
+    }
+}
+
+//public static partial class Kata
+//{
+//    public static int Health = 100;
+//    public static int Position = 0;
+//    public static int Coins = 0;
+
+//    public static void PlayTurn()
+//    {
+//        Log.AddRange(new[] {"RollDice", "Move", "Combat", "GetCoins", "BuyHealth", "PrintStatus"});
+//    }
+//}
+
+
+//    public static partial class Kata
+//    {
+//        public static int Health = 100;
+//        public static int Position = 0;
+//        public static int Coins = 0;
+
+//        public static void PlayTurn()
+//        {
+//            RolDice();
+//            Move();
+//            Combat();
+//            GetCoins();
+//            BuyHealth();
+//            PrintStatus();
+//        }
+
+//        public static void RolDice()
+//        {
+//            Log.Add("RollDice");
+//        }
+//    }
+
+
+
---
+++ CodeWarsTests/GrasshopperBugSquashingTests.cs
@@ -0,0 +1,59 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperBugSquashingTests
+    {
+        [Test, Description("GrasshopperBugSquashing should define variables and store values")]
+        public void VariableTest()
+        {
+            Assert.AreEqual(100, GrasshopperBugSquashing.Health);
+            Assert.AreEqual(0, GrasshopperBugSquashing.Position);
+            Assert.AreEqual(0, GrasshopperBugSquashing.Coins);
+        }
+
+        [Test, Description("PlayTurn should not throw an exception")]
+        public void ErrorTest()
+        {
+            Assert.DoesNotThrow(() => GrasshopperBugSquashing.PlayTurn());
+        }
+
+        [Test, Description("should roll dice first")]
+        public void OrderTest1()
+        {
+            Assert.AreEqual("RollDice", actual: GrasshopperBugSquashing.Log[0]);
+        }
+
+        [Test, Description("should move second")]
+        public void OrderTest2()
+        {
+            Assert.AreEqual("Move", GrasshopperBugSquashing.Log[1]);
+        }
+
+        [Test, Description("should combat third")]
+        public void OrderTest3()
+        {
+            Assert.AreEqual("Combat", GrasshopperBugSquashing.Log[2]);
+        }
+
+        [Test, Description("should get coins fourth")]
+        public void OrderTest4()
+        {
+            Assert.AreEqual("GetCoins", GrasshopperBugSquashing.Log[3]);
+        }
+
+        [Test, Description("should buy health fifth")]
+        public void OrderTest5()
+        {
+            Assert.AreEqual("BuyHealth", GrasshopperBugSquashing.Log[4]);
+        }
+
+        [Test, Description("should print status sixth")]
+        public void OrderTest6()
+        {
+            Assert.AreEqual("PrintStatus", GrasshopperBugSquashing.Log[5]);
+        }
+    }
+}
