9fe39f5c4b96a5b724c2de2d0572bc332fdaf01d
a-kozhanov <a.kozhanov@outlook.com>
2019-11-23 22:31:23 +0300

    Added solution "Keep up the hoop" 8 kyu

---
+++ CodeWars/KeepUpHoop.cs
@@ -0,0 +1,24 @@
+﻿/*
+https://www.codewars.com/kata/keep-up-the-hoop/train/csharp
+https://www.codewars.com/kata/keep-up-the-hoop/solutions/csharp
+
+8 kyu
+Keep up the hoop
+
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+-If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+-If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+*/
+
+
+namespace CodeWars
+{
+    public class KeepUpHoop
+    {
+        public static string HoopCount(int n)
+        {
+            return n > 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+        }
+    }
+}
---
+++ CodeWarsTests/KeepUpHoopTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KeepUpHoopTests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            Assert.AreEqual("Keep at it until you get it", KeepUpHoop.HoopCount(6), "Should work for 6");
+            Assert.AreEqual("Great, now move on to tricks", KeepUpHoop.HoopCount(22), "Should work for 22");
+        }
+    }
+}
