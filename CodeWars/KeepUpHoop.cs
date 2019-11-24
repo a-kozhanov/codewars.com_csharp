a4355b9be8bdd142ad4ca28d60a60eae143add42
a-kozhanov <a.kozhanov@outlook.com>
2019-11-24 22:55:46 +0300

    Fixed solution "Keep up the hoop"

--- CodeWars/KeepUpHoop.cs
+++ CodeWars/KeepUpHoop.cs
@@ -18,7 +18,7 @@ namespace CodeWars
     {
         public static string HoopCount(int n)
         {
-            return n > 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+            return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
         }
     }
 }
