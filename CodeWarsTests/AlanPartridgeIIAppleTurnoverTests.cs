cc192487e57568c2d899355192c518c483cec5fe
a-kozhanov <a.kozhanov@outlook.com>
2019-11-25 22:36:37 +0300

    Added solution "Alan Partridge II - Apple Turnover" 8 kyu

---
+++ CodeWarsTests/AlanPartridgeIIAppleTurnoverTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AlanPartridgeIIAppleTurnoverTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("It's hotter than the sun!!", AlanPartridgeIIAppleTurnover.Apple("50"));
+            Assert.AreEqual("Help yourself to a honeycomb Yorkie for the glovebox.", AlanPartridgeIIAppleTurnover.Apple(4));
+        }
+    }
+}
