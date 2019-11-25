4a3fc5933394617c392f343cc02e22b4b1715e61
a-kozhanov <a.kozhanov@outlook.com>
2019-11-26 00:26:55 +0300

    Added solution "Training JS #7: if..else and ternary operator" 8 kyu

---
+++ CodeWars/TrainingJS7.cs
@@ -0,0 +1,20 @@
+﻿/*
+https://www.codewars.com/kata/training-js-number-7-if-dot-else-and-ternary-operator/train/csharp
+https://www.codewars.com/kata/57202aefe8d6c514300001fd/solutions/csharp/me/best_practice
+
+8 kyu
+Training JS #7: if..else and ternary operator
+
+*/
+
+
+namespace CodeWars
+{
+    public class TrainingJS7
+    {
+        public static int SaleHotDogs(int n)
+        {
+            return n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+        }
+    }
+}
---
+++ CodeWarsTests/TrainingJS7Tests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TrainingJS7Tests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(100, TrainingJS7.SaleHotDogs(1));
+            Assert.AreEqual(400, TrainingJS7.SaleHotDogs(4));
+            Assert.AreEqual(475, TrainingJS7.SaleHotDogs(5));
+            Assert.AreEqual(855, TrainingJS7.SaleHotDogs(9));
+            Assert.AreEqual(900, TrainingJS7.SaleHotDogs(10));
+            Assert.AreEqual(9000, TrainingJS7.SaleHotDogs(100));
+        }
+    }
+}
