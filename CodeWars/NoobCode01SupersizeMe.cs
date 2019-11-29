044b0d56d20b5ab960da1b2d5c0b0e669c44719c
a-kozhanov <a.kozhanov@outlook.com>
2019-11-30 00:36:54 +0300

    Added solution "noobCode 01: SUPERSIZE ME.... or rather, this integer!" 8 kyu

---
+++ CodeWars/NoobCode01SupersizeMe.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/noobcode-01-supersize-me-dot-dot-dot-or-rather-this-integer/train/csharp
+
+
+8 kyu
+noobCode 01: SUPERSIZE ME.... or rather, this integer!
+
+Write a function that rearranges an integer into its largest possible value.
+
+Kata.SuperSize(123456) //654321
+Kata.SuperSize(105) // 510
+Kata.SuperSize(12) // 21
+If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NoobCode01SupersizeMe
+    {
+        public static long SuperSize(long num)
+        {
+            return long.Parse(num.ToString().OrderByDescending(x => x).ToArray());
+        }
+    }
+}
---
+++ CodeWarsTests/NoobCode01SupersizeMeTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NoobCode01SupersizeMeTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(96, NoobCode01SupersizeMe.SuperSize(69));
+            Assert.AreEqual(531, NoobCode01SupersizeMe.SuperSize(513));
+            Assert.AreEqual(7210, NoobCode01SupersizeMe.SuperSize(2017));
+            Assert.AreEqual(441, NoobCode01SupersizeMe.SuperSize(414));
+            Assert.AreEqual(987610, NoobCode01SupersizeMe.SuperSize(608719));
+            Assert.AreEqual(987654321, NoobCode01SupersizeMe.SuperSize(123456789));
+            Assert.AreEqual(710000000000, NoobCode01SupersizeMe.SuperSize(700000000001));
+            Assert.AreEqual(666666, NoobCode01SupersizeMe.SuperSize(666666));
+            Assert.AreEqual(2, NoobCode01SupersizeMe.SuperSize(2));
+        }
+    }
+}
