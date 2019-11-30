bf4f9ac092124bc4b8146ebf0a7acc1832fcb80d
a-kozhanov <a.kozhanov@outlook.com>
2019-11-30 11:55:58 +0300

    Added solution "Grasshopper - If/else syntax debug" 8 kyu

---
+++ CodeWars/GrasshopperIfElseSyntaxDebug.cs
@@ -0,0 +1,23 @@
+﻿/*
+https://www.codewars.com/kata/grasshopper-if-slash-else-syntax-debug/train/csharp
+https://www.codewars.com/kata/57089707fe2d01529f00024a/solutions/csharp
+
+8 kyu
+Grasshopper - If/else syntax debug
+
+If/else syntax debug
+While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive. 
+Unfortunately, Greg made some errors while creating the function.
+
+checkAlive/CheckAlive should return true if the player's health is greater than 0 or false if it is 0 or below.
+*/
+
+namespace CodeWars
+{
+    public class GrasshopperIfElseSyntaxDebug
+    {
+        public int Health { get; set; } = 100;
+        public bool CheckAlive() => Health > 0;
+    }
+
+}
---
+++ CodeWarsTests/GrasshopperIfElseSyntaxDebugTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperIfElseSyntaxDebugTests
+    {
+        [Test, Description("Should return the proper result with fixed tests")]
+        public void FixedTest()
+        {
+            GrasshopperIfElseSyntaxDebug greg = new GrasshopperIfElseSyntaxDebug(); //Player greg = new Player();
+            greg.Health = 5;
+            Assert.AreEqual(true, greg.CheckAlive());
+
+            GrasshopperIfElseSyntaxDebug joe = new GrasshopperIfElseSyntaxDebug(); //Player joe = new Player();
+            joe.Health = 0;
+            Assert.AreEqual(false, joe.CheckAlive());
+        }
+    }
+}
