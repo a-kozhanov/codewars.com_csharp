86fba91b1519c7ebe8f157c031aeb89866f671c6
a-kozhanov <a.kozhanov@outlook.com>
2019-12-01 21:34:44 +0300

    Added solution "Regular Ball Super Ball" 8 kyu

---
+++ CodeWars/RegularBallSuperBall.cs
@@ -0,0 +1,40 @@
+﻿/*
+https://www.codewars.com/kata/regular-ball-super-ball/csharp
+https://www.codewars.com/kata/regular-ball-super-ball/solutions/csharp
+
+8 kyu
+Regular Ball Super Ball
+
+Create a class Ball.
+
+Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+namespace CodeWars
+{
+    public class RegularBallSuperBall
+    {
+        public string ballType { get; }
+        public RegularBallSuperBall(string ballType = "regular") => this.ballType = ballType;
+    }
+
+    public class Ball
+    {
+        public string ballType { get; private set; }
+
+        public Ball(string ballType = "regular")
+        {
+            this.ballType = ballType;
+        }
+    }
+
+
+}
---
+++ CodeWarsTests/RegularBallSuperBallTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RegularBallSuperBallTests
+    {
+        [Test]
+        public void DefaultConstructorShouldSetBallTypeToRegular()
+        {
+            Assert.AreEqual("regular", new RegularBallSuperBall().ballType);
+        }
+
+        [Test]
+        public void ConstructorWithArgumentShouldSetBallTypeAsExpected()
+        {
+            Assert.AreEqual("super", new RegularBallSuperBall("super").ballType);
+        }
+    }
+}
