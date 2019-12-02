d72a21e8e6621da78a0b1ebdb380faeb6987dbdc
a-kozhanov <a.kozhanov@outlook.com>
2019-12-02 18:20:14 +0300

    Added solution "Grasshopper - Array Mean" 8 kyu

---
+++ CodeWars/GrasshopperArrayMean.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/grasshopper-array-mean/train/csharp
+
+
+8 kyu
+Grasshopper - Array Mean
+
+Find Mean
+Find the mean (average) of a list of numbers in an array.
+
+Information
+To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+
+1+3+5+7 = 16
+2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+16/4 = 4
+3. The mean (or average) of this list is 4
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class GrasshopperArrayMean
+    {
+        public static int FindAverage(int[] nums) => (int)nums.Average();
+    }
+}
---
+++ CodeWarsTests/GrasshopperArrayMeanTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperArrayMeanTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(1, GrasshopperArrayMean.FindAverage(new[] { 1 }));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            Assert.AreEqual(4, GrasshopperArrayMean.FindAverage(new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void ThirdTest()
+        {
+            Assert.AreEqual(-5, GrasshopperArrayMean.FindAverage(new[] { -10, -5, -5, 0 }));
+        }
+
+        [Test]
+        public void FourthTest()
+        {
+            Assert.AreEqual(0, GrasshopperArrayMean.FindAverage(new[] { 0 }));
+        }
+    }
+}
