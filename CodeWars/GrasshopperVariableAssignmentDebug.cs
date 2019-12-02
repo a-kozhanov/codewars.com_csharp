1ab8578f003ed5d02e2c93025ecf632997a1d468
a-kozhanov <a.kozhanov@outlook.com>
2019-12-02 19:50:09 +0300

    Added solution "Grasshopper - Variable Assignment Debug" 8 kyu

---
+++ CodeWars/GrasshopperVariableAssignmentDebug.cs
@@ -0,0 +1,20 @@
+﻿/*
+https://www.codewars.com/kata/grasshopper-variable-assignment-debug/train/csharp
+https://www.codewars.com/kata/grasshopper-variable-assignment-debug/solutions
+
+8 kyu
+Grasshopper - Variable Assignment Debug
+
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+namespace CodeWars
+{
+    public class GrasshopperVariableAssignmentDebug
+    {
+        public const string a = "dev";
+        public const string b = "Lab";
+        public static string name = a + b;
+    }
+}
---
+++ CodeWarsTests/GrasshopperVariableAssignmentDebugTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperVariableAssignmentDebugTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual("expected", "actual");
+        }
+    }
+}
