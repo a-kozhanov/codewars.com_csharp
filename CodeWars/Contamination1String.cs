75195d81381afb87a4a6ddb75c6fc7503fb58e62
a-kozhanov <a.kozhanov@outlook.com>
2019-12-02 23:36:46 +0300

    Added solution "Contamination #1 -String-" 8 kyu

---
+++ CodeWars/Contamination1String.cs
@@ -0,0 +1,38 @@
+﻿/*
+https://www.codewars.com/kata/contamination-number-1-string/train/csharp
+https://www.codewars.com/kata/contamination-number-1-string/solutions
+
+8 kyu
+Contamination #1 -String-
+
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class Contamination1String
+    {
+        public static string Contamination(string text, string character)
+        {
+            return string.Concat(Enumerable.Repeat(character, text.Length));
+        }
+
+        //public static string Contamination(string text, string character) => new Regex(@".").Replace(text, character);
+    }
+}
---
+++ CodeWarsTests/Contamination1StringTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class Contamination1StringTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("zzz", Contamination1String.Contamination("abc", "z"));
+            Assert.AreEqual("", Contamination1String.Contamination("", "z"));
+            Assert.AreEqual("", Contamination1String.Contamination("abc", String.Empty));
+            Assert.AreEqual("&&&&&&&&", Contamination1String.Contamination("_3ebzgh4", "&"));
+            Assert.AreEqual("      ", Contamination1String.Contamination("//case", " "));
+        }
+    }
+}
