554f9ba17b018655bf10ec0760d9b4791a7d73cb
a-kozhanov <a.kozhanov@outlook.com>
2019-12-02 20:14:39 +0300

    Added solution "Grasshopper - Basic Function Fixer" 8 kyu

---
+++ CodeWars/GrasshopperBasicFunctionFixer.cs
@@ -0,0 +1,21 @@
+﻿/*
+https://www.codewars.com/kata/grasshopper-basic-function-fixer/train/csharp
+https://www.codewars.com/kata/56200d610758762fb0000002/solutions/csharp
+
+8 kyu
+Grasshopper - Basic Function Fixer
+
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. 
+It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+*/
+
+namespace CodeWars
+{
+    public class GrasshopperBasicFunctionFixer
+    {
+        public static int AddFive(int num) => num + 5;
+    }
+}
---
+++ CodeWarsTests/GrasshopperBasicFunctionFixerTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperBasicFunctionFixerTests
+    {
+        [Test]
+        [TestCase(5, ExpectedResult = 10)]
+        [TestCase(0, ExpectedResult = 5)]
+        [TestCase(-5, ExpectedResult = 0)]
+        public static int FixedTest(int num)
+        {
+            return GrasshopperBasicFunctionFixer.AddFive(num);
+        }
+    }
+}
