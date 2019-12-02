4c0cc84f15897c7ef88b88a9ee6c048da6f879f9
a-kozhanov <a.kozhanov@outlook.com>
2019-12-02 19:59:01 +0300

    Added solution "Grasshopper - Debug sayHello" 8 kyu

---
+++ CodeWars/GrasshopperDebugSayHello.cs
@@ -0,0 +1,23 @@
+﻿/*
+https://www.codewars.com/kata/grasshopper-debug-sayhello/train/csharp
+https://www.codewars.com/kata/5625618b1fe21ab49f00001f/solutions/csharp
+
+8 kyu
+Grasshopper - Debug sayHello
+
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+namespace CodeWars
+{
+    public class GrasshopperDebugSayHello
+    {
+        public static string SayHello(string name) => $"Hello, {name}";
+    }
+}
+
---
+++ CodeWarsTests/GrasshopperDebugSayHelloTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GrasshopperDebugSayHelloTests
+    {
+        [Test]
+        [TestCase("Mr. Spock", ExpectedResult = "Hello, Mr. Spock")]
+        [TestCase("Captain Kirk", ExpectedResult = "Hello, Captain Kirk")]
+        [TestCase("Liutenant Uhura", ExpectedResult = "Hello, Liutenant Uhura")]
+        [TestCase("Dr. McCoy", ExpectedResult = "Hello, Dr. McCoy")]
+        public static string FixedTest(string name)
+        {
+            return GrasshopperDebugSayHello.SayHello(name);
+        }
+    }
+}
