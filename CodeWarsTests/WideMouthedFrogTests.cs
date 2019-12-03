0e3a2e3deabf9df18cf221747d8308b0c7e7bc08
a-kozhanov <a.kozhanov@outlook.com>
2019-12-03 21:42:52 +0300

    Added solution "The Wide-Mouthed frog!" 8 kyu

---
+++ CodeWarsTests/WideMouthedFrogTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WideMouthedFrogTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("wide", WideMouthedFrog.MouthSize("toucan"));
+            Assert.AreEqual("wide", WideMouthedFrog.MouthSize("ant bear"));
+            Assert.AreEqual("small", WideMouthedFrog.MouthSize("alligator"));
+        }
+    }
+}
