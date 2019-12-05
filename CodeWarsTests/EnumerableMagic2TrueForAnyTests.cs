13ae5e36329e401b294962202843801311b347b0
a-kozhanov <a.kozhanov@outlook.com>
2019-12-06 00:43:06 +0300

    Added solution "Enumerable Magic #2 - True for Any?" 8 kyu

---
+++ CodeWarsTests/EnumerableMagic2TrueForAnyTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    class EnumerableMagic2TrueForAnyTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(true, EnumerableMagic2TrueForAny.Any(new int[] { 1, 2, 3, 4 }, v => v > 3));
+            Assert.AreEqual(false, EnumerableMagic2TrueForAny.Any(new int[] { 1, 2, 3, 4 }, v => v > 4));
+        }
+    }
+}
