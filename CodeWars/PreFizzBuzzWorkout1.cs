9b0e76a9a1ed3361dece2c198e51e23373787756
a-kozhanov <a.kozhanov@outlook.com>
2019-12-06 22:53:16 +0300

    Added solution "Pre-FizzBuzz Workout #1" 8 kyu

---
+++ CodeWars/PreFizzBuzzWorkout1.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/pre-fizzbuzz-workout-number-1/train/csharp
+https://www.codewars.com/kata/569e09850a8e371ab200000b/solutions/csharp
+
+8 kyu
+Pre-FizzBuzz Workout #1
+
+This is the first step to understanding FizzBuzz.
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+Your expected output is an array of positive integers from 1 to n (inclusive).
+Your job is to write an algorithm that gets you from the input to the output.
+
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class PreFizzBuzzWorkout1
+    {
+        public static int[] PreFizz(int n)
+        {
+            return Enumerable.Range(1, n).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/PreFizzBuzzWorkout1Tests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PreFizzBuzzWorkout1Tests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { 1 }, PreFizzBuzzWorkout1.PreFizz(1), "Array should be from 1 to 1");
+            Assert.AreEqual(new int[] { 1, 2 }, PreFizzBuzzWorkout1.PreFizz(2), "Array should be from 1 to 2");
+            Assert.AreEqual(new int[] { 1, 2, 3 }, PreFizzBuzzWorkout1.PreFizz(3), "Array should be from 1 to 3");
+            Assert.AreEqual(new int[] { 1, 2, 3, 4 }, PreFizzBuzzWorkout1.PreFizz(4), "Array should be from 1 to 4");
+            Assert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, PreFizzBuzzWorkout1.PreFizz(5), "Array should be from 1 to 5");
+        }
+    }
+}
