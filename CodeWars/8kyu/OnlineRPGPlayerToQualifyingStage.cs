01cddd3b714951c93d5993eb5723ca7d31e75b58
a-kozhanov <a.kozhanov@outlook.com>
2019-12-06 21:03:12 +0300

    Moved files

---
+++ CodeWars/8kyu/OnlineRPGPlayerToQualifyingStage.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/online-rpg-player-to-qualifying-stage/train/csharp
+https://www.codewars.com/kata/55849d76acd73f6cc4000087/solutions/csharp
+
+8 kyu
+Online RPG: player to qualifying stage?
+
+Let's imagine we have a popular online RPG. A player begins with a score of 0 in class E5. A1 is the highest level a player can achieve.
+
+Now let's say the players wants to rank up to class E4. To do so the player needs to achieve at least 100 points to enter the qualifying stage.
+
+Write a script that will check to see if the player has achieved at least 100 points in his class. If so, he enters the qualifying stage.
+
+In that case, we return, "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.".
+
+Otherwise return, False/false (according to the language n use).
+
+NOTE
+: Remember, in C# you have to cast your output value to Object type!
+*/
+//using System;
+
+namespace CodeWars
+{
+    public class OnlineRPGPlayerToQualifyingStage
+    {
+        public static object PlayerRankUp(int points)
+        {
+            return points < 100
+                ? (object)false
+                : "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.";
+        }
+    }
+
+
+
+}
---
+++ CodeWarsTests/8kyu/SimpleFun261WhoseMoveTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun261WhoseMoveTests
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual("white", SimpleFun261WhoseMove.WhoseMove("black", false));
+            Assert.AreEqual("white", SimpleFun261WhoseMove.WhoseMove("white", true));
+            Assert.AreEqual("black", SimpleFun261WhoseMove.WhoseMove("white", false));
+        }
+    }
+}
