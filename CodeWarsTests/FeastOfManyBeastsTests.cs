42ac03d3d43acb63bf7af0c32f7c7adcd60ea966
a-kozhanov <a.kozhanov@outlook.com>
2019-12-07 10:08:56 +0300

    Added solution "The Feast of Many Beasts" 8 kyu

---
+++ CodeWarsTests/FeastOfManyBeastsTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FeastOfManyBeastsTests
+    {
+        [TestCase("great blue heron", "garlic naan")]
+        [TestCase("chickadee", "chocolate cake")]
+        public void BasicTestsTrue(string beast, string dish)
+        {
+            Assert.IsTrue(FeastOfManyBeasts.Feast(beast, dish));
+        }
+
+        [TestCase("brown bear", "bear claw")]
+        public void BasicTestsFalse(string beast, string dish)
+        {
+            Assert.IsFalse(FeastOfManyBeasts.Feast(beast, dish));
+        }
+    }
+}
