b73431017852fe3a09b7fa123b0483d59a95fc37
a-kozhanov <a.kozhanov@outlook.com>
2019-12-10 21:12:39 +0300

    Added solution "Price of Mangoes" 8 kyu

---
+++ CodeWarsTests/PriceOfMangoesTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PriceOfMangoesTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(6, PriceOfMangoes.Mango(3, 3));
+            Assert.AreEqual(30, PriceOfMangoes.Mango(9, 5));
+        }
+    }
+}
