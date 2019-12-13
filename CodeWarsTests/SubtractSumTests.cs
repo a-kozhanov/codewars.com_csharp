7cc5708f418bab9bda87283b869a2680b417b3c8
a-kozhanov <a.kozhanov@outlook.com>
2019-12-13 11:22:18 +0300

    Added solution "Subtract the Sum" 8 kyu

---
+++ CodeWarsTests/SubtractSumTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataSubtractSumTests
+    {
+        [Test]
+        public void Test1()
+        {
+            StringAssert.AreEqualIgnoringCase("apple", KataSubtractSum.SubtractSum(11));
+        }
+    }
+}
