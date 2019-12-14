350829e8ee9bd3191395bfef56d376e34a3bb0dd
a-kozhanov <a.kozhanov@outlook.com>
2019-12-14 19:59:15 +0300

    Added solution "Take an Arrow to the knee, Functionally" 8 kyu

---
+++ CodeWars/TakeArrowToKneeFunctionally.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/take-an-arrow-to-the-knee-functionally/train/csharp
+https://www.codewars.com/kata/559f3123e66a7204f000009f/solutions/csharp
+
+8 kyu
+Take an Arrow to the knee, Functionally
+
+Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class TakeArrowToKneeFunctionally
+    {
+        public static string ArrowFunc(int[] arr)
+        {
+            return string.Concat(arr.Select(c => (char)c));
+        }
+    }
+}
---
+++ CodeWarsTests/TakeArrowToKneeFunctionallyTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TakeArrowToKneeFunctionallyTests
+    {
+        [Test]
+        public static void FixedTests()
+        {
+            Assert.AreEqual("Test", TakeArrowToKneeFunctionally.ArrowFunc(new int[] { 84, 101, 115, 116 }), "");
+        }
+    }
+}
