bbe45d2be4d7e531682ce86296e0ed568ae731c0
a-kozhanov <a.kozhanov@outlook.com>
2019-12-15 19:30:18 +0300

    Added solution "pick a set of first elements" 8 kyu

---
+++ CodeWars/PickSetOfFirstElements.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/pick-a-set-of-first-elements/train/csharp
+https://www.codewars.com/kata/572b77262bedd351e9000076/solutions/csharp
+
+8 kyu
+pick a set of first elements
+
+Write a JavaScript function to get the first element of an array. 
+Passing a parameter n (default=1) will return the first n elements of the array. 
+If n == 0 return an empty array ([]);
+
+Examples
+var arr = new object[] { 'a', 'b', 'c', 'd', 'e' };
+Kata.TakeFirstElements(arr); => new object[] { 'a' }
+Kata.TakeFirstElements(arr, 2); => new object[] { 'a', 'b' }
+Kata.TakeFirstElements(arr, 3); => new object[] { 'a', 'b', 'c' }
+Kata.TakeFirstElements(arr, 0); => new object[] { }
+Note:
+Don't modify array argument.
+*/
+
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class PickSetOfFirstElements
+    {
+        public static object[] TakeFirstElements(object[] array, int n = 1)
+        {
+            return array.Take(n).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/PickSetOfFirstElementsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PickSetOfFirstElementsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var arr = new object[] { 'a', 'b', 'c', 'd', 'e' };
+            Assert.AreEqual(new object[] { 'a' }, PickSetOfFirstElements.TakeFirstElements(arr));
+            Assert.AreEqual(new object[] { 'a', 'b' }, PickSetOfFirstElements.TakeFirstElements(arr, 2));
+        }
+    }
+}
