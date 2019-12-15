3816c6e9f90f818e7f0a78502eac5ce186800d08
a-kozhanov <a.kozhanov@outlook.com>
2019-12-15 20:45:11 +0300

    Added solution "Add new item (collections are passed by reference)" 8 kyu

---
+++ CodeWarsTests/AddNewItemTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AddNewItemTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            List<int> listOfNumbers = new List<int>() { 1, 2, 2, 2, 4, 3, 4, 5, 6, 7 };
+            Assert.AreEqual((AddNewItem.AddExtra(listOfNumbers)).Count, listOfNumbers.Count + 1);
+        }
+    }
+}
