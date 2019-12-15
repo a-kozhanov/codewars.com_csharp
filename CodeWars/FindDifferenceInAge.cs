b0893cc6ce472fad0d1be13785295b4d7cd9814e
a-kozhanov <a.kozhanov@outlook.com>
2019-12-15 19:16:12 +0300

    Added solution "Find the Difference in Age between Oldest and Youngest Family Members" 8 kyu

---
+++ CodeWars/FindDifferenceInAge.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/find-the-difference-in-age-between-oldest-and-youngest-family-members/train/csharp
+https://www.codewars.com/kata/5720a1cb65a504fdff0003e2/solutions/csharp
+
+8 kyu
+Find the Difference in Age between Oldest and Youngest Family Members
+
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order. 
+The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. 
+Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+
+*/
+
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindDifferenceInAge
+    {
+        public static int[] DifferenceInAges(int[] ages)
+        {
+            return new[] { ages.Min(), ages.Max(), ages.Max() - ages.Min() };
+        }
+    }
+}
---
+++ CodeWarsTests/FindDifferenceInAgeTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindDifferenceInAgeTests
+    {
+        [Test, Description("Should return expected results for sample tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[] { 6, 82, 76 }, FindDifferenceInAge.DifferenceInAges(new int[] { 82, 15, 6, 38, 35 }));
+            Assert.AreEqual(new int[] { 14, 99, 85 }, FindDifferenceInAge.DifferenceInAges(new int[] { 57, 99, 14, 32 }));
+        }
+    }
+}
