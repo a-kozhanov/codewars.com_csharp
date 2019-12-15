2c1f769f6835a2f5db05e6b4c950b5c36afe7826
a-kozhanov <a.kozhanov@outlook.com>
2019-12-15 19:02:57 +0300

    Added solution "Collatz Conjecture (3n+1)" 8 kyu

---
+++ CodeWarsTests/CollatzConjectureTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CollatzConjectureTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(1u).Returns(0u);
+                yield return new TestCaseData(5u).Returns(5u);
+                yield return new TestCaseData(6u).Returns(8u);
+                yield return new TestCaseData(23u).Returns(15u);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public uint SampleTest(uint n) => CollatzConjecture.Hotpo(n);
+    }
+}
--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -13,21 +13,8 @@ namespace ConsoleTestApp
     {
         static void Main(string[] args)
         {
-            Dictionary<string[], string> bisics = new Dictionary<string[], string>()
-            {
-                {new []{"Mike", "Millington"},  "Malware Mike"},
-                {new []{"Fahima", "Tash"},"Function T-Rex"},
-                {new []{"Daisy", "Petrovic"},"Data Payload"},
-                {new []{"Barny", "White"},"Beta Worm"},
-                {new []{"Hank", "Kutz"},"Half-life Killer"},
-                {new []{"123abc", "Pinkman"},"Your name must start with a letter from A - Z."}
-            };
-
-            foreach (KeyValuePair<string[], string> keyValuePair in bisics)
-            {
-                Console.WriteLine(CrashOverride.AliasGen(keyValuePair.Key[0], keyValuePair.Key[1]));
-            }
-
+            Console.WriteLine(CollatzConjecture.Hotpo(23u));
+            CollatzConjecture.Hotpo(6u);
         }
     }
 }
