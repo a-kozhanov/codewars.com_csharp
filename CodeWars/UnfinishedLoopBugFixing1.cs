7654ceb6f46d8def933433428285c534d0ab9287
a-kozhanov <a.kozhanov@outlook.com>
2019-12-18 23:55:51 +0300

    Added solution "Unfinished Loop - Bug Fixing #1" 8 kyu

---
+++ CodeWars/UnfinishedLoopBugFixing1.cs
@@ -0,0 +1,47 @@
+﻿/*
+https://www.codewars.com/kata/unfinished-loop-bug-fixing-number-1/train/csharp
+https://www.codewars.com/kata/55c28f7304e3eaebef0000da/solutions/csharp
+
+8 kyu
+Unfinished Loop - Bug Fixing #1
+
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished For Loop!
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class UnfinishedLoopBugFixing1
+    {
+        public static List<int> CreateList(int number)
+        {
+            //List<int> list = new List<int>();
+
+            //for (int counter = 1; counter <= number)
+            //{
+            //    list.Add(counter);
+            //}
+
+            //return list;
+
+            return Enumerable.Range(1, number).ToList();
+        }
+
+        //public static List<int> CreateList(int number) => Enumerable.Range(1, number).ToList();
+
+
+        //public static List<int> CreateList(int number)
+        //{
+        //    List<int> nums = new List<int>();
+        //    for (var i = 1; i <= number; i++)
+        //    {
+        //        nums.Add(i);
+        //    }
+
+        //    return nums;
+        //}
+
+    }
+}
---
+++ CodeWarsTests/UnfinishedLoopBugFixing1Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class UnfinishedLoopBugFixing1Tests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            List<int> myValues = new List<int>(new int[] { 1 });
+            Assert.AreEqual(myValues, UnfinishedLoopBugFixing1.CreateList(1));
+
+            myValues = new List<int>(new int[] { 1, 2 });
+            Assert.AreEqual(myValues, UnfinishedLoopBugFixing1.CreateList(2));
+
+            myValues = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(myValues, UnfinishedLoopBugFixing1.CreateList(6));
+        }
+    }
+}
