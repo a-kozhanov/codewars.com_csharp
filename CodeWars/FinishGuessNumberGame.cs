435e82361833569464c6cfa7d5fb5f1a2e86cdd8
a-kozhanov <a.kozhanov@outlook.com>
2019-12-19 15:25:22 +0300

    Added solution "Finish Guess the Number Game" 8 kyu

---
+++ CodeWars/FinishGuessNumberGame.cs
@@ -0,0 +1,92 @@
+﻿/*
+https://www.codewars.com/kata/finish-guess-the-number-game/train/csharp
+https://www.codewars.com/kata/568018a64f35f0c613000054/solutions/csharp
+
+8 kyu
+Finish Guess the Number Game
+
+Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+If the user tries to guess WRONG more than the limit the function should throw an error
+If the user guess wrong it should lose a life and return false (if you guess correctly you shouldn't remove a life)
+If the user guess right it should return true
+Can you finish the game so all the rules are met?
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class FinishGuessNumberGame
+    {
+        private int _lives;
+        public int Number { get; }
+
+        private int Lives => _lives--;
+
+        //public Guesser(int number, int lives)
+        public FinishGuessNumberGame(int number, int lives)
+        {
+            Number = number;
+            _lives = lives;
+        }
+
+        public bool Guess(int n)
+        {
+            if (Lives <= 0) throw new Exception("WRONG");
+            if (Number == n) return true;
+            //Lives--;
+            return false;
+        }
+    }
+}
+
+
+
+//using System;
+
+//public class Guesser
+//{
+//    private int Number;
+//    private int Lives;
+
+//    public Guesser(int number, int lives)
+//    {
+//        Number = number;
+//        Lives = lives;
+//    }
+
+//    public bool Guess(int n)
+//    {
+//        if (Lives < 1) throw new Exception("You gone!");
+
+//        if (n == Number) return true;
+
+//        --Lives;
+//        return false;
+//    }
+//}
+
+
+//using System;
+
+//public class Guesser
+//{
+//    private Int32 _number;
+//    private Int32 _lives;
+
+//    public Guesser(Int32 Number, Int32 Lives)
+//    {
+//        _number = Number;
+//        _lives = Lives;
+//    }
+
+//    public Boolean Guess(Int32 Guess)
+//    {
+//        if (_lives <= 0)
+//        {
+//            throw new Exception();
+//        }
+//        return (Guess == _number) || (_lives-- == _lives);
+//    }
+//}
---
+++ CodeWarsTests/FinishGuessNumberGameTests.cs
@@ -0,0 +1,48 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FinishGuessNumberGameTests
+    {
+        [Test]
+        public void _0_Correct_Guess_Should_Return_True()
+        {
+            FinishGuessNumberGame FinishGuessNumberGame = new FinishGuessNumberGame(10, 2);
+            FinishGuessNumberGame.Guess(10);
+            FinishGuessNumberGame.Guess(10);
+            FinishGuessNumberGame.Guess(10);
+            FinishGuessNumberGame.Guess(10);
+            Assert.IsTrue(FinishGuessNumberGame.Guess(10));
+        }
+
+        [Test]
+        public void _1_Wrong_Guess_Should_Return_False()
+        {
+            FinishGuessNumberGame FinishGuessNumberGame = new FinishGuessNumberGame(10, 2);
+            FinishGuessNumberGame.Guess(1);
+            Assert.IsFalse(FinishGuessNumberGame.Guess(1));
+        }
+
+        [Test]
+        public void _3_Lives_Ran_Out_Should_Throw()
+        {
+            FinishGuessNumberGame FinishGuessNumberGame = new FinishGuessNumberGame(10, 2);
+            FinishGuessNumberGame.Guess(1);
+            FinishGuessNumberGame.Guess(2);
+
+            bool pass = false;
+            try
+            {
+                FinishGuessNumberGame.Guess(10);
+            }
+            catch
+            {
+                pass = true;
+            }
+
+            Assert.IsTrue(pass, "Expected error thrown");
+        }
+    }
+}
