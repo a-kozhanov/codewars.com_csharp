0e2fa250f33b0e0109150892b9d6d052f7d12707
a-kozhanov <a.kozhanov@outlook.com>
2019-12-21 16:54:06 +0300

    Added solution "Points of Reflection" 8 kyu

---
+++ CodeWars/PointsOfReflection.cs
@@ -0,0 +1,75 @@
+﻿/*
+https://www.codewars.com/kata/points-of-reflection/train/csharp
+https://www.codewars.com/kata/57bfea4cb19505912900012c/solutions/csharp
+
+8 kyu
+Points of Reflection
+
+"Point reflection" or "point symmetry" is a basic concept in geometry where a given point, P, 
+at a given position relative to a mid-point, Q has a corresponding point, P1, 
+which is the same distance from Q but in the opposite direction.
+
+Task
+Given two points P and Q, output the symmetric point of point P about Q. 
+Each argument is a two-element array of integers representing the point's X and Y coordinates. 
+Output should be in the same format, giving the X and Y coordinates of point P1. 
+You do not have to validate the input.
+
+This kata was inspired by the Hackerrank challenge Find Point
+
+*/
+
+namespace CodeWars
+{
+    public class PointsOfReflection
+    {
+        public static int[] SymmetricPoint(int[] p, int[] q)
+        {
+            return new[] { q[0] * 2 - p[0], q[1] * 2 - p[1] };
+        }
+    }
+}
+
+
+//public class Kata
+//{
+//    public static int[] SymmetricPoint(int[] p, int[] q) => new[] { 2 * q[0] - p[0], 2 * q[1] - p[1] };
+//}
+
+
+//public class Kata
+//{
+//    public static int[] SymmetricPoint(int[] p, int[] q) => new int[] { 2 * q[0] - p[0], 2 * q[1] - p[1] };
+//}
+
+
+//public class Kata
+//{
+//    public static int[] SymmetricPoint(int[] p, int[] q)
+//    {
+//        int x = (q[0] * 2) - p[0];
+//        int y = (q[1] * 2) - p[1];
+//        return new int[] { x, y };
+//    }
+//}
+
+
+//using System;
+//public class Kata
+//{
+//    public static int[] SymmetricPoint(int[] p, int[] q)
+//    {
+//        return new int[] { 2 * q[0] - p[0], 2 * q[1] - p[1] };
+//    }
+//}
+
+
+//public class Kata
+//{
+//    public static int[] SymmetricPoint(int[] p, int[] q)
+//    {
+//        p[0] = 2 * q[0] - p[0];
+//        p[1] = 2 * q[1] - p[1];
+//        return p;
+//    }
+//}
---
+++ CodeWarsTests/PointsOfReflectionTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PointsOfReflectionTests
+    {
+        [Test, Description("Your solution should work for some fixed tests")]
+        public void FixedTest()
+        {
+            Assert.AreEqual(new int[] { 2, 2 }, PointsOfReflection.SymmetricPoint(new int[] { 0, 0 }, new int[] { 1, 1 }));
+            Assert.AreEqual(new int[] { -6, -18 }, PointsOfReflection.SymmetricPoint(new int[] { 2, 6 }, new int[] { -2, -6 }));
+            Assert.AreEqual(new int[] { -30, 30 }, PointsOfReflection.SymmetricPoint(new int[] { 10, -10 }, new int[] { -10, 10 }));
+            Assert.AreEqual(new int[] { -25, 37 }, PointsOfReflection.SymmetricPoint(new int[] { 1, -35 }, new int[] { -12, 1 }));
+            Assert.AreEqual(new int[] { -1014, -443 }, PointsOfReflection.SymmetricPoint(new int[] { 1000, 15 }, new int[] { -7, -214 }));
+            Assert.AreEqual(new int[] { 0, 0 }, PointsOfReflection.SymmetricPoint(new int[] { 0, 0 }, new int[] { 0, 0 }));
+        }
+    }
+}
