bf419944f7bab2a68fbeda97a2708c5a4aab437c
a-kozhanov <a.kozhanov@outlook.com>
2019-12-21 20:02:37 +0300

    Added solution "Exclamation marks series #2: Remove all exclamation marks from the end of sentence" 8 kyu

---
+++ CodeWars/ExclamationMarksSeries2.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/exclamation-marks-series-number-2-remove-all-exclamation-marks-from-the-end-of-sentence/train/csharp
+https://www.codewars.com/kata/57faece99610ced690000165/solutions/csharp
+
+8 kyu
+Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+Note
+Please don't post issue about difficulty or duplicate.
+*/
+
+namespace CodeWars
+{
+    public class ExclamationMarksSeries2
+    {
+        public static string Remove(string s)
+        {
+            return s.TrimEnd('!');
+        }  
+        
+        //public static string Remove(string s) => s.TrimEnd('!');
+    }
+}
---
+++ CodeWarsTests/ExclamationMarksSeries2Tests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExclamationMarksSeries2Tests
+    {
+        [Test, Description("It should work for basic tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Hi", ExclamationMarksSeries2.Remove("Hi!"));
+            Assert.AreEqual("Hi", ExclamationMarksSeries2.Remove("Hi!!!"));
+            Assert.AreEqual("!Hi", ExclamationMarksSeries2.Remove("!Hi"));
+            Assert.AreEqual("!Hi", ExclamationMarksSeries2.Remove("!Hi!"));
+            Assert.AreEqual("Hi! Hi", ExclamationMarksSeries2.Remove("Hi! Hi"));
+            Assert.AreEqual("Hi", ExclamationMarksSeries2.Remove("Hi"));
+        }      
+    }
+}
