73d6c8726fc2d387886566e21f19342c0a57e8c1
a-kozhanov <a.kozhanov@outlook.com>
2019-12-21 23:55:07 +0300

    Added solution "Geometry Basics: Circle Area in 2D" 8 kyu

---
+++ CodeWars/GeometryBasicsCircleAreaIn2D.cs
@@ -0,0 +1,94 @@
+﻿/*
+https://www.codewars.com/kata/geometry-basics-circle-area-in-2d/train/csharp
+https://www.codewars.com/kata/58e3f824a33b52c1dc0001c0/solutions/csharp
+
+8 kyu
+Geometry Basics: Circle Area in 2D
+
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+The Circle class can be seen below:
+
+public class Circle
+{
+  public Point Center { get; private set; }
+  public double Radius { get; private set; }
+
+  public Circle(Point center, double radius)
+  {
+    this.Center = center;
+    this.Radius = radius;
+  }
+}
+And the Point class can be seen below:
+
+public class Point
+{
+  public double X { get; private set; }
+  public double Y { get; private set; }
+
+  public Point(double x, double y)
+  {
+    this.X = x;
+    this.Y = y;
+  }
+}
+
+Documentation:
+Kata.CircleArea Method (Circle)
+Returns the area of a circle.
+
+Syntax
+public static double CircleArea(
+Circle circle
+)
+
+Parameters
+circle
+Type: Circle
+The circle to calculate the area of.
+
+Return Value
+Type: System.Double
+A double representing the area of the circle.
+Tests round answers to 6 decimal places.
+
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class GeometryBasicsCircleAreaIn2D
+    {
+        public class Circle
+        {
+            public Point Center;
+            public double Radius;
+
+            public Circle(Point center, double radius)
+            {
+                Center = center;
+                Radius = radius;
+            }
+        }
+
+        public class Point
+        {
+            public int x;
+            public int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public static double CircleArea(Circle circle)
+        {
+            return Math.Round(Math.PI * Math.Pow(circle.Radius, 2), 6);
+        }
+    }
+}
---
+++ CodeWarsTests/GeometryBasicsCircleAreaIn2DTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GeometryBasicsCircleAreaIn2DTests
+    {
+        private static double epsilon = 1e-6;
+  
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(GeometryBasicsCircleAreaIn2D.CircleArea
+                (
+                    new GeometryBasicsCircleAreaIn2D.Circle(new GeometryBasicsCircleAreaIn2D.Point(10, 10), 30)
+                    ), 
+                Is.EqualTo(2827.433388).Within(epsilon));
+            
+            Assert.That(GeometryBasicsCircleAreaIn2D.CircleArea(new GeometryBasicsCircleAreaIn2D.Circle(new GeometryBasicsCircleAreaIn2D.Point(25, -70), 30)), Is.EqualTo(2827.433388).Within(epsilon));
+            Assert.That(GeometryBasicsCircleAreaIn2D.CircleArea(new GeometryBasicsCircleAreaIn2D.Circle(new GeometryBasicsCircleAreaIn2D.Point(-15, 5), 0)), Is.EqualTo(0).Within(epsilon));
+            Assert.That(GeometryBasicsCircleAreaIn2D.CircleArea(new GeometryBasicsCircleAreaIn2D.Circle(new GeometryBasicsCircleAreaIn2D.Point(-15, 5), 12.5)), Is.EqualTo(490.873852).Within(epsilon));
+        }  
+    }
+}
