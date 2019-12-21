927d325e36ad8bdde9b902ddce01ce81fb1a8b4c
a-kozhanov <a.kozhanov@outlook.com>
2019-12-21 16:55:47 +0300

    Added solution "Classy Extentions" 8 kyu

---
+++ CodeWarsTests/ClassyExtentionsTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ClassyExtentionsTests
+    {
+        [Test]
+        public void Test()
+        {
+            ClassyExtentions.Cat cat = new ClassyExtentions.Cat("Mr Whiskers");
+            Assert.AreEqual("Mr Whiskers meows.", cat.Speak());
+            Assert.That(cat is ClassyExtentions.Animal, "The Cat class should inherit from Animal");
+
+            cat = new ClassyExtentions.Cat("Lamp");
+            Assert.AreEqual("Lamp meows.", cat.Speak());
+            Assert.That(cat is ClassyExtentions.Animal, "The Cat class should inherit from Animal");
+
+            cat = new ClassyExtentions.Cat("$$Money Bags$$");
+            Assert.AreEqual("$$Money Bags$$ meows.", cat.Speak());
+            Assert.That(cat is ClassyExtentions.Animal, "The Cat class should inherit from Animal");
+        }
+    }
+}
