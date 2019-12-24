739a9c5467e3698eb4247299b85d20cf0942476d
a-kozhanov <a.kozhanov@outlook.com>
2019-12-24 22:31:12 +0300

    Added solution "Geometry Basics: Distance between points in 2D" 8 kyu

---
+++ CodeWars/GeometryBasicsDistanceBetweenPointsIn2D.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/geometry-basics-distance-between-points-in-2d/train/csharp
+https://www.codewars.com/kata/58dced7b702b805b200000be/solutions/csharp
+
+8 kyu
+Geometry Basics: Distance between points in 2D
+
+This series of katas will introduce you to basics of doing geometry with computers.
+Point objects have x and y attributes (X and Y in C#) attributes.
+Write a function calculating distance between Point a and Point b.
+Tests round answers to 6 decimal places.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class GeometryBasicsDistanceBetweenPointsIn2D
+    {
+        public class Point
+        {
+            public double X;
+            public double Y;
+
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        
+        public static double DistanceBetweenPoints(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+    }
+}
---
+++ CodeWarsTests/GeometryBasicsDistanceBetweenPointsIn2DTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GeometryBasicsDistanceBetweenPointsIn2DTests
+    {
+        private static double epsilon = 1e-6;
+    
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(0, Is.EqualTo(GeometryBasicsDistanceBetweenPointsIn2D.DistanceBetweenPoints(new GeometryBasicsDistanceBetweenPointsIn2D.Point(3, 3), new GeometryBasicsDistanceBetweenPointsIn2D.Point(3, 3))).Within(epsilon));
+            Assert.That(5, Is.EqualTo(GeometryBasicsDistanceBetweenPointsIn2D.DistanceBetweenPoints(new GeometryBasicsDistanceBetweenPointsIn2D.Point(1, 6), new GeometryBasicsDistanceBetweenPointsIn2D.Point(4, 2))).Within(epsilon));
+            Assert.That(10.728001, Is.EqualTo(GeometryBasicsDistanceBetweenPointsIn2D.DistanceBetweenPoints(new GeometryBasicsDistanceBetweenPointsIn2D.Point(-10.2, 12.5), new GeometryBasicsDistanceBetweenPointsIn2D.Point(0.3, 14.7))).Within(epsilon));
+        }       
+    }
+}
