94f96eed487239087fb8a770a9006ee2ef150bf2
a-kozhanov <a.kozhanov@outlook.com>
2019-12-27 20:45:17 +0300

    Added solution "Summing a number's digits" 7 kyu

---
+++ CodeWars/SummingNnumbersDigits.cs
@@ -0,0 +1,51 @@
+﻿/*
+https://www.codewars.com/kata/summing-a-numbers-digits/train/csharp
+https://www.codewars.com/kata/52f3149496de55aded000410/solutions/csharp
+
+7 kyu
+Summing a number's digits
+
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
+
+  SumDigits(10)  // Returns 1
+  SumDigits(99)  // Returns 18
+  SumDigits(-32) // Returns 5
+Let's assume that all numbers in the input will be integer values.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SummingNnumbersDigits
+    {
+        public static int SumDigits(int number)
+        {
+            return Math.Abs(number).ToString().Select(x => int.Parse(x.ToString())).Sum();
+        }
+
+        // public static int SumDigits(int number)
+        // {
+        //     return (int) number.ToString().Sum(x => char.IsNumber(x) ? char.GetNumericValue(x) : 0);
+        // }
+        //
+        // public static int SumDigits(int n) => n.ToString().Sum(t => t == '-' ? 0 : t - 48);
+        //
+        //
+        // public static int SumDigits(int number)
+        // {
+        //     List<int> digits=new List<int>();
+        //
+        //     foreach(char c in Math.Abs(number).ToString())
+        //         digits.Add(int.Parse(c.ToString()));
+        //
+        //
+        //     return digits.Sum();
+        // }
+        //
+        //
+        // public static int SumDigits(int number) => (int)(Math.Abs(number) + "").Select(char.GetNumericValue).Sum();
+        
+    }
+}
---
+++ CodeWarsTests/SummingNnumbersDigitsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SummingNnumbersDigitsTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(10).Returns(1);
+                yield return new TestCaseData(99).Returns(18);
+                yield return new TestCaseData(-32).Returns(5);
+            }
+        }
+  
+        [Test, TestCaseSource("testCases")]
+        public int FixedTest(int number) => SummingNnumbersDigits.SumDigits(number);      
+    }
+}
