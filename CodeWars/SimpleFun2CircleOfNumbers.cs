5ec03c0a26d473512761f9be252874ac2c2d4f96
a-kozhanov <a.kozhanov@outlook.com>
2019-12-28 23:29:17 +0300

    Added solution "Simple Fun #2: Circle of Numbers" 7 kyu

---
+++ CodeWars/SimpleFun2CircleOfNumbers.cs
@@ -0,0 +1,41 @@
+﻿/*
+https://www.codewars.com/kata/simple-fun-number-2-circle-of-numbers/train/csharp
+https://www.codewars.com/kata/58841cb52a077503c4000015/solutions/csharp
+
+7 kyu
+Simple Fun #2: Circle of Numbers
+
+Task
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+
+Given n and firstNumber/first_number, find the number which is written in the radially opposite position to firstNumber.
+
+Example
+For n = 10 and firstNumber = 2, the output should be
+
+circleOfNumbers n firstNumber == 7
+
+Input/Output
+[input] integer n
+
+A positive even integer.
+
+Constraints: 4 ≤ n ≤ 1000.
+
+[input] integer firstNumber/first_number
+
+Constraints: 0 ≤ firstNumber ≤ n - 1
+
+[output] an integer
+*/
+
+namespace CodeWars
+{
+    public class SimpleFun2CircleOfNumbers
+    {
+        public int CircleOfNumbers(int circle, int number)
+        {
+            return (circle / 2 + number) % circle;
+        }
+    }
+}
---
+++ CodeWarsTests/SimpleFun2CircleOfNumbersTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun2CircleOfNumbersTests
+    {
+        [Test]
+        public void TestCase()
+        {
+            var kata = new SimpleFun2CircleOfNumbers();
+            Assert.AreEqual(7, kata.CircleOfNumbers(10, 2), "");
+            Assert.AreEqual(2, kata.CircleOfNumbers(10, 7), "");
+            Assert.AreEqual(3, kata.CircleOfNumbers(4, 1), "");
+            Assert.AreEqual(0, kata.CircleOfNumbers(6, 3), "");
+        }
+    }
+}
