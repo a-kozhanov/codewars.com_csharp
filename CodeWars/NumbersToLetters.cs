f1791fb1efc5173e6f108d30f7645112d8d35bb4
a-kozhanov <a.kozhanov@outlook.com>
2019-12-28 18:06:49 +0300

    Added solution "Numbers to Letters" 7 kyu

---
+++ CodeWars/NumbersToLetters.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/numbers-to-letters/train/csharp
+
+
+7 kyu
+Numbers to Letters
+
+Given an array of numbers, you must return a string. 
+The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. 
+You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+All inputs will be valid.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NumbersToLetters
+    {
+        public static string Switcher(string[] x)
+        {
+            var letters = "zyxwvutsrqponmlkjihgfedcba!? ";
+            return string.Concat(x.Select(n => "zyxwvutsrqponmlkjihgfedcba!? "[int.Parse(n) - 1]));
+        }
+    }
+}
