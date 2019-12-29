9238c28e6d9ee05b5211be922b8018c52ca1d6ba
a-kozhanov <a.kozhanov@outlook.com>
2019-12-29 22:51:16 +0300

    Added solution "JavaScript Array Filter" 7 kyu

---
+++ CodeWars/JavaScriptArrayFilter.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/javascript-array-filter/train/csharp
+https://www.codewars.com/kata/514a6336889283a3d2000001/solutions/csharp
+
+7 kyu
+JavaScript Array Filter
+
+Starting with .NET Framework 3.5, C# supports a Where (in the System.Linq namespace) method which allows a user to filter arrays based on a predicate. 
+Use the Where method to complete the function given.
+
+Enumerable.Where documentation: https://msdn.microsoft.com/en-us/library/bb534803(v=vs.110).aspx
+
+The solution would work like the following:
+Kata.GetEvenNumbers(new int[] {2, 4, 5, 6}) => new int[] {2, 4, 6}
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class JavaScriptArrayFilter
+    {
+        public static int[] GetEvenNumbers(int[] numbers)
+        {
+            return numbers.Where(n => n % 2 == 0).ToArray();
+        }
+
+        // public static int[] GetEvenNumbers(int[] numbers) => numbers.Where(x => (x & 1) == 0).ToArray();
+    }
+}
---
+++ CodeWarsTests/JavaScriptArrayFilterTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class JavaScriptArrayFilterTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] {1, 2})
+                    .Returns(new int[] {2});
+                yield return new TestCaseData(new int[] {2, 6, 8, 10})
+                    .Returns(new int[] {2, 6, 8, 10});
+                yield return new TestCaseData(new int[] {12, 14, 15})
+                    .Returns(new int[] {12, 14});
+                yield return new TestCaseData(new int[] {13, 15})
+                    .Returns(new int[] { });
+                yield return new TestCaseData(new int[] {1, 3, 9})
+                    .Returns(new int[] { });
+                yield return new TestCaseData(new int[] {-1, -3, -9})
+                    .Returns(new int[] { });
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int[] FixedTests(int[] numbers) => JavaScriptArrayFilter.GetEvenNumbers(numbers);
+    }
+}
