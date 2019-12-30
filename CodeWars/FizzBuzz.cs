4f29c1ea7cbcfedd74e89021190e494946727046
a-kozhanov <a.kozhanov@outlook.com>
2019-12-30 14:15:55 +0300

    Added solution "Fizz / Buzz" 7 kyu

---
+++ CodeWars/FizzBuzz.cs
@@ -0,0 +1,58 @@
+﻿/*
+https://www.codewars.com/kata/fizz-slash-buzz/train/csharp
+https://www.codewars.com/kata/51dda84f91f5b5608b0004cc/solutions/csharp
+
+7 kyu
+Fizz / Buzz
+
+Write a function that takes an integer and returns an array [A, B, C], 
+where A is the number of multiples of 3 (but not 5) below the given integer, 
+B is the number of multiples of 5 (but not 3) below the given integer and 
+C is the number of multiples of 3 and 5 below the given integer.
+
+For example, solution(20) should return [5, 2, 1]
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FizzBuzz
+    {
+        public static int[] Solution(int num)
+        {
+            var a = (num - 1) / 3;
+            var b = (num - 1) / 5;
+            var c = (num - 1) / 15;
+
+            return new[] {a - c, b - c, c};
+        }
+
+        // public static int[] Solution(int number)
+        // {
+        //     int c = (int)Math.Floor((double)(number - 1) / 15);
+        //     int a = (int)Math.Floor((double)(number - 1) / 3) - c;
+        //     int b = (int)Math.Floor((double)(number - 1) / 5) - c;
+        //
+        //     return new int[] {a, b, c};
+        // }
+
+
+        // public static int[] Solution(int number) => new int[] {--number / 3 - number / 15, number / 5 - number / 15, number / 15};
+
+        // public static int[] Solution(int number) {
+        //     int count3 = --number / 3;
+        //     int count5 = number / 5;
+        //     int count15 = number / 15;
+        //     return new int[3] { count3 - count15, count5 - count15, count15 };
+        // }
+
+        // public static int[] Solution(int number)
+        // {
+        //     int sBy1 = number-1;
+        //     int dBy15 = sBy1/15;
+        //
+        //     return new int[]{ sBy1/3 - dBy15, sBy1/5 - dBy15 ,dBy15 }; 
+        // }
+    }
+}
---
+++ CodeWarsTests/FizzBuzzTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        private static object[] sampleTestCases = new object[]
+        {
+            new object[] {20, new int[] {5, 2, 1}},
+            new object[] {2, new int[] {0, 0, 0}},
+            new object[] {30, new int[] {8, 4, 1}},
+            new object[] {300, new int[] {80, 40, 19}},
+        };
+
+        [Test, TestCaseSource("sampleTestCases")]
+        public void SampleTest(int number, int[] expected)
+        {
+            Assert.AreEqual(expected, FizzBuzz.Solution(number));
+        }
+
+        private static Random rnd = new Random();
+
+        private static int[] solution(int number) => new int[]
+            {--number / 3 - number / 15, number / 5 - number / 15, number / 15};
+
+        [Test]
+        public void RandomTests()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                int test = rnd.Next(1, 1000);
+                int[] expected = solution(test);
+                int[] actual = FizzBuzz.Solution(test);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
