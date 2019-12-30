52b626533741b95ff2c5043cd563ee14aff5d8d3
a-kozhanov <a.kozhanov@outlook.com>
2019-12-30 19:23:03 +0300

    Added solution "Return substring instance count" 7 kyu

---
+++ CodeWars/ReturnSubstringInstanceCount.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/return-substring-instance-count/train/csharp
+https://www.codewars.com/kata/5168b125faced29f66000005/solutions/csharp
+
+7 kyu
+Return substring instance count
+
+Complete the solution so that it returns the number of times the search_text is found within the full_text.
+
+Usage example:
+Kata.SubstringCount("aa_bb_cc_dd_bb_e", "bb") // should return 2 since bb shows up twice
+Kata.SubstringCount("aaabbbccc", "bbb") // should return 1
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ReturnSubstringInstanceCount
+    {
+        // public static int SubstringCount(string fullText, string searchText)
+        // {
+        //     return fullText.Split(searchText).Length - 1;
+        // }
+
+        // public static int SubstringCount(string fullText, string searchText) => Regex.Matches(fullText, searchText).Count;
+
+        public static int SubstringCount(string t, string s) => Regex.Matches(t, s).Count;
+    }
+}
---
+++ CodeWarsTests/ReturnSubstringInstanceCountTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReturnSubstringInstanceCountTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("abcdeb", "b").Returns(2);
+                yield return new TestCaseData("abcdeb", "a").Returns(1);
+                yield return new TestCaseData("ccddeeccddeecc", "cc").Returns(3);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int SampleTest(string fullText, string searchText) =>
+            ReturnSubstringInstanceCount.SubstringCount(fullText, searchText);
+    }
+}
