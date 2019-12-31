fd369d4152dffaa869a37d2d338f6182809b0539
a-kozhanov <a.kozhanov@outlook.com>
2019-12-31 12:33:17 +0300

    Added solution "Reverse words" 7 kyu

---
+++ CodeWars/ReverseWords.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/reverse-words/train/csharp
+https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/solutions/csharp
+
+7 kyu
+Reverse words
+
+Complete the function that accepts a string parameter, 
+and reverses each word in the string. 
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataReverseWords
+    {
+        public static string ReverseWords(string str)
+        {
+            return string.Join(" ", str.Split().Select(w => string.Concat(w.Reverse())));
+            //return string.Join(" ", str.Split(' ').Select(i => new string(i.Reverse().ToArray())));
+        }
+    }
+}
---
+++ CodeWarsTests/ReverseWordsTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataReverseWordsTests
+    {
+        [Test]
+        public void Example()
+        {
+            Assert.AreEqual("sihT si na !elpmaxe", KataReverseWords.ReverseWords("This is an example!"));
+        }
+    }
+}
