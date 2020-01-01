ff2cc609611f36d02a4c7d5ee0f5da72a539017a
a-kozhanov <a.kozhanov@outlook.com>
2020-01-01 14:23:37 +0300

    Added solution "Ninja vs Samurai: Strike" 7 kyu

---
+++ CodeWars/NinjaVsSamuraiStrike.cs
@@ -0,0 +1,62 @@
+﻿/*
+https://www.codewars.com/kata/ninja-vs-samurai-strike/train/csharp
+https://www.codewars.com/kata/517b0f33cd023d848d000001/solutions/csharp
+
+7 kyu
+Ninja vs Samurai: Strike
+
+Something is wrong with our Warrior class. 
+The strike method does not work correctly. 
+The following shows an example of this code being used:
+
+var ninja = new Warrior("Ninja");
+var samurai = new Warrior("Samurai");
+
+samurai.Strike(ninja, 3);
+// ninja.Health should == 70
+Can you figure out what is wrong?
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class NinjaVsSamuraiStrike
+    {
+        public class Warrior
+        {
+            private string _name;
+            public int Health { get; private set; } = 100;
+
+            public Warrior(string name)
+            {
+                _name = name;
+            }
+
+            public void Strike(Warrior enemy, int swings) => enemy.Health -= 10 * swings;
+            // {
+            //     enemy.Health = Math.Max(0, enemy.Health - swings * 10);
+            // }
+        }
+    }
+}
+
+
+// using System;
+//
+// public class Warrior
+// {
+//     private string name;
+//
+//     public Warrior(string name)
+//     {
+//         this.name = name;
+//     }
+//
+//     public int Health { get; set; } = 100;
+//
+//     public void Strike(Warrior enemy, int swings)
+//     {
+//         enemy.Health = Math.Max(0, enemy.Health - (swings * 10));
+//     }
+// }
---
+++ CodeWarsTests/NinjaVsSamuraiStrikeTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NinjaVsSamuraiStrikeTests
+    {
+        private NinjaVsSamuraiStrike.Warrior ninja = new NinjaVsSamuraiStrike.Warrior("Ninja");
+        private NinjaVsSamuraiStrike.Warrior samurai = new NinjaVsSamuraiStrike.Warrior("Samurai");
+
+        [Test]
+        public void Tests()
+        {
+            samurai.Strike(ninja, 4);
+            Assert.AreEqual(60, ninja.Health);
+        }      
+    }
+}
