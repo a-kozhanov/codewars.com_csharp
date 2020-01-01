50f6fc1d2dde5e07a2c86aaea62f6455544ed6ac
a-kozhanov <a.kozhanov@outlook.com>
2020-01-01 17:38:01 +0300

    Added solution "Remove duplicate words" 7 kyu

---
+++ CodeWars/RemoveDuplicateWords.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/remove-duplicate-words/train/csharp
+https://www.codewars.com/kata/5b39e3772ae7545f650000fc/solutions/csharp
+
+7 kyu
+Remove duplicate words
+
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+'alpha beta gamma delta'
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataRemoveDuplicateWords
+    {
+        public static string RemoveDuplicateWords(string s)
+        {
+            return string.Join(" ", s.Split().Distinct());
+        }
+    }
+}
---
+++ CodeWarsTests/RemoveDuplicateWordsTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataRemoveDuplicateWordsTests
+    {
+        [Test, Description("Should handle sample case")]
+        public void ExampleTest()
+        {
+            Assert.That(
+                KataRemoveDuplicateWords.RemoveDuplicateWords(
+                    "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"),
+                Is.EqualTo("alpha beta gamma delta"));
+        }
+    }
+}
