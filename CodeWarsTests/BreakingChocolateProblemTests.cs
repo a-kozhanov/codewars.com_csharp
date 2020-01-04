60071fcc313eb0f40e150e20a32f229d76b55b56
a-kozhanov <a.kozhanov@outlook.com>
2020-01-04 13:11:21 +0300

    Added solution "Breaking chocolate problem" 7 kyu

---
+++ CodeWarsTests/BreakingChocolateProblemTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BreakingChocolateProblemTests
+    {
+        [Test]
+        public void SimpleTests()
+        {
+            Assert.AreEqual(24, BreakingChocolateProblem.BreakChocolate(5, 5));
+            Assert.AreEqual(0, BreakingChocolateProblem.BreakChocolate(1, 1));
+        }
+    }
+}
