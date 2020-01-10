6a3c114e35ccbd39884cc27a5936f2459eb7de88
a-kozhanov <a.kozhanov@outlook.com>
2020-01-10 21:20:17 +0300

    Added solution "Factorial" 7 kyu (by wichu)

---
+++ CodeWarsTests/Factorial2Tests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class Factorial2Tests
+    {
+        [Test]
+        public void FactorialOf0ShouldBe1()
+        {
+            Assert.AreEqual(1, Factorial2.Factorial(0));
+        }
+
+        [Test]
+        public void FactorialOf1ShouldBe1()
+        {
+            Assert.AreEqual(1, Factorial2.Factorial(1));
+        }
+
+        [Test]
+        public void FactorialOf2ShouldBe2()
+        {
+            Assert.AreEqual(2, Factorial2.Factorial(2));
+        }
+
+        [Test]
+        public void FactorialOf3ShouldBe6()
+        {
+            Assert.AreEqual(6, Factorial2.Factorial(3));
+        }
+    }
+}
