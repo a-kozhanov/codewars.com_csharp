72654c4b197d3fb35a16f297030a01c1ef8de914
a-kozhanov <a.kozhanov@outlook.com>
2020-01-12 22:36:51 +0300

    Added solution "Borrower Speak" 7 kyu

---
+++ CodeWars/BorrowerSpeak.cs
@@ -0,0 +1,53 @@
+﻿/*
+https://www.codewars.com/kata/57d2ba8095497e484e00002e/train/csharp
+https://www.codewars.com/kata/57d2ba8095497e484e00002e/solutions/csharp
+
+7 kyu
+Borrower Speak
+
+The borrowers are tiny tiny fictional people. 
+As tiny tiny people they have to be sure they aren't spotted, or more importantly, stepped on.
+As a result, the borrowers talk very very quietly. 
+They find that capitals and punctuation of any sort lead them to raise their voices and put them in danger.
+The young borrowers have begged their parents to stop using caps and punctuation.
+Change the input text 's' to new borrower speak. Help save the next generation!
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class BorrowerSpeak
+    {
+        public static string Borrow(string s)
+        {
+            return Regex.Replace(s.ToLower(), "\\W*", "");
+        }
+
+        // public static string Borrow(string s) => Regex.Replace(s, @"[^\w]", "").ToLower();
+
+
+        // public static string Borrow(string s)
+        // {
+        //     return string.Concat(s.ToLower().Where(c => c >= 'a' && c <= 'z'));
+        // }
+
+        // public class Kata
+        // {
+        //     public static string Borrow(string s) => string.Concat(s.Where(char.IsLetter)).ToLower();
+        // }
+
+        // public static string Borrow(string s)
+        // {
+        //     return string.Concat(s.Select(c => Char.IsLetter(c)? c.ToString().ToLower(): ""));
+        // }
+
+        // public static string Borrow(string s) => string.Concat(s.ToLower().Where(char.IsLetter));
+
+        // public static string Borrow(string s)
+        // {
+        //     return string.Concat(s.Where(char.IsLetter)).ToLower();
+        // }
+    }
+}
---
+++ CodeWarsTests/BorrowerSpeakTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BorrowerSpeakTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("whatfickdamncake", BorrowerSpeak.Borrow("WhAt! FiCK! DaMn CAke?"));
+            Assert.AreEqual("thebigpeoplehere", BorrowerSpeak.Borrow("THE big PeOpLE Here!!"));
+            Assert.AreEqual("iamatinyboy", BorrowerSpeak.Borrow("i AM a TINY BoY!!"));
+        }
+    }
+}
