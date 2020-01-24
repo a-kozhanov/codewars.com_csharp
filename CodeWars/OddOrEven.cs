992b863db63afaba3986fcfb4dfafec608eac5af
a-kozhanov <a.kozhanov@outlook.com>
2020-01-24 20:16:31 +0300

    Added solution "Odd or Even?" 7 kyu

---
+++ CodeWars/OddOrEven.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/5949481f86420f59480000e7/train/csharp
+https://www.codewars.com/kata/5949481f86420f59480000e7/solutions/csharp
+
+7 kyu
+Odd or Even?
+
+Task:
+Given an vector of numbers, determine whether the sum of all of the numbers is odd or even.
+
+Give your answer as a String matching 'odd' or 'even'.
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Example:
+odd_or_even(vec![0]) returns "even"
+odd_or_even(vec![0, 1, 4]) returns "odd"
+odd_or_even(vec![0, -1, -5]) returns "even"
+Have fun!
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataOddOrEven
+    {
+        public static string OddOrEven(int[] a)
+        {
+            return a.Sum() % 2 == 0 ? "even" : "odd";
+        }
+    }
+}
---
+++ CodeWarsTests/OddOrEvenTests.cs
@@ -0,0 +1,49 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataOddOrEvenTests
+    {
+        [Test, Description("Edge tests")]
+        public void EdgeTests()
+        {
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {0}));
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {1}));
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] { }));
+        }
+
+        [Test, Description("Even tests")]
+        public void EvenTests()
+        {
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {0, 1, 5}));
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {0, 1, 3}));
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {1023, 1, 2}));
+        }
+
+        [Test, Description("Negative Even tests")]
+        public void NegativeEvenTests()
+        {
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {0, -1, -5}));
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {0, -1, -3}));
+            Assert.AreEqual("even", KataOddOrEven.OddOrEven(new int[] {-1023, 1, -2}));
+        }
+
+        [Test, Description("Odd tests")]
+        public void OddTests()
+        {
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {0, 1, 2}));
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {0, 1, 4}));
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {1023, 1, 3}));
+        }
+
+        [Test, Description("Negative Odd tests")]
+        public void NegativeOddTests()
+        {
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {0, -1, 2}));
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {0, 1, -4}));
+            Assert.AreEqual("odd", KataOddOrEven.OddOrEven(new int[] {-1023, -1, 3}));
+        }
+    }
+}
