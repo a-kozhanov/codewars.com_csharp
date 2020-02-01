ef3e5b0802fbe9e7af462fc01070194a9961de73
a-kozhanov <a.kozhanov@outlook.com>
2020-02-02 01:14:12 +0300

    Added solution "Number Of Occurrences" 7 kyu

---
+++ CodeWars/NumberOfOccurrences.cs
@@ -0,0 +1,72 @@
+﻿/*
+https://www.codewars.com/kata/52829c5fe08baf7edc00122b/train/csharp
+https://www.codewars.com/kata/52829c5fe08baf7edc00122b/solutions/csharp
+
+7 kyu
+Number Of Occurrences
+
+Write a functionthat returns the number of occurrences of an element in an array.
+
+Examples
+var sample = { 1, 0, 2, 2, 3 };
+NumberOfOccurrences(0, sample) == 1;
+NumberOfOccurrences(4, sample) == 0;
+NumberOfOccurrences(2, sample) == 2;
+NumberOfOccurrences(3, sample) == 1;
+*/
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataNumberOfOccurrences
+    {
+        public static int NumberOfOccurrences(int x, int[] xs)
+        {
+            //return xs.Count(n => n == x);
+            return xs.Count(x.Equals);
+        }
+
+
+        // public static int NumberOfOccurrences(int x, int[] xs) => (from int i in xs
+        //     where i == x
+        //     select i).Count();
+
+
+        // public static int NumberOfOccurrences(int x, int[] xs)
+        // {
+        //     int count = 0;
+        //     foreach (int i in xs) if (i == x) count += 1;
+        //     return count;
+        // }
+
+
+        // public static int NumberOfOccurrences(int x, int[] xs)
+        // {
+        //     return xs.Count(x.Equals);
+        // }
+
+        // public static int NumberOfOccurrences(int x, int[] xs)
+        // {
+        //     List<int> numbers = new List<int>(xs);
+        //     return numbers.Where(i => i == x).Count();
+        // }
+
+
+        // public static int NumberOfOccurrences(int x, int[] xs)
+        // {
+        //     return xs.Where(element => element == x).Count();
+        // }
+
+
+        // public static int NumberOfOccurrences(int x, int[] xs)
+        // {
+        //     return xs.Count(i => i.Equals(x));
+        // }
+
+
+        // public static int NumberOfOccurrences(int x, int[] xs)=>xs.Count(d=>d.Equals(x));
+    }
+}
---
+++ CodeWarsTests/NumberOfOccurrencesTests.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataNumberOfOccurrencesTests
+    {
+        [Test]
+        public void Should_Work_On_Empty_List()
+        {
+            Assert.AreEqual(0,
+                KataNumberOfOccurrences.NumberOfOccurrences(2, new int[0]));
+        }
+
+        [Test]
+        public void Should_Work_On_Examples()
+        {
+            var example = new int[] {4, 0, 4};
+
+            Assert.AreEqual(2,
+                KataNumberOfOccurrences.NumberOfOccurrences(4, example));
+
+            Assert.AreEqual(0,
+                KataNumberOfOccurrences.NumberOfOccurrences(1, example));
+
+            Assert.AreEqual(1,
+                KataNumberOfOccurrences.NumberOfOccurrences(0, example));
+        }
+    }
+}
