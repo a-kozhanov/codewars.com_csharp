d3b387c558438b0232eddc113835c1d5d663fcb2
a-kozhanov <a.kozhanov@outlook.com>
2020-02-05 22:34:24 +0300

    Added solution "Sum of angles" 7 kyu

---
+++ CodeWars/SumOfAngles.cs
@@ -0,0 +1,22 @@
+﻿/*
+https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/csharp
+https://www.codewars.com/kata/5a03b3f6a1c9040084001765/solutions/csharp
+
+7 kyu
+Sum of angles
+
+Find the total sum of angles in an n sided shape. N will be greater than 2.
+*/
+
+
+namespace CodeWars
+{
+    public class SumOfAngles
+    {
+        public static int Angle(int n)
+        {
+            return 180 * (n - 2);
+            //return n * 180 - 360;
+        }
+    }
+}
---
+++ CodeWarsTests/SumOfAnglesTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SumOfAnglesTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(180, SumOfAngles.Angle(3));
+            Assert.AreEqual(360, SumOfAngles.Angle(4));
+        }
+    }
+}
