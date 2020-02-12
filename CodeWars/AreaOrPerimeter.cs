5b942ed343a5a1a5d5f4270266b9680e7bd89b2b
a-kozhanov <a.kozhanov@outlook.com>
2020-02-12 23:03:23 +0300

    Added not completed solution "Area or Perimeter" 8 kyu

---
+++ CodeWars/AreaOrPerimeter.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/csharp
+
+
+8 kyu
+Area or Perimeter
+
+
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+area_or_perimeter(6, 10) --> 32
+area_or_perimeter(4, 4) --> 16
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+
+
+namespace CodeWars
+{
+    public class KataAreaOrPerimeter
+    {
+        public static int AreaOrPerimeter(int l, int w)
+        {
+            // var r = l / (2 * 3.14);
+            //
+            //
+            // if (l == w)
+            // {
+            //     //Console.WriteLine((l + w) * 2);
+            // }
+            // else
+            // {
+            //     //Console.WriteLine((l + w) * (l + w));
+            // }
+
+            return l == w ? (l + w) * 2 : (l + w) * (l + w);
+        }
+    }
+}
---
+++ CodeWarsTests/AreaOrPerimeterTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataAreaOrPerimeterTests
+    {
+        [Test]
+        public void FixedTest()
+        {
+            Assert.AreEqual(KataAreaOrPerimeter.AreaOrPerimeter(4, 4), 16);
+            Assert.AreEqual(KataAreaOrPerimeter.AreaOrPerimeter(6, 10), 32);
+        }
+    }
+}
