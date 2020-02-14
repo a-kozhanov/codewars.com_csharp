d514c229b5fa0ac8c750b5c729ea56621d068633
a-kozhanov <a.kozhanov@outlook.com>
2020-02-14 23:05:15 +0300

    Added solution "Regexp Basics - is it a six bit unsigned number?" 7 kyu

---
+++ CodeWars/RegexpBasicsIsItSixBitUnsignedNumber.cs
@@ -0,0 +1,73 @@
+﻿/*
+https://www.codewars.com/kata/567e8dbb9b6f4da558000030/train/csharp
+https://www.codewars.com/kata/567e8dbb9b6f4da558000030/solutions/csharp
+
+7 kyu
+Regexp Basics - is it a six bit unsigned number?
+
+Implement String#six_bit_number?, 
+which should return true if given object is a number representable by 6 bit unsigned integer (0-63), false otherwise.
+
+It should only accept numbers in canonical representation, so no leading +, extra 0s, spaces etc.
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public static class RegexpBasicsIsItSixBitUnsignedNumber
+    {
+        public static bool SixBitNumber(this string str)
+        {
+            return Regex.IsMatch(str, @"^(6[0-3]|[1-5][0-9]|[0-9])\z");
+        }
+
+        // public static bool SixBitNumber(this string s) => IsMatch(s, @"^(\d|[1-5]\d|6[0-3])\z");
+
+
+        // public static bool SixBitNumber(this string str)
+        // {
+        //     if (Regex.IsMatch(str, @"(^0.+|^-0.+|^.+[\n\r]$)"))
+        //     {
+        //         return false;
+        //     }
+        //
+        //     bool result = (Regex.IsMatch(str, @"^([0-9]|[1-5][0-9]|6[0-3])$")) ? true : false;
+        //     return result;
+        // }
+
+
+        // public static bool SixBitNumber(this string str)
+        // {
+        //     Console.WriteLine(str);
+        //     return Regex.IsMatch(str, @"^([0-9]|[1-5]\d|[6][0-3])\z");
+        // }
+
+        // public static bool SixBitNumber(this string str)
+        // {
+        //     // Your code here
+        //     if (!str.Equals(str.Trim()))
+        //         return false;
+        //     return str != "" && Regex.IsMatch(str, "^(6[0-3]|[1-5][0-9]|[0-9])$");
+        // }
+
+        // public static bool SixBitNumber(this string str)
+        // {
+        //     if (Regex.IsMatch(str, @"^([0-9]|[1-5][0-9]|[1-6][0-3])\z") && !(str.StartsWith("0") && str.Length > 1))
+        //         return true;
+        //     else
+        //         return false;
+        // }
+
+        // public static bool SixBitNumber(this string str)
+        // {
+        //     return Regex.IsMatch(str, @"^(\d|[1-5]\d|6[0-3])\z");
+        // }
+
+        // public static bool SixBitNumber(this string str)
+        // {
+        //     return Regex.IsMatch(str, @"\A(\d|[1-5]\d|6[0-3])\z");
+        // }
+    }
+}
---
+++ CodeWarsTests/RegexpBasicsIsItSixBitUnsignedNumberTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RegexpBasicsIsItSixBitUnsignedNumberTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(false, "".SixBitNumber());
+            Assert.AreEqual(true, "0".SixBitNumber());
+            Assert.AreEqual(false, "00".SixBitNumber());
+            Assert.AreEqual(true, "55".SixBitNumber());
+            Assert.AreEqual(true, "63".SixBitNumber());
+            Assert.AreEqual(false, "64".SixBitNumber());
+            Assert.AreEqual(false, "-0".SixBitNumber());
+            Assert.AreEqual(false, "-5".SixBitNumber());
+            Assert.AreEqual(false, "05".SixBitNumber());
+            Assert.AreEqual(true, "5".SixBitNumber());
+            Assert.AreEqual(false, " ".SixBitNumber());
+        }
+    }
+}
