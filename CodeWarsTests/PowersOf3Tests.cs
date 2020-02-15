1a7a7e468aaefd6522f2334fbd4be5d233ae6264
a-kozhanov <a.kozhanov@outlook.com>
2020-02-15 14:24:59 +0300

    Added solution "Powers of 3" 7 kyu

---
+++ CodeWarsTests/PowersOf3Tests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PowersOf3Tests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(0, PowersOf3.LargestPower(3));
+            Assert.AreEqual(1, PowersOf3.LargestPower(4));
+        }
+    }
+}
