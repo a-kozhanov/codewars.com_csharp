22a5037a94f3bd3252bc8c997a1ecd70f52e42a0
a-kozhanov <a.kozhanov@outlook.com>
2020-02-17 00:11:25 +0300

    Added solution "Break camelCase" 6 kyu

---
+++ CodeWars/6kyu/BreakCamelCase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Text.RegularExpressions.Regex;
+
+/*
+https://www.codewars.com/kata/5208f99aee097e6552000148/train/csharp
+https://www.codewars.com/kata/5208f99aee097e6552000148/solutions/csharp
+
+6 kyu
+Break camelCase
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+Kata.BreakCamelCase("camelCasing") => "camel Casing"
+*/
+
+namespace CodeWars
+{
+    public static class KataBreakCamelCase
+    {
+        public static string BreakCamelCase(string str)
+        {
+            return Replace(str, "([A-Z])", " $1");
+        }
+
+        // public static string BreakCamelCase(string str) => new Regex("([A-Z])").Replace(str, " $1");
+
+
+        // public static string BreakCamelCase(string str)
+        // {
+        //     return string.Concat(str.Select(c => Char.IsUpper(c) ? " " + c : c.ToString()));
+        // }
+
+
+        // public static string BreakCamelCase(string str)
+        // {
+        //     string result = string.Empty;
+        //
+        //     foreach (char c in str)
+        //         if (char.IsUpper(c))
+        //             result = new string(string.Concat(result, " ", c.ToString()).ToCharArray());
+        //         else
+        //             result = new string(string.Concat(result, c.ToString()).ToCharArray());
+        //
+        //     return result;
+        // }
+
+
+        // public static string BreakCamelCase(string word)
+        // {
+        //     var sb = new StringBuilder();
+        //     foreach (var symbol in word)
+        //     {
+        //         if (char.IsUpper(symbol)) sb.Append(" ");
+        //         sb.Append(symbol);
+        //     }
+        //
+        //     return sb.ToString().Trim();
+        // }
+
+
+        // public static string BreakCamelCase(string str)
+        // {
+        //     return String.Join("", str.Select(x => char.IsUpper(x) ? " " + x : x.ToString()));
+        // }
+
+
+        // public static string BreakCamelCase(string s)
+        // {
+        //     var z = "";
+        //     foreach (var c in s)
+        //     {
+        //         z += char.IsUpper(c) ? $" {c}" : $"{c}";
+        //     }
+        //
+        //     return z;
+        // }
+
+
+        // public static string BreakCamelCase(string str)
+        // {
+        //     string m_return = "";
+        //     foreach (char m_char in str)
+        //     {
+        //         if (m_char >= 65 && m_char <= 90) m_return += " ";
+        //         m_return += m_char;
+        //     }
+        //
+        //     return m_return;
+        // }
+
+
+        // public static string BreakCamelCase(string str) =>
+        //     String.Join("", str.Select(c => Char.IsUpper(c) ? " " + c : "" + c).ToArray());
+    }
+}
---
+++ CodeWarsTests/6kyu/BreakCamelCaseTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataBreakCamelCaseTests
+    {
+        // [TestFixture]
+        // public class Sample_Tests
+        // {
+        //     private static IEnumerable<TestCaseData> testCases
+        //     {
+        //         get
+        //         {
+        //             yield return new TestCaseData("camelCasing").Returns("camel Casing");
+        //             yield return new TestCaseData("camelCasingTest").Returns("camel Casing Test");
+        //         }
+        //     }
+        //
+        //     [Test, TestCaseSource("testCases")]
+        //     public string Test(string str) => KataBreakCamelCase.BreakCamelCase(str);
+        // }
+
+
+        // [TestFixture]
+        // public class Random_Tests
+        // {
+        //     private static Random rnd = new Random();
+        //
+        //     private static string solution(string str) => new Regex("([A-Z])").Replace(str, " $1");
+        //
+        //     [Test, Description("Random Tests")]
+        //     public void RandomTests()
+        //     {
+        //         const int Tests = 100;
+        //
+        //         for (int i = 0; i < Tests; ++i)
+        //         {
+        //             StringBuilder sb = new StringBuilder();
+        //             sb.Append((char) rnd.Next(97, 123));
+        //             int length = rnd.Next(10, 200);
+        //
+        //             for (int j = 0; j < length; ++j)
+        //             {
+        //                 char c = (char) rnd.Next(97, 123);
+        //                 if (rnd.Next(0, 5) == 0)
+        //                 {
+        //                     c = Char.ToUpper(c);
+        //                 }
+        //
+        //                 sb.Append(c);
+        //             }
+        //
+        //             string str = sb.ToString();
+        //
+        //             string expected = solution(str);
+        //             string actual = KataBreakCamelCase.BreakCamelCase(str);
+        //
+        //             Assert.AreEqual(expected, actual);
+        //         }
+        //     }
+        //}
+    }
+}
