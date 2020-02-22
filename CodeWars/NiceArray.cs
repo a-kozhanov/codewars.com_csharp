f07963dce7f531bedb3893d5f5c9c1eeb34037af
a-kozhanov <a.kozhanov@outlook.com>
2020-02-22 22:09:04 +0300

    Added solution "Nice Array" 7 kyu

---
+++ CodeWars/NiceArray.cs
@@ -0,0 +1,99 @@
+﻿/*
+https://www.codewars.com/kata/59b844528bcb7735560000a0/train/csharp
+https://www.codewars.com/kata/59b844528bcb7735560000a0/solutions/csharp
+
+7 kyu
+Nice Array
+
+A Nice array is defined to be an array where for every value n in the array, 
+there is also an element n-1 or n+1 in the array.
+
+example:
+[2,10,9,3] is Nice array because
+
+2=3-1
+10=9+1
+3=2+1
+9=10-1
+
+Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else false. 
+You should also return false if input array has no elements.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NiceArray
+    {
+        public static bool IsNice(int[] arr)
+        {
+            Console.WriteLine($"{string.Join(", ", arr)}; Length: {arr.Length}; Any: {arr.Any()}");
+            return arr.Any() && arr.All(x => arr.Contains(x - 1) || arr.Contains(x + 1));
+        }
+
+        // public static bool IsNice(int[] arr)
+        // {
+        //     return arr.Length > 0 && arr.All(x => arr.Contains(x + 1) || arr.Contains(x - 1));
+        // }
+
+        // public static bool IsNice(int[] arr)
+        // {
+        //     if (arr.ToList().Count == 0)
+        //     {
+        //         return false;
+        //     }
+        //     else
+        //     {
+        //         foreach (var u in arr)
+        //         {
+        //             if ((!arr.Contains(u + 1)) && (!arr.Contains(u - 1)))
+        //             {
+        //                 return false;
+        //             }
+        //         }
+        //
+        //         return true;
+        //     }
+        // }
+
+
+        // public static bool IsNice(int[] arr)
+        // {
+        //     if (arr.Length <= 1)
+        //     {
+        //         return false;
+        //     }
+        //
+        //     foreach (int i in arr)
+        //     {
+        //         if (!(arr.Contains(i - 1)) && !(arr.Contains(i + 1)))
+        //         {
+        //             return false;
+        //         }
+        //     }
+        //
+        //     return true;
+        // }
+
+
+        // public static bool IsNice(int[] arr) =>
+        //     arr.Length == 0 ? false : arr.All(x => arr.Except(new[] {x}).Any(y => Math.Abs(x - y) == 1));
+
+
+        // public static bool IsNice(int[] arr)
+        // {
+        //     return arr.Length > 0 && arr.Count(x => arr.Contains(x - 1) || arr.Contains(x + 1)) == arr.Length;
+        // }
+
+        // public static bool IsNice(int[] arr)
+        // {
+        //     if (arr.Length == 0) return false;
+        //     for (int i = 0; i < arr.Length; i++)
+        //         if (!arr.Contains(arr[i] + 1) && !arr.Contains(arr[i] - 1))
+        //             return false;
+        //     return true;
+        // }
+    }
+}
---
+++ CodeWarsTests/NiceArrayTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NiceArrayTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, NiceArray.IsNice(new int[] {2, 10, 9, 3}));
+            Assert.AreEqual(false, NiceArray.IsNice(new int[] {3, 4, 5, 7}));
+            Assert.AreEqual(true, NiceArray.IsNice(new int[] {3, 2, 1, 0}));
+        }
+    }
+}
