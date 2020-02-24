a921a9a869341220b324bfb882869c130a6bd4bf
a-kozhanov <a.kozhanov@outlook.com>
2020-02-24 20:01:48 +0300

    Added solution "Sum of two lowest positive integers" 7 kyu

---
+++ CodeWars/SumOftwoLowestPositiveIntegers.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/csharp
+https://www.codewars.com/kata/558fc85d8fd1938afb000014/solutions/csharp
+
+7 kyu
+Sum of two lowest positive integers
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
+No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SumOftwoLowestPositiveIntegers
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            return numbers.OrderBy(x => x).Take(2).Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/SumOftwoLowestPositiveIntegersTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SumOftwoLowestPositiveIntegersTests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] numbers = {5, 8, 12, 19, 22};
+            Assert.AreEqual(13, SumOftwoLowestPositiveIntegers.sumTwoSmallestNumbers(numbers));
+        }
+    }
+}
