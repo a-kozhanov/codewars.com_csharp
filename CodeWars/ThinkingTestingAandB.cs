0dc7eae53422670b07ac0d8f8d8d0c042ce88e96
a-kozhanov <a.kozhanov@outlook.com>
2020-02-25 09:06:48 +0300

    Added solution "Thinking & Testing: A and B?" 7 kyu

---
+++ CodeWars/ThinkingTestingAandB.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/csharp
+https://www.codewars.com/kata/56d904db9963e9cf5000037d/solutions/csharp
+
+7 kyu
+Thinking & Testing: A and B?
+
+No Story
+No Description
+Only by Thinking and Testing
+Look at result of testcase, guess the code!
+*/
+
+
+
+namespace CodeWars
+{
+    public class ThinkingTestingAandB
+    {
+        public int Testit(int a, int b)
+        {
+            return a | b;
+        }
+
+        // public System.Func<int,int,int> Testit = (a, b) => a | b;
+
+        // public int Testit(int a, int b) => a | b;
+
+        // public int Testit(int a, int b)
+        // {
+        //     return (a + b) - (a & b);
+        // }
+
+    }
+}
---
+++ CodeWarsTests/ThinkingTestingAandBTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkingTestingAandBTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            //a+b?
+            Assert.AreEqual(1, new ThinkingTestingAandB().Testit(0, 1));
+            Assert.AreEqual(3, new ThinkingTestingAandB().Testit(1, 2));
+            Assert.AreEqual(30, new ThinkingTestingAandB().Testit(10, 20));
+            //a*b?
+            Assert.AreEqual(1, new ThinkingTestingAandB().Testit(1, 1));
+            Assert.AreEqual(3, new ThinkingTestingAandB().Testit(1, 3));
+            //Click "Attempt" try more testcase...
+        }
+    }
+}
