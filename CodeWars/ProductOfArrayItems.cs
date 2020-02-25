954f10ea9073dc2cec36fe6c386b70263ab316cd
a-kozhanov <a.kozhanov@outlook.com>
2020-02-25 22:36:42 +0300

    Added solution "Product of Array Items" 7 kyu

---
+++ CodeWars/ProductOfArrayItems.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/5901f361927288d961000013/train/csharp
+https://www.codewars.com/kata/5901f361927288d961000013/solutions/csharp
+
+7 kyu
+Product of Array Items
+
+Calculate the product of all elements in an array.
+
+If the array is null, you should throw ArgumentNullException and if the array is empty, 
+you should throw InvalidOperationException.
+
+As a challenge, try writing your method in just one line of code. 
+It's possible to have only 36 characters within your method.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ProductOfArrayItems
+    {
+        public static int Product(int[] values)
+        {
+            return values.Aggregate((x, y) => x * y);
+        }
+    }
+}
---
+++ CodeWarsTests/ProductOfArrayItemsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ProductOfArrayItemsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(540, ProductOfArrayItems.Product(new int[] {5, 4, 1, 3, 9}));
+            Assert.AreEqual(-672, ProductOfArrayItems.Product(new int[] {-2, 6, 7, 8}));
+            Assert.AreEqual(10, ProductOfArrayItems.Product(new int[] {10}));
+            Assert.AreEqual(0, ProductOfArrayItems.Product(new int[] {0, 2, 9, 7}));
+        }
+
+        [Test]
+        public void ArgumentNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => { ProductOfArrayItems.Product(null); });
+        }
+
+        [Test]
+        public void InvalidOperationTest()
+        {
+            Assert.Throws<InvalidOperationException>(() => { ProductOfArrayItems.Product(new int[] { }); });
+        }
+    }
+}
