ccc4c0e5f0860cd8c7a03e4c7afa148bfb97a88b
a-kozhanov <a.kozhanov@outlook.com>
2020-02-29 22:47:42 +0300

    Added solution "Jaden Casing Strings" 7 kyu

---
+++ CodeWarsTests/JadenCasingStringsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class JadenCasingStringsTests
+    {
+        [Test]
+        public void FixedTest()
+        {
+            Assert.AreEqual("How Can Mirrors Be Real If Our Eyes Aren't Real",
+                "How can mirrors be real if our eyes aren't real".ToJadenCase(),
+                "Strings didn't match.");
+        }
+    }
+}
