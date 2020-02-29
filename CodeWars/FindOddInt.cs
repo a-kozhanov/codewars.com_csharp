8dbc8e7ab04bf5aa703e6c60817d1622847104ce
a-kozhanov <a.kozhanov@outlook.com>
2020-02-29 21:00:41 +0300

    Added solution "Find the odd int" 6 kyu

---
+++ CodeWars/FindOddInt.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/54da5a58ea159efa38000836/train/csharp
+https://www.codewars.com/kata/54da5a58ea159efa38000836/solutions/csharp
+
+6 kyu
+Find the odd int
+
+Given an array, find the integer that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindOddInt
+    {
+        public static int find_it(int[] seq)
+        {
+            //return seq.First(x => seq.Count(c => c == x) == 3);
+            return seq.Aggregate((a, b) => a ^ b);
+        }
+
+
+        // public static int find_it(int[] seq)
+        // {
+        //     return seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
+        // }
+
+
+        // public static int find_it(int[] seq)
+        // {
+        //     return seq.First(x => seq.Count(s => s == x) % 2 == 1);
+        // }
+    }
+}
---
+++ CodeWarsTests/FindOddIntTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindOddIntTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(5, FindOddInt.find_it(new[] {20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}));
+        }
+    }
+}
