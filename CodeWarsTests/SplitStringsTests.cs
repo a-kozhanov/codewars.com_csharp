6d92929dcd77c953242e3dfc8e9354b845f433ca
a-kozhanov <a.kozhanov@outlook.com>
2020-02-29 11:40:46 +0300

    Added solution "Split Strings" 6 kyu

---
+++ CodeWarsTests/SplitStringsTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SplitStringsTests
+    {
+        [TestFixture]
+        public class SplitStringTests
+        {
+            [Test]
+            public void BasicTests()
+            {
+                Assert.AreEqual(new string[] {"ab", "c_"}, SplitStrings.Solution("abc"));
+                Assert.AreEqual(new string[] {"ab", "cd", "ef"}, SplitStrings.Solution("abcdef"));
+            }
+        }
+    }
+}
