a80d5eedb6eb0160f02bd47a4a40c54e84a7abf9
a-kozhanov <a.kozhanov@outlook.com>
2020-03-03 19:51:46 +0300

    Added solution "Thinkful - Number Drills: Blue and red marbles" 8 kyu

---
+++ CodeWarsTests/ThinkfulNumberDrillsBlueAndEedMarblesTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkfulNumberDrillsBlueAndEedMarblesTests
+    {
+        private const double Delta = 0.0000001;
+
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(ThinkfulNumberDrillsBlueAndEedMarbles.GuessBlue(5, 5, 2, 3), 0.6, Delta);
+            Assert.AreEqual(ThinkfulNumberDrillsBlueAndEedMarbles.GuessBlue(5, 7, 4, 3), 0.2, Delta);
+            Assert.AreEqual(ThinkfulNumberDrillsBlueAndEedMarbles.GuessBlue(12, 18, 4, 6), 0.4, Delta);
+        }
+    }
+}
