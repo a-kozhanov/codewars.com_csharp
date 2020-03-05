6cfcd03d5e4e9bf7fabe9a02d87da4b7f883305a
a-kozhanov <a.kozhanov@outlook.com>
2020-03-05 19:45:36 +0300

    Added solution "A Rule of Divisibility by 7" 7 kyu

---
+++ CodeWarsTests/RuleOfDivisibilityBy7Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RuleOfDivisibilityBy7Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Basic Tests");
+            Assert.AreEqual(new long[] {10, 2}, RuleOfDivisibilityBy7.Seven(1021));
+            Assert.AreEqual(new long[] {7, 2}, RuleOfDivisibilityBy7.Seven(1603));
+            Assert.AreEqual(new long[] {35, 1}, RuleOfDivisibilityBy7.Seven(371));
+            Assert.AreEqual(new long[] {42, 1}, RuleOfDivisibilityBy7.Seven(483));
+        }
+    }
+}
