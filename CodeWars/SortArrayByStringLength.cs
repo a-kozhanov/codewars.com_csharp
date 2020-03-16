60a08087b774a42fb4a7679e3f338d298bf2e52e
a-kozhanov <a.kozhanov@outlook.com>
2020-03-16 18:54:17 +0300

    Added solution "Sort array by string length" 7 kyu

---
+++ CodeWars/SortArrayByStringLength.cs
@@ -0,0 +1,45 @@
+﻿/*
+https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/csharp
+https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/solutions/csharp
+
+7 kyu
+Sort array by string length
+
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, 
+ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, 
+so you will not have to decide how to order multiple strings of the same length.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SortArrayByStringLength
+    {
+        public static string[] SortByLength(string[] array)
+        {
+            return array.OrderBy(v => v.Length).ToArray();
+        }
+
+        // public static string[] SortByLength(string[] array)
+        // {
+        //     Array.Sort(array, (x, y) => x.Length.CompareTo(y.Length));
+        //     return array;
+        // }
+
+
+        // public static string[] SortByLength(string[] array)
+        // {
+        //     Array.Sort(array, (x, y) => x.Length.CompareTo(y.Length));
+        //     return array;
+        // }
+    }
+}
---
+++ CodeWarsTests/SortArrayByStringLengthTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SortArrayByStringLengthTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(new string[] {"I", "To", "Beg", "Life"},
+                SortArrayByStringLength.SortByLength(new string[] {"Beg", "Life", "I", "To"}));
+            Assert.AreEqual(new string[] {"", "Pizza", "Brains", "Moderately"},
+                SortArrayByStringLength.SortByLength(new string[] {"", "Moderately", "Brains", "Pizza"}));
+            Assert.AreEqual(new string[] {"Short", "Longer", "Longest"},
+                SortArrayByStringLength.SortByLength(new string[] {"Longer", "Longest", "Short"}));
+        }
+    }
+}
