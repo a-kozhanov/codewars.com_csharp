73a51ba272ee95f6ace457aec75d27308a999454
a-kozhanov <a.kozhanov@outlook.com>
2020-03-17 21:19:23 +0300

    Added solution "Find the capitals" 7 kyu

---
+++ CodeWars/FindCapitals.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/539ee3b6757843632d00026b/train/csharp
+https://www.codewars.com/kata/539ee3b6757843632d00026b/solutions/csharp
+
+7 kyu
+Find the capitals
+
+Instructions
+Write a function that takes a single string (word) as argument. 
+The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Assert.AreEqual(Kata.Capitals("CodEWaRs"), new int[]{0,3,4,6});
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class FindCapitals
+    {
+        public static int[] Capitals(string word)
+        {
+            //return word.Select((x, i) => char.IsUpper(x) ? i : -1).Where(x => x >= 0).ToArray();
+            return Enumerable.Range(0, word.Length).Where(i => char.IsUpper(word, i)).ToArray();
+            //return Regex.Matches(word, "[A-Z]").Select(m => m.Index).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/FindCapitalsTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindCapitalsTests
+    {
+        [Test]
+        public void CodEWaRs()
+        {
+            Assert.AreEqual(FindCapitals.Capitals("CodEWaRs"), new int[] {0, 3, 4, 6});
+        }
+    }
+}
