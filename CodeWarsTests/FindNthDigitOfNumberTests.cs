ce50169d1bb357ade0175d914b951ee5026b13ce
a-kozhanov <a.kozhanov@outlook.com>
2020-03-18 00:22:50 +0300

    Added solution "Find the nth Digit of a Number" 7 kyu

---
+++ CodeWarsTests/FindNthDigitOfNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindNthDigitOfNumberTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(5, FindNthDigitOfNumber.FindDigit(5673, 4));
+            Assert.AreEqual(2, FindNthDigitOfNumber.FindDigit(129, 2));
+            Assert.AreEqual(8, FindNthDigitOfNumber.FindDigit(-2825, 3));
+            Assert.AreEqual(0, FindNthDigitOfNumber.FindDigit(-456, 4));
+            Assert.AreEqual(0, FindNthDigitOfNumber.FindDigit(0, 20));
+            Assert.AreEqual(-1, FindNthDigitOfNumber.FindDigit(65, 0));
+            Assert.AreEqual(-1, FindNthDigitOfNumber.FindDigit(24, -8));
+        }
+    }
+}
