ff43a1e0f7b1e86afbc2e3be5ffa800a66fcdab2
a-kozhanov <a.kozhanov@outlook.com>
2020-03-19 18:08:21 +0300

    Added solution "Sum of a Beach" 7 kyu

---
+++ CodeWars/SumOfBeach.cs
@@ -0,0 +1,67 @@
+﻿/*
+https://www.codewars.com/kata/5b37a50642b27ebf2e000010/train/csharp
+https://www.codewars.com/kata/5b37a50642b27ebf2e000010/solutions/csharp
+
+7 kyu
+Sum of a Beach
+
+Beaches are filled with sand, water, fish, and sun. 
+Given a string, calculate how many times the words "Sand", 
+"Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+
+Examples
+SumOfABeach("WAtErSlIde")                    ==>  1
+SumOfABeach("GolDeNSanDyWateRyBeaChSuNN")    ==>  3
+SumOfABeach("gOfIshsunesunFiSh")             ==>  4
+SumOfABeach("cItYTowNcARShoW")               ==>  0
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class SumOfBeach
+    {
+        public static int SumOfABeach(string s)
+        {
+            return Regex.Matches(s, "(?i)sand|water|fish|sun").Count;
+        }
+
+        // public static int SumOfABeach(string s)
+        // {
+        //     return Regex.Matches(s.ToLower(), "water|fish|sand|sun").Count;
+        // }
+
+        // public static int SumOfABeach(string s)
+        // {
+        //     return Regex.Matches(s, "(sand|sun|water|fish)", RegexOptions.IgnoreCase).Count;
+        // }
+
+        // public static int SumOfABeach(string s)
+        // {
+        //     return s.ToLower().Split(new string[] {"sand", "water", "fish", "sun"}, StringSplitOptions.None).Length - 1;
+        // }
+
+        // public static int SumOfABeach(string s) => s.ToLower().Replace(" ", "").Replace("sand", " ")
+        //     .Replace("water", " ").Replace("fish", " ").Replace("sun", " ").Count(i => i == ' ');
+
+
+        // public static int SumOfABeach(string s) =>
+        //     s.ToLower().Split(new string[] {"sand", "water", "fish", "sun"}, StringSplitOptions.None).Length - 1;
+
+
+        // public static int SumOfABeach(string s)
+        // {
+        //     return Regex.Matches(s, @"[Ss][Aa][Nn][Dd]|[Ww][Aa][Tt][Ee][Rr]|[Ff][Ii][Ss][Hh]|[Ss][Uu][Nn]").Count;
+        // }
+
+
+        // public static int SumOfABeach(string s)
+        // {
+        //     return s.ToLower().Replace("sand", "$").Replace("water", "$").Replace("fish", "$").Replace("sun", "$")
+        //         .Where(x => x == '$').Count();
+        // }
+    }
+}
---
+++ CodeWarsTests/SumOfBeachTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SumOfBeachTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(1, SumOfBeach.SumOfABeach("SanD"));
+            Assert.AreEqual(1, SumOfBeach.SumOfABeach("sunshine"));
+            Assert.AreEqual(4, SumOfBeach.SumOfABeach("sunsunsunsun"));
+            Assert.AreEqual(1, SumOfBeach.SumOfABeach("123FISH321"));
+        }
+    }
+}
