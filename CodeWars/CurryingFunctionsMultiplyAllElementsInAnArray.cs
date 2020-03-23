4d173919dd9b98bed2857ae55938116b07d9ed53
a-kozhanov <a.kozhanov@outlook.com>
2020-03-24 00:58:55 +0300

    Added solution "Currying functions: multiply all elements in an array" 7 kyu

---
+++ CodeWars/CurryingFunctionsMultiplyAllElementsInAnArray.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/csharp
+https://www.codewars.com/kata/586909e4c66d18dd1800009b/solutions/csharp
+
+7 kyu
+Currying functions: multiply all elements in an array
+
+To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. 
+This function must return another function, which takes a single integer as an argument and returns a new array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+Example:
+
+multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+Here's a nice Youtube video about currying, which might help you if this is new to you.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class CurryingFunctionsMultiplyAllElementsInAnArray
+    {
+        public static Func<int, int[]> MultiplyAll(int[] a)
+        {
+            //return i => a.Select(x => x * i).ToArray();
+            return i => Array.ConvertAll(a, x => i * x);
+        }
+    }
+}
---
+++ CodeWarsTests/CurryingFunctionsMultiplyAllElementsInAnArrayTests.cs
@@ -0,0 +1,35 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CurryingFunctionsMultiplyAllElementsInAnArrayTests
+    {
+        [Test, Description("must return an array")]
+        public void ArrayTest()
+        {
+            Assert.IsTrue(CurryingFunctionsMultiplyAllElementsInAnArray.MultiplyAll(new int[] {1})(1).GetType()
+                .IsArray);
+        }
+
+        [Test, Description("array has correct length")]
+        public void LengthTest()
+        {
+            Assert.IsTrue(CurryingFunctionsMultiplyAllElementsInAnArray.MultiplyAll(new int[] {1, 2})(1).Length == 2);
+        }
+
+        [Test, Description("returned array has correct values")]
+        public void ValuesTest()
+        {
+            Assert.AreEqual(CurryingFunctionsMultiplyAllElementsInAnArray.MultiplyAll(new int[] {1, 2, 3})(1),
+                new int[] {1, 2, 3});
+            Assert.AreEqual(CurryingFunctionsMultiplyAllElementsInAnArray.MultiplyAll(new int[] {1, 2, 3})(2),
+                new int[] {2, 4, 6});
+            Assert.AreEqual(CurryingFunctionsMultiplyAllElementsInAnArray.MultiplyAll(new int[] {1, 2, 3})(0),
+                new int[] {0, 0, 0});
+            Assert.AreEqual(CurryingFunctionsMultiplyAllElementsInAnArray.MultiplyAll(new int[] { })(10), new int[] { },
+                "should return an empty array");
+        }
+    }
+}
