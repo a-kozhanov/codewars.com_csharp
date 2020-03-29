52d74d16576989aed72399b681c17bbbcf808040
a-kozhanov <a.kozhanov@outlook.com>
2020-03-29 14:12:27 +0300

    Added solution "Predict your age!" 7 kyu

---
+++ CodeWarsTests/PredictYourAgeTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PredictYourAgeTests
+    {
+        [Test]
+        public void FixedTest()
+        {
+            Assert.AreEqual(86, PredictYourAge.PredictAge(65, 60, 75, 55, 60, 63, 64, 45));
+            Assert.AreEqual(79, PredictYourAge.PredictAge(32, 54, 76, 65, 34, 63, 64, 45));
+        }
+    }
+}
