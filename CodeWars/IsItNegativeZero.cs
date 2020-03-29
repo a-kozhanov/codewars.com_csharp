d8ac0ec67b67c1e91fc671b9db4fc8eaa88d47d6
a-kozhanov <a.kozhanov@outlook.com>
2020-03-29 12:41:03 +0300

    Added solution "Is It Negative Zero (-0)?" 7 kyu

---
+++ CodeWars/IsItNegativeZero.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/5c5086287bc6600001c7589a/train/csharp
+https://www.codewars.com/kata/5c5086287bc6600001c7589a/solutions/csharp
+
+7 kyu
+Is It Negative Zero (-0)?
+
+There exist two zeroes: +0 (or just 0) and -0.
+Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+In JavaScript / TypeScript / Coffeescript the input will be a number.
+In Python / Java / C / NASM / Haskell / the input will be a float.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class IsItNegativeZero
+    {
+        public static bool IsNegativeZero(double n)
+        {
+            //return BitConverter.DoubleToInt64Bits(n) == BitConverter.DoubleToInt64Bits(-0.0);
+            //return double.IsNegativeInfinity(1.0 / n);
+            //return 1 / n == Double.NegativeInfinity;
+            //return double.IsNegativeInfinity(1 / n);
+            //return double.NegativeInfinity == 1 / n;
+            //return double.IsNegative(n) && n == 0;
+            //return 1 / n < 0 && n == 0;
+            //return n == 0 && 1 / n < 0;     
+            //return 1 / n == 1 / -0.0;
+
+            return n == 0 & 1 / n < 0;
+        }
+    }
+}
---
+++ CodeWarsTests/IsItNegativeZeroTests.cs
@@ -0,0 +1,35 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IsItNegativeZeroTests
+    {
+        [Test, Description("should return true for -0")]
+        public void Test1()
+        {
+            Assert.AreEqual(true, IsItNegativeZero.IsNegativeZero(-0.0));
+        }
+
+        [Test, Description("should return false for non-negative-zero numbers")]
+        public void Test2()
+        {
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(double.NegativeInfinity));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-5.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-4.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-3.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-2.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-1.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-double.MaxValue));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(0.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(-double.MinValue));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(1.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(2.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(3.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(4.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(5.0));
+            Assert.AreEqual(false, IsItNegativeZero.IsNegativeZero(double.PositiveInfinity));
+        }
+    }
+}
