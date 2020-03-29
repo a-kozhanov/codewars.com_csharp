d6416360b88e0b1bb791eebf50cd1f1aed028d9a
a-kozhanov <a.kozhanov@outlook.com>
2020-03-29 14:23:55 +0300

    Added solution "Calculate snooker break total" 7 kyu

---
+++ CodeWars/CalculateSnookerBreakTotal.cs
@@ -0,0 +1,248 @@
+﻿/*
+https://www.codewars.com/kata/5ae5f2bd73ea5863790005b6/train/csharp
+https://www.codewars.com/kata/5ae5f2bd73ea5863790005b6/solutions/csharp
+
+7 kyu
+Calculate snooker break total
+
+The aim of this kata is to calculate the total for a snooker break. The rules of snooker for those who dont know are described here.
+
+Given an array of SnookerBall enums (see below), calculate the total of the break. If the break is not valid (see rules for more info on valid breaks), then an exception should be thrown
+
+Assumptions:
+
+All balls are on the table when running the code - ie 15 reds and 1 of each colour
+public enum SnookerBall
+{
+    Red = 1,
+    Yellow = 2,
+    Green = 3,
+    Brown = 4,
+    Blue = 5,
+    Pink = 6,
+    Black = 7,
+}
+*/
+
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class CalculateSnookerBreakTotal
+    {
+        public enum SnookerBall
+        {
+            Red = 1,
+            Yellow = 2,
+            Green = 3,
+            Brown = 4,
+            Blue = 5,
+            Pink = 6,
+            Black = 7,
+        }
+
+        public static int SumBreak(SnookerBall[] pots)
+        {
+            return pots.Take(30).Select((b, i) => new {b, i}).Any(x => x.i % 2 == 1 ? (int) x.b == 1 : (int) x.b != 1)
+                ? throw new Exception()
+                : pots.Skip(30).Select((b, i) => new {b, i}).Any(x => (int) x.b != x.i + 2)
+                    ? throw new Exception()
+                    : pots.Sum(b => (int) b);
+        }
+
+
+        // public static int SumBreak(SnookerBall[] pots)
+        // {
+        //     int ball_count = 0;
+        //     int score = 0;
+        //     foreach (SnookerBall ball in pots)
+        //     {
+        //         score += (int) ball;
+        //         if (++ball_count <= 30)
+        //         {
+        //             if (ball_count % 2 == 1)
+        //             {
+        //                 if (ball != SnookerBall.Red)
+        //                 {
+        //                     throw new Exception();
+        //                 }
+        //             }
+        //             else
+        //             {
+        //                 if (ball == SnookerBall.Red)
+        //                 {
+        //                     throw new Exception();
+        //                 }
+        //             }
+        //         }
+        //         else if ((int) ball != ball_count - 29)
+        //         {
+        //             throw new Exception();
+        //         }
+        //     }
+        //
+        //     return score;
+        // }
+
+
+        // public static int SumBreak(SnookerBall[] pots)
+        // {
+        //     bool rf = true;
+        //     int sc = 0, rc = 15, pr = 0;
+        //     foreach (SnookerBall ball in pots)
+        //     {
+        //         if (rf && rc > 0)
+        //         {
+        //             if (ball != SnookerBall.Red) throw new Exception();
+        //             ++sc;
+        //             --rc;
+        //         }
+        //         else
+        //         {
+        //             int cr = (int) ball;
+        //             if (rc == 0 && pr != 7 && pr + 1 != cr) throw new Exception();
+        //             pr = cr;
+        //             sc += cr;
+        //         }
+        //
+        //         rf = !rf;
+        //     }
+        //
+        //     return sc;
+        // }
+
+
+        // public static int SumBreak(SnookerBall[] pots)
+        // {
+        //     int point = (int) pots[0];
+        //     int result = point;
+        //     int prev = point;
+        //     for (int i = 1; i < pots.Length; i++, result += point)
+        //     {
+        //         if (pots[i] == pots[i - 1])
+        //             throw new Exception();
+        //
+        //         SnookerBall ball = pots[i];
+        //         point = (int) ball;
+        //         if (ball == SnookerBall.Red || ball == SnookerBall.Black)
+        //             continue;
+        //         if (point <= prev)
+        //             throw new Exception();
+        //         prev = point;
+        //     }
+        //
+        //     if (result < 15)
+        //         throw new Exception();
+        //     return result;
+        // }
+
+
+        // public static int SumBreak(SnookerBall[] pots)
+        // {
+        //     if (pots.Length < 3)
+        //         throw new Exception();
+        //     foreach (SnookerBall ball in pots)
+        //     {
+        //         Console.Write(ball);
+        //         Console.Write(" ");
+        //     }
+        //
+        //     int point = (int) pots[0];
+        //     int result = point;
+        //     int prev = point;
+        //     for (int i = 1; i < pots.Length; i++)
+        //     {
+        //         SnookerBall ball = pots[i];
+        //         if (ball == pots[i - 1])
+        //             throw new Exception();
+        //         point = (int) ball;
+        //         result += point;
+        //         if (ball == SnookerBall.Red || ball == SnookerBall.Black)
+        //             continue;
+        //         if (point <= prev)
+        //             throw new Exception();
+        //         prev = point;
+        //     }
+        //
+        //     return result;
+        // }
+
+
+        // public static int SumBreak(SnookerBall[] pots)
+        // {
+        //     int curr, prev = -1, total = 0, n = pots.Length, i = 0, red = -1;
+        //     while (i < n)
+        //     {
+        //         curr = (int) pots[i];
+        //         Console.WriteLine(String.Format("  [{0}] = {1} ({2})", i, curr, pots[i]));
+        //         if (curr == 1)
+        //         {
+        //             if (prev == 1)
+        //             {
+        //                 throw new System.Exception("Invalid Break - Red cannot follow Red");
+        //             }
+        //
+        //             red = 0;
+        //         }
+        //         else
+        //         {
+        //             if (red == -1)
+        //             {
+        //                 throw new System.Exception("Invalid Break - runout must begin with Yellow ball");
+        //             }
+        //
+        //             red += 1;
+        //             if (red == 2 && curr != 2)
+        //             {
+        //                 throw new System.Exception("Invalid Break - runout must begin with Yellow ball");
+        //             }
+        //             else
+        //             {
+        //                 if (prev >= curr && red != 2)
+        //                 {
+        //                     throw new System.Exception("Invalid Break - colour balls shot out of order");
+        //                 }
+        //             }
+        //         }
+        //
+        //         total += curr;
+        //         prev = curr;
+        //         i++;
+        //     }
+        //
+        //     return total;
+        // }
+
+
+        // public static int SumBreak(SnookerBall[] pots)
+        // {
+        //     var valid = new int[]
+        //     {
+        //         1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2, 3, 4, 5, 6,
+        //         7
+        //     };
+        //     for (var i = 0; i < pots.Count(); i++)
+        //     {
+        //         switch (valid[i])
+        //         {
+        //             case 0:
+        //                 if ((int) pots[i] == 1)
+        //                     throw new Exception("Should be any colour now");
+        //                 break;
+        //             case 1:
+        //                 if ((int) pots[i] != 1)
+        //                     throw new Exception("Should be a Red now");
+        //                 break;
+        //             default:
+        //                 if ((int) pots[i] != valid[i])
+        //                     throw new Exception("Should be a " + (SnookerBall) valid[i] + " now");
+        //                 break;
+        //         }
+        //     }
+        //
+        //     return pots.Sum(a => (int) a);
+        // }
+    }
+}
---
+++ CodeWarsTests/CalculateSnookerBreakTotalTests.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CalculateSnookerBreakTotalTests
+    {
+        [Test]
+        public void BasicTestFoul()
+        {
+            var pots = new CalculateSnookerBreakTotal.SnookerBall[] {CalculateSnookerBreakTotal.SnookerBall.Pink,};
+            Assert.Throws<Exception>(() => { CalculateSnookerBreakTotal.SumBreak(pots); });
+        }
+
+        public static int ActualSnookerBreak(CalculateSnookerBreakTotal.SnookerBall[] pots)
+        {
+            if (pots == null || pots.Length == 0)
+            {
+                return 0;
+            }
+
+            int tot = 0;
+            for (int i = 0; i < pots.Length; i++)
+            {
+                int shot = i + 1;
+                CalculateSnookerBreakTotal.SnookerBall colour = pots[i];
+
+                bool even = shot % 2 == 0;
+
+                if (shot <= 30)
+                {
+                    if (even)
+                    {
+                        if (colour == CalculateSnookerBreakTotal.SnookerBall.Red)
+                        {
+                            throw new Exception("FOUL");
+                        }
+                    }
+                    else
+                    {
+                        if (colour != CalculateSnookerBreakTotal.SnookerBall.Red)
+                        {
+                            throw new Exception("FOUL");
+                        }
+                    }
+                }
+                else if (shot == 31)
+                {
+                    if (colour != CalculateSnookerBreakTotal.SnookerBall.Yellow)
+                    {
+                        throw new Exception("FOUL");
+                    }
+                }
+                else if (shot == 32)
+                {
+                    if (colour != CalculateSnookerBreakTotal.SnookerBall.Green)
+                    {
+                        throw new Exception("FOUL");
+                    }
+                }
+                else if (shot == 33)
+                {
+                    if (colour != CalculateSnookerBreakTotal.SnookerBall.Brown)
+                    {
+                        throw new Exception("FOUL");
+                    }
+                }
+                else if (shot == 34)
+                {
+                    if (colour != CalculateSnookerBreakTotal.SnookerBall.Blue)
+                    {
+                        throw new Exception("FOUL");
+                    }
+                }
+                else if (shot == 35)
+                {
+                    if (colour != CalculateSnookerBreakTotal.SnookerBall.Pink)
+                    {
+                        throw new Exception("FOUL");
+                    }
+                }
+                else if (shot == 36)
+                {
+                    if (colour != CalculateSnookerBreakTotal.SnookerBall.Black)
+                    {
+                        throw new Exception("FOUL");
+                    }
+                }
+
+                tot += (int) colour;
+            }
+
+            return tot;
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            var pots = new CalculateSnookerBreakTotal.SnookerBall[]
+            {
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Pink
+            };
+            int tot = CalculateSnookerBreakTotal.SumBreak(pots);
+
+            Assert.AreEqual(15, tot);
+        }
+
+        [Test]
+        public void BasicTest147()
+        {
+            var pots = new CalculateSnookerBreakTotal.SnookerBall[]
+            {
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Yellow, CalculateSnookerBreakTotal.SnookerBall.Green,
+                CalculateSnookerBreakTotal.SnookerBall.Brown, CalculateSnookerBreakTotal.SnookerBall.Blue,
+                CalculateSnookerBreakTotal.SnookerBall.Pink,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+            };
+            int tot = CalculateSnookerBreakTotal.SumBreak(pots);
+
+            Assert.AreEqual(147, tot);
+        }
+
+        [Test]
+        public void SoClose()
+        {
+            var pots = new CalculateSnookerBreakTotal.SnookerBall[]
+            {
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Yellow, CalculateSnookerBreakTotal.SnookerBall.Green,
+                CalculateSnookerBreakTotal.SnookerBall.Brown, CalculateSnookerBreakTotal.SnookerBall.Blue,
+                CalculateSnookerBreakTotal.SnookerBall.Pink,
+            };
+            int tot = CalculateSnookerBreakTotal.SumBreak(pots);
+
+            Assert.AreEqual(140, tot);
+        }
+
+        [Test]
+        public void Invalid()
+        {
+            var pots = new CalculateSnookerBreakTotal.SnookerBall[]
+            {
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Yellow, CalculateSnookerBreakTotal.SnookerBall.Green,
+                CalculateSnookerBreakTotal.SnookerBall.Brown, CalculateSnookerBreakTotal.SnookerBall.Blue,
+                CalculateSnookerBreakTotal.SnookerBall.Pink,
+                CalculateSnookerBreakTotal.SnookerBall.Pink,
+            };
+            Assert.Throws<Exception>(() => { CalculateSnookerBreakTotal.SumBreak(pots); });
+        }
+
+        [Test]
+        public void BasicTestInvalidOrder()
+        {
+            var pots = new CalculateSnookerBreakTotal.SnookerBall[]
+            {
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red, CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black, CalculateSnookerBreakTotal.SnookerBall.Red,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+                CalculateSnookerBreakTotal.SnookerBall.Yellow, CalculateSnookerBreakTotal.SnookerBall.Brown,
+                CalculateSnookerBreakTotal.SnookerBall.Green, CalculateSnookerBreakTotal.SnookerBall.Pink,
+                CalculateSnookerBreakTotal.SnookerBall.Blue,
+                CalculateSnookerBreakTotal.SnookerBall.Black,
+            };
+            Assert.Throws<Exception>(() => { CalculateSnookerBreakTotal.SumBreak(pots); });
+        }
+
+        [Test]
+        public void RandomTests()
+        {
+            var r = new Random();
+
+            for (int i = 0; i < 50; i++)
+            {
+                var pots = new List<CalculateSnookerBreakTotal.SnookerBall>();
+
+                int length = (r.Next() % 36) + 1;
+                for (int j = 0; j < length; j++)
+                {
+                    int shot = j + 1;
+                    bool even = shot % 2 == 0;
+
+                    // 50/50 chance for potting a red on odd go...
+                    if (shot <= 30 && !even)
+                    {
+                        bool isRed = r.Next() % 2 == 0;
+                        if (isRed)
+                        {
+                            pots.Add(CalculateSnookerBreakTotal.SnookerBall.Red);
+                        }
+                        else
+                        {
+                            var pot = (CalculateSnookerBreakTotal.SnookerBall) ((r.Next() % 6) + 2);
+                            pots.Add(pot);
+                        }
+                    }
+                    else
+                    {
+                        var pot = (CalculateSnookerBreakTotal.SnookerBall) ((r.Next() % 7) + 1);
+                        pots.Add(pot);
+                    }
+                }
+
+                try
+                {
+                    int expected = ActualSnookerBreak(pots.ToArray());
+                    int actual = CalculateSnookerBreakTotal.SumBreak(pots.ToArray());
+
+                    Assert.AreEqual(expected, actual);
+                }
+                catch
+                {
+                    Assert.Throws<Exception>(() => { CalculateSnookerBreakTotal.SumBreak(pots.ToArray()); });
+                }
+            }
+        }
+    }
+}
