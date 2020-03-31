9e81d8799ae37ed0ca97d3daaf734be682360fce
a-kozhanov <a.kozhanov@outlook.com>
2020-03-31 22:49:12 +0300

    Added solution "Find the vowels" 7 kyu

---
+++ CodeWars/FindVowels.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/5680781b6b7c2be860000036/train/csharp
+
+7 kyu
+Find the vowels
+
+We want to know the index of the vowels in a given word, for example, 
+there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+*NOTE: Vowels in this context refers to English Language Vowels - a e i o u y *
+
+NOTE: this is indexed from [1..n] (not zero indexed!)
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class FindVowels
+    {
+        public static int[] VowelIndices(string word)
+        {
+            //return word.ToLower().Select((x, i) => "aeiouy".Contains(x) ? ++i : -1).Where(x => x > -1).ToArray();
+            //return Enumerable.Range(1, word.Length).Where(i => "aeiouyAEIOUY".Contains(word[i - 1])).ToArray();
+            //return Enumerable.Range(1, word.Length).Where(i => "aeiouy".Contains(word.ToLower()[i - 1])).ToArray();
+            //return Enumerable.Range(1, word.Length).Where(i => "aoueiyAEIOUY".Contains(word[i - 1])).ToArray();
+            //return word.Select((x, i) => i + 1).Where((x, j) => "aoueiy".Contains(word.ToLower()[j])).ToArray();
+            //return word.Select((x, i) => i + 1).Where((x, j) => "aoueiy".Contains(word.ToLower()[j])).ToArray();
+            //return word.Select((x, i) => ++i).Where((x, i) => "aoueiyAEIOUY".Contains(word[i])).ToArray();
+
+            //return word.Select((x, i) => ++i).Where(x => "aoueiyAEIOUY".Contains(word[x - 1])).ToArray();
+            return Regex.Matches(word, "(?i)[aeiouy]").Select(m => m.Index + 1).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/FindVowelsTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindVowelsTests
+    {
+        [Test]
+        public void FixedTest()
+        {
+            Assert.AreEqual(new int[] { }, FindVowels.VowelIndices("mmm"));
+            Assert.AreEqual(new int[] {1, 5}, FindVowels.VowelIndices("apple"));
+            Assert.AreEqual(new int[] {2, 4}, FindVowels.VowelIndices("super"));
+            Assert.AreEqual(new int[] {1, 3, 6}, FindVowels.VowelIndices("orange"));
+            Assert.AreEqual(new int[] {2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 25, 27, 29, 31, 32, 33},
+                FindVowels.VowelIndices("supercalifragilisticexpialidocious"));
+        }
+    }
+}
