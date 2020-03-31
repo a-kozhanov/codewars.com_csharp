e5ab39a42472be7c2a99a7f6d81700f59e1f8dd1
a-kozhanov <a.kozhanov@outlook.com>
2020-04-01 00:56:04 +0300

    Added solution "Round to the next multiple of 5" 7 kyu

---
+++ CodeWars/RoundToNextMultipleOf5.cs
@@ -0,0 +1,62 @@
+﻿/*
+https://www.codewars.com/kata/55d1d6d5955ec6365400006d/train/csharp
+
+7 kyu
+Round to the next multiple of 5.
+
+Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+*/
+
+
+using System;
+
+namespace CodeWars
+{
+    public class RoundToNextMultipleOf5
+    {
+        public static int RoundToNext5(int n)
+        {
+            //return (int) Math.Round(Math.Ceiling(n / 5.0)) * 5;
+            //return (int) Math.Ceiling((double) n / 5d) * 5;
+            //return n / 5 * 5 + (n % 5 > 0 ? 5 : 0);
+            //return n + (5 + (5 - n) % 5) % 5;
+
+            // while (n % 5 != 0)
+            // {
+            //     n++;
+            // }
+            //
+            // return n;
+
+            // while (true)
+            // {
+            //     if (n % 5 != 0)
+            //         n += 1;
+            //     else
+            //         return n;
+            // }
+
+            // return (n % 5 == 0)
+            //     ? n
+            //     : RoundToNext5(n + 1);
+
+            return n + (5 - n % 5) % 5;
+        }
+    }
+}
---
+++ CodeWarsTests/RoundToNextMultipleOf5Tests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RoundToNextMultipleOf5Tests
+    {
+        [Test]
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(1, ExpectedResult = 5)]
+        [TestCase(3, ExpectedResult = 5)]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(7, ExpectedResult = 10)]
+        [TestCase(39, ExpectedResult = 40)]
+        public static int FixedTest(int n)
+        {
+            return RoundToNextMultipleOf5.RoundToNext5(n);
+        }
+    }
+}
