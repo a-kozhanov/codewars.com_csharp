7f8a8f7f3383eb7f69cbbba70124af6274c3921c
a-kozhanov <a.kozhanov@outlook.com>
2020-04-06 00:28:06 +0300

    Added solution "Responsible Drinking" 7 kyu

---
+++ CodeWars/ResponsibleDrinking.cs
@@ -0,0 +1,53 @@
+﻿/*
+https://www.codewars.com/kata/5aee86c5783bb432cd000018/train/csharp
+
+7 kyu
+Responsible Drinking
+
+Welcome to the Codewars Bar!
+Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+
+Your fellow coders have bought you several drinks tonight in the form of a string. 
+Return a string suggesting how many glasses of water you should drink to not be hungover.
+
+Example parties:
+Input 0:
+"1 beer"
+
+Output 0:
+"1 glass of water"
+
+Explaination 0:
+You drank one standard drink
+
+Input 1:
+"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"
+
+Output 1:
+"10 glasses of water"
+
+Explaination 1:
+You drank ten standard drinks
+
+Note:
+To keep the things simple, we'll considere that any "numbered thing" in the string is a drink. 
+Even "1 bear" => "1 glass of water" or "1 chainsaw and 2 pools" => "3 glasses of water"...
+*/
+
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ResponsibleDrinking
+    {
+        public string hydrate(string drinkString)
+        {
+            //var s = drinkString.Where(char.IsDigit).Sum(c => int.Parse(c.ToString()));
+            //var s = Regex.Matches(drinkString, "\\d").Sum(m => int.Parse(m.Value));
+            var s = drinkString.Where(char.IsDigit).Sum(char.GetNumericValue);
+            return $"{s} glass{(s > 1 ? "es" : "")} of water";
+        }
+    }
+}
---
+++ CodeWarsTests/ResponsibleDrinkingTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ResponsibleDrinkingTests
+    {
+        [Test]
+        public void drinkinTest1()
+        {
+            ResponsibleDrinking drinkin = new ResponsibleDrinking();
+            string expected = "1 glass of water";
+            string actual = drinkin.hydrate("1 beer");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void drinkinTest3()
+        {
+            ResponsibleDrinking drinkin = new ResponsibleDrinking();
+            string expected = "10 glasses of water";
+            string actual = drinkin.hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
