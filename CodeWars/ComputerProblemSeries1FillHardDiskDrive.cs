a00c96f80406825d47a2362e07a73b224240c545
a-kozhanov <a.kozhanov@outlook.com>
2020-04-08 19:52:26 +0300

    Added solution "Computer problem series #1: Fill the Hard Disk Drive" 7 kyu

---
+++ CodeWars/ComputerProblemSeries1FillHardDiskDrive.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/5d49c93d089c6e000ff8428c/train/csharp
+ 
+7 kyu
+Computer problem series #1: Fill the Hard Disk Drive
+
+Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive.
+
+Input:
+Array of file sizes (0 <= s <= 100)
+Capacity of the HD (0 <= c <= 500)
+Output:
+Number of files that can be fully saved in the HD
+
+Examples:
+save([4,4,4,3,3], 12) -> 3
+# 4+4+4 <= 12, but 4+4+4+3 > 12
+
+save([4,4,4,3,3], 11) -> 2
+# 4+4 <= 11, but 4+4+4 > 11
+
+Do not expect any negative or invalid inputs.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ComputerProblemSeries1FillHardDiskDrive
+    {
+        public static int Save1(int[] sizes, int hd)
+        {
+            //return sizes.TakeWhile((x, i) => sizes.Take(i + 1).Sum() <= hd).Count();
+            //return Enumerable.Range(0, sizes.Length).Count(i => sizes.Take(i + 1).Sum() <= hd);
+            //return sizes.Where((x, i) => sizes.Take(i + 1).Sum() <= hd).Count();
+            return sizes.Aggregate((x, y) => x + y);
+        }
+
+
+        public static int Save(int[] sizes, int hd)
+        {
+            var sum = 0;
+            var count = 0;
+
+            foreach (var t in sizes)
+            {
+                sum += t;
+                if (sum <= hd) count++;
+                else break;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ CodeWarsTests/ComputerProblemSeries1FillHardDiskDriveTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ComputerProblemSeries1FillHardDiskDriveTests
+    {
+        [Test, Description("Calculate number of files")]
+        [TestCase(new int[] {4, 4, 4, 3, 3}, 12, 3)]
+        [TestCase(new int[] {4, 4, 4, 3, 3}, 11, 2)]
+        [TestCase(new int[] {4, 8, 15, 16, 23, 42}, 108, 6)]
+        [TestCase(new int[] {13}, 13, 1)]
+        [TestCase(new int[] {1, 2, 3, 4}, 250, 4)]
+        [TestCase(new int[] {100}, 500, 1)]
+        [TestCase(new int[] {11, 13, 15, 17, 19}, 8, 0)]
+        [TestCase(new int[] {45}, 12, 0)]
+        public void Test(int[] sizes, int hd, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, ComputerProblemSeries1FillHardDiskDrive.Save(sizes, hd));
+        }
+    }
+}
