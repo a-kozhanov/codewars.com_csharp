5cf4f15a57b8870f95e01fb9909a75dc1cd118f5
a-kozhanov <a.kozhanov@outlook.com>
2020-04-10 00:22:53 +0300

    Added solution "Number of Decimal Digits" 7 kyu

---
+++ CodeWars/NumberOfDecimalDigits.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/csharp
+
+7 kyu
+Number of Decimal Digits
+
+Determine the total number of digits in the integer (n>=0) given as input to the function. 
+For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NumberOfDecimalDigits
+    {
+        public static int Digits(ulong n)
+        {
+            //return n.ToString().Length;
+            //return n.ToString().Count();
+            //return (n + "").Length;
+            //return $"{n}".Count();
+            return $"{n}".Length;
+        }
+    }
+}
---
+++ CodeWarsTests/NumberOfDecimalDigitsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NumberOfDecimalDigitsTests
+    {
+        [Test]
+        public void Digits()
+        {
+            Assert.AreEqual(1, NumberOfDecimalDigits.Digits(5ul));
+            Assert.AreEqual(5, NumberOfDecimalDigits.Digits(12345ul));
+            Assert.AreEqual(10, NumberOfDecimalDigits.Digits(9876543210ul));
+        }
+    }
+}
