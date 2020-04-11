6b12250f51df826847e84772a5b4226ad11dfb4b
a-kozhanov <a.kozhanov@outlook.com>
2020-04-11 18:51:06 +0300

    Added solution "Changing letters" 7 kyu

---
+++ CodeWars/ChangingLetters.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/5831c204a31721e2ae000294/train/csharp
+
+7 kyu
+Changing letters
+
+When provided with a String, capitalize all vowels
+
+For example:
+Input : "Hello World!"
+Output : "HEllO WOrld!"
+
+Note: Y is not a vowel in this kata.
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ChangingLetters
+    {
+        public static string Swap(string s)
+        {
+            //return string.Concat(s.Select(x => "aeiou".Contains(x) ? x.ToString().ToUpper() : x.ToString()));
+            //return new string(s.Select(x => "aeiou".Contains(x) ? char.ToUpper(x) : x).ToArray());
+            //return string.Concat(s.Select(x => "aeiou".Contains(l) ? $"{x}".ToUpper() : $"{x}"));
+            //return string.Concat(s.Select(x => "aeiou".Contains(x) ? char.ToUpper(x) : x));
+            //return string.Concat(s.Select(x => "aeiou".Contains(x) ? (char) (x - 32) : x));
+            //return Regex.Replace(s, "[aeiou]", m => m.ToString().ToUpper());        
+            return Regex.Replace(s, "[aeiou]", m => m.Value.ToUpper());
+        }
+    }
+}
---
+++ CodeWarsTests/ChangingLettersTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    public class ChangingLettersTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("HelloWorld!").Returns("HEllOWOrld!");
+                yield return new TestCaseData("Sunday").Returns("SUndAy");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string s) => ChangingLetters.Swap(s);
+    }
+}
