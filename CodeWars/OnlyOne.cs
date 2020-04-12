d87fbb5e4563094b9add9f4086f86cb4b8265470
a-kozhanov <a.kozhanov@outlook.com>
2020-04-12 15:35:43 +0300

    Added solution "Only one" 7 kyu

---
+++ CodeWars/OnlyOne.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/5734c38da41454b7f700106e/train/csharp
+
+7 kyu
+Only one
+
+Given any number of boolean flags function should return true if and only if one of them is true while others are false. 
+If function is called without arguments it should return false.
+
+onlyOne() --> false
+onlyOne(true, false, false) --> true
+onlyOne(true, false, false, true) --> false
+onlyOne(false, false, false, false) --> false  
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataOnlyOne
+    {
+        public static bool OnlyOne(params bool[] flags)
+        {
+            //return flags.Sum(i => i ? 1 : 0) == 1;
+            //return flags.Max(i => i ? 1 : 0) == 1;
+            //return flags.Count(b => b).Equals(1);
+            return flags.Count(b => b) == 1;
+        }
+    }
+}
---
+++ CodeWarsTests/OnlyOneTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataOnlyOneTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(true, KataOnlyOne.OnlyOne(true, false));
+        }
+    }
+}
