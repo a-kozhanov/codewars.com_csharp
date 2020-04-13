2a38b314db116659da04668421cdecc85dc40b6b
a-kozhanov <a.kozhanov@outlook.com>
2020-04-14 01:21:41 +0300

    Added solution "Sum of integers in string" 7 kyu

---
+++ CodeWars/SumOfIntegersInString.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/598f76a44f613e0e0b000026/train/csharp
+
+7 kyu
+Sum of integers in string
+
+Your task in this kata is to implement a function that calculates the sum of the integers inside a string. 
+For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+Note: only positive integers will be tested.
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataSumOfIntegersInString
+    {
+        public static int SumOfIntegersInString(string s)
+        {
+            //return Regex.Split(s, "\\D").Where(x => x != "").Sum(int.Parse);
+            return Regex.Matches(s, "\\d+").Sum(x => int.Parse(x.Value));
+        }
+    }
+}
---
+++ CodeWarsTests/SumOfIntegersInStringTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataSumOfIntegersInStringTests
+    {
+        private static object[] sampleTests = new object[]
+        {
+            new object[] {"12.4", 16},
+            new object[] {"h3ll0w0rld", 3},
+            new object[] {"2 + 3 = ", 5},
+            new object[] {"Our company made approximately 1 million in gross revenue last quarter.", 1},
+            new object[] {"The Great Depression lasted from 1929 to 1939.", 3868},
+            new object[] {"Dogs are our best friends.", 0},
+            new object[] {"C4t5 are 4m4z1ng.", 18},
+            new object[] {"The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", 3635},
+        };
+
+        [Test, TestCaseSource("sampleTests")]
+        public void SampleTest(string test, int expected) =>
+            Assert.AreEqual(expected, KataSumOfIntegersInString.SumOfIntegersInString(test));
+    }
+}
