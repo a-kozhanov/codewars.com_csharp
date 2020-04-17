33ad1c0a0f775dac729453b5895411ad7af6c2e3
a-kozhanov <a.kozhanov@outlook.com>
2020-04-18 00:33:16 +0300

    Added solution "Multiply characters" 7 kyu

---
+++ CodeWars/MultiplyCharacters.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/52e9aa89b5acdd26d3000127/train/csharp
+
+7 kyu
+Multiply characters
+
+Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+Expected results:
+
+Kata.Spam(1)  => "hue";
+Kata.Spam(6)  => "huehuehuehuehuehue";
+Kata.Spam(14) => "huehuehuehuehuehuehuehuehuehuehuehuehuehue";
+*/
+
+using System.Linq;
+using System.Text;
+
+namespace CodeWars
+{
+    public class MultiplyCharacters
+    {
+        public static string Spam(int n, string s = "hue")
+        {
+            //return new StringBuilder(s.Length * n).AppendJoin(s, new string[n + 1]).ToString();
+            //return new StringBuilder(s.Length * n).Insert(0, s, n).ToString();
+            //return string.Concat(Enumerable.Repeat("hue", n));
+            //return "".PadLeft(n, '#').Replace("#", "hue");
+            return new string('#', n).Replace("#", "hue");
+        }
+    }
+}
---
+++ CodeWarsTests/MultiplyCharactersTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MultiplyCharactersTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("hue", MultiplyCharacters.Spam(1));
+            Assert.AreEqual("huehuehuehuehuehue", MultiplyCharacters.Spam(6));
+            Assert.AreEqual("huehuehuehuehuehuehuehuehuehuehuehuehuehue", MultiplyCharacters.Spam(14));
+        }
+    }
+}
