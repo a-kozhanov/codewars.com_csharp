a3a1e5ed65b144d7fda28db33707299af956f203
a-kozhanov <a.kozhanov@outlook.com>
2020-04-21 01:24:41 +0300

    Added solution "Alphabet war" 7 kyu

---
+++ CodeWarsTests/AlphabetWarTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataAlphabetWarTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("Right side wins!", KataAlphabetWar.AlphabetWar("z"));
+            Assert.AreEqual("Let's fight again!", KataAlphabetWar.AlphabetWar("zdqmwpbs"));
+            Assert.AreEqual("Right side wins!", KataAlphabetWar.AlphabetWar("zzzzs"));
+            Assert.AreEqual("Left side wins!", KataAlphabetWar.AlphabetWar("wwwwwwz"));
+        }
+    }
+}
