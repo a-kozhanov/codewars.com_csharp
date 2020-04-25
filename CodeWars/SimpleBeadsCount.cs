8bdf14e8c0fc5dfd34371f5d5def276726a9337c
a-kozhanov <a.kozhanov@outlook.com>
2020-04-25 17:22:15 +0300

    Added solution "Simple beads count" 7 kyu

---
+++ CodeWars/SimpleBeadsCount.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/csharp
+
+7 kyu
+Simple beads count
+
+Two red beads are placed between every two blue beads. There are N blue beads. 
+After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, 
+TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+*/
+
+
+using System;
+
+namespace CodeWars
+{
+    public class SimpleBeadsCount
+    {
+        public static int CountRedBeads(int n)
+        {
+            //return n < 2 ? 0 : n + (n - 2);
+            //return n < 2 ? 0 : 2 * (n - 1);
+            //return n > 1 ? (n - 1) * 2 : 0;
+            //return n > 0 ? 2 * (n - 1) : 0;
+            //return n != 0 ? (n - 1) * 2 : 0;
+            //return n < 2 ? 0 : 2 * (n - 1);
+            //return Math.Max(0, 2 * n - 2);
+            //return n == 0 ? 0 : n * 2 - 2;
+            //return n > 1 ? 2 * n - 2 : 0;
+            //return n < 2 ? 0 : 2 * n - 2;
+            //return n > 1 ? 2 * n - 2 : 0;
+            //return n > 1 ? 2 * n - 2 : 0;
+            //return n < 2 ? 0 : 2 * n - 2;
+            //return n > 1 ? 2 * n - 2 : 0;
+
+            return n > 0 ? (n - 1) * 2 : 0;
+        }
+    }
+}
---
+++ CodeWarsTests/SimpleBeadsCountTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleBeadsCountTests
+    {
+        [Test, Description("Basic Tests")]
+        public void SampleTest()
+        {
+            Assert.That(SimpleBeadsCount.CountRedBeads(0), Is.EqualTo(0));
+            Assert.That(SimpleBeadsCount.CountRedBeads(1), Is.EqualTo(0));
+            Assert.That(SimpleBeadsCount.CountRedBeads(3), Is.EqualTo(4));
+            Assert.That(SimpleBeadsCount.CountRedBeads(5), Is.EqualTo(8));
+        }
+    }
+}
