217cb63ecd59f3cfdacf07a4abf187f1bff5d2ad
a-kozhanov <a.kozhanov@outlook.com>
2020-04-26 15:55:13 +0300

    Added solution "FIXME: Get Full Name" 7 kyu

---
+++ CodeWars/FixmeGetFullName.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/597c684822bc9388f600010f/train/csharp
+
+7 kyu
+FIXME: Get Full Name
+
+The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null (None in Python), but may be empty.
+
+Task
+Fix the bug so we can all go home early.
+*/
+
+namespace CodeWars
+{
+    public class FixmeGetFullName
+    {
+        // public string FullName { get; }
+        // public Dinglemouse(string firstName, string lastName) => FullName = $"{firstName} {lastName}".Trim();
+        // public FixmeGetFullName(string firstName, string lastName) => FullName = $"{firstName} {lastName}".Trim();
+
+        public string FullName { get; }
+
+        // public Dinglemouse(string firstName, string lastName)
+        public FixmeGetFullName(string firstName, string lastName)
+        {
+            FullName = $"{firstName} {lastName}".Trim();
+        }
+    }
+}
---
+++ CodeWarsTests/FixmeGetFullNameTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FixmeGetFullNameTests
+    {
+        [Test, Description("Sample Test")]
+        public void SampleTest()
+        {
+            // Assert.AreEqual("Clint Eastwood", new FIXMEGetFullName.Dinglemouse("Clint", "Eastwood").FullName);
+            Assert.AreEqual("Clint Eastwood", new FixmeGetFullName("Clint", "Eastwood").FullName);
+        }
+    }
+}
