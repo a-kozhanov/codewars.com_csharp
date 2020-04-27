7ba42a039601772c5998866015fbf1843f667353
a-kozhanov <a.kozhanov@outlook.com>
2020-04-28 00:29:07 +0300

    Added solution "Indexed capitalization" 7 kyu

---
+++ CodeWars/IndexedCapitalization.cs
@@ -0,0 +1,40 @@
+﻿/*
+https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1/train/csharp
+
+7 kyu
+Indexed capitalization
+
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+
+Be sure to also try:
+
+Alternate capitalization
+
+String array revisal
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class IndexedCapitalization
+    {
+        public static string Capitalize(string s, List<int> idxs)
+        {
+            //return string.Concat(Enumerable.Range(0, s.Length).Select(i => idxs.Contains(i) ? char.ToUpper(s.ElementAt(i)) : s.ElementAt(i)));
+            //return string.Concat(s.Select((c, i) => idxs.Exists(m => m == i) ? char.ToUpper(c) : c));
+            //return new string(s.Select((c, i) => idxs.Contains(i) ? char.ToUpper(c) : c).ToArray());
+            return string.Concat(s.Select((c, i) => idxs.Contains(i) ? char.ToUpper(c) : c));
+        }
+    }
+}
---
+++ CodeWarsTests/IndexedCapitalizationTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IndexedCapitalizationTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("abcdef", new List<int> {1, 2, 5}).Returns("aBCdeF");
+                yield return new TestCaseData("abcdef", new List<int> {1, 2, 5, 100}).Returns("aBCdeF");
+                yield return new TestCaseData("abcdef", new List<int> {1, 100, 2, 5}).Returns("aBCdeF");
+                yield return new TestCaseData("codewars", new List<int> {1, 3, 5, 50}).Returns("cOdEwArs");
+                yield return new TestCaseData("abracadabra", new List<int> {2, 6, 9, 10}).Returns("abRacaDabRA");
+                yield return new TestCaseData("codewarriors", new List<int> {5}).Returns("codewArriors");
+                yield return new TestCaseData("indexinglessons", new List<int> {0}).Returns("Indexinglessons");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string s, List<int> idxs) =>
+            IndexedCapitalization.Capitalize(s, idxs);
+    }
+}
