c4dcda718a5bd9f45077d2f2046f024236e61ae9
a-kozhanov <a.kozhanov@outlook.com>
2020-04-27 19:17:42 +0300

    Added solution "Counting Array Elements" 7 kyu

---
+++ CodeWars/CountingArrayElements.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/5569b10074fe4a6715000054/train/csharp
+
+7 kyu
+Counting Array Elements
+
+Write a function that takes an array and counts the number of each unique element present.
+
+Kata.Count(new List<string> {"James", "James", "John"}) =>
+  new Dictionary<string, int> {{"James", 2}, {"John", 1}};
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class CountingArrayElements
+    {
+        public static Dictionary<string, int> Count(List<string> lst)
+        {
+            //return lst.Distinct().ToDictionary(x => x, x => lst.Count(s => s == x));
+            return lst.ToLookup(x => x).ToDictionary(x => x.Key, x => x.Count());
+            //return lst.GroupBy(s => s).ToDictionary(x => x.Key, x => x.Count());
+        }
+    }
+}
---
+++ CodeWarsTests/CountingArrayElementsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CountingArrayElementsTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new Dictionary<string, int> {{"a", 2}, {"b", 3}},
+                CountingArrayElements.Count(new List<string> {"a", "b", "b", "a", "b"}));
+        }
+    }
+}
