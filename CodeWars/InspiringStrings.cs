821c21c2597f4421bf9a257963df29223edc4ba7
a-kozhanov <a.kozhanov@outlook.com>
2020-04-29 18:13:26 +0300

    Added solution "Inspiring Strings" 7 kyu

---
+++ CodeWars/InspiringStrings.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/5939ab6eed348a945f0007b2/train/csharp
+
+7 kyu
+Inspiring Strings
+
+When given a string of space separated words, return the word with the longest length. 
+If there are multiple words with the longest length, return the last instance of the word with the longest length.
+
+Example:
+'red white blue' //returns string value of white
+'red blue gold' //returns gold
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class InspiringStrings
+    {
+        public static string LongestWord(string stringOfWords)
+        {
+            //return stringOfWords.Split().Aggregate((a, b) => a.Length > b.Length ? a : b);
+            return stringOfWords.Split().OrderBy(w => w.Length).Last();
+        }
+    }
+}
---
+++ CodeWarsTests/InspiringStringsTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class InspiringStringsTests
+    {
+        private static object[] Basic_Test_Cases = new object[]
+        {
+            new object[] {"a b c d e fgh", "fgh"},
+            new object[] {"one two three", "three"},
+            new object[] {"red blue grey", "grey"},
+        };
+
+        [Test, TestCaseSource(typeof(InspiringStringsTests), "Basic_Test_Cases")]
+        public void Basic_Test(string test, string expected)
+        {
+            Assert.AreEqual(expected, InspiringStrings.LongestWord(test));
+        }
+    }
+}
