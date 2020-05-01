16fb9a8f0be54f1cc4fa258d18cc921c0bdb4f90
a-kozhanov <a.kozhanov@outlook.com>
2020-05-02 00:06:24 +0300

    Added solution "Convert boolean values to strings 'Yes' or 'No'" 7 kyu

---
+++ CodeWars/ConvertBooleanValuesToString.cs
@@ -0,0 +1,20 @@
+﻿/*
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/csharp
+
+8 kyu
+Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+
+namespace CodeWars
+{
+    public class ConvertBooleanValuesToString
+    {
+        public static string boolToWord(bool word)
+        {
+            return word ? "Yes" : "No";
+        }
+    }
+}
---
+++ CodeWarsTests/ConvertBooleanValuesToStringTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertBooleanValuesToStringTests
+    {
+        [Test]
+        public void boolToWordReturned1()
+        {
+            Assert.AreEqual("Yes", ConvertBooleanValuesToString.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+
+        [Test]
+        public void boolToWordReturned2()
+        {
+            Assert.AreEqual("No", ConvertBooleanValuesToString.boolToWord(false));
+            Console.WriteLine("Expected No");
+        }
+
+        [Test]
+        public void boolToWordReturned3()
+        {
+            Assert.AreEqual("Yes", ConvertBooleanValuesToString.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+    }
+}
