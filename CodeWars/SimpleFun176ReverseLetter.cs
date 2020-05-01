e7c19ed4b831799453a75f6e31bf77b155b23be4
a-kozhanov <a.kozhanov@outlook.com>
2020-05-01 14:54:15 +0300

    Added solution "Simple Fun #176: Reverse Letter" 7 kyu

---
+++ CodeWars/SimpleFun176ReverseLetter.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/csharp
+
+7 kyu
+Simple Fun #176: Reverse Letter
+
+Task
+Given a string str, reverse it omitting all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class SimpleFun176ReverseLetter
+    {
+        public string ReverseLetter(string str)
+        {
+            //return new string(str.Where(char.IsLetter).Reverse().ToArray());
+            //return string.Concat(Regex.Replace(str, "[^a-z]", "").Reverse());
+            return string.Concat(str.Where(char.IsLetter).Reverse());
+        }
+    }
+}
---
+++ CodeWarsTests/SimpleFun176ReverseLetterTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun176ReverseLetterTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun176ReverseLetter();
+
+            Assert.AreEqual("nahsirk", kata.ReverseLetter("krishan"));
+
+            Assert.AreEqual("nortlu", kata.ReverseLetter("ultr53o?n"));
+
+            Assert.AreEqual("cba", kata.ReverseLetter("ab23c"));
+
+            Assert.AreEqual("nahsirk", kata.ReverseLetter("krish21an"));
+        }
+    }
+}
