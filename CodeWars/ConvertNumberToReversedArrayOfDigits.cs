40656d1e6fecaedff805a5d7cf25d3862a50ce14
a-kozhanov <a.kozhanov@outlook.com>
2020-05-03 12:11:18 +0300

    Added solution "Convert number to reversed array of digits" 8 kyu

---
+++ CodeWars/ConvertNumberToReversedArrayOfDigits.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/csharp
+
+8 kyu
+Convert number to reversed array of digits
+
+Given a random number:
+
+C#: long;
+C++: unsigned long;
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ConvertNumberToReversedArrayOfDigits
+    {
+        public static long[] Digitize(long n)
+        {
+            //return $"{n}".Select(c => Convert.ToInt64(c - '0')).Reverse().ToArray();
+            //return $"{n}".Select(c => long.Parse(c + "")).Reverse().ToArray();
+            //return $"{n}".Select(c => long.Parse($"{c}")).Reverse().ToArray();
+            //return $"{n}".Select(c => (long) (c - 48)).Reverse().ToArray();
+            return $"{n}".Select(c => (long) c - '0').Reverse().ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/ConvertNumberToReversedArrayOfDigitsTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertNumberToReversedArrayOfDigitsTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(new long[] {1, 3, 2, 5, 3}, ConvertNumberToReversedArrayOfDigits.Digitize(35231));
+        }
+    }
+}
