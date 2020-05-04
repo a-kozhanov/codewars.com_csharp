865732b5a1ca3d8906d8b1ad374646ffc0a7549b
a-kozhanov <a.kozhanov@outlook.com>
2020-05-04 14:22:48 +0300

    Added solution "Split In Parts" 7 kyu

---
+++ CodeWars/KataSplitInParts.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/5650ab06d11d675371000003/train/csharp
+
+7 kyu
+Split In Parts
+
+The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+
+Example:
+
+Split the below string into other strings of size #3
+
+'supercalifragilisticexpialidocious'
+
+Will return a new string
+'sup erc ali fra gil ist ice xpi ali doc iou s'
+Assumptions:
+
+String length is always greater than 0
+String has no spaces
+Size is always positive
+*/
+
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataSplitInParts
+    {
+        public static string SplitInParts(string s, int partLength)
+        {
+            //return Regex.Replace(s, string.Format(".{{{0}}}(?!$)", partLength), "$0 ");
+            //return new Regex(new string('.', partLength)).Replace(s, "$0 ").Trim();
+            //return string.Join(" ", Regex.Matches(s, $".{{1,{partLength}}}"));
+            //return new Regex($".{{1,{partLength}}}").Replace(s, "$0 ").Trim();
+            //return string.Join(" ", Regex.Matches(s, $".{{1,{partLength}}}"));
+            //return Regex.Replace(s, "(.{" + partLength + "})", "$1 ").Trim();
+            return Regex.Replace(s, $"(.{{{partLength}}})", "$1 ").Trim();
+        }
+    }
+}
---
+++ CodeWarsTests/SplitInPartsTests.cs
@@ -0,0 +1,41 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataSplitInPartsTests
+    {
+        [TestCase]
+        //Fixed values
+        public void splitString1()
+        {
+            string str = "supercalifragilisticexpialidocious";
+            Assert.AreEqual("sup erc ali fra gil ist ice xpi ali doc iou s", KataSplitInParts.SplitInParts(str, 3));
+        }
+
+        [TestCase]
+        //Fixed values
+        public void splitString2()
+        {
+            string str = "HelloKata";
+            Assert.AreEqual("Hel loK ata", KataSplitInParts.SplitInParts(str, 3));
+        }
+
+        [TestCase]
+        //Fixed values
+        public void splitString3()
+        {
+            string str = "HelloKata";
+            Assert.AreEqual("H e l l o K a t a", KataSplitInParts.SplitInParts(str, 1));
+        }
+
+        [TestCase]
+        //Fixed values
+        public void splitString4()
+        {
+            string str = "HelloKata";
+            Assert.AreEqual("HelloKata", KataSplitInParts.SplitInParts(str, 9));
+        }
+    }
+}
