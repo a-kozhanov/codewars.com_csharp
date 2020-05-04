eb0c2e38d7588bebc722b1df1d20b0f7e0f80da1
a-kozhanov <a.kozhanov@outlook.com>
2020-05-04 20:36:32 +0300

    Added solution "Consecutive Ducks" 7 kyu

---
+++ CodeWars/ConsecutiveDucks.cs
@@ -0,0 +1,48 @@
+﻿/*
+https://www.codewars.com/kata/5dae2599a8f7d90025d2f15f
+
+7 kyu
+Consecutive Ducks
+
+Positive integers have so many gorgeous features. 
+Some of them could be expressed as a sum of two or more consecutive positive numbers.
+
+Consider an Example :
+10 , could be expressed as a sum of 1 + 2 + 3 + 4.
+Task
+Given Positive integer, N , 
+Return true if it could be expressed as a sum of two or more consecutive positive numbers , OtherWise return false .
+
+Notes
+Guaranteed constraint : 2 ≤ N ≤ (2^31) -1 .
+Input >> Output Examples:
+
+* consecutiveDucks(9)  ==>  return (true)  //  9 , could be expressed as a sum of ( 2 + 3 + 4 ) or ( 4 + 5 )
+* consecutiveDucks(64)  ==>  return (false)
+* consecutiveDucks(42)  ==>  return (true) //  42 , could be expressed as a sum of ( 9 + 10 + 11 + 12 )
+
+Playing with Numbers Series
+Playing With Lists/Arrays Series
+Bizarre Sorting-katas
+For More Enjoyable Katas
+ALL translations are welcomed
+Enjoy Learning !!
+Zizou
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataConsecutiveDucks
+    {
+        public static bool ConsecutiveDucks(int n)
+        {
+            // your code here !Number.isInteger( Math.log2(num));
+            return Math.Log2(n) % 1 != 0;
+            return Math.Log(n, 2) % 1 == 0 ? false : true;
+            return (n & (n - 1)) != 0;
+            return (n-- & n) != 0;
+        }
+    }
+}
---
+++ CodeWarsTests/ConsecutiveDucksTests.cs
@@ -0,0 +1,69 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConsecutiveDucksTests
+    {
+        [Test]
+        public void SmallValues()
+        {
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(69));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(8));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(57));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(6));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(13));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(16));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(91));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(75));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(38));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(25));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(32));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(65));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(13));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(16));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(99));
+        }
+
+        [Test]
+        public void MediumValues()
+        {
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(522));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(974));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(755));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(512));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(739));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(1006));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(838));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(1092));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(727));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(648));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(1024));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(851));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(541));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(1011));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(822));
+        }
+
+        [Test]
+        public void LargeValues()
+        {
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(382131));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(118070));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(17209));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(32768));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(161997));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(400779));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(198331));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(325482));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(88441));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(648));
+            Assert.AreEqual(false, KataConsecutiveDucks.ConsecutiveDucks(65536));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(323744));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(183540));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(65271));
+            Assert.AreEqual(true, KataConsecutiveDucks.ConsecutiveDucks(5263987));
+        }
+    }
+}
