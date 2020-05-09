2cdba70fdecff676c6b332523e3924640321902e
a-kozhanov <a.kozhanov@outlook.com>
2020-05-09 11:18:44 +0300

    Added solution "Switcheroo" 7 kyu

---
+++ CodeWars/Switcheroo.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/57f759bb664021a30300007d/train/csharp
+
+7 kyu
+Switcheroo
+
+Given a string made up of letters a, b, and/or c, 
+switch the position of letters a and b (change a to b and vice versa). 
+Leave any incidence of c untouched.
+
+Example:
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataSwitcheroo
+    {
+        public static string Switcheroo(string x)
+        {
+            //return string.Concat(x.Select(c => c == 'a' ? 'b' : c == 'b' ? 'a' : 'c'));
+            //return Regex.Replace(x, "[ab]", m => m.Value == "a" ? "b" : "a");
+            return x.Replace('a', 'x').Replace('b', 'a').Replace('x', 'b');
+        }
+    }
+}
---
+++ CodeWarsTests/SwitcherooTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataSwitcherooTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("bac", KataSwitcheroo.Switcheroo("abc"));
+            Assert.AreEqual("bbbacccabbb", KataSwitcheroo.Switcheroo("aaabcccbaaa"));
+            Assert.AreEqual("ccccc", KataSwitcheroo.Switcheroo("ccccc"));
+        }
+    }
+}
