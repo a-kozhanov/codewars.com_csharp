cfc98d3e409f3eb81a2382e8051327681cce6ce8
a-kozhanov <a.kozhanov@outlook.com>
2020-05-12 00:58:18 +0300

    Added solution "Converting integer to currency format" 7 kyu

---
+++ CodeWars/ConvertingIntegerToCurrencyFormat.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/54e9554c92ad5650fe00022b/csharp
+
+7 kyu
+Converting integer to currency format
+
+Write a function that takes an integer in input and outputs a string with currency format.
+
+Integer in currency format is expressed by a string of number where every three characters are separated by comma.
+
+For example:
+
+123456 --> "123,456"
+Input will always be an positive integer, so don't worry about type checking or negative/float values.
+*/
+
+using System.Globalization;
+
+namespace CodeWars
+{
+    public class ConvertingIntegerToCurrencyFormat
+    {
+        public static string ToCurrency(int price)
+        {
+            //return price.ToString("N0", CultureInfo.CreateSpecificCulture("en-En"));
+            //return price.ToString("N0", CultureInfo.GetCultureInfo("en"));
+            //return price.ToString("N0");
+            return $"{price:N0}";
+        }
+    }
+}
---
+++ CodeWarsTests/ConvertingIntegerToCurrencyFormatTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertingIntegerToCurrencyFormatTests
+    {
+        [Test]
+        [TestCase(100000, ExpectedResult="100,000")]
+        [TestCase(5678545, ExpectedResult="5,678,545")]
+        public static string FixedTest(int price)
+        {
+            return ConvertingIntegerToCurrencyFormat.ToCurrency(price);
+        }
+    }
+}
