1d0e828e86d500a6bbfe5893183b6dda77ce2aff
a-kozhanov <a.kozhanov@outlook.com>
2020-05-13 18:01:43 +0300

    Added solution "Digits explosion" 7 kyu

---
+++ CodeWars/DigitsExplosion.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/585b1fafe08bae9988000314/csharp
+
+7 kyu
+Digits explosion
+
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+Digits.Explode("312") = "333122"
+Digits.Explode("102269") = "12222666666999999999"
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class DigitsExplosion
+    {
+        public static string Explode(string s)
+        {
+            //return new string(s.SelectMany(d => Enumerable.Repeat(d, int.Parse(d.ToString()))).ToArray());
+            //return string.Concat(s.Select(l => new string(l, (int) char.GetNumericValue(l))));
+            //return string.Concat(s.Select(x => new string(x, int.Parse($"{x}"))));
+            //return string.Concat(s.Select(n => new string(n, n - '0')));
+            return string.Concat(s.Select(n => new string(n, n - 48)));
+        }
+    }
+}
---
+++ CodeWarsTests/DigitsExplosionTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DigitsExplosionTests
+    {
+        [TestCase("312", "333122")]
+        [TestCase("102269", "12222666666999999999")]
+        public static void ExplodeTests(string s, string result)
+        {
+            Assert.AreEqual(result, DigitsExplosion.Explode(s));
+        }
+    }
+}
