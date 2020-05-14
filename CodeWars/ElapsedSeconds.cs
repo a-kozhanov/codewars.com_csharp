f33f9299ce652bcf854e9e179c155c4d6f3b3d24
a-kozhanov <a.kozhanov@outlook.com>
2020-05-14 13:33:53 +0300

    Added solution "Elapsed Seconds" 7 kyu

---
+++ CodeWars/ElapsedSeconds.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/517b25a48557c200b800000c/csharp
+
+7 kyu
+Elapsed Seconds
+
+Complete the function so that it returns the number of seconds that have elapsed between the start and end times given.
+
+Tips:
+The start/end times are given as Date (JS/CoffeeScript), DateTime (C#), Time (Nim), datetime(Python) and Time (Ruby) instances.
+The start time will always be before the end time.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataElapsedSeconds
+    {
+        public static int ElapsedSeconds(DateTime startDate, DateTime endDate)
+        {
+            //return Convert.ToInt32((endDate - startDate).TotalSeconds);
+            //return (int) endDate.Subtract(startDate).TotalSeconds;
+            return (int) (endDate - startDate).TotalSeconds;
+        }
+    }
+}
---
+++ CodeWarsTests/ElapsedSecondsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataElapsedSecondsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var start = new DateTime(2013, 1, 1, 0, 0, 1);
+            var end = new DateTime(2013, 1, 1, 0, 0, 2);
+            var end2 = new DateTime(2013, 1, 1, 0, 0, 20);
+            var end3 = new DateTime(2013, 1, 1, 0, 1, 20);
+            Assert.AreEqual(1, KataElapsedSeconds.ElapsedSeconds(start, end));
+            Assert.AreEqual(19, KataElapsedSeconds.ElapsedSeconds(start, end2));
+            Assert.AreEqual(79, KataElapsedSeconds.ElapsedSeconds(start, end3));
+        }
+    }
+}
