6b6859a2a7f648a3f9fba26ba592950cef0b3833
a-kozhanov <a.kozhanov@outlook.com>
2020-05-16 17:33:51 +0300

    Added solution "Credit card issuer checking" 7 kyu

---
+++ CodeWarsTests/CreditCardIssuerCheckingTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CreditCardIssuerCheckingTests
+    {
+        [TestCase(4111111111111111, "VISA")]
+        [TestCase(6011111111111117, "Discover")]
+        [TestCase(9111111111111111, "Unknown")]
+        public void SampleTests(long input, string result)
+        {
+            Assert.That(CreditCardIssuerChecking.getIssuer(input), Is.EqualTo(result));
+        }
+    }
+}
