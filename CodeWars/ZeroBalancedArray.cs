537c0d42d68ea657878a4269e6ddd202f8342d0c
a-kozhanov <a.kozhanov@outlook.com>
2020-05-16 17:40:30 +0300

    Added solution "zero-balanced Array" 7 kyu

---
+++ CodeWars/ZeroBalancedArray.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/59c6fa6972851e8959000067/csharp
+
+7 kyu
+zero-balanced Array
+
+An array is called zero-balanced if its elements sum to 0 and for each positive element n, 
+there exists another element that is the negative of n. 
+Write a function named ìsZeroBalanced that returns true if its argument is zero-balanced array, else return false. 
+Note that an empty array will not sum to zero.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ZeroBalancedArray
+    {
+        public static bool IsZeroBalanced(List<int> xs)
+        {
+            return xs.Any() && xs.Sum() == 0 && xs.All(i => xs.Contains(-i));
+
+
+            //return xs.Count > 0 && xs.All(x => xs.Contains(-x)) && xs.Sum() == 0;
+            //return xs.Any() && xs.Sum() == 0 && xs.All(e => xs.Contains(-e));
+            //return xs.Count > 0 && xs.Sum() == 0 && xs.All(x => xs.Contains(x * -1));
+
+            //return xs.Count > 0 && xs.Sum() == 0 && xs.All(v => xs.Contains(v * -1));
+        }
+    }
+}
---
+++ CodeWarsTests/ZeroBalancedArrayTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ZeroBalancedArrayTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(false, ZeroBalancedArray.IsZeroBalanced(new List<int> {3}));
+            Assert.AreEqual(true, ZeroBalancedArray.IsZeroBalanced(new List<int> {0, 0, 0, 0, 0, 0}));
+        }
+    }
+}
