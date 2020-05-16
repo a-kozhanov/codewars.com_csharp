76d599d7986fc35b12d37b9c524bb25be7e1020d
a-kozhanov <a.kozhanov@outlook.com>
2020-05-16 17:34:53 +0300

    Added solution "Functional Addition" 7 kyu

---
+++ CodeWars/FunctionalAddition.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/538835ae443aae6e03000547/csharp
+
+7 kyu
+Functional Addition
+
+Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+Func<double, double> AddOne = Kata.Add(1);
+AddOne(3) => 4
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class FunctionalAddition
+    {
+        public static Func<double, double> Add(double n)
+        {
+            return x => x + n;
+        }
+    }
+}
---
+++ CodeWarsTests/FunctionalAdditionTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FunctionalAdditionTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(4, FunctionalAddition.Add(1)(3));
+        }
+    }
+}
