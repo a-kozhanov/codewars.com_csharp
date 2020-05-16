04dd29520dc49aa34085917b9bdc04ea909d88f1
a-kozhanov <a.kozhanov@outlook.com>
2020-05-16 17:37:40 +0300

    Added solution "Remove Duplicates" 7 kyu

---
+++ CodeWars/RemoveDuplicates7kyu.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/53e30ec0116393fe1a00060b/csharp
+
+7 kyu
+Remove Duplicates
+
+Remove Duplicates
+You are to write a function called unique that takes an array of integers and returns the array with duplicates removed. 
+It must return the values in the same order as first seen in the given array. 
+Thus no sorting should be done, 
+if 52 appears before 10 in the given array then it should also be that 52 appears before 10 in the returned array.
+
+Assumptions
+All values given are integers (they can be positive or negative).
+You are given an array but it may be empty.
+They array may have duplicates or it may not.
+Example
+puts unique([1, 5, 2, 0, 2, -3, 1, 10]).inspect
+[1, 5, 2, 0, -3, 10]
+
+puts unique([]).inspect
+[]
+
+puts unique([5, 2, 1, 3]).inspect
+[5, 2, 1, 3]
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class RemoveDuplicates7kyu
+    {
+        public static List<int> Unique(List<int> integers)
+        {
+            //return integers.GroupBy(x => x).Select(x => x.Key).ToList();
+            //return new HashSet<int>(integers).ToList();
+            //return integers.ToHashSet().ToList();
+            return integers.Distinct().ToList();
+        }
+    }
+}
---
+++ CodeWarsTests/RemoveDuplicates7kyuTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RemoveDuplicates7kyuTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(new List<int>() { }, RemoveDuplicates7kyu.Unique(new List<int>() { }));
+            Assert.AreEqual(new List<int>() {5, 2, 1, 3}, RemoveDuplicates7kyu.Unique(new List<int>() {5, 2, 1, 3}));
+            Assert.AreEqual(new List<int>() {1, 5, 2, 0, -3, 10},
+                RemoveDuplicates7kyu.Unique(new List<int>() {1, 5, 2, 0, 2, -3, 1, 10}));
+        }
+    }
+}
