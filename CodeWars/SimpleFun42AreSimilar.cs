75a5fa7ba117ce72769beca6d203762902ca047e
a-kozhanov <a.kozhanov@outlook.com>
2020-05-17 11:36:37 +0300

    Added solution "Simple Fun #42: Are Similar?" 7 kyu

---
+++ CodeWars/SimpleFun42AreSimilar.cs
@@ -0,0 +1,51 @@
+﻿/*
+https://www.codewars.com/kata/588820169ab1e053240000e0/csharp
+
+7 kyu
+Simple Fun #42: Are Similar?
+
+Task
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements.
+
+Given two arrays, check whether they are similar.
+
+Example
+For A = [1, 2, 3] and B = [1, 2, 3], the output should be true;
+
+For A = [1, 2, 3] and B = [2, 1, 3], the output should be true;
+
+For A = [1, 2, 2] and B = [2, 1, 1], the output should be false.
+
+Input/Output
+[input] integer array A
+
+Array of integers.
+
+Constraints: 3 ≤ A.length ≤ 10000, 1 ≤ A[i] ≤ 1000.
+
+[input] integer array B
+
+Array of integers of the same length as A.
+
+Constraints: B.length = A.length, 1 ≤ B[i] ≤ 1000.
+
+[output] a boolean value
+
+true if A and B are similar, false otherwise.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun42AreSimilar
+    {
+        public bool AreSimilar(int[] a, int[] b)
+        {
+            //return a.OrderBy(x => x).SequenceEqual(b.OrderBy(x => x)) && a.Where((x, y) => x != b[y]).Count() <= 2;
+            //return a.Sum() == b.Sum() && a.Zip(b, (left, right) => left == right ? 0 : 1).Sum() < 3;
+            return a.Sum() == b.Sum() && a.Where((x, y) => x != b[y]).Count() < 3;
+        }
+    }
+}
---
+++ CodeWarsTests/SimpleFun42AreSimilarTests.cs
@@ -0,0 +1,47 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun42AreSimilarTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun42AreSimilar();
+
+            Assert.AreEqual(true, kata.AreSimilar(new int[] {1, 2, 3}, new int[] {2, 1, 3}));
+
+            Assert.AreEqual(true, kata.AreSimilar(new int[] {1, 2, 3}, new int[] {1, 2, 3}));
+
+            Assert.AreEqual(false, kata.AreSimilar(new int[] {1, 2, 2}, new int[] {2, 1, 1}));
+
+            Assert.AreEqual(false, kata.AreSimilar(new int[] {1, 2, 3}, new int[] {1, 10, 2}));
+
+            Assert.AreEqual(true, kata.AreSimilar(new int[] {2, 3, 1}, new int[] {1, 3, 2}));
+
+            Assert.AreEqual(false, kata.AreSimilar(new int[] {290, 341, 854}, new int[] {290, 341, 290}));
+
+            Assert.AreEqual(false, kata.AreSimilar(new int[] {1, 1, 2}, new int[] {1, 2, 2}));
+
+            Assert.AreEqual(false, kata.AreSimilar(
+                new int[]
+                {
+                    57, 237, 965, 951, 520, 123, 926, 182, 236, 801, 295, 864, 196, 763, 296, 169, 668, 855, 67, 44,
+                    808, 810, 794, 723, 204, 505, 404, 275, 359, 668, 413, 995, 167, 639, 514, 747, 259, 426, 87, 434,
+                    60, 119, 295, 369, 357, 476, 522, 291, 760, 189, 34, 924, 522, 810, 976, 247, 443, 241, 746, 15,
+                    719, 650, 824, 569, 388, 301, 696, 558, 248, 549, 911, 242, 767, 633, 985, 689, 514, 425, 365, 729,
+                    983, 112, 515, 169, 633, 489
+                },
+                new int[]
+                {
+                    57, 746, 965, 951, 520, 123, 926, 182, 236, 801, 295, 864, 196, 763, 296, 169, 668, 855, 67, 44,
+                    808, 810, 404, 723, 204, 505, 794, 275, 359, 668, 241, 995, 167, 639, 514, 747, 259, 426, 87, 434,
+                    60, 119, 295, 369, 357, 476, 522, 291, 760, 189, 34, 924, 522, 810, 976, 247, 443, 413, 237, 15,
+                    719, 650, 824, 569, 388, 301, 696, 558, 248, 549, 911, 242, 767, 633, 985, 689, 514, 425, 365, 729,
+                    983, 112, 515, 169, 633, 489
+                }));
+        }
+    }
+}
