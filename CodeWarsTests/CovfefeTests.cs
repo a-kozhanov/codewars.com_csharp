e7ba31e20c1adb3acf0bbdd6032b4ab1949aacf7
a-kozhanov <a.kozhanov@outlook.com>
2020-05-21 21:21:33 +0300

    Added solution "Covfefe" 7 kyu

---
+++ CodeWarsTests/CovfefeTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataCovfefeTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("covfefe", KataCovfefe.Covfefe("coverage"));
+            Assert.AreEqual("covfefe covfefe", KataCovfefe.Covfefe("coverage coverage"));
+            Assert.AreEqual("nothing covfefe", KataCovfefe.Covfefe("nothing"));
+            Assert.AreEqual("double space  covfefe", KataCovfefe.Covfefe("double space "));
+            Assert.AreEqual("covfefe covfefe", KataCovfefe.Covfefe("covfefe"));
+            Assert.AreEqual(" covfefe", KataCovfefe.Covfefe(""));
+        }
+    }
+}
