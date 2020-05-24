7d910cfe346b420ba8c8d3982b5873ab82a98379
a-kozhanov <a.kozhanov@outlook.com>
2020-05-25 00:28:42 +0300

    Added solution "Calculate Two People's Individual Ages" 7 kyu

---
+++ CodeWars/CalculateTwoPeoplesIndividualAges.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/58e0bd6a79716b7fcf0013b1/csharp
+
+7 kyu
+Calculate Two People's Individual Ages
+
+Create a function that takes in the sum and age difference of two people, calculates their individual ages, 
+and returns a pair of values (oldest age first) if those exist or null/None if:
+
+sum < 0
+difference < 0
+Either of the calculated ages come out to be negative
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class CalculateTwoPeoplesIndividualAges
+    {
+        public static double[] GetAges(int sum, int difference)
+        {
+            // Console.WriteLine($"sum: {sum}; difference: {difference}");
+            // var a = (double) (sum + difference) / 2;
+            // var b = (double) (sum - difference) / 2;
+            // if (sum < 0 || difference < 0 || sum < difference) return null;
+            // return new[] {a, b};
+
+            return sum < 0 || difference < 0 || sum < difference
+                ? null
+                : new[] {(sum + difference) / 2d, (sum - difference) / 2d};
+        }
+    }
+}
---
+++ CodeWarsTests/CalculateTwoPeoplesIndividualAgesTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CalculateTwoPeoplesIndividualAgesTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new double[] {14, 10}, CalculateTwoPeoplesIndividualAges.GetAges(24, 4), "it returns {14, 10} when passed 24, 4");
+            Assert.AreEqual(new double[] {38.5, 24.5}, CalculateTwoPeoplesIndividualAges.GetAges(63, 14),
+                "it returns {38.5, 24.5} when passed 63, 14");
+            Assert.AreEqual(null, CalculateTwoPeoplesIndividualAges.GetAges(63, -14), "it returns null when passed 63, -14");
+        }
+    }
+}
