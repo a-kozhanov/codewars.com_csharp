206833a02ded350928a72ed468c42ae58a2aa314
a-kozhanov <a.kozhanov@outlook.com>
2020-05-27 20:28:24 +0300

    Added solution "Easy Time Convert" 7 kyu

---
+++ CodeWars/EasyTimeConvert.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/5a084a098ba9146690000969/csharp
+
+7 kyu
+Easy Time Convert
+
+This kata requires you to convert minutes (int) to hours and minutes in the format hh:mm (string).
+
+If the input is 0 or negative value, then you should return "00:00"
+
+Hint: use the modulo operation to solve this challenge. 
+The modulo operation simply returns the remainder after a division. 
+For example the remainder of 5 / 2 is 1, so 5 modulo 2 is 1.
+
+Example
+If the input is 78, then you should return "01:18", because 78 minutes converts to 1 hour and 18 minutes.
+
+Good luck! :D
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class EasyTimeConvert
+    {
+        public static string TimeConvert(int num)
+        {
+            //return $"{((num < 0 ? 0 : num) / 60):D2}:{((num < 0 ? 0 : num) % 60):D2}";
+            //return num > 0 ? $"{num / 60:D2}:{num % 60:D2}" : "00:00";
+            //return num < 0 ? "00:00" : (num / 60 * 100 + num % 60).ToString("00:00");
+            //return num < 0 ? "00:00" : $"{num / 60 * 100 + num % 60:00:00}";
+            //return num <= 0 ? "00:00" : $"{num / 60:00}:{num % 60:00}";
+            //return num <= 0 ? "00:00" : $"{num / 60:D2}:{num % 60:D2}";
+            // return num > 0 ? $"{num / 60:00}:{num % 60:00}" : "00:00";
+            return num > 0 ? $"{num / 60:D2}:{num % 60:D2}" : "00:00";
+        }
+    }
+}
---
+++ CodeWarsTests/EasyTimeConvertTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EasyTimeConvertTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(0).Returns("00:00");
+                yield return new TestCaseData(-6).Returns("00:00");
+                yield return new TestCaseData(8).Returns("00:08");
+                yield return new TestCaseData(32).Returns("00:32");
+                yield return new TestCaseData(75).Returns("01:15");
+                yield return new TestCaseData(63).Returns("01:03");
+                yield return new TestCaseData(134).Returns("02:14");
+                yield return new TestCaseData(180).Returns("03:00");
+                yield return new TestCaseData(970).Returns("16:10");
+                yield return new TestCaseData(565757).Returns("9429:17");
+            }
+        }
+  
+        [Test, TestCaseSource("testCases")]
+        public string Test(int num) =>
+            EasyTimeConvert.TimeConvert(num);
+    }
+}
