37eb1f3eceac16322695e37ac8c578a3aa4efce7
a-kozhanov <a.kozhanov@outlook.com>
2020-05-29 00:06:26 +0300

    Added solution "Number of Divisions" 7 kyu

---
+++ CodeWars/NumberOfDivisions.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/5913152be0b295cf99000001/csharp
+
+7 kyu
+Number of Divisions
+
+Calculate how many times a number can be divided by a given number.
+
+Example
+For example the number 6 can be divided by 2 two times:
+
+1. 6 / 2 = 3
+2. 3 / 2 = 1 remainder = 1
+100 can be divided by 2 six times:
+
+1. 100 / 2 = 50
+2. 50 / 2 = 25
+3. 25 / 2 = 12 remainder 1
+4. 12 / 2 = 6
+5. 6 / 2 = 3
+6. 3 / 2 = 1 remainder 1
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class NumberOfDivisions
+    {
+        public static int Divisions(int n, int divisor)
+        {
+            return (int) Math.Log(n, divisor);
+
+            // var count = 0;
+            // while (n >= divisor)
+            // {
+            //     count++;
+            //     n /= divisor;
+            // }
+            // return count;
+        }
+    }
+}
---
+++ CodeWarsTests/NumberOfDivisionsTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NumberOfDivisionsTests
+    {
+        [Test, Description("Sample Tests")]
+        public void Test()
+        {
+            Assert.AreEqual(2, NumberOfDivisions.Divisions(6, 2));
+            Assert.AreEqual(6, NumberOfDivisions.Divisions(100, 2));
+            Assert.AreEqual(4, NumberOfDivisions.Divisions(2450, 5));
+            Assert.AreEqual(8, NumberOfDivisions.Divisions(9999, 3));
+            Assert.AreEqual(0, NumberOfDivisions.Divisions(2, 3));
+            Assert.AreEqual(1, NumberOfDivisions.Divisions(5, 5));
+        }
+    }
+}
