6328b188d144b4d9a45ece0d3f608400bce1fb15
a-kozhanov <a.kozhanov@outlook.com>
2020-06-03 19:07:24 +0300

    Added solution "Number of People in the Bus" 7 kyu

---
+++ CodeWarsTests/NumberOfPeopleInBusTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NumberOfPeopleInBusTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            List<int[]> peopleList = new List<int[]>() {new[] {10, 0}, new[] {3, 5}, new[] {5, 8}};
+            Assert.AreEqual(5, NumberOfPeopleInBus.Number(peopleList));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            List<int[]> peopleList = new List<int[]>()
+                {new[] {3, 0}, new[] {9, 1}, new[] {4, 10}, new[] {12, 2}, new[] {6, 1}, new[] {7, 10}};
+            Assert.AreEqual(17, NumberOfPeopleInBus.Number(peopleList));
+        }
+
+        [Test]
+        public void ThirdTest()
+        {
+            List<int[]> peopleList = new List<int[]>()
+                {new[] {3, 0}, new[] {9, 1}, new[] {4, 8}, new[] {12, 2}, new[] {6, 1}, new[] {7, 8}};
+            Assert.AreEqual(21, NumberOfPeopleInBus.Number(peopleList));
+        }
+    }
+}
