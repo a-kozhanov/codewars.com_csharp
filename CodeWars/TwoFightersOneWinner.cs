e7e4c3fc0384a306db248d725b323001315d1cef
a-kozhanov <a.kozhanov@outlook.com>
2020-06-04 23:44:01 +0300

    Added solution "Two fighters, one winner." 7 kyu

---
+++ CodeWars/TwoFightersOneWinner.cs
@@ -0,0 +1,68 @@
+﻿/*
+https://www.codewars.com/kata/577bd8d4ae2807c64b00045b/csharp
+
+7 kyu
+Two fighters, one winner.
+
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Example:
+public class Fighter {
+  public string Name;
+  public int Health, DamagePerAttack;
+  public Fighter(string name, int health, int damagePerAttack) {
+    this.Name = name;
+    this.Health = health;
+    this.DamagePerAttack = damagePerAttack;
+  }
+}
+*/
+
+namespace CodeWars
+{
+    public class Fighter
+    {
+        public string Name;
+        public int Health, DamagePerAttack;
+
+        public Fighter(string name, int health, int damagePerAttack)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.DamagePerAttack = damagePerAttack;
+        }
+    }
+
+    public class TwoFightersOneWinner
+    {
+        public static string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
+        {
+            var att = (fighter1.Health - 1) / fighter2.DamagePerAttack;
+            var def = (fighter2.Health - 1) / fighter1.DamagePerAttack;
+
+            return att > def ? fighter1.Name : def > att ? fighter2.Name : firstAttacker;
+
+
+            // var att = fighter1.Name == firstAttacker ? fighter1 : fighter2;
+            // var def = fighter1.Name != firstAttacker ? fighter1 : fighter2;
+            //
+            // var attacker = firstAttacker == fighter1.Name ? fighter1 : fighter2;
+            // var defender = firstAttacker == fighter1.Name ? fighter2 : fighter1;
+            //
+            // while (true)
+            // {
+            //     def.Health -= att.DamagePerAttack;
+            //     if (def.Health <= 0) return att.Name;
+            //
+            //     att.Health -= def.DamagePerAttack;
+            //     if (att.Health <= 0) return def.Name;
+            // }
+        }
+    }
+}
---
+++ CodeWarsTests/TwoFightersOneWinnerTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TwoFightersOneWinnerTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Lew",
+                TwoFightersOneWinner.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"));
+            Assert.AreEqual("Harry",
+                TwoFightersOneWinner.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"));
+            Assert.AreEqual("Harald",
+                TwoFightersOneWinner.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
+            Assert.AreEqual("Harald",
+                TwoFightersOneWinner.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"));
+            Assert.AreEqual("Harald",
+                TwoFightersOneWinner.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"));
+            Assert.AreEqual("Harald",
+                TwoFightersOneWinner.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5),
+                    "Harald"));
+        }
+    }
+}
