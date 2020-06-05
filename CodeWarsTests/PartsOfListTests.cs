aac74298c1771321eb38787f190c9a0dacebe464
a-kozhanov <a.kozhanov@outlook.com>
2020-06-05 13:21:51 +0300

    Added solution "Parts of a list" 7 kyu

---
+++ CodeWarsTests/PartsOfListTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PartsOfListTests
+    {
+        // private static void testing(string actual, string expected)
+        // {
+        //     Assert.AreEqual(expected, actual);
+        // }
+
+        [Test]
+        public static void test0()
+        {
+            Console.WriteLine("Fixed Tests Partlist");
+
+            // String[] s1 = new String[] {"cdIw", "tzIy", "xDu", "rThG"};
+            // String a = "[[cdIw, tzIy xDu rThG], [cdIw tzIy, xDu rThG], [cdIw tzIy xDu, rThG]]";
+            // testing(cdecode.Helper.Array2DToString(PartList.Partlist(s1)), a); 
+            //
+            // s1 = new String[] {"I", "wish", "I", "hadn't", "come"};
+            // a = "[[I, wish I hadn't come], [I wish, I hadn't come], [I wish I, hadn't come], [I wish I hadn't, come]]";
+            // testing(cdecode.Helper.Array2DToString(PartList.Partlist(s1)), a); 
+            //
+            // s1 = new String[] {"vJQ", "anj", "mQDq", "sOZ"};
+            // a = "[[vJQ, anj mQDq sOZ], [vJQ anj, mQDq sOZ], [vJQ anj mQDq, sOZ]]";
+            // testing(cdecode.Helper.Array2DToString(PartList.Partlist(s1)), a);        
+        }
+    }
+}
--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -1,37 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Globalization;
-using System.Linq;
-using System.Numerics;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using CodeWars;
-using Microsoft.VisualBasic;
-
-namespace ConsoleTestApp
+﻿namespace ConsoleTestApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //var arr = new int[] {1, 1, 1, 2, 2, 3};
-            var arr = new int[] {96, 56, 24, 46, 75, 46, 75, 21, 46, 21, 75, 96, 56, 96, 56};
-
-            var x = arr.Count(i => i == 1);
-            var y = arr.Count(i => i == 2);
-
-            // var x = arr.Where(i => arr.Count(v => v == i) == 1);
-            // var y = arr.Where(i => arr.Count(v => v == i) == 2);
-
-            Console.WriteLine(string.Join(", ", arr.GroupBy(o => o)));
-            Console.WriteLine(string.Join(", ", arr.OrderBy(o => o)));
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(x * x * y);
         }
     }
 }
