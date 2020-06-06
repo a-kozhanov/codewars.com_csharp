00dfac61104fef7d89787b53608c3eddd5dbb4c5
a-kozhanov <a.kozhanov@outlook.com>
2020-06-07 01:03:25 +0300

    Added solution "Average Scores" 7 kyu

---
+++ CodeWars/AverageScores.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/57b68bc7b69bfc8209000307/csharp
+
+7 kyu
+Average Scores
+
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. 
+You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class AverageScores
+    {
+        public static double Average(int[] scores)
+        {
+            //return (int) Math.Round((double) scores.Sum() / scores.Length);
+            return (int) Math.Round(scores.Average());
+            //return Convert.ToInt32(scores.Average());
+        }
+    }
+}
---
+++ CodeWarsTests/AverageScoresTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AverageScoresTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var scores = new int[] {49, 3, 5, 300, 7};
+            Assert.AreEqual(73, AverageScores.Average(scores));
+
+            scores = new int[] {90, 98, 89, 100, 100, 86, 94};
+            Assert.AreEqual(94, AverageScores.Average(scores));
+        }
+    }
+}
