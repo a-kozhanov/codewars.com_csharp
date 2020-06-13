1c9010da0ed13e9d8bc91288766a654d204bb52e
a-kozhanov <a.kozhanov@outlook.com>
2020-06-13 18:04:03 +0300

    Added solution "SevenAte9" 7 kyu

---
+++ CodeWars/SevenAte9.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/559f44187fa851efad000087/csharp
+
+7 kyu
+SevenAte9
+
+Write a function that removes every lone 9 that is inbetween 7s.
+
+Kata.SevenAteNine("79712312") => "79712312"
+Kata.SevenAteNine("79797") => "777"
+Input: String Output: String
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class SevenAte9
+    {
+        public static string SevenAteNine(string str)
+        {
+            //return str.Replace("797", "77").Replace("797", "77");
+            //return Regex.Replace(str, "(?<=7)9(?=7)", "");
+            return Regex.Replace(str, "79(?=7)", "7");
+        }
+    }
+}
---
+++ CodeWarsTests/SevenAte9Tests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SevenAte9Tests
+    {
+        [TestCase("165561786121789797", "16556178612178977")]
+        [TestCase("797", "77")]
+        [TestCase("7979797", "7777")]
+        public void ExampleTests(string input, string expected)
+        {
+            Assert.That(SevenAte9.SevenAteNine(input), Is.EqualTo(expected));
+        }
+    }
+}
