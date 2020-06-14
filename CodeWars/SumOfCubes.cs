91ae81fc44fc1aa55d3b4daa72ae4609cd2f9e6e
a-kozhanov <a.kozhanov@outlook.com>
2020-06-14 17:32:40 +0300

    Added solution "Sum of Cubes" 7 kyu

---
+++ CodeWars/SumOfCubes.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/59a8570b570190d313000037/csharp
+
+7 kyu
+Sum of Cubes
+
+Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples:
+
+sumCubes(2) // 9
+// sum of the cubes of 1 and 2 is 1 + 8
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SumOfCubes
+    {
+        public static long SumCubes(int n)
+        {
+            //return (long) Enumerable.Range(1, n).Select(x => Math.Pow(x, 3)).Sum();
+            //return (long) Enumerable.Range(1, n).Sum(x => Math.Pow(x, 3));
+            //return Enumerable.Range(1, n).Sum(x => (long) x * x * x);
+            //return (long) Math.Pow((n * n + n) / 2, 2);
+            return (long) Math.Pow(n * (n + 1) / 2, 2);
+        }
+    }
+}
---
+++ CodeWarsTests/SumOfCubesTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SumOfCubesTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(1, SumOfCubes.SumCubes(1));
+            Assert.AreEqual(9, SumOfCubes.SumCubes(2));
+            Assert.AreEqual(36, SumOfCubes.SumCubes(3));
+            Assert.AreEqual(100, SumOfCubes.SumCubes(4));
+            Assert.AreEqual(3025, SumOfCubes.SumCubes(10));
+            Assert.AreEqual(58155876, SumOfCubes.SumCubes(123));
+        }
+    }
+}
