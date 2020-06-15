6e71c7abd3cd1aa408b9de9b35ac78921daa7972
a-kozhanov <a.kozhanov@outlook.com>
2020-06-15 19:00:23 +0300

    Added solution "Lost number in number sequence" 7 kyu

---
+++ CodeWars/LostNumberInNumberSequence.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/595aa94353e43a8746000120/csharp
+
+7 kyu
+Lost number in number sequence
+
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the array and no difference with it, your function should return the int 0.
+
+Note that N may be 1 or less (in the latter case, the first array will be []).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class LostNumberInNumberSequence
+    {
+        public static int FindDeletedNumber(List<int> startingList, List<int> mixedList)
+        {
+            return startingList.Except(mixedList).FirstOrDefault(x => x < mixedList.DefaultIfEmpty(2).Max());
+        }
+    }
+}
---
+++ CodeWarsTests/LostNumberInNumberSequenceTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class LostNumberInNumberSequenceTests
+    {
+        [Test]
+        public void MissingNumberTest()
+        {
+            Assert.AreEqual(6,
+                LostNumberInNumberSequence.FindDeletedNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new List<int>() {5, 7, 1, 9, 4, 8, 2, 3}));
+
+            Assert.AreEqual(1, LostNumberInNumberSequence.FindDeletedNumber(new List<int>() {1}, new List<int>()));
+
+            Assert.AreEqual(0, LostNumberInNumberSequence.FindDeletedNumber(new List<int>(), new List<int>()));
+        }
+
+        [Test]
+        public void NoMissingNumberTest()
+        {
+            Assert.AreEqual(0,
+                LostNumberInNumberSequence.FindDeletedNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7},
+                    new List<int>() {2, 3, 6, 1, 5, 4, 7}));
+
+            Assert.AreEqual(0,
+                LostNumberInNumberSequence.FindDeletedNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new List<int>() {5, 7, 6, 9, 4, 8, 1, 2, 3}));
+        }
+
+        [Test]
+        public void NilTest()
+        {
+            Assert.AreEqual(0, LostNumberInNumberSequence.FindDeletedNumber(new List<int>(), new List<int>()));
+        }
+    }
+}
