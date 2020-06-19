096a7fc46fb6c763d4f501836cd21ff25d0942db
a-kozhanov <a.kozhanov@outlook.com>
2020-06-19 19:24:53 +0300

    Added solution "The reject() function" 7 kyu

---
+++ CodeWars/RejectFunction.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/52988f3f7edba9839c00037d/csharp
+
+7 kyu
+The reject() function
+
+Implement a function which filters out array values which satisfy the given predicate.
+
+Kata.Reject(new int[] {1, 2, 3, 4, 5, 6}, (n) => n % 2 == 0)  =>  new int[] {1, 3, 5}
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class RejectFunction
+    {
+        public static int[] Reject(int[] array, Func<int, bool> predicate)
+        {
+            //return array.Where(i => !predicate(i)).ToArray();
+            return Array.FindAll(array, i => !predicate(i));
+        }
+    }
+}
---
+++ CodeWarsTests/RejectFunctionTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RejectFunctionTests
+    {
+        [Test, Description("Sample Test")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[] {1, 3, 5},
+                RejectFunction.Reject(new int[] {1, 2, 3, 4, 5, 6}, (n) => n % 2 == 0));
+        }
+    }
+}
