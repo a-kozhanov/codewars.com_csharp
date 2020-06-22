b248c15afec1d924c1279eccf24594a294038706
a-kozhanov <a.kozhanov@outlook.com>
2020-06-22 12:33:37 +0300

    Added solution "Convert an array of strings to array of numbers" 7 kyu

---
+++ CodeWars/ConvertAnArrayOfStringsToArrayOfNumbers.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/5783d8f3202c0e486c001d23/csharp
+
+7 kyu
+Convert an array of strings to array of numbers
+
+Oh no!
+Some really funny web dev gave you an array of numbers from his API response as an array of strings!
+
+You need to cast the whole array to the correct type.
+
+Create the function
+
+JavaScript: toNumberArray(array)
+Ruby: toNumberArray(array)
+C#: ToDoubleArray(array)
+that takes as a parameter an array of numbers represented as strings and outputs an array of numbers.
+
+ie:["1", "2", "3"] to [1, 2, 3]
+
+Note that you can receive floats as well.
+*/
+
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ConvertAnArrayOfStringsToArrayOfNumbers
+    {
+        public static double[] ToDoubleArray(string[] stringArray)
+        {
+            //double.Parse(s, CultureInfo.CreateSpecificCulture("en-US"))
+            //double.Parse(s, CultureInfo.GetCultureInfo("en"))
+            return stringArray.Select(s => double.Parse(s, CultureInfo.GetCultureInfo("en"))).ToArray();
+
+            //return stringArray.Select(Convert.ToDouble).ToArray();
+            //return Array.ConvertAll(stringArray, double.Parse);
+            //return stringArray.Select(double.Parse).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/ConvertAnArrayOfStringsToArrayOfNumbersTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertAnArrayOfStringsToArrayOfNumbersTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Console.WriteLine("Running ToDoubleArray(\"1.1\",\"2.2\",\"3.3\")...");
+
+            Assert.AreEqual(new double[] {1.1, 2.2, 3.3},
+                ConvertAnArrayOfStringsToArrayOfNumbers.ToDoubleArray(new string[] {"1.1", "2.2", "3.3"}));
+        }
+    }
+}
