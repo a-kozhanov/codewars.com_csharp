dff13d34c3bd86f9f6700ccae2813b32b41c984b
a-kozhanov <a.kozhanov@outlook.com>
2020-06-23 19:40:43 +0300

    Added solution "Find twins" 7 kyu

---
+++ CodeWars/FindTwins.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/5834315e06f227a6ac000099/csharp
+
+7 kyu
+Find twins
+
+Agent 47, you have a new task! Among citizens of the city X are hidden 2 dangerous criminal twins. Your task is to identify them and eliminate!
+
+Given an array of integers, your task is to find two same numbers and return one of them, for example in array [2, 3, 6, 34, 7, 8, 2] answer is 2.
+
+If there are no twins in the city - return None or the equivalent in the language that you are using.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindTwins
+    {
+        public static object Elimination(int[] arr)
+        {
+            // var result = arr.Where(x => arr.Count(i => i == x) > 1).DefaultIfEmpty(-1).FirstOrDefault();
+            // return result != -1 ? result : (object) null;
+
+            //return arr.ToLookup(n => n).FirstOrDefault(g => g.Count() == 2)?.Key;
+            return arr.GroupBy(i => i).FirstOrDefault(g => g.Count() == 2)?.Key;
+        }
+    }
+}
---
+++ CodeWarsTests/FindTwinsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindTwinsTests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual(1, FindTwins.Elimination(new int[] {2, 5, 34, 1, 22, 1}));
+            Assert.AreEqual(2, FindTwins.Elimination(new int[] {2, 2, 34, 1, 22}));
+            Assert.AreEqual(null, FindTwins.Elimination(new int[] {2, 5, 34, 1, 22}));
+        }
+    }
+}
