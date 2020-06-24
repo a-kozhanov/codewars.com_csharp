ec731de8b1d0da962ce432fe0951bfb916e0f342
a-kozhanov <a.kozhanov@outlook.com>
2020-06-24 18:55:40 +0300

    Added solution "Interview Question (easy)" 7 kyu

---
+++ CodeWars/InterviewQuestionEasy.cs
@@ -0,0 +1,38 @@
+﻿/*
+https://www.codewars.com/kata/5b358a1e228d316283001892/csharp
+
+7 kyu
+Interview Question (easy)
+
+You receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using an asterisk (*).
+
+For example:
+
+"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+As you can see, the letter c is shown only once, but wih 2 asterisks.
+
+The return string should include only the letters (not the dashes, spaces, apostrophes, etc). 
+There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
+
+Note that the return string must list the letters in order of their first appearence in the original string.
+
+More examples:
+
+"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+Have fun! ;)
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class InterviewQuestionEasy
+    {
+        public static string GetStrings(string city)
+        {
+            return string.Join(",",
+                city.Where(char.IsLetter).GroupBy(char.ToLower).Select(g => $"{g.Key}:{new string('*', g.Count())}"));
+        }
+    }
+}
---
+++ CodeWarsTests/InterviewQuestionEasyTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class InterviewQuestionEasyTests
+    {
+        [Test]
+        public void GenericTests()
+        {
+            Assert.AreEqual("c:**,h:*,i:*,a:*,g:*,o:*", InterviewQuestionEasy.GetStrings("Chicago"));
+            Assert.AreEqual("b:*,a:*,n:*,g:*,k:**,o:*", InterviewQuestionEasy.GetStrings("Bangkok"));
+            Assert.AreEqual("l:*,a:**,s:**,v:*,e:*,g:*", InterviewQuestionEasy.GetStrings("Las Vegas"));
+        }
+    }
+}
