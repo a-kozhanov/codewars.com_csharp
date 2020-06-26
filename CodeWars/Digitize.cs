a142c8fcc88d1ab764e7c67392194ed9da3eb00c
a-kozhanov <a.kozhanov@outlook.com>
2020-06-26 22:52:31 +0300

    Added solution "Digitize" 7 kyu

---
+++ CodeWars/Digitize.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/5417423f9e2e6c2f040002ae/csharp
+
+7 kyu
+Digitize
+
+Given a non-negative integer, return an array / a list of the individual digits in order.
+
+Examples:
+123 => [1,2,3]
+1 => [1]
+8675309 => [8,6,7,5,3,0,9]
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class Digitize
+    {
+        public static int[] digitize(int n)
+        {
+            //return n.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+            //return Array.ConvertAll(n.ToString().ToArray(), c => (int) c - 48);
+            //return n.ToString().Select(c => c - '0').ToArray();
+            //return n.ToString().Select(c => c - 48).ToArray();
+            
+            return $"{n}".Select(c => int.Parse($"{c}")).ToArray();
+            //return $"{n}".Select(c => c - '0').ToArray();
+            //return $"{n}".Select(c => c - 48).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/DigitizeTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DigitizeTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(new int[] {1, 2, 3}, Digitize.digitize(123));
+            Assert.AreEqual(new int[] {1}, Digitize.digitize(1));
+            Assert.AreEqual(new int[] {0}, Digitize.digitize(0));
+            Assert.AreEqual(new int[] {1, 2, 3, 0}, Digitize.digitize(1230));
+            Assert.AreEqual(new int[] {8, 6, 7, 5, 3, 0, 9}, Digitize.digitize(8675309));
+        }
+    }
+}
