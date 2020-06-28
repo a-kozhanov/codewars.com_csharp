9059ac7d633556ac9e29b012e816564d76537d96
a-kozhanov <a.kozhanov@outlook.com>
2020-06-28 21:32:09 +0300

    Added solution "max diff - easy" 7 kyu

---
+++ CodeWarsTests/MaxDiffEasyTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MaxDiffEasyTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(MaxDiffEasy.maxDiff(new[] {0, 1, 2, 3, 4, 5, 6}), 6);
+            Assert.AreEqual(MaxDiffEasy.maxDiff(new[] {-0, 1, 2, -3, 4, 5, -6}), 11);
+            Assert.AreEqual(MaxDiffEasy.maxDiff(new[] {0, 1, 2, 3, 4, 5, 16}), 16);
+            Assert.AreEqual(MaxDiffEasy.maxDiff(new[] {16}), 0);
+            Assert.AreEqual(MaxDiffEasy.maxDiff(new int[] { }), 0);
+        }
+    }
+}
