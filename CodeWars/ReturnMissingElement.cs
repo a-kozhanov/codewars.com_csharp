32008a546b3936ae7262c356567475d0d69fde6e
a-kozhanov <a.kozhanov@outlook.com>
2020-06-29 22:36:02 +0300

    Added solution "Return the Missing Element" 7 kyu

---
+++ CodeWars/ReturnMissingElement.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/5299413901337c637e000004
+
+7 kyu
+Return the Missing Element
+
+Fellow code warrior, we need your help! We seem to have lost one of our array elements, 
+and we need your help to retrieve it! Our array, superImportantArray, 
+was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+Write a function called getMissingElement that accepts an array of unique integers between 0 and 9 (inclusive), 
+and returns the missing element.
+
+Examples:
+Kata.GetMissingElement( [0, 5, 1, 3, 2, 9, 7, 6, 4] ); // returns 8
+Kata.GetMissingElement( [9, 2, 4, 5, 7, 0, 8, 6, 1] ); // returns 3
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ReturnMissingElement
+    {
+        public static int GetMissingElement(int[] superImportantArray)
+        {
+            //return new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}.Except(superImportantArray).First();
+            return Enumerable.Range(0, 10).First(i => !superImportantArray.Contains(i));
+            //return Enumerable.Range(0, 10).Except(superImportantArray).First();
+            //return 45 - superImportantArray.Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/ReturnMissingElementTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReturnMissingElementTests
+    {
+        [Test]
+        public void EightIsMissing()
+        {
+            Assert.AreEqual(8, ReturnMissingElement.GetMissingElement(new int[9] {0, 5, 1, 3, 2, 9, 7, 6, 4}));
+        }
+
+        [Test]
+        public void ThreeIsMissing()
+        {
+            Assert.AreEqual(3, ReturnMissingElement.GetMissingElement(new int[9] {9, 2, 4, 5, 7, 0, 8, 6, 1}));
+        }
+
+        [Test]
+        public void ThreeIsMissing9()
+        {
+            Assert.AreEqual(9, ReturnMissingElement.GetMissingElement(new int[9] {3, 2, 4, 5, 7, 0, 8, 6, 1}));
+        }
+
+        [Test]
+        public void ThreeIsMissing1()
+        {
+            Assert.AreEqual(1, ReturnMissingElement.GetMissingElement(new int[9] {9, 2, 4, 5, 7, 0, 8, 6, 3}));
+        }
+    }
+}
