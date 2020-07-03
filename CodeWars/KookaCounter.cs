ab44444076268d060f46b166399dc4cca5d2a71d
a-kozhanov <a.kozhanov@outlook.com>
2020-07-03 19:59:17 +0300

    Added solution "Kooka-Counter" 7 kyu

---
+++ CodeWars/KookaCounter.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/58e8cad9fd89ea0c6c000258/csharp
+
+7 kyu
+Kooka-Counter
+
+A family of kookaburras are in my backyard.
+I can't see them all, but I can hear them!
+How many kookaburras are there?
+
+Hint
+The trick to counting kookaburras is to listen carefully
+The males go HaHaHa...
+The females go hahaha...
+And they always alternate male/female
+
+^ Kata Note : No validation is necessary; only valid input will be passed :-)
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataKookaCounter
+    {
+        public static int KookaCounter(string laughing)
+        {
+            //return laughing.Any() ? Regex.Matches(laughing.Replace("a", ""), "Hh|hH").Count + 1 : 0;
+            return Regex.Matches(laughing, "(Ha)+|(ha)+").Count;
+        }
+    }
+}
---
+++ CodeWarsTests/KookaCounterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KookaCounterTests
+    {
+        private static object[] Basic_Test_Cases = new object[]
+        {
+            new object[] {String.Empty, 0},
+            new object[] {"hahahahaha", 1},
+            new object[] {"hahahahahaHaHaHa", 2},
+            new object[] {"HaHaHahahaHaHa", 3}
+        };
+
+        [Test, TestCaseSource(typeof(KookaCounterTests), "Basic_Test_Cases")]
+        public void Basic_Test(string laughing, int expected)
+        {
+            Assert.AreEqual(expected, KataKookaCounter.KookaCounter(laughing));
+        }
+    }
+}
