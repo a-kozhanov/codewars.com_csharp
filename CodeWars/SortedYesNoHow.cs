9d63d154c7e888ec471c2b3430c9419000e0d0fa
a-kozhanov <a.kozhanov@outlook.com>
2020-07-06 21:21:57 +0300

    Added solution "Sorted? yes? no? how?" 7 kyu

---
+++ CodeWars/SortedYesNoHow.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/580a4734d6df748060000045/csharp
+
+7 kyu
+Sorted? yes? no? how?
+
+Complete the method which accepts an array of integers, and returns one of the following:
+
+"yes, ascending" - if the numbers in the array are sorted in an ascending order
+"yes, descending" - if the numbers in the array are sorted in a descending order
+"no" - otherwise
+
+You can assume the array will always be valid, and there will always be one correct answer.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SortedYesNoHow
+    {
+        public static string IsSortedAndHow(int[] array)
+        {
+            //return array.OrderByDescending(i => i).SequenceEqual(array) ? "yes, descending" : array.OrderBy(i => i).SequenceEqual(array) ? "yes, ascending" : "no";
+            //return array.SequenceEqual(array.OrderBy(i => i)) ? "yes, ascending" : array.SequenceEqual(array.OrderByDescending(i => i)) ? "yes, descending" : "no";
+            //return array.SequenceEqual(array.OrderBy(i => i)) ? "yes, ascending" : array.SequenceEqual(array.OrderByDescending(i => i)) ? "yes, descending" : "no";
+            //return array.SequenceEqual(array.OrderBy(i => i)) ? "yes, ascending" : array.SequenceEqual(array.OrderBy(i => -i)) ? "yes, descending" : "no";
+            //return array.OrderBy(i => i).SequenceEqual(array) ? "yes, ascending" : array.OrderBy(i => -i).SequenceEqual(array) ? "yes, descending" : "no";
+            return array.OrderBy(i => i).SequenceEqual(array) ? "yes, ascending" :
+                array.OrderBy(i => -i).SequenceEqual(array) ? "yes, descending" : "no";
+        }
+    }
+}
---
+++ CodeWarsTests/SortedYesNoHowTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SortedYesNoHowTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("yes, ascending", SortedYesNoHow.IsSortedAndHow(new[] {1, 2}));
+            Assert.AreEqual("yes, descending", SortedYesNoHow.IsSortedAndHow(new[] {15, 7, 3, -8}));
+            Assert.AreEqual("no", SortedYesNoHow.IsSortedAndHow(new[] {4, 2, 30}));
+        }
+    }
+}
