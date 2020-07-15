a8caf61499adfaf285e5b7eebbb956e32d87e435
a-kozhanov <a.kozhanov@outlook.com>
2020-07-15 09:09:57 +0300

    Added solution "Decode the Morse code" 6 kyu

---
+++ CodeWarsTests/DecodeMorseCodeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DecodeMorseCodeTests
+    {
+        [Test]
+        public void MorseCodeDecoderBasicTest_1()
+        {
+            try
+            {
+                string input = ".... . -.--   .--- ..- -.. .";
+                string expected = "HEY JUDE";
+
+                string actual = DecodeMorseCode.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " +
+                            ex.Message);
+            }
+        }
+    }
+}
