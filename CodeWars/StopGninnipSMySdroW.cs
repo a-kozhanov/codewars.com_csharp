404c28628551375d2027edca3f59674a851ce3d9
a-kozhanov <a.kozhanov@outlook.com>
2020-07-21 23:40:34 +0300

    Added solution "Stop gninnipS My sdroW!" 6 kyu

---
+++ CodeWars/StopGninnipSMySdroW.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/5264d2b162488dc400000001/csharp
+
+6 kyu
+Stop gninnipS My sdroW!
+
+Write a function that takes in a string of one or more words, and returns the same string, 
+but with all five or more letter words reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. 
+Spaces will be included only when more than one word is present.
+
+Examples: 
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class StopGninnipSMySdroW
+    {
+        public static string SpinWords(string sentence)
+        {
+            //return string.Join(" ", sentence.Split().Select(s => s.Length > 4 ? string.Concat(s.Reverse()) : s));
+            return Regex.Replace(sentence, @"\w{5,}", m => string.Concat(m.Value.Reverse()));
+        }
+    }
+}
---
+++ CodeWarsTests/StopGninnipSMySdroWTests.cs
@@ -0,0 +1,47 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StopGninnipSMySdroWTests
+    {
+        [Test]
+        public static void Test1()
+        {
+            Assert.AreEqual("emocleW", StopGninnipSMySdroW.SpinWords("Welcome"));
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            Assert.AreEqual("Hey wollef sroirraw", StopGninnipSMySdroW.SpinWords("Hey fellow warriors"));
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            Assert.AreEqual("This is a test", StopGninnipSMySdroW.SpinWords("This is a test"));
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            Assert.AreEqual("This is rehtona test", StopGninnipSMySdroW.SpinWords("This is another test"));
+        }
+
+        [Test]
+        public static void Test5()
+        {
+            Assert.AreEqual("You are tsomla to the last test",
+                StopGninnipSMySdroW.SpinWords("You are almost to the last test"));
+        }
+
+        [Test]
+        public static void Test6()
+        {
+            Assert.AreEqual("Just gniddik ereht is llits one more",
+                StopGninnipSMySdroW.SpinWords("Just kidding there is still one more"));
+        }
+    }
+}
