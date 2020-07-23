83a9eff60bd611ae18961c85d210fb29e26c05a1
a-kozhanov <a.kozhanov@outlook.com>
2020-07-23 18:51:48 +0300

    Added solution "Take a Ten Minute Walk" 6 kyu

---
+++ CodeWarsTests/TakeTenMinuteWalkTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TakeTenMinuteWalkTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true,
+                TakeTenMinuteWalk.IsValidWalk(new string[] {"n", "s", "n", "s", "n", "s", "n", "s", "n", "s"}),
+                "should return true");
+
+            Assert.AreEqual(false,
+                TakeTenMinuteWalk.IsValidWalk(new string[]
+                    {"w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"}), "should return false");
+
+            Assert.AreEqual(false, TakeTenMinuteWalk.IsValidWalk(new string[] {"w"}), "should return false");
+
+            Assert.AreEqual(false,
+                TakeTenMinuteWalk.IsValidWalk(new string[] {"n", "n", "n", "s", "n", "s", "n", "s", "n", "s"}),
+                "should return false");
+        }
+    }
+}
