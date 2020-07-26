860ca26c9be7c6176c68d9e55ec91c6512af7d7a
a-kozhanov <a.kozhanov@outlook.com>
2020-07-26 04:36:50 +0300

    Added solution "Array.diff" 6 kyu

---
+++ CodeWars/6kyu/ArrayDiff.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/523f5d21c841566fde000009/csharp
+
+6 kyu
+Array.diff
+
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+
+Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+If a value is present in b, all of its occurrences must be removed from the other:
+
+Kata.ArrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3}
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataArrayDiff
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            //return b.Aggregate(a, (c, n) => c.Where(i => i != n).ToArray());
+            //return a.Where(i => !Array.Exists(b, m => m == i)).ToArray();
+            //return a.Where(x => b.All(i => i != x)).ToArray();
+            //return a.Where(x => !b.Contains(x)).ToArray();
+            return Array.FindAll(a, m => !b.Contains(m));
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/ArrayDiffTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataArrayDiffTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[] {2}, KataArrayDiff.ArrayDiff(new int[] {1, 2}, new int[] {1}));
+            Assert.AreEqual(new int[] {2, 2}, KataArrayDiff.ArrayDiff(new int[] {1, 2, 2}, new int[] {1}));
+            Assert.AreEqual(new int[] {1}, KataArrayDiff.ArrayDiff(new int[] {1, 2, 2}, new int[] {2}));
+            Assert.AreEqual(new int[] {1, 2, 2}, KataArrayDiff.ArrayDiff(new int[] {1, 2, 2}, new int[] { }));
+            Assert.AreEqual(new int[] { }, KataArrayDiff.ArrayDiff(new int[] { }, new int[] {1, 2}));
+        }
+    }
+}
