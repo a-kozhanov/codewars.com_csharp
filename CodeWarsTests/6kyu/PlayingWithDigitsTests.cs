11cfd3c9c81c724dd322dc724429e7e5d9a860bd
a-kozhanov <a.kozhanov@outlook.com>
2020-07-28 12:16:44 +0300

    Added solution "Playing with digits" 6 kyu

---
+++ CodeWarsTests/6kyu/PlayingWithDigitsTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PlayingWithDigitsTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, PlayingWithDigits.digPow(89, 1));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(-1, PlayingWithDigits.digPow(92, 1));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(51, PlayingWithDigits.digPow(46288, 3));
+        }
+    }
+}
