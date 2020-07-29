bbbf80504288c33f195b26f0c741869bec745183
a-kozhanov <a.kozhanov@outlook.com>
2020-07-29 09:17:28 +0300

    Added solution "Human Readable Time" 5 kyu

---
+++ CodeWars/5kyu/HumanReadableTime.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/52685f7382004e774f0001f7/csharp
+
+5 kyu
+Human Readable Time
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class HumanReadableTime
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            // var t = TimeSpan.FromSeconds(seconds);
+            // return $"{(int) t.TotalHours:00}:{t.Minutes:00}:{t.Seconds:00}";
+
+            return $"{seconds / 3600:00}:{seconds / 60 % 60:00}:{seconds % 60:00}";
+            //return $"{seconds / 3600:d2}:{seconds / 60 % 60:d2}:{seconds % 60:d2}";
+        }
+    }
+}
---
+++ CodeWarsTests/5kyu/HumanReadableTimeTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HumanReadableTimeTests
+    {
+        [Test]
+        public void HumanReadableTest()
+        {
+            Assert.AreEqual("00:00:00", HumanReadableTime.GetReadableTime(0));
+            Assert.AreEqual("00:00:05", HumanReadableTime.GetReadableTime(5));
+            Assert.AreEqual("00:01:00", HumanReadableTime.GetReadableTime(60));
+            Assert.AreEqual("23:59:59", HumanReadableTime.GetReadableTime(86399));
+            Assert.AreEqual("99:59:59", HumanReadableTime.GetReadableTime(359999));
+        }
+    }
+}
