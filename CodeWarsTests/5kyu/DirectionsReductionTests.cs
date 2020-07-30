40e31602dff18d173278dabb8a1b954441159438
a-kozhanov <a.kozhanov@outlook.com>
2020-07-30 15:39:58 +0300

    Added solution "Directions Reduction" 5 kyu

---
+++ CodeWarsTests/5kyu/DirectionsReductionTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DirectionsReductionTests
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] a = new string[] {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"};
+            string[] b = new string[] {"WEST"};
+            Assert.AreEqual(b, DirectionsReduction.dirReduc(a));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string[] a = new string[] {"NORTH", "WEST", "SOUTH", "EAST"};
+            string[] b = new string[] {"NORTH", "WEST", "SOUTH", "EAST"};
+            Assert.AreEqual(b, DirectionsReduction.dirReduc(a));
+        }
+    }
+}
