a291e5d5f2f2ca9844e8cbb3ef9fff577d2f6a91
a-kozhanov <a.kozhanov@outlook.com>
2020-08-01 13:37:37 +0300

    Added solution "IQ Test" 6 kyu

---
+++ CodeWarsTests/6kyu/IQTestTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IQTestTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, IQTest.Test("2 4 7 8 10"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, IQTest.Test("1 2 2"));
+        }
+    }
+}
