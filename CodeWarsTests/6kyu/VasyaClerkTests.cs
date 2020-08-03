32bf2ea9844ac6e539729eda2ef614e7ba48946c
a-kozhanov <a.kozhanov@outlook.com>
2020-08-03 12:02:06 +0300

    Added solution "Vasya - Clerk" 6 kyu

---
+++ CodeWarsTests/6kyu/VasyaClerkTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class VasyaClerkTests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] peopleInLine = new int[] {25, 25, 50, 50};
+            Assert.AreEqual("YES", VasyaClerk.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] peopleInLine = new int[] {25, 100};
+            Assert.AreEqual("NO", VasyaClerk.Tickets(peopleInLine));
+        }
+    }
+}
