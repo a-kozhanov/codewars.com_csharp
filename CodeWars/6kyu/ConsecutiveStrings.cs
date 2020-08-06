64c0b01b0f6602a0326ff4c178ca92e3f9f18a80
a-kozhanov <a.kozhanov@outlook.com>
2020-08-06 22:59:03 +0300

    Added solution "Consecutive strings" 6 kyu

---
+++ CodeWars/6kyu/ConsecutiveStrings.cs
@@ -0,0 +1,100 @@
+﻿/*
+https://www.codewars.com/kata/56a5d994ac971f1ac500003e/csharp
+
+6 kyu
+Consecutive strings
+
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Example:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+Note
+consecutive strings : follow one after another without an interruption
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ConsecutiveStrings
+    {
+        public static String LongestConsec(string[] strarr, int k)
+        {
+            return k > strarr.Length
+                ? ""
+                : Enumerable.Range(0, strarr.Length - k + 1).Select(i => string.Concat(strarr.Skip(i).Take(k)))
+                    .OrderBy(x => -x.Length).First();
+
+
+            // return k > strarr.Length
+            //     ? ""
+            //     : Enumerable.Range(0, strarr.Length - k + 1 > 0 ? strarr.Length - k + 1 : 0)
+            //         .Select(i => string.Concat(strarr.Skip(i).Take(k)))
+            //         .OrderBy(x => -x.Length).First();
+
+
+            // var count = strarr.Length - k + 1;
+            // return Enumerable.Range(0, count > 0 ? count : 0)
+            //     .Select(i => string.Join("", strarr.Skip(i).Take(k)))
+            //     .Aggregate(string.Empty, (max, current) => current.Length > max.Length ? current : max);
+
+
+            // return !strarr.Any() || k < 1 || k > strarr.Length
+            //     ? ""
+            //     : strarr.Take(strarr.Length - k + 1).Select((x, i) => x + string.Concat(strarr.Skip(i + 1).Take(k - 1)))
+            //         .OrderByDescending(x => x.Length).First();
+
+
+            // var count = strarr.Length - k + 1;
+            // return Enumerable.Range(0, count > 0 ? count : 0)
+            //     .Select(i => string.Concat(strarr.Skip(i).Take(k)))
+            //     .Aggregate("", (max, current) => current.Length > max.Length ? current : max);
+
+            // return k > strarr.Length
+            //     ? ""
+            //     : Enumerable.Range(0, strarr.Length - k + 1).Select(i => string.Concat(strarr.Skip(i).Take(k)))
+            //         .OrderBy(x => -x.Length).DefaultIfEmpty(" ").FirstOrDefault() ?? "";
+
+
+            // return !strarr.Any() || k <= 0 || k > strarr.Length
+            //     ? string.Empty
+            //     : strarr.Take(strarr.Length - k + 1).Select((x, i) => x + string.Concat(strarr.Skip(i + 1).Take(k - 1)))
+            //         .OrderByDescending(x => x.Length).First();
+
+
+            // return strarr.Length == 0 || k > strarr.Length || k <= 0
+            //     ? String.Empty
+            //     : String.Join(String.Empty,
+            //         Enumerable.Range(0, strarr.Length - k + 1)
+            //             .Select(i => strarr.Skip(i).Take(k))
+            //             .OrderByDescending(chunk => chunk.Sum(s => s.Length))
+            //             .First()
+            //     );
+
+
+            // int n = strarr.Length;
+            // return (n == 0 || k > n || k <= 0)
+            //     ? ""
+            //     : strarr.Select((x, index) => (k + index <= n) ? string.Join("", strarr.Skip(index).Take(k)) : "")
+            //         .OrderByDescending(x => x.Length).First();
+
+
+            // var count = strarr.Length - k + 1;
+            // return Enumerable.Range(0, count > 0 ? count : 0)
+            //     .Select(i => string.Join("", strarr.Skip(i).Take(k)))
+            //     .Aggregate(string.Empty, (max, current) => current.Length > max.Length ? current : max);
+
+
+            // return s.Length == 0 || s.Length < k || k <= 0
+            //     ? ""
+            //     : Enumerable.Range(0, s.Length - k + 1)
+            //         .Select(x => string.Join("", s.Skip(x).Take(k)))
+            //         .OrderByDescending(x => x.Length)
+            //         .First();
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/ConsecutiveStringsTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConsecutiveStringsTests
+    {
+        private static void testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests");
+            testing(
+                ConsecutiveStrings.LongestConsec(
+                    new String[] {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2),
+                "abigailtheta");
+            testing(
+                ConsecutiveStrings.LongestConsec(
+                    new String[] {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"},
+                    1), "oocccffuucccjjjkkkjyyyeehh");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { }, 3), "");
+            testing(
+                ConsecutiveStrings.LongestConsec(
+                    new String[]
+                        {"itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2),
+                "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+            testing(
+                ConsecutiveStrings.LongestConsec(new String[] {"wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"},
+                    2), "wlwsasphmxxowiaxujylentrklctozmymu");
+            testing(
+                ConsecutiveStrings.LongestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas"}, -2),
+                "");
+            testing(ConsecutiveStrings.LongestConsec(new String[] {"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"}, 3),
+                "ixoyx3452zzzzzzzzzzzz");
+            testing(
+                ConsecutiveStrings.LongestConsec(new String[] {"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"}, 15),
+                "");
+            testing(ConsecutiveStrings.LongestConsec(new String[] {"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"}, 0),
+                "");
+        }
+
+        //-----------------------        
+        private static String LongestConsecSol(string[] strarr, int k)
+        {
+            int n = strarr.Length;
+            if ((n == 0) || (k > n) || (k <= 0)) return "";
+            int[] arr = strarr.Select(w => w.Length).ToArray();
+            int sm = 0;
+            for (int i = 0; i < k; i++) sm += arr[i];
+            int mx_sm = sm, mx_nd = k - 1;
+            for (int u = k; u < n; u++)
+            {
+                sm = sm + arr[u] - arr[u - k];
+                if (sm > mx_sm)
+                {
+                    mx_sm = sm;
+                    mx_nd = u;
+                }
+            }
+
+            int start = mx_nd - k + 1;
+            string[] sl = new List<string>(strarr).GetRange(start, k).ToArray();
+            return String.Join("", sl);
+        }
+
+        private static string[] DoEx(int k)
+        {
+            int j = 0;
+            string[] a1 = new string[k];
+            while (j < k)
+            {
+                string res = "";
+                int n = -1;
+                for (int i = 0; i < rnd.Next(3, 10); i++)
+                {
+                    n = rnd.Next(97, 122);
+                    for (int u = 0; u < rnd.Next(1, 3); u++)
+                        res += (char) n;
+                }
+
+                a1[j] = res;
+                j++;
+            }
+
+            return a1;
+        }
+
+        //-----------------------
+        private static Random rnd = new Random();
+
+        [Test]
+        public static void RandomTest1()
+        {
+            Console.WriteLine("200 Random Tests");
+            for (int i = 0; i < 200; i++)
+            {
+                String[] s1 = DoEx(rnd.Next(100, 200));
+                int n = rnd.Next(1, s1.Length + 2);
+                testing(ConsecutiveStrings.LongestConsec(s1, n), LongestConsecSol(s1, n));
+            }
+        }
+    }
+}
