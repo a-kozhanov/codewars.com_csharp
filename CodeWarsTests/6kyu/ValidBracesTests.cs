a2108864d45c348398b157563df6d0f659bb8bb1
a-kozhanov <a.kozhanov@outlook.com>
2020-08-08 15:36:31 +0300

    Added solution "Valid Braces" 6 kyu

---
+++ CodeWarsTests/6kyu/ValidBracesTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ValidBracesTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, ValidBraces.validBraces("()"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(false, ValidBraces.validBraces("[(])"));
+        }
+    }
+}
