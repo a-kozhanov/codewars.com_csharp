34e021b1301e8a6668436d358d82f85218c8781e
a-kozhanov <a.kozhanov@outlook.com>
2020-08-13 22:06:38 +0300

    Added solution "Simple remove duplicates" 7 kyu

---
+++ CodeWars/7kyu/SimpleRemoveDuplicates.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/5ba38ba180824a86850000f7/csharp
+
+7 kyu
+Simple remove duplicates
+
+In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
+
+// Remove the 3's at indices 0 and 3
+// followed by removing a 4 at index 1
+solve([3, 4, 4, 3, 6, 3]); // => [4, 6, 3]
+More examples can be found in the test cases.
+
+Good luck!
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleRemoveDuplicates
+    {
+        public static int[] solve(int[] arr)
+        {
+            //return arr.Reverse().GroupBy(x => x).Select(gr => gr.Key).Reverse().ToArray();
+            //return arr.Distinct().OrderBy(x => arr.ToList().LastIndexOf(x)).ToArray();
+            //return new HashSet<int>(arr.Reverse()).Reverse().ToArray();
+            //return arr.Reverse().Distinct().Reverse().ToArray();
+            return arr.Reverse().Union(arr).Reverse().ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleRemoveDuplicatesTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleRemoveDuplicatesTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(new int[] {4, 6, 3}, SimpleRemoveDuplicates.solve(new int[] {3, 4, 4, 3, 6, 3}));
+            Assert.AreEqual(new int[] {1, 2, 3}, SimpleRemoveDuplicates.solve(new int[] {1, 2, 1, 2, 1, 2, 3}));
+            Assert.AreEqual(new int[] {1, 2, 3, 4}, SimpleRemoveDuplicates.solve(new int[] {1, 2, 3, 4}));
+            Assert.AreEqual(new int[] {4, 5, 2, 1}, SimpleRemoveDuplicates.solve(new int[] {1, 1, 4, 5, 1, 2, 1}));
+        }
+    }
+}
