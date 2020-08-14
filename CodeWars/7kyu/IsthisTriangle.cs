aeea7bfb1d0cbadbb78096b04964886f4f5a2187
a-kozhanov <a.kozhanov@outlook.com>
2020-08-14 11:39:46 +0300

    Added solution "Is this a triangle?" 7 kyu

---
+++ CodeWars/7kyu/IsthisTriangle.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/56606694ec01347ce800001b/csharp
+
+7 kyu
+Is this a triangle?
+
+Implement a method that accepts 3 integer values a, b, c. 
+The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+More info:
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class IsthisTriangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+            //public static bool IsTriangle(params int[] sides)
+        {
+            return a + b > c && b + c > a && a + c > b;
+
+            // var longest = sides.Max();
+            // return sides.Sum() - longest > longest;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/IsthisTriangleTests.cs
@@ -0,0 +1,39 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IsthisTriangleTests
+    {
+        [Test]
+        public void IsTriangle_ValidPostiveNumbers_ReturnsTrue()
+        {
+            Assert.IsTrue(IsthisTriangle.IsTriangle(5, 7, 10));
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 2)]
+        public void IsTriangle_SumOfSidesEqualOtherSide_ReturnsFalse(int a, int b, int c)
+        {
+            Assert.IsFalse(IsthisTriangle.IsTriangle(a, b, c));
+        }
+
+        [TestCase(-1, 2, 3)]
+        [TestCase(1, -2, 3)]
+        [TestCase(1, 2, -3)]
+        public void IsTriangle_NegativeSides_ReturnsFalse(int a, int b, int c)
+        {
+            Assert.IsFalse(IsthisTriangle.IsTriangle(a, b, c));
+        }
+
+        [TestCase(1, 2, 4)]
+        [TestCase(4, 1, 2)]
+        [TestCase(1, 4, 2)]
+        public void IsTriangle_PositiveSides_ReturnsFalse(int a, int b, int c)
+        {
+            Assert.IsFalse(IsthisTriangle.IsTriangle(a, b, c));
+        }
+    }
+}
