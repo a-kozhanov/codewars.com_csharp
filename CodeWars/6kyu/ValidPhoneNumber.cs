398cd9f4389e929de2cf3e23ddd92df0af2c59cc
a-kozhanov <a.kozhanov@outlook.com>
2020-08-20 10:46:47 +0300

    Added solution "Valid Phone Number" 6 kyu

---
+++ CodeWars/6kyu/ValidPhoneNumber.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/525f47c79f2f25a4db000025/csharp
+
+6 kyu
+Valid Phone Number
+
+Write a function that accepts a string, and returns true if it is in the form of a phone number.
+Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+Only worry about the following format:
+(123) 456-7890 (don't forget the space after the close parentheses)
+
+Examples:
+validPhoneNumber("(123) 456-7890")  =>  returns true
+validPhoneNumber("(1111)555 2345")  => returns false
+validPhoneNumber("(098) 123 4567")  => returns false
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataValidPhoneNumber
+    {
+        public static bool ValidPhoneNumber(string phoneNumber)
+        {
+            //return "(000) 000-0000" == string.Concat(phoneNumber.Select(x => char.IsDigit(x) ? '0' : x));
+            //return Regex.Match(phoneNumber, @"^\((\d{3})\)\s\d{3}[-]\d{4}$").Success;
+            //return Regex.Replace(phoneNumber, @"\d", "#").Equals(@"(###) ###-####");
+            //return new Regex(@"^\(\d{3}\) \d{3}-\d{4}$").IsMatch(phoneNumber);
+            //return Regex.IsMatch(phoneNumber, @"^\(\d{3}\) \d{3}-\d{4}\z");
+            return Regex.IsMatch(phoneNumber, @"^\(\d{3}\) \d{3}-\d{4}$");
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/ValidPhoneNumberTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataValidPhoneNumberTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.IsTrue(KataValidPhoneNumber.ValidPhoneNumber("(123) 456-7890"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("(1111)5X5 2345"));
+        }
+
+        [Test]
+        public void FixedTests()
+        {
+            Assert.IsTrue(KataValidPhoneNumber.ValidPhoneNumber("(123) 456-7890"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("(1111)555 2345"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("(098) 123 4567"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("(123)456-7890"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("abc(123)456-7890"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("(123)456-7890abc"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("abc(123)456-7890abc"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("abc(123) 456-7890"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("(123) 456-7890abc"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("abc(123) 456-7890abc"));
+            Assert.IsFalse(KataValidPhoneNumber.ValidPhoneNumber("xyz(123) 456-7890xyz"));
+        }
+
+        private static Random rnd = new Random();
+
+        private static string generatePhoneNumber()
+        {
+            return
+                $"({(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}) {(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}-{(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}{(char) rnd.Next(48, 58)}";
+        }
+
+        private static string generateFakePhoneNumber()
+        {
+            return String.Concat(generatePhoneNumber().OrderBy(_ => rnd.Next()));
+        }
+
+        private static bool solution(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, @"^\(\d{3}\) \d{3}-\d{4}$");
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            const int Tests = 1000;
+
+            for (int i = 0; i < Tests; ++i)
+            {
+                string phoneNumber =
+                    new Func<string>[] {generatePhoneNumber, generateFakePhoneNumber}[rnd.Next(0, 2)]();
+
+                bool expected = solution(phoneNumber);
+                bool actual = KataValidPhoneNumber.ValidPhoneNumber(phoneNumber);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
