1f27e29257fa715141a118aae2e3b1066291ac12
a-kozhanov <a.kozhanov@outlook.com>
2020-08-21 23:42:43 +0300

    Added solution "Digital cypher" 7 kyu

---
+++ CodeWarsTests/7kyu/DigitalCypherTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DigitalCypherTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(new int[] {20, 12, 18, 30, 21}, DigitalCypher.Encode("scout", 1939));
+            Assert.AreEqual(new int[] {14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8}, DigitalCypher.Encode("masterpiece", 1939));
+       
+        }
+    }
+}
