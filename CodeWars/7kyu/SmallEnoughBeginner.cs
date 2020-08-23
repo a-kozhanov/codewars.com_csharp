61448ef42478f84614e7fa7971b8f0d36dcfafbf
a-kozhanov <a.kozhanov@outlook.com>
2020-08-23 22:51:53 +0300

    Added solution "Small enough? - Beginner" 7 kyu

---
+++ CodeWars/7kyu/SmallEnoughBeginner.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/57cc981a58da9e302a000214/csharp
+
+7 kyu
+Small enough? - Beginner
+
+You will be given an array and a limit value. 
+You must check that all values in the array are below or equal to the limit value. 
+If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SmallEnoughBeginner
+    {
+        public static bool SmallEnough(int[] a, int limit)
+        {
+            //return a.Count(x => x <= limit) == a.Length;
+            //return Array.TrueForAll(a, x => x <= limit);
+            //return a.Count(x => x > limit) == 0;
+            //return !a.Any(x => x > limit);
+            //return a.All(x => x <= limit);
+            return limit >= a.Max();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SmallEnoughBeginnerTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SmallEnoughBeginnerTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(true, SmallEnoughBeginner.SmallEnough(new int[] {66, 101}, 200));
+
+            Assert.AreEqual(false,
+                SmallEnoughBeginner.SmallEnough(new int[] {78, 117, 110, 99, 104, 117, 107, 115}, 100));
+
+            Assert.AreEqual(true, SmallEnoughBeginner.SmallEnough(new int[] {101, 45, 75, 105, 99, 107}, 107));
+
+            Assert.AreEqual(true,
+                SmallEnoughBeginner.SmallEnough(new int[] {80, 117, 115, 104, 45, 85, 112, 115}, 120));
+        }
+    }
+}
