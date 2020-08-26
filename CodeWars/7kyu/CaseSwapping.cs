2b2074898ae922ef413c911e623201769383e521
a-kozhanov <a.kozhanov@outlook.com>
2020-08-26 18:46:03 +0300

    Added solution "Case swapping" 7 kyu

---
+++ CodeWars/7kyu/CaseSwapping.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/5590961e6620c0825000008f/csharp
+
+7 kyu
+Case swapping
+
+Given a string, swap the case for each of the letters.
+
+e.g. CodEwArs --> cODeWaRS
+
+Examples
+""           ->   ""
+"CodeWars"   ->   "cODEwARS"
+"abc"        ->   "ABC"
+"ABC"        ->   "abc"
+"123235"     ->   "123235"
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class CaseSwapping
+    {
+        public static string Swap(string str)
+        {
+            //return string.Concat(string.Empty, str.Select(c => c >= 'a' && c <= 'z' ? char.ToUpper(c) : char.ToLower(c)));
+            //return str.Aggregate("", (s, c) => s + (char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)));
+            //return string.Concat(str.Select(x => char.IsUpper(x) ? char.ToLower(x) : char.ToUpper(x)));
+            //return string.Concat(str.Select(x => char.IsLetter(x) ? (char) (x ^ ' ') : x));
+            return string.Concat(str.Select(c => char.IsLetter(c) ? (char) (c ^ 32) : c));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CaseSwappingTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CaseSwappingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("hELLOwORLD", CaseSwapping.Swap("HelloWorld"));
+            Assert.AreEqual("cODEwARS", CaseSwapping.Swap("CodeWars"));
+        }
+    }
+}
