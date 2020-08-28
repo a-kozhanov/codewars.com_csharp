e7fd46fc51e6254c81130342a349a2b38140ae1b
a-kozhanov <a.kozhanov@outlook.com>
2020-08-28 19:14:22 +0300

    Added solution "Convert string to camel case" 6 kyu

---
+++ CodeWars/6kyu/ConvertStringToCamelCase.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/517abf86da9663f1d2000003/csharp
+
+6 kyu
+Convert string to camel case
+
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+The first word within the output should be capitalized only if the original word was capitalized 
+(known as Upper Camel Case, also often referred to as Pascal case).
+
+Examples
+Kata.ToCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+Kata.ToCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ConvertStringToCamelCase
+    {
+        public static string ToCamelCase(string str)
+        {
+            //return Regex.Replace(str, @"[_-](\w)", m => m.Groups[1].Value.ToUpper());
+            //return Regex.Replace(str, @"(-|_)(\w)", m => m.Value.ToUpper().Substring(1));
+            return string.Concat(str.Split('-', '_').Select((x, i) => i > 0 ? char.ToUpper(x[0]) + x[1..] : x));
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/ConvertStringToCamelCaseTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertStringToCamelCaseTests
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("theStealthWarrior", ConvertStringToCamelCase.ToCamelCase("the_stealth_warrior"),
+                "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+
+            Assert.AreEqual("TheStealthWarrior", ConvertStringToCamelCase.ToCamelCase("The-Stealth-Warrior"),
+                "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+        }
+    }
+}
