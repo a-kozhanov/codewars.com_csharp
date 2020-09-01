ccbb788a11c185de464e449c79d587f2a915349a
a-kozhanov <a.kozhanov@outlook.com>
2020-09-01 18:10:39 +0300

    Added solution "Scramblies" 5 kyu

---
+++ CodeWars/5kyu/Scramblies.cs
@@ -0,0 +1,46 @@
+﻿/*
+https://www.codewars.com/kata/55c04b4cc56a697bb0000048/csharp
+
+5 kyu
+Scramblies
+
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered
+Input strings s1 and s2 are null terminated.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+
+
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataScramblies
+    {
+        public static bool Scramble(string str1, string str2)
+        {
+            //var s = str1.ToList();
+            //return str2.All(c => s.Remove(c));
+
+            //return str2.All(x => str1.Count(y => y == x) >= str2.Count(y => y == x));
+            //return str2.ToLookup(x => x).All(c => str1.ToLookup(e => e)[c.Key].Count() >= c.Count());
+            //return str1.Aggregate(str2, (current, c) => new Regex(c.ToString()).Replace(current, "", 1)) == "";
+            //return str2.GroupBy(c => c).All(g => str1.Where(c => c == g.Key).Count() >= g.Count());
+            //return str2.GroupBy(c => c).All(g => str1.Count(c => c == g.Key) >= g.Count());
+
+            //return str1.Aggregate(str2, (current, c) => new Regex($"{c}").Replace(current, "", 1)) == "";
+            //return str1.Aggregate(str2, (s, c) => new Regex($"{c}").Replace(s, "", 1)) == "";
+            //return !str1.Aggregate(str2, (s, c) => new Regex($"{c}").Replace(s, "", 1)).Any();
+            return !str1.Aggregate(str2, (s, c) => new Regex($"{c}").Replace(s, "", 1)).Any();
+        }
+    }
+}
---
+++ CodeWarsTests/5kyu/ScrambliesTests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataScrambliesTests
+    {
+        private static void testing(bool actual, bool expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            testing(KataScramblies.Scramble("rkqodlw", "world"), true);
+            testing(KataScramblies.Scramble("cedewaraaossoqqyt", "codewars"), true);
+            testing(KataScramblies.Scramble("katas", "steak"), false);
+            testing(KataScramblies.Scramble("scriptjavx", "javascript"), false);
+            testing(KataScramblies.Scramble("scriptingjava", "javascript"), true);
+            testing(KataScramblies.Scramble("scriptsjava", "javascripts"), true);
+            testing(KataScramblies.Scramble("javscripts", "javascript"), false);
+            testing(KataScramblies.Scramble("aabbcamaomsccdd", "commas"), true);
+            testing(KataScramblies.Scramble("commas", "commas"), true);
+            testing(KataScramblies.Scramble("sammoc", "commas"), true);
+        }
+    }
+}
