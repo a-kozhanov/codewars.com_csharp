49d8088bb2ea400de9b424889a42455bce34b5c4
a-kozhanov <a.kozhanov@outlook.com>
2020-09-02 21:29:24 +0300

    Added solution "Where my anagrams at?" 5 kyu

---
+++ CodeWars/5kyu/WhereMyAnagramsAt.cs
@@ -0,0 +1,40 @@
+﻿/*
+https://www.codewars.com/kata/523a86aa4230ebb5420001e1/csharp
+
+5 kyu
+Where my anagrams at?
+
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. 
+For example:
+'abba' & 'baab' == true
+'abba' & 'bbaa' == true
+'abba' & 'abbba' == false
+'abba' & 'abca' == false
+
+Write a function that will find all the anagrams of a word from a list. 
+You will be given two inputs a word and an array with words. 
+You should return an array of all the anagrams or an empty array if there are none. 
+
+For example:
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class WhereMyAnagramsAt
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            //return words.Where(s => string.Concat(s.OrderBy(c => c)) == string.Concat(word.OrderBy(c => c))).ToList();
+            //return words.FindAll(s => string.Concat(s.OrderBy(c => c)) == string.Concat(word.OrderBy(c => c)));
+            //return words.Where(s => s.OrderBy(c => c).SequenceEqual(word.OrderBy(c => c))).ToList();
+            return words.FindAll(s => s.OrderBy(c => c).SequenceEqual(word.OrderBy(c => c)));
+        }
+    }
+}
---
+++ CodeWarsTests/5kyu/WhereMyAnagramsAtTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WhereMyAnagramsAtTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new List<string> {"a"},
+                WhereMyAnagramsAt.Anagrams("a", new List<string> {"a", "b", "c", "d"}));
+            
+            Assert.AreEqual(new List<string> {"carer", "arcre", "carre"},
+                WhereMyAnagramsAt.Anagrams("racer",
+                    new List<string>
+                        {"carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr"}));
+        }
+    }
+}
