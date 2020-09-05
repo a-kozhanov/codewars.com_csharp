c413bb337b069bf3ff0e21339673987d43b8b930
a-kozhanov <a.kozhanov@outlook.com>
2020-09-05 11:48:21 +0300

    Added solution "Binary Addition" 7 kyu

---
+++ CodeWars/7kyu/BinaryAddition.cs
@@ -0,0 +1,24 @@
+﻿/*
+https://www.codewars.com/kata/551f37452ff852b7bd000139/csharp
+
+7 kyu
+Binary Addition
+
+Implement a function that adds two numbers together and returns their sum in binary. 
+The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class BinaryAddition
+    {
+        public static string AddBinary(int a, int b)
+        {
+            return Convert.ToString(a + b, 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BinaryAdditionTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BinaryAdditionTests
+    {
+        [TestFixture]
+        public class AddBinaryTest
+        {
+            [Test]
+            public void TestExample()
+            {
+                Assert.AreEqual("11", BinaryAddition.AddBinary(1, 2), "Should return \"11\" for 1 + 2");
+            }
+        }
+    }
+}
