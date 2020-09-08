2290f65c480eb8ccd6576122a4fca5cfa1ab42c1
a-kozhanov <a.kozhanov@outlook.com>
2020-09-08 22:57:53 +0300

    Added solution "Write Number in Expanded Form" 6 kyu

---
+++ CodeWars/6kyu/WriteNumberInExpandedForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/5842df8ccbd22792a4000245/csharp
+
+6 kyu
+Write Number in Expanded Form
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+Kata.ExpandedForm(12); # Should return "10 + 2"
+Kata.ExpandedForm(42); # Should return "40 + 2"
+Kata.ExpandedForm(70304); # Should return "70000 + 300 + 4"
+NOTE: All numbers will be whole numbers greater than 0.
+
+If you liked this kata, check out part 2!!
+*/
+
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace CodeWars
+{
+    public class WriteNumberInExpandedForm
+    {
+        public static string ExpandedForm(long num)
+        {
+            return string.Join(" + ",
+                $"{num}".Select((c, i) => c + new string('0', $"{num}".Length - i - 1)).Where(x => x[0] != '0'));
+
+            //return string.Join(" + ", $"{num}".Select((c, i) => $"{c}".PadRight($"{num}".Length - i - 1, '0')).Where(x => x[0] != '0'));
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/WriteNumberInExpandedFormTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WriteNumberInExpandedFormTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(WriteNumberInExpandedForm.ExpandedForm(12), Is.EqualTo("10 + 2"));
+            Assert.That(WriteNumberInExpandedForm.ExpandedForm(42), Is.EqualTo("40 + 2"));
+            Assert.That(WriteNumberInExpandedForm.ExpandedForm(70304), Is.EqualTo("70000 + 300 + 4"));
+        }
+    }
+}
