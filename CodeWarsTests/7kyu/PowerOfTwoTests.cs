0298aac98fe2f8b0de269158eece6637d63673f8
a-kozhanov <a.kozhanov@outlook.com>
2020-09-10 18:46:21 +0300

    Added solution "Power of two" 7 kyu

---
+++ CodeWarsTests/7kyu/PowerOfTwoTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataPowerOfTwoTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(2).Returns(true);
+                yield return new TestCaseData(4096).Returns(true);
+                yield return new TestCaseData(5).Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool SampleTest(int n) => KataPowerOfTwo.PowerOfTwo(n);
+    }
+}
