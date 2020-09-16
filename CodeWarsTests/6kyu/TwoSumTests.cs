c9e3b61da09afdb63c4a89733985c4e7d4b72ddc
a-kozhanov <a.kozhanov@outlook.com>
2020-09-16 11:21:21 +0300

    Added solution "Two Sum" 6 kyu

---
+++ CodeWarsTests/6kyu/TwoSumTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataTwoSumTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new[] {0, 2}, KataTwoSum.TwoSum(new[] {1, 2, 3}, 4).OrderBy(a => a).ToArray());
+            Assert.AreEqual(new[] {1, 2}, KataTwoSum.TwoSum(new[] {1234, 5678, 9012}, 14690).OrderBy(a => a).ToArray());
+            Assert.AreEqual(new[] {0, 1}, KataTwoSum.TwoSum(new[] {2, 2, 3}, 4).OrderBy(a => a).ToArray());
+        }
+    }
+}
