6bf2653e402f825aef9e531e27c4e60082b5c1b8
a-kozhanov <a.kozhanov@outlook.com>
2020-09-19 21:58:35 +0300

    Added solution "String array duplicates" 6 kyu

---
+++ CodeWars/6kyu/StringArrayDuplicates.cs
@@ -0,0 +1,45 @@
+﻿/*
+https://www.codewars.com/kata/59f08f89a5e129c543000069/csharp
+
+6 kyu
+String array duplicates
+
+In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+dup(["kelless","keenness"]) = ["keles","kenes"].
+
+Strings will be lowercase only, no spaces. See test cases for more examples.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Alternate capitalization
+
+Vowel consonant lexicon
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class StringArrayDuplicates
+    {
+        public static string[] dup(string[] arr)
+        {
+            return arr.Select(x => Regex.Replace(x, @"(\w)\1+", "$1")).ToArray();
+            //return arr.Select(x => Regex.Replace(x, "(.)\\1+", "$1")).ToArray();
+            //return arr.Select(x => Regex.Replace(x, @"(\S)\1+", "$1")).ToArray();
+
+            //return arr.Select(s => s.Aggregate("", (x, y) => x.EndsWith(y) ? x : x + y)).ToArray();
+
+            // return arr.Select(s => $"{s[0]}" + string.Concat(s.Skip(1).Select((c, i) => c == s[i] ? "" : $"{c}")))
+            //     .ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/StringArrayDuplicatesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StringArrayDuplicatesTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(new String[] {"codewars", "picaniny", "hubububo"},
+                StringArrayDuplicates.dup(new String[] {"ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo"}));
+
+            Assert.AreEqual(new String[] {"abracadabra", "alote", "asese"},
+                StringArrayDuplicates.dup(new String[] {"abracadabra", "allottee", "assessee"}));
+
+            Assert.AreEqual(new String[] {"keles", "kenes"},
+                StringArrayDuplicates.dup(new String[] {"kelless", "keenness"}));
+        }
+    }
+}
