df6092e6390de0cf5fefe0063de3b43e3bddf984
a-kozhanov <a.kozhanov@outlook.com>
2020-09-22 08:46:08 +0300

    Added solution "Check the exam" 8 kyu

---
+++ CodeWars/8kyu/CheckExam.cs
@@ -0,0 +1,50 @@
+﻿/*
+https://www.codewars.com/kata/5a3dd29055519e23ec000074/
+
+8 kyu
+Check the exam
+
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. 
+The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, 
+giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, 
+represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class KataCheckExam
+    {
+        public static int CheckExam(string[] arr1, string[] arr2)
+        {
+            int sum = 0;
+            for (var i = 0; i < arr2.Length; i++)
+            {
+                if (arr1[i] == arr2[i])
+                    sum += 4;
+                else if (arr2[i] == "")
+                    sum += 0;
+                else
+                    sum += -1;
+            }
+
+            return sum > 0 ? sum : 0;
+
+
+            //return Math.Max(arr2.Select((c, i) => c == "" ? 0 : c == arr1[i] ? 4 : -1).Sum(), 0);
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/CheckExamTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataCheckExamTests
+    {
+        [TestCase(new string[] {"a", "a", "b", "b"}, new string[] {"a", "c", "b", "d"}, 6)]
+        [TestCase(new string[] {"a", "a", "c", "b"}, new string[] {"a", "a", "b", ""}, 7)]
+        [TestCase(new string[] {"a", "a", "b", "c"}, new string[] {"a", "a", "b", "c"}, 16)]
+        [TestCase(new string[] {"b", "c", "b", "a"}, new string[] {"", "a", "a", "c"}, 0)]
+        [TestCase(new string[] {"a", "c", "b", "d"}, new string[] {"", "", "", ""}, 0)]
+        public void BasicTests(string[] arr1, string[] arr2, int score)
+        {
+            Assert.AreEqual(score, KataCheckExam.CheckExam(arr1, arr2));
+        }
+
+        [Test]
+        public void RandomTests()
+        {
+            var letsAnswers = new string[] {"a", "b", "c", "d"};
+            var letsStudent = new string[] {"a", "b", "c", "d", ""};
+
+            for (int i = 0; i <= 100; i++)
+            {
+                string[] rndAnswers = new string[letsAnswers.Length];
+                string[] rndStudent = new string[letsAnswers.Length];
+
+                for (int j = 0; j < letsAnswers.Length; j++)
+                {
+                    rndAnswers[j] = letsAnswers[Rnd.Next(0, letsAnswers.Length)];
+                    rndStudent[j] = letsStudent[Rnd.Next(0, letsStudent.Length)];
+                }
+
+                int expected = Solution(rndAnswers, rndStudent);
+                int actual = KataCheckExam.CheckExam(rndAnswers, rndStudent);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        private static readonly Random Rnd = new Random();
+
+        private static int Solution(string[] arr1, string[] arr2)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (arr1[i] == arr2[i])
+                    sum += 4;
+                else if (arr2[i] == "")
+                    sum += 0;
+                else
+                    sum += -1;
+            }
+
+            return sum > 0 ? sum : 0;
+        }
+    }
+}
