c02732fbf8ce97a03e7ee94df6247eeecaede0ce
a-kozhanov <a.kozhanov@outlook.com>
2020-09-26 10:51:53 +0300

    Added solution "Validate my Password" 6 kyu

---
+++ CodeWars/6kyu/ValidateMyPassword.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/59c01248bf10a47bd1000046/csharp
+
+6 kyu
+Validate my Password
+
+I will give you a string. You respond with "VALID" if the string meets the requirements or "INVALID" if it does not.
+
+Passwords must abide by the following requirements:
+More than 3 characters but less than 20.
+Must contain only alphanumeric characters.
+Must contain letters and numbers.
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ValidateMyPassword
+    {
+        public static string validator(string password)
+        {
+            //^(?=.*\d)(?=.*[a-zA-Z])(\w{4,20})$
+            //^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(\w{4,20})$
+            return Regex.IsMatch(password, @"^(?=.*\d)(?=.*[a-zA-Z])(\w{4,20})$") ? "VALID" : "INVALID";
+
+            // return Regex.Match(password, @"^(?=.*\d)(?=.*[a-zA-Z])(\w{4,20})$").Success
+            //     ? "VALID"
+            //     : "INVALID";
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/ValidateMyPasswordTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ValidateMyPasswordTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("VALID", ValidateMyPassword.validator("Username123"));
+            Assert.AreEqual("INVALID", ValidateMyPassword.validator("Username"));
+            Assert.AreEqual("INVALID", ValidateMyPassword.validator("123"));
+            Assert.AreEqual("INVALID", ValidateMyPassword.validator("Username123!"));
+            Assert.AreEqual("INVALID", ValidateMyPassword.validator("ThisPasswordIsTooLong1234"));
+        }
+    }
+}
