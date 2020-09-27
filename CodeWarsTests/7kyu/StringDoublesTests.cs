a6ebe3bc070a0ceae96103d2c50fc17a71096e71
a-kozhanov <a.kozhanov@outlook.com>
2020-09-28 00:49:20 +0300

    Added solution "String doubles" 7 kyu

---
+++ CodeWarsTests/7kyu/StringDoublesTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StringDoublesTests
+    {
+        [Test, Description("Basic Tests")]
+        public void BasicTest()
+        {
+            Assert.That(StringDoubles.Doubles("abbbzz"), Is.EqualTo("ab"));
+            Assert.That(StringDoubles.Doubles("zzzzykkkd"), Is.EqualTo("ykd"));
+            Assert.That(StringDoubles.Doubles("abbcccdddda"), Is.EqualTo("aca"));
+            Assert.That(StringDoubles.Doubles("vvvvvoiiiiin"), Is.EqualTo("voin"));
+            Assert.That(StringDoubles.Doubles("rrrmooomqqqqj"), Is.EqualTo("rmomj"));
+            Assert.That(StringDoubles.Doubles("xxbnnnnnyaaaaam"), Is.EqualTo("bnyam"));
+        }
+    }
+}
