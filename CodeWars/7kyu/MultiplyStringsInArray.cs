5cf928a6bdc3422e5530779166adbc6a39b536fe
a-kozhanov <a.kozhanov@outlook.com>
2020-09-30 09:42:32 +0300

    Added solution "Multiply the strings in the array" 7 kyu

---
+++ CodeWars/7kyu/MultiplyStringsInArray.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/59b2963132779166d2001018/csharp
+
+7 kyu
+Multiply the strings in the array
+
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+Kata.ArrMultiply(new string[] {"9", "6"}) => "54"
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class MultiplyStringsInArray
+    {
+        public static string ArrMultiply(string[] arr)
+        {
+            //return $"{int.Parse(arr[0]) * int.Parse(arr[1])}";
+
+            return arr.Aggregate((a, b) => $"{int.Parse(a) * int.Parse(b)}");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MultiplyStringsInArrayTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MultiplyStringsInArrayTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {new string[] {"4", "5"}}).Returns("20");
+                yield return new TestCaseData(new[] {new string[] {"2", "-5"}}).Returns("-10");
+                yield return new TestCaseData(new[] {new string[] {"9", "0"}}).Returns("0");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string[] arr) => MultiplyStringsInArray.ArrMultiply(arr);
+    }
+}
