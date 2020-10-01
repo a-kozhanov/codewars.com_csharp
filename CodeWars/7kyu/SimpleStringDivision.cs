047d22f527e40ed60f17f5cede460d971a0f0c94
a-kozhanov <a.kozhanov@outlook.com>
2020-10-02 00:19:32 +0300

    Added solution "Simple string division" 7 kyu

---
+++ CodeWars/7kyu/SimpleStringDivision.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/5b83c1c44a6acac33400009a/csharp
+
+7 kyu
+Simple string division
+
+In this Kata, you will be given a number in form of a string and an integer k and 
+your task is to insert k commas into the string and determine which of the partitions is the largest.
+
+For example:
+solve('1234',1) = 234 because ('1','234') or ('12','34') or ('123','4').
+solve('1234',2) = 34 because ('1','2','34') or ('1','23','4') or ('12','3','4'). 
+solve('1234',3) = 4
+solve('2020',1) = 202
+More examples in test cases. Good luck!
+
+Please also try Simple remove duplicates
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleStringDivision
+    {
+        public static long solve(String st, int k)
+        {
+            return Enumerable.Range(0, k + 1).Max(i => long.Parse(string.Concat(st.Skip(i).Take(st.Length - k))));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleStringDivisionTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleStringDivisionTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(23, SimpleStringDivision.solve("123", 1));
+            Assert.AreEqual(234, SimpleStringDivision.solve("1234", 1));
+            Assert.AreEqual(34, SimpleStringDivision.solve("1234", 2));
+            Assert.AreEqual(4, SimpleStringDivision.solve("1234", 3));
+        }
+    }
+}
