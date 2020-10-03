32e36c4d5c4e5a88c5a06f6bf12cedf719e4fe68
a-kozhanov <a.kozhanov@outlook.com>
2020-10-04 00:11:40 +0300

    Added solution "Email Address Obfuscator" 7 kyu

---
+++ CodeWarsTests/7kyu/EmailAddressObfuscatorTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EmailAddressObfuscatorTests
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual("test [at] 123 [dot] com",
+                EmailAddressObfuscator.Obfuscate("test@123.com"));
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual("Code_warrior [at] foo [dot] ac [dot] uk",
+                EmailAddressObfuscator.Obfuscate("Code_warrior@foo.ac.uk"));
+        }
+    }
+}
