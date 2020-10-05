3bb3cc3becc0d748aa833375d92e9d630b2c23bf
a-kozhanov <a.kozhanov@outlook.com>
2020-10-05 10:47:29 +0300

    Added solution "Get the integers between two numbers" 7 kyu

---
+++ CodeWars/7kyu/GetIntegersBetweenTwoNumbers.cs
@@ -0,0 +1,41 @@
+﻿/*
+https://www.codewars.com/kata/598057c8d95a04f33f00004e/csharp
+
+7 kyu
+Get the integers between two numbers
+
+Build a function that can get all the integers between two given numbers.
+
+Example:
+
+(2,9)
+
+Should give you this output back:
+
+[ 3, 4, 5, 6, 7, 8 ]
+
+If startNum is the same as endNum, return an empty array.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class GetIntegersBetweenTwoNumbers
+    {
+        public static int[] Range(int startNum, int endNum)
+        {
+            // var result = new List<int>();
+            // for (var i = startNum + 1; i < endNum; i++)
+            // {
+            //     result.Add(i);
+            // }
+            // return result.ToArray();
+
+            //return endNum <= startNum ? new int[0] : Enumerable.Range(startNum + 1, endNum - startNum - 1).ToArray();
+            return Enumerable.Range(startNum + 1, Math.Max(endNum - startNum - 1, 0)).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GetIntegersBetweenTwoNumbersTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GetIntegersBetweenTwoNumbersTests
+    {
+        private static object[] Basic_Test_Cases = new object[]
+        {
+            new object[] {2, 9, new int[] {3, 4, 5, 6, 7, 8}},
+            new object[] {6, 8, new int[] {7}},
+            new object[] {210, 210, new int[] { }},
+        };
+
+        [Test, TestCaseSource(typeof(GetIntegersBetweenTwoNumbersTests), "Basic_Test_Cases")]
+        public void Basic_Test(int startNum, int endNum, int[] expected)
+        {
+            Assert.AreEqual(expected, GetIntegersBetweenTwoNumbers.Range(startNum, endNum));
+        }
+
+        // Note: Random tests output total user code runtime under its section.
+    }
+}
