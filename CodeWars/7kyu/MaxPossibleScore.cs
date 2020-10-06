9825838fbea0e2da2b937cb1bb35dcd5eb1a866c
a-kozhanov <a.kozhanov@outlook.com>
2020-10-07 00:18:57 +0300

    Added solution "maxPossibleScore" 7 kyu

---
+++ CodeWars/7kyu/MaxPossibleScore.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/59656c69253c365e58000046/csharp
+
+7 kyu
+maxPossibleScore
+
+You're a teacher preparing a test for your students. Each question is worth some number of points. 
+Some of the questions are new to the students, while others are questions they have already seen and practiced. 
+Your scoring system doubles the value of new questions. Your job is to determine the maximum possible score.
+
+Write a function doubleValue that accepts 1) an object of questions (as keys) and points (values) and 2) an array of new questions. 
+The function should return the test's maximum possible score as an integer, where questions that are new are worth double points.
+
+You can assume that all questions are unique.
+
+Questions are case sensitive.
+
+Example: maxPossibleScore({"a": 1, "b": 2, "c": 3}, ["a", "c"]); // 1 * 2 + 2 + 3 * 2 = 10
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataMaxPossibleScore
+    {
+        public static int MaxPossibleScore(Dictionary<object, int> obj, object[] arr)
+        {
+            //return obj.Select(x => Array.IndexOf(arr, x.Key) >= 0 ? x.Value * 2 : x.Value).Sum();
+            //return obj.Sum(i => arr.Contains(i.Key) ? i.Value * 2 : i.Value);//.Sum();
+            //return obj.Select(x => arr.Contains(x.Key) ? x.Value * 2 : x.Value).Sum();
+            //return obj.Sum(x => arr.Contains(x.Key) ? x.Value * 2 : x.Value);
+            //return obj.Keys.Sum(q => (arr.Contains(q) ? 2 : 1) * obj[q]);
+            return obj.Sum(v => v.Value * (arr.Contains(v.Key) ? 2 : 1));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MaxPossibleScoreTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataMaxPossibleScoreTests
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual(1,
+                KataMaxPossibleScore.MaxPossibleScore(new Dictionary<object, int> {{"a", 1}}, new object[] { }));
+            Assert.AreEqual(2,
+                KataMaxPossibleScore.MaxPossibleScore(new Dictionary<object, int> {{"a", 1}}, new object[] {"a"}));
+            Assert.AreEqual(29,
+                KataMaxPossibleScore.MaxPossibleScore(new Dictionary<object, int> {{"a", 1}, {"b", 14}},
+                    new object[] {"b"}));
+            Assert.AreEqual(14,
+                KataMaxPossibleScore.MaxPossibleScore(new Dictionary<object, int> {{"a", 1}, {"b", 2}, {"c", 4}},
+                    new object[] {"a", "b", "c"}));
+            Assert.AreEqual(23,
+                KataMaxPossibleScore.MaxPossibleScore(new Dictionary<object, int> {{"a", 2}, {"b", 5}, {"c", 8}},
+                    new object[] {"c"}));
+        }
+    }
+}
