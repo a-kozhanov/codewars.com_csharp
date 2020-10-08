eabdcfd9e6a462b7955e9acf474c775a7844a0ed
a-kozhanov <a.kozhanov@outlook.com>
2020-10-08 09:26:14 +0300

    Added solution "isEven? - Bitwise Series" 7 kyu

---
+++ CodeWars/7kyu/IsEvenBitwiseSeries.cs
@@ -0,0 +1,38 @@
+﻿/*
+https://www.codewars.com/kata/592a33e549fe9840a8000ba1/csharp
+
+7 kyu
+isEven? - Bitwise Series
+
+Is the number even?
+If the numbers is even return true. If it's odd, return false.
+
+Oh yeah... the following symbols/commands have been disabled!
+
+use of ```%```
+use of ```.even?``` in Ruby
+use of ```mod``` in Python
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class IsEvenBitwiseSeries
+    {
+        public static bool IsEven(int n)
+        {
+            //Math.DivRem(n, 2, out var result);
+            //return result == 0;
+
+            //return Math.IEEERemainder(n, 2) == 0;
+            //return ((double) n / 2).Equals(n / 2);
+            //return Convert.ToString(n, 2).Last() == '0';
+            //return (n >> 1) << 1 == n;
+            //return n / 2 * 2 == n;
+            //return (n & 0x1) == 0;
+            return (n & 1) == 0;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/IsEvenBitwiseSeriesTests.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IsEvenBitwiseSeriesTests
+    {
+        // [Test]
+        // public void IsEven_SourceCode_DoesNotContainModOperator()
+        // {
+        //     var text = File.ReadAllText("/workspace/solution.txt");
+        //     Assert.IsFalse(text.Contains("%"));
+        // }
+
+        [TestCase(true, 2)]
+        [TestCase(false, 3)]
+        [TestCase(true, 14)]
+        [TestCase(false, 15)]
+        [TestCase(true, 26)]
+        [TestCase(false, 27)]
+        public void IsEven_CalledWithAnyInt_ReturnsCorrectResult(bool expected, int n)
+        {
+            var message = $"{n} is ";
+            message += expected ? "even" : "odd";
+            Assert.AreEqual(expected, IsEvenBitwiseSeries.IsEven(n), message);
+        }
+    }
+}
