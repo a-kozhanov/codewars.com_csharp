e12ca9f1115fab693347dbfcc2277c592509f007
a-kozhanov <a.kozhanov@outlook.com>
2020-10-09 11:19:24 +0300

    Added solution "Print count and numbers" 7 kyu

---
+++ CodeWars/7kyu/PrintCountAndNumbers.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/559af787b4b8eac78b000022/csharp
+
+7 kyu
+Print count and numbers
+
+Given a string of integers, count how many times that integer repeats itself, 
+then return a string showing the count and the integer.
+
+Example: countMe('1123') (count_me in Ruby)
+
+Here 1 comes twice so <count><integer> will be "21"
+then 2 comes once so <count><integer> will be "12"
+then 3 comes once so <count><integer> will be "13"
+hence output string will be "211213".
+
+Similarly countMe('211213') will return '1221121113' (1 time 2, 2 times 1, 1 time 2, 1 time 1, 1 time 3)
+
+Return "" for empty, nil or non numeric strings
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class PrintCountAndNumbers
+    {
+        public static string CountMe(string data)
+        {
+            return data.Any(char.IsLetter) ? "" : Regex.Replace(data, @"(.)\1*", m => $"{m.Length}{m.Value[0]}");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/PrintCountAndNumbersTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PrintCountAndNumbersTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("1123").Returns("211213");
+                yield return new TestCaseData("1").Returns("11");
+                yield return new TestCaseData("a").Returns("");
+                yield return new TestCaseData("11").Returns("21");
+                yield return new TestCaseData("a123").Returns("");
+                yield return new TestCaseData("21").Returns("1211");
+                yield return new TestCaseData("1211").Returns("111221");
+                yield return new TestCaseData("12322212223443").Returns("111213321132132413");
+                yield return new TestCaseData("").Returns("");
+                yield return new TestCaseData("123a").Returns("");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string data) => PrintCountAndNumbers.CountMe(data);
+    }
+}
