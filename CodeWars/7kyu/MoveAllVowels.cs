ea462e7aef0f61877d2c88bf7472c8d0e5f43f4f
a-kozhanov <a.kozhanov@outlook.com>
2020-10-11 15:10:38 +0300

    Added solution "Move all vowels" 7 kyu

---
+++ CodeWars/7kyu/MoveAllVowels.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/56bf3287b5106eb10f000899/csharp
+
+7 kyu
+Move all vowels
+
+Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.
+
+Vowels are (in this kata): a, e, i, o, u
+
+Note: all provided input strings are lowercase.
+
+Examples
+"day"    ==>  "dya"
+"apple"  ==>  "pplae"
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class MoveAllVowels
+    {
+        public static string MoveVowel(string input)
+        {
+            //return string.Concat(input.Where(x => !"aeiou".Contains(x))) + string.Concat(input.Where(x => "aeiou".Contains(x)));
+            //return Regex.Replace(input, "[aeiou]", "") + string.Concat(input.Where("aeiou".Contains));
+            //return Regex.Replace(input, "[aeiou]", "") + Regex.Replace(input, "[^aeiou]", "");
+            //return input.OrderBy(c => "aeiou".Contains(c)).Aggregate("", (x, y) => x + y);
+            return string.Concat(input.OrderBy(x => "aeiou".Contains(x)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MoveAllVowelsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MoveAllVowelsTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("day").Returns("dya");
+                yield return new TestCaseData("apple").Returns("pplae");
+                yield return new TestCaseData("peace").Returns("pceae");
+                yield return new TestCaseData("maker").Returns("mkrae");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string input) => MoveAllVowels.MoveVowel(input);
+    }
+}
