edaf740e88fb995d7bd301f66af437234f27c36a
a-kozhanov <a.kozhanov@outlook.com>
2020-10-13 11:04:59 +0300

    Added solution "Exclamation marks series #5: Remove all exclamation marks from the end of words" 7 kyu

---
+++ CodeWars/7kyu/ExclamationMarksSeries5RemoveAllExclamation.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/57faf32df815ebd49e000117/csharp
+
+7 kyu
+Exclamation marks series #5: Remove all exclamation marks from the end of words
+
+Description:
+Remove all exclamation marks from the end of words. Words are separated by spaces in the sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi Hi"
+remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ExclamationMarksSeries5RemoveAllExclamation
+    {
+        public static string Remove(string s)
+        {
+            //return new Regex(@"(?<=\w)!+").Replace(s, "");
+            //return new Regex(@"\b!+").Replace(s, "");
+            //return Regex.Replace(s, @"\b!+", "");
+            //return Regex.Replace(s, @"\b!*", "");
+            return Regex.Replace(s, @"\b!+", "");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ExclamationMarksSeries5RemoveAllExclamationTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExclamationMarksSeries5RemoveAllExclamationTests
+    {
+        [Test, Description("It should work for basic tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Hi", ExclamationMarksSeries5RemoveAllExclamation.Remove("Hi!"));
+            Assert.AreEqual("Hi", ExclamationMarksSeries5RemoveAllExclamation.Remove("Hi!!!"));
+            Assert.AreEqual("!Hi", ExclamationMarksSeries5RemoveAllExclamation.Remove("!Hi"));
+            Assert.AreEqual("!Hi", ExclamationMarksSeries5RemoveAllExclamation.Remove("!Hi!"));
+            Assert.AreEqual("Hi Hi", ExclamationMarksSeries5RemoveAllExclamation.Remove("Hi! Hi"));
+            Assert.AreEqual("!!!Hi !!hi !hi", ExclamationMarksSeries5RemoveAllExclamation.Remove("!!!Hi !!hi!!! !hi"));
+        }
+    }
+}
