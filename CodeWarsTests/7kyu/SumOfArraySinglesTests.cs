79f9ad663c282de3e5723203b87c3d90ec76acfe
a-kozhanov <a.kozhanov@outlook.com>
2020-11-06 20:03:09 +0300

    Updated test "SumOfArraySinglesTests"

--- CodeWarsTests/7kyu/SumOfArraySinglesTests.cs
+++ CodeWarsTests/7kyu/SumOfArraySinglesTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CodeWars;
 using NUnit.Framework;
 
@@ -15,6 +17,68 @@ namespace CodeWarsTests
             Assert.AreEqual(12, SumOfArraySingles.Repeats(new List<int> {16, 0, 11, 4, 8, 16, 0, 11}));
             Assert.AreEqual(22, SumOfArraySingles.Repeats(new List<int> {5, 17, 18, 11, 13, 18, 11, 13}));
             Assert.AreEqual(24, SumOfArraySingles.Repeats(new List<int> {5, 10, 19, 13, 10, 13}));
+
+            //Assert.AreEqual(13, SumOfArraySingles.Repeats(new List<int> {7, 9, 9, 9, 9, 9, 9, 6}));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; ++i)
+            {
+                // Init List
+                List<int> source = new List<int>();
+                for (int j = 0; j < 100; ++j)
+                {
+                    source.Add(rnd.Next(0, 1000));
+                }
+
+                source = source.Distinct().ToList();
+
+                // Unique check
+                while (source.Count <= 4)
+                {
+                    source = source.Concat(source.Select(x => x + 1000)).Distinct().ToList();
+                }
+
+                // Dup every number but last two
+                source = source.Concat(source.Take(source.Count - 2)).ToList();
+
+                // Shuffle list (Fisher-Yates Shuffle)
+                int n = source.Count;
+                for (int j = 0; j < n; j++)
+                {
+                    int r = j + rnd.Next(n - j);
+                    int t = source[r];
+                    source[r] = source[j];
+                    source[j] = t;
+                }
+
+                Assert.AreEqual(SolutionRepeats(source), SumOfArraySingles.Repeats(source));
+            }
+        }
+
+        public static int SolutionRepeats(List<int> source)
+        {
+            var seen = new HashSet<int>();
+            int sum = 0;
+
+            foreach (int item in source)
+            {
+                if (!seen.Contains(item))
+                {
+                    seen.Add(item);
+                    sum += item;
+                }
+                else
+                {
+                    sum -= item;
+                }
+            }
+
+            return sum;
         }
     }
 }
