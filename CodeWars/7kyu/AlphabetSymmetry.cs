5d7847cdc26c66a7e8c6f648cba568bc9e7a0376
a-kozhanov <a.kozhanov@outlook.com>
2020-11-06 19:51:57 +0300

    Added solution "Alphabet symmetry" 7 kyu

---
+++ CodeWars/7kyu/AlphabetSymmetry.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0/csharp
+
+7 kyu
+Alphabet symmetry
+
+Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. 
+In the alphabet, a and b are also in positions 1 and 2. 
+Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+See test cases for more examples.
+
+Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Last digit symmetry
+
+Alternate capitalization
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class AlphabetSymmetry
+    {
+        public static List<int> Solve(List<string> arr)
+        {
+            //return arr.Select(v => v.Where((c, i) => char.ToLower(c) - 97 == i).Count()).ToList();
+            //return arr.Select(s => s.ToUpper().Where((c, i) => c - 65 == i).Count()).ToList();
+            //return arr.Select(s => s.Where((c, i) => (c & 31) == i + 1).Count()).ToList();
+            return arr.Select(s => s.Where((c, i) => c % 32 == i + 1).Count()).ToList();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/AlphabetSymmetryTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AlphabetSymmetryTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.That(AlphabetSymmetry.Solve(new List<string> {"abode", "ABc", "xyzD"}),
+                Is.EqualTo(new List<int> {4, 3, 1}));
+
+            Assert.That(AlphabetSymmetry.Solve(new List<string> {"abide", "ABc", "xyz"}),
+                Is.EqualTo(new List<int> {4, 3, 0}));
+
+            Assert.That(AlphabetSymmetry.Solve(new List<string> {"IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"}),
+                Is.EqualTo(new List<int> {6, 5, 7}));
+
+            Assert.That(AlphabetSymmetry.Solve(new List<string> {"encode", "abc", "xyzD", "ABmD"}),
+                Is.EqualTo(new List<int> {1, 3, 1, 3}));
+        }
+    }
+}
