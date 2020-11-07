82effb8820cd2f1d93e7e9fe367065b7f6e87503
a-kozhanov <a.kozhanov@outlook.com>
2020-11-07 14:27:34 +0300

    Added solution "Insert dashes" 7 kyu

---
+++ CodeWars/7kyu/InsertDashes.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/55960bbb182094bc4800007b/csharp
+
+7 kyu
+Insert dashes
+
+Write a function insertDash(num)/InsertDash(int num) that will insert dashes ('-') between each two odd numbers in num. 
+For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd number.
+
+Note that the number will always be non-negative (>= 0).
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class InsertDashes
+    {
+        public static string InsertDash(int num)
+        {
+            return Regex.Replace($"{num}", "([1,3,5,7,9])(?=[1,3,5,7,9])", "$1-");
+
+            //return Regex.Replace(num.ToString(), "[13579](?=[13579])", x => x.Value + "-");
+
+            // return num.ToString().Aggregate("0",
+            //     (a, b) => int.Parse(b.ToString()) % 2 == 1 && int.Parse(a[^1].ToString()) % 2 == 1
+            //         ? a + "-" + b
+            //         : a + b)[1..];
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/InsertDashesTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class InsertDashesTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("4547-9-3", InsertDashes.InsertDash(454793));
+            Assert.AreEqual("123456", InsertDashes.InsertDash(123456));
+            Assert.AreEqual("1003-567", InsertDashes.InsertDash(1003567));
+        }
+    }
+}
