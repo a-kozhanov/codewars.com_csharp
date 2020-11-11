1ff0cfbf235de75cd5c0bedaab7f8e96bbaea942
a-kozhanov <a.kozhanov@outlook.com>
2020-11-11 22:04:06 +0300

    Added solution "Sum and Multiply" 7 kyu

---
+++ CodeWars/7kyu/SumAndMultiply.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/59971206e06bbf4407002382/csharp
+
+7 kyu
+Sum and Multiply
+
+Write a function that accepts two parameters (sum and multiply) and find two numbers [x, y], where x + y = sum and x * y = multiply.
+
+Example:
+
+sum = 12 and multiply = 32
+
+In this case, x equals 4 and y equals 8.
+
+x = 4
+
+y = 8
+
+Because
+
+x + y = 4 + 8 = 12 = sum
+
+x * y = 4 * 8 = 32 = multiply
+
+The result should be [4, 8].
+
+Note:
+
+0 <= x <= 1000
+
+0 <= y <= 1000
+
+If there is no solution, your function should return null (or None in python).
+
+You should return an array (list in python) containing the two values [x, y] and it should be sorted in ascending order.
+
+One last thing: x and y are integers (no decimals).
+*/
+
+namespace CodeWars
+{
+    public class KataSumAndMultiply
+    {
+        public static int[] SumAndMultiply(int sum, int multiply)
+        {
+            for (var i = 0; i <= sum / 2; ++i)
+            {
+                if ((sum - i) * i == multiply) return new[] {i, sum - i};
+            }
+
+            return null;
+            
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SumAndMultiplyTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataSumAndMultiplyTests
+    {
+        private static object[] testCases = new object[]
+        {
+            new object[] {13, 12, new int[] {1, 12}},
+            new object[] {6, 9, new int[] {3, 3}},
+            new object[] {200, 8458, null},
+            new object[] {15, 56, new int[] {7, 8}},
+        };
+
+        [Test, TestCaseSource("testCases")]
+        public void Test(int sum, int multiply, int[] expected) =>
+            Assert.AreEqual(expected, KataSumAndMultiply.SumAndMultiply(sum, multiply));
+    }
+}
