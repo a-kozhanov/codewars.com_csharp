ff45267c3d8d61d18016d35cbfeed85d0ea74673
a-kozhanov <a.kozhanov@outlook.com>
2020-11-15 12:11:44 +0300

    Added solution "Consecutive items" 7 kyu

---
+++ CodeWars/7kyu/ConsecutiveItems.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/5f6d533e1475f30001e47514/csharp
+
+7 kyu
+Consecutive items
+
+You are given a list of unique integers arr, and two integers a and b. 
+Your task is to find out whether or not a and b appear consecutively in arr, 
+and return a boolean value (True if a and b are consecutive, False otherwise).
+
+It is guaranteed that a and b are both present in arr.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ConsecutiveItems
+    {
+        public static bool Consecutive(int[] arr, int a, int b)
+        {
+            return Math.Abs(arr.ToList().IndexOf(a) - arr.ToList().IndexOf(b)) == 1;
+            //return Math.Abs(Array.IndexOf(arr, a) - Array.IndexOf(arr, b)) == 1;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ConsecutiveItemsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConsecutiveItemsTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(ConsecutiveItems.Consecutive(new int[]{1, 3, 5, 7}, 3, 7), false);
+            Assert.AreEqual(ConsecutiveItems.Consecutive(new int[]{1, 3, 5, 7}, 3, 1), true);
+            Assert.AreEqual(ConsecutiveItems.Consecutive(new int[]{1, 6, 9, -3, 4, -78, 0}, -3, 4), true);
+        }
+    }
+}
