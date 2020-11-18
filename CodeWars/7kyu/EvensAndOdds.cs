f75915dd911596152dbe09dcdbc05ddc2a517955
a-kozhanov <a.kozhanov@outlook.com>
2020-11-18 20:56:51 +0300

    Added solution "Evens and Odds" 7 kyu

---
+++ CodeWars/7kyu/EvensAndOdds.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/583ade15666df5a64e000058/csharp
+
+7 kyu
+Evens and Odds
+
+This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataEvensAndOdds
+    {
+        public static string EvensAndOdds(int num)
+        {
+            //return num % 2 == 0 ? Convert.ToString(num, 2) : Convert.ToString(num, 16);
+            //return num % 2 == 0 ? Convert.ToString(num, 2) : num.ToString("x");
+            //return num % 2 == 0 ? Convert.ToString(num, 2) : $"{num:x}";
+            //return Convert.ToString(num, (num & 1) == 1 ? 16 : 2);
+            return Convert.ToString(num, num % 2 == 0 ? 2 : 16);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/EvensAndOddsTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataEvensAndOddsTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("10", KataEvensAndOdds.EvensAndOdds(2));
+            Assert.AreEqual("d", KataEvensAndOdds.EvensAndOdds(13));
+        }
+    }
+}
