21f027988b3c3613cb4f0d64144efb45ed16aa13
a-kozhanov <a.kozhanov@outlook.com>
2020-11-19 15:33:30 +0300

    Added solution "Exclamation marks series #3: Remove all exclamation marks from sentence except at the end" 7 kyu

---
+++ CodeWars/7kyu/ExclamationMarksSeries3RemoveAll.cs
@@ -0,0 +1,38 @@
+﻿/*
+https://www.codewars.com/kata/57faefc42b531482d5000123/csharp
+
+7 kyu
+Exclamation marks series #3: Remove all exclamation marks from sentence except at the end
+
+Description:
+Remove all exclamation marks from sentence except at the end.
+
+Examples
+remove("Hi!") == "Hi!"
+remove("Hi!!!") == "Hi!!!"
+remove("!Hi") == "Hi"
+remove("!Hi!") == "Hi!"
+remove("Hi! Hi!") == "Hi Hi!"
+remove("Hi") == "Hi"
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ExclamationMarksSeries3RemoveAll
+    {
+        public static string Remove(string s)
+        {
+            return Regex.Replace(s, "(?!!*$)!", "");
+            //return Regex.Replace(s, "!(?!!*$)", "");
+            //return Regex.Replace(s, @"!+(?!!*$)", "");
+            //return Regex.Replace(s, @"!+([^!])", "$1");
+            //return Regex.Replace(s, "(?>!+)(?!$)", "");
+            //return Regex.Replace(s, @"(?>!+)(?!$)", "");
+            //return new Regex(@"!+(?!!*$)").Replace(s, "");
+            //return new Regex(@"!+(?!!*$)").Replace(s, "");
+            //return s.Replace("!", "") + Regex.Match(s, @"!+$").Value;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ExclamationMarksSeries3RemoveAllTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExclamationMarksSeries3RemoveAllTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test, Description("It should work for basic tests")]
+            public void SampleTest()
+            {
+                Assert.AreEqual("Hi!", ExclamationMarksSeries3RemoveAll.Remove("Hi!"));
+                Assert.AreEqual("Hi!!!", ExclamationMarksSeries3RemoveAll.Remove("Hi!!!"));
+                Assert.AreEqual("Hi", ExclamationMarksSeries3RemoveAll.Remove("!Hi"));
+                Assert.AreEqual("Hi!", ExclamationMarksSeries3RemoveAll.Remove("!Hi!"));
+                Assert.AreEqual("Hi Hi!", ExclamationMarksSeries3RemoveAll.Remove("Hi! Hi!"));
+                Assert.AreEqual("Hi", ExclamationMarksSeries3RemoveAll.Remove("Hi"));
+            }
+        }
+    }
+}
