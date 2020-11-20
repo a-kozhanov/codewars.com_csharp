29b455ca9961221072fd69ca66556b95c20d5122
a-kozhanov <a.kozhanov@outlook.com>
2020-11-20 15:14:08 +0300

    Added solution "Find The Duplicated Number in a Consecutive Unsorted List" 7 kyu

---
+++ CodeWars/7kyu/FindDuplicatedNumberConsecutiveUnsortedList.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/558dd9a1b3f79dc88e000001/csharp
+
+7 kyu
+Find The Duplicated Number in a Consecutive Unsorted List
+
+
+You are given an array of n+1 integers 1 through n. In addition there is a single duplicate integer.
+
+The array is unsorted.
+
+An example valid array would be [3, 2, 5, 1, 3, 4]. It has the integers 1 through 5 and 3 is duplicated. [1, 2, 4, 5, 5] would not be valid as it is missing 3.
+
+You should return the duplicate value as a single integer.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindDuplicatedNumberConsecutiveUnsortedList
+    {
+        public static int FindDup(int[] arr)
+        {
+            //return arr.GroupBy(x => x).OrderByDescending(g => g.Count()).First().Key;
+            //return arr.GroupBy(x => x).First(x => x.Count() == 2).First();
+            //return arr.Sum() - Enumerable.Range(1, arr.Length - 1).Sum();
+            //return arr.Distinct().First(c => arr.Count(q => q == c) > 1);
+            //return arr.GroupBy(x => x).Single(x => x.Count() == 2).Key;
+            //return arr.GroupBy(x => x).First(x => x.Count() == 2).Key;
+            //return arr.Sum() - arr.Length * (arr.Length - 1) / 2;
+            //return arr.First(n => arr.Count(i => i == n) == 2);
+            //return arr.Sum() - arr.Distinct().Sum();
+            return arr.Sum() - arr.Union(arr).Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FindDuplicatedNumberConsecutiveUnsortedListTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindDuplicatedNumberConsecutiveUnsortedListTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {new int[] {1, 1, 2, 3}}).Returns(1);
+                yield return new TestCaseData(new[] {new int[] {5, 4, 3, 2, 1, 1}}).Returns(1);
+                yield return new TestCaseData(new[] {new int[] {1, 3, 2, 5, 4, 5, 7, 6}}).Returns(5);
+                yield return new TestCaseData(new[] {new int[] {8, 2, 6, 3, 7, 2, 5, 1, 4}}).Returns(2);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(int[] arr) => FindDuplicatedNumberConsecutiveUnsortedList.FindDup(arr);
+    }
+}
