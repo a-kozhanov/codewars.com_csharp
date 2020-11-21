71e8d91a478fce92c9744bb5d26b62e53f52f671
a-kozhanov <a.kozhanov@outlook.com>
2020-11-21 16:45:31 +0300

    Added solution "Largest pair sum in array" 7 kyu

---
+++ CodeWars/7kyu/LargestPairSumInArray.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/556196a6091a7e7f58000018/csharp
+
+7 kyu
+Largest pair sum in array
+
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class LargestPairSumInArray
+    {
+        public static int LargestPairSum(int[] numbers)
+        {
+            //return Enumerable.Range(0, numbers.Length).Select(x => numbers.Take(x).Select(i => i + numbers[x])).SelectMany(x => x).Max();
+            //return numbers.OrderByDescending(i => i).Take(2).Sum();
+            //return numbers.OrderBy(i => i).ToArray()[^2..].Sum();
+            return numbers.OrderBy(i => i).TakeLast(2).Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/LargestPairSumInArrayTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class LargestPairSumInArrayTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(-16, LargestPairSumInArray.LargestPairSum(new int[] {-8, -8, -16, -18, -19}));
+            Assert.AreEqual(0, LargestPairSumInArray.LargestPairSum(new int[] {-100, -29, -24, -19, 19}));
+            Assert.AreEqual(10, LargestPairSumInArray.LargestPairSum(new int[] {1, 2, 3, 4, 6, -1, 2}));
+            Assert.AreEqual(42, LargestPairSumInArray.LargestPairSum(new int[] {10, 14, 2, 23, 19}));
+        }
+    }
+}
