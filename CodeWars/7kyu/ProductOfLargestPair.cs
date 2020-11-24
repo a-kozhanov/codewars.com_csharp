0f4eb7516d5a25354cb376c2a152f12bed7aa525
a-kozhanov <a.kozhanov@outlook.com>
2020-11-24 23:43:44 +0300

    Added solution "Product of Largest Pair" 7 kyu

---
+++ CodeWars/7kyu/ProductOfLargestPair.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/5784c89be5553370e000061b/csharp
+
+7 kyu
+Product of Largest Pair
+
+Rick wants a faster way to get the product of the largest pair in an array. 
+Your task is to create a performant solution to find the product of the largest two integers in a unique array of positive numbers.
+All inputs will be valid.
+Passing [2, 6, 3] should return 18, the product of [6, 3].
+
+Disclaimer: Mr. Roll will only accept solutions that are faster than his, which has a running time O(nlogn).
+
+Kata.MaxProduct(new int[] { 2, 1, 5, 0, 4, 3 });              // 20
+Kata.MaxProduct(new int[] { 7, 8, 9 })     ;                  // 72
+Kata.MaxProduct(new int[] { 33, 231, 454, 11, 9, 99, 57 });   // 104874
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ProductOfLargestPair
+    {
+        public static int MaxProduct(int[] array)
+        {
+            return array.OrderBy(x => -x).Take(2).Aggregate((a, b) => a * b);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ProductOfLargestPairTests.cs
@@ -0,0 +1,38 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ProductOfLargestPairTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(218842, ProductOfLargestPair.MaxProduct(new int[] {56, 335, 195, 443, 6, 494, 252}));
+
+            Assert.AreEqual(194740, ProductOfLargestPair.MaxProduct(new int[] {154, 428, 455, 346}));
+
+            Assert.AreEqual(187827,
+                ProductOfLargestPair.MaxProduct(new int[]
+                    {39, 135, 47, 275, 37, 108, 265, 457, 2, 133, 316, 330, 153, 253, 321, 411}));
+
+            Assert.AreEqual(87984, ProductOfLargestPair.MaxProduct(new int[] {136, 376, 10, 146, 105, 63, 234}));
+
+            Assert.AreEqual(218536,
+                ProductOfLargestPair.MaxProduct(new int[]
+                    {354, 463, 165, 62, 472, 53, 347, 293, 252, 378, 420, 398, 255, 89}));
+
+            Assert.AreEqual(192672,
+                ProductOfLargestPair.MaxProduct(new int[] {346, 446, 26, 425, 432, 349, 123, 269, 285, 93, 75, 14}));
+
+            Assert.AreEqual(95680, ProductOfLargestPair.MaxProduct(new int[] {134, 320, 266, 299}));
+
+            Assert.AreEqual(139496,
+                ProductOfLargestPair.MaxProduct(
+                    new int[] {114, 424, 53, 272, 128, 215, 25, 329, 272, 313, 100, 24, 252}));
+
+            Assert.AreEqual(174750, ProductOfLargestPair.MaxProduct(new int[] {375, 56, 337, 466, 203}));
+        }
+    }
+}
