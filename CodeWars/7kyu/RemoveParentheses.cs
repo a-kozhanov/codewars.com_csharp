082eb5d29f9ed62d51f7383be9a7f3cb289b2eb9
a-kozhanov <a.kozhanov@outlook.com>
2020-11-25 12:35:51 +0300

    Added solution "Remove the parentheses" 7 kyu

---
+++ CodeWars/7kyu/RemoveParentheses.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/5f7c38eb54307c002a2b8cc8/csharp
+
+6 kyu
+Remove the parentheses
+
+Remove the parentheses
+In this kata you are given a string for example:
+
+"example(unwanted thing)example"
+Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+
+The example above would return:
+
+"exampleexample"
+Notes
+Other than parentheses only letters and spaces can occur in the string. Don't worry about other brackets like "[]" and "{}" as these will never appear.
+There can be multiple parentheses.
+The parentheses can be nested.
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataRemoveParentheses
+    {
+        public static string RemoveParentheses(string s)
+        {
+            // return s.Contains('(') ? RemoveParentheses(Regex.Replace(s, @"\([^()]*\)", "")) : s;
+
+            //return Regex.Replace(s, @"(?=\()((?<p>\()|(?<-p>\))|.)+?(?(p)(?!))", "");
+
+            while (s.Contains('('))
+            {
+                s = Regex.Replace(s, @"\([^()]+\)", "");
+            }
+
+            return s;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/RemoveParenthesesTests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RemoveParenthesesTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("exampleexample",
+                KataRemoveParentheses.RemoveParentheses("example(unwanted thing)example"));
+
+            Assert.AreEqual("example  example",
+                KataRemoveParentheses.RemoveParentheses("example (unwanted thing) example"));
+
+            Assert.AreEqual("a e", KataRemoveParentheses.RemoveParentheses("a (bc d)e"));
+
+            Assert.AreEqual("a", KataRemoveParentheses.RemoveParentheses("a(b(c))"));
+
+            Assert.AreEqual("hello example  something",
+                KataRemoveParentheses.RemoveParentheses("hello example (words(more words) here) something"));
+
+            Assert.AreEqual("  ",
+                KataRemoveParentheses.RemoveParentheses("(first group) (second group) (third group)"));
+        }
+    }
+}
