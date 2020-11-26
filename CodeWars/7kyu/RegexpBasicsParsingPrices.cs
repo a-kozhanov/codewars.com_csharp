1a448ecd5a42bc51187c475cd3a2805b7c010e6e
a-kozhanov <a.kozhanov@outlook.com>
2020-11-27 00:04:04 +0300

    Updated solution "Regexp basics - parsing prices" 7 kyu

---
+++ CodeWars/7kyu/RegexpBasicsParsingPrices.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/56833b76371e86f8b6000015/train/csharp
+
+7 kyu
+Regexp basics - parsing prices
+
+Implement String#to_cents, which should parse prices expressed as $1.23 and return number of cents, or in case of bad format return nil.
+*/
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public static class RegexpBasicsParsingPrices
+    {
+        public static int? ToCents(this string price)
+        {
+            var s = Regex.Replace(Regex.Match(price, @"^\$\d{1,}\.\d{2}\z").Value, @"(\.|\$)", "");
+            return s.Any() ? int.Parse(s) : (int?) null;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/RegexpBasicsParsingPricesTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RegexpBasicsParsingPricesTests
+    {
+        [Test]
+        public void _0_BasicTests()
+        {
+            Assert.AreEqual(null, "".ToCents());
+            Assert.AreEqual(null, "1".ToCents());
+            Assert.AreEqual(null, "1.23".ToCents());
+            Assert.AreEqual(null, "$1".ToCents());
+            Assert.AreEqual(123, "$1.23".ToCents());
+            Assert.AreEqual(9999, "$99.99".ToCents());
+            Assert.AreEqual(1234567890, "$12345678.90".ToCents());
+            Assert.AreEqual(969, "$9.69".ToCents());
+            Assert.AreEqual(970, "$9.70".ToCents());
+            Assert.AreEqual(971, "$9.71".ToCents());
+            Assert.AreEqual(69, "$0.69".ToCents());
+            Assert.AreEqual(null, "$9.69$4.3.7".ToCents());
+            Assert.AreEqual(null, "$9.692".ToCents());
+        }
+    }
+}
