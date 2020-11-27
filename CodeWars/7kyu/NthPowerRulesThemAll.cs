76817b40cddff9eae3d8f618144fdd364ad4b450
a-kozhanov <a.kozhanov@outlook.com>
2020-11-27 21:06:27 +0300

    Added solution "Nth power rules them all!" 7 kyu

---
+++ CodeWars/7kyu/NthPowerRulesThemAll.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/58aed2cafab8faca1d000e20/csharp
+
+7 kyu
+Nth power rules them all!
+
+You are provided with an array of positive integers and an additional integer n (n > 1).
+Calculate the sum of each value in the array to the nth power. Then subtract the sum of the original array.
+
+Examples
+{1, 2, 3}, 3  -->  (1^3 + 2^3 + 3^3 ) - (1 + 2 + 3)  -->  36 - 6  -->  30
+{1, 2}, 5     -->  (1^5 + 2^5) - (1 + 2)             -->  33 - 3  -->  30
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NthPowerRulesThemAll
+    {
+        public static int ModifiedSum(int[] a, int n)
+        {
+            //return a.Aggregate(0, (acc, curr) => (int) Math.Pow(curr, n) + acc - curr);
+            //return (int) a.Select(x => Math.Pow(x, n)).Sum() - a.Sum();
+            return (int) a.Sum(x => Math.Pow(x, n) - x);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/NthPowerRulesThemAllTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NthPowerRulesThemAllTests
+    {
+        private static object[] Basic_Test_Cases = new object[]
+        {
+            new object[] {new int[] {1, 2, 3}, 3, 30},
+            new object[] {new int[] {1, 2}, 5, 30},
+        };
+
+        [Test, TestCaseSource(typeof(NthPowerRulesThemAllTests), nameof(Basic_Test_Cases))]
+        public void Basic_Test(int[] array, int exponent, int expected)
+        {
+            Assert.AreEqual(expected, NthPowerRulesThemAll.ModifiedSum(array, exponent),
+                "Solution should work for the examples provided in the Description");
+        }
+    }
+}
