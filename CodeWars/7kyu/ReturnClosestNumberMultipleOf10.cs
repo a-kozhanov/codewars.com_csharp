5ddfe0e523e9f92ada830cee36b6f30e8b49c116
a-kozhanov <a.kozhanov@outlook.com>
2020-11-29 12:48:02 +0300

    Added solution "Return the closest number multiple of 10" 7 kyu

---
+++ CodeWars/7kyu/ReturnClosestNumberMultipleOf10.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/58249d08b81f70a2fc0001a4/csharp
+
+7 kyu
+Return the closest number multiple of 10
+
+Given a number return the closest number to it that is divisible by 10.
+
+Example input:
+22
+25
+37
+
+Expected output:
+20
+30
+40
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class ReturnClosestNumberMultipleOf10
+    {
+        public int ClosestMultiple10(int num)
+        {
+            return (int) (Math.Round(num / 10d, MidpointRounding.AwayFromZero) * 10);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ReturnClosestNumberMultipleOf10Tests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReturnClosestNumberMultipleOf10Tests
+    {
+        private ReturnClosestNumberMultipleOf10 kata = new ReturnClosestNumberMultipleOf10();
+
+        [Test]
+        public void _0_ShouldReturn10ForNumbersFrom10To14()
+        {
+            for (int i = 10; i <= 14; ++i)
+            {
+                Assert.AreEqual(10, kata.ClosestMultiple10(i));
+            }
+        }
+
+        [Test]
+        public void _1_ShouldReturn20ForNumbersFrom15To20()
+        {
+            for (int i = 15; i <= 20; ++i)
+            {
+                Assert.AreEqual(20, kata.ClosestMultiple10(i));
+            }
+        }
+    }
+}
