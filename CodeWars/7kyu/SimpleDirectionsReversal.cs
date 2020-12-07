607512cd780268f2b17e25a8848d3aa483d61f78
a-kozhanov <a.kozhanov@outlook.com>
2020-12-07 09:32:15 +0300

    Add solution "Simple directions reversal" 7 kyu

---
+++ CodeWars/7kyu/SimpleDirectionsReversal.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/5b94d7eb1d5ed297680000ca/solutions/csharp
+
+7 kyu
+Simple directions reversal
+
+In this Kata, you will be given directions and your task will be to find your way back.
+
+solve(["Begin on Road A","Right on Road B","Right on Road C","Left on Road D"]) = ['Begin on Road D', 'Right on Road C', 'Left on Road B', 'Left on Road A']
+solve(['Begin on Lua Pkwy', 'Right on Sixth Alley', 'Right on 1st Cr']) =  ['Begin on 1st Cr', 'Left on Sixth Alley', 'Left on Lua Pkwy']
+More examples in test cases.
+
+Good luck!
+
+Please also try Simple remove duplicates
+*/
+
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleDirectionsReversal
+    {
+        public static string[] solve(string[] arr)
+        {
+            var dirs = arr.Reverse().Select(x => x.StartsWith("Right") ? "Left" : x.StartsWith("Left") ? "Right" : "")
+                .ToArray();
+            return arr.Reverse().Select((x, i) => (i == 0 ? "Begin" : dirs[i - 1]) + x[x.IndexOf(' ')..]).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleDirectionsReversalTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleDirectionsReversalTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(new String[] {"Begin on Road D", "Right on Road C", "Left on Road B", "Left on Road A"},
+                SimpleDirectionsReversal.solve(new String[]
+                    {"Begin on Road A", "Right on Road B", "Right on Road C", "Left on Road D"}));
+
+            Assert.AreEqual(new String[] {"Begin on 9th Dr", "Right on First Road", "Left on 3rd Blvd"},
+                SimpleDirectionsReversal.solve(new String[]
+                    {"Begin on 3rd Blvd", "Right on First Road", "Left on 9th Dr"}));
+
+            Assert.AreEqual(new String[] {"Begin on Road A"},
+                SimpleDirectionsReversal.solve(new String[] {"Begin on Road A"}));
+        }
+    }
+}
