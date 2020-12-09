207b949f4f4a7eb7979896cd6c9c35dfd6d6a2d2
a-kozhanov <a.kozhanov@outlook.com>
2020-12-09 12:19:37 +0300

    Add solution "Simple Fun #20: First Reverse Try" 7 kyu

---
+++ CodeWars/7kyu/SimpleFun20FirstReverseTry.cs
@@ -0,0 +1,49 @@
+﻿/*
+https://www.codewars.com/kata/5886c6b2f3b6ae33dd0000be/csharp
+
+7 kyu
+Simple Fun #20: First Reverse Try
+
+Task
+Reversing an array can be a tough task, especially for a novice programmer. 
+Mary just started coding, so she would like to start with something basic at first. 
+Instead of reversing the array entirely, she wants to swap just its first and last elements.
+
+Given an array arr, swap its first and last elements and return the resulting array.
+
+Example
+For arr = [1, 2, 3, 4, 5], the output should be [5, 2, 3, 4, 1]
+
+Input/Output
+[input] integer array arr
+
+Constraints: 0 ≤ arr.length ≤ 50, -1000 ≤ arr[i] ≤ 1000
+
+[output] an integer array
+
+Array arr with its first and its last elements swapped.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun20FirstReverseTry
+    {
+        public int[] FirstReverseTry(int[] arr)
+        {
+            // return arr.Length > 1
+            //     ? new int[] { }.Concat(new[] {arr.Last()}).Concat(arr.Skip(1).SkipLast(1)).Concat(new[] {arr.First()}).ToArray()
+            //     : arr;
+
+            // return arr.Length > 1
+            //     ? new int[] { }.Concat(new[] {arr[^1]}).Concat(arr[1..^1]).Concat(new[] {arr[0]}).ToArray()
+            //     : arr;
+
+            //return arr.Length > 1 ? arr.Skip(1).SkipLast(1).Prepend(arr.Last()).Append(arr.First()).ToArray() : arr;
+            //return arr.Length > 1 ? arr.Skip(1).SkipLast(1).Prepend(arr[^1]).Append(arr[0]).ToArray() : arr;
+            return arr.Length > 1 ? arr[1..^1].Prepend(arr[^1]).Append(arr[0]).ToArray() : arr;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun20FirstReverseTryTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun20FirstReverseTryTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun20FirstReverseTry();
+
+            Assert.AreEqual(new int[] {5, 2, 3, 4, 1}, kata.FirstReverseTry(new int[] {1, 2, 3, 4, 5}));
+
+            Assert.AreEqual(new int[] { }, kata.FirstReverseTry(new int[] { }));
+
+            Assert.AreEqual(new int[] {111}, kata.FirstReverseTry(new int[] {111}));
+
+            Assert.AreEqual(new int[] {324, 54, 12, 3, 4, 56, 23, 12, 5, 23},
+                kata.FirstReverseTry(new int[] {23, 54, 12, 3, 4, 56, 23, 12, 5, 324}));
+        }
+    }
+}
