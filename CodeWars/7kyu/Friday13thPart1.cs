a781319b5e07a5fa96f8d49626e912062e5437c7
a-kozhanov <a.kozhanov@outlook.com>
2020-12-14 00:05:05 +0300

    Add solution "Friday the 13th Part 1" 7 kyu

---
+++ CodeWars/7kyu/Friday13thPart1.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/5925acf31a9825d616000e74/train/csharp
+
+7 kyu
+Friday the 13th Part 1
+
+It's Friday the 13th, and Jason is ready for his first killing spree!
+
+Create a function, killcount, that accepts two arguments: an array of array pairs (the conselor's name and intelligence - ["Chad", 2]) and an integer representing Jason's intellegence.
+
+Ruby, Python, Crystal:
+
+counselors = [["Chad", 2], ["Tommy", 9]]
+jason = 7
+JavaScript:
+
+var counselors = [["Chad", 2], ["Tommy", 9]];
+var jason = 7;
+PHP:
+
+$counselors = [["Chad", 2], ["Tommy", 9]];
+$jason = 7;
+Your function must return an array of the names of all the counselors who can be outsmarted and killed by Jason.
+
+alt text
+
+Happy Friday the 13th!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class Friday13thPart1
+    {
+        public static string[] KillCount(Dictionary<string, int> counselors, int jason)
+        {
+            return counselors.Where(k => k.Value < jason).Select(k => k.Key).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/Friday13thPart1Tests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class Friday13thPart1Tests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Dictionary<string, int> counselors = new Dictionary<string, int>
+            {
+                {"Mike", 7},
+                {"Alysa", 3}
+            };
+            Assert.AreEqual(new string[] {"Alysa"}, Friday13thPart1.KillCount(counselors, 7));
+        }
+    }
+}
