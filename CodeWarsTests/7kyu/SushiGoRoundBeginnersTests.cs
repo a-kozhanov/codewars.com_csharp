f2fbfd7a97abd20fa592015385698a7099f84a04
a-kozhanov <a.kozhanov@outlook.com>
2020-12-13 16:09:33 +0300

    Add solution "Sushi-go-round (Beginner's)" 7 kyu

---
+++ CodeWarsTests/7kyu/SushiGoRoundBeginnersTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SushiGoRoundBeginnersTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(4, SushiGoRoundBeginners.TotalBill("rr"));
+            Assert.AreEqual(8, SushiGoRoundBeginners.TotalBill("rr rrr"));
+            Assert.AreEqual(16, SushiGoRoundBeginners.TotalBill("rr rrr rrr rr"));
+            Assert.AreEqual(34, SushiGoRoundBeginners.TotalBill("rrrrrrrrrrrrrrrrrr   rr r"));
+            Assert.AreEqual(0, SushiGoRoundBeginners.TotalBill(""));
+        }
+    }
+}
