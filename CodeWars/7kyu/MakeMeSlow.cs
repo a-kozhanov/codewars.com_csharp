f8d07b61e07737f50119a590fc740e0baf5f9d15
a-kozhanov <a.kozhanov@outlook.com>
2020-12-17 20:25:34 +0300

    Add solution "Make Me Slow" 7 kyu

---
+++ CodeWars/7kyu/MakeMeSlow.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/57f59da8d3978bb31f000152/csharp
+
+7 kyu
+Make Me Slow
+
+Make me slow! Calling makeMeSlow() should take at least 7 seconds.
+*/
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class KataMakeMeSlow
+    {
+        public static void MakeMeSlow()
+        {
+            Thread.Sleep(7000);
+            //Task.Delay(7000).GetAwaiter().GetResult();
+            //System.Threading.Thread.Sleep(8000);
+
+            // var end = DateTime.Now.AddMilliseconds(7000);
+            // while (DateTime.Now < end);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MakeMeSlowTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MakeMeSlowTests
+    {
+        [Test]
+        public void Test()
+        {
+            DateTime start = DateTime.Now;
+            KataMakeMeSlow.MakeMeSlow();
+            Assert.IsTrue((DateTime.Now - start).TotalMilliseconds >= 7000);
+        }
+    }
+}
