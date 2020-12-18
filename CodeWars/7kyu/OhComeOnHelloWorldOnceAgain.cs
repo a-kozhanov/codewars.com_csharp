a6e8d0a0130bfdde00a9a5e8ecb2ea546010aee6
a-kozhanov <a.kozhanov@outlook.com>
2020-12-18 23:18:38 +0300

    Add solution "Oh come on! Hello world! once again?" 7 kyu

---
+++ CodeWars/7kyu/OhComeOnHelloWorldOnceAgain.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/594a7ea087a7c6cbe60000d6/csharp
+
+7 kyu
+Oh come on! Hello world! once again?
+
+Task
+Write a function f (in C# F) that returns Hello, world! string. But there is one thing, you cannot use helowrd letters.
+
+The C# test cases ignore checking for banned letters in words class, 
+string and public. The C++ test cases ignore checking for banned letters in words string, std and return.
+
+Have fun!
+*/
+
+namespace CodeWars
+{
+    public class OhComeOnHelloWorldOnceAgain
+    {
+        public static string F()
+        {
+            return "\x48\x65\x6c\x6c\x6f, \x77\x6f\x72\x6c\x64!";
+            //return "\x48\x65\x6c\x6c\x6f\x2c\x20\x77\x6f\x72\x6c\x64\x21";
+            //return "\u0048\u0065\u006c\u006c\u006f\u002c\u0020\u0077\u006f\u0072\u006c\u0064\u0021";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/OhComeOnHelloWorldOnceAgainTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class OhComeOnHelloWorldOnceAgainTests
+    {
+        [Test]
+        public void CheckForHelloWorld()
+        {
+            Assert.AreEqual("Hello, world!", OhComeOnHelloWorldOnceAgain.F());
+        }
+    }
+}
