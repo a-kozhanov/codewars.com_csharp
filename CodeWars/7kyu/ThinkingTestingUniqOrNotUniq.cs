88b071d2c9a21759226f4e28d5209e6a728d9877
a-kozhanov <a.kozhanov@outlook.com>
2020-12-20 16:42:51 +0300

    Add solution "Thinking & Testing : Uniq or not Uniq" 7 kyu

---
+++ CodeWars/7kyu/ThinkingTestingUniqOrNotUniq.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/56d949281b5fdc7666000004/csharp
+
+7 kyu
+Thinking & Testing : Uniq or not Uniq
+
+No Story
+No Description
+Only by Thinking and Testing
+Look at result of testcase, guess the code!
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ThinkingTestingUniqOrNotUniq
+    {
+        public int[] Testit(int[] a, int[] b)
+        {
+            //return Enumerable.Concat(a.Distinct(), b.Distinct()).OrderBy(i => i).ToArray();
+            return a.Distinct().Concat(b.Distinct()).OrderBy(i => i).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ThinkingTestingUniqOrNotUniqTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkingTestingUniqOrNotUniqTests
+    {
+        [Test]
+        public void TestCase()
+        {
+            //return a.Concat(b) ?
+            Assert.AreEqual(new int[] {0, 1}, new ThinkingTestingUniqOrNotUniq().Testit(new int[] {0}, new int[] {1}),
+                "");
+            Assert.AreEqual(new int[] {1, 2, 3, 4},
+                new ThinkingTestingUniqOrNotUniq().Testit(new int[] {1, 2}, new int[] {3, 4}), "");
+            Assert.AreEqual(new int[] {1, 2, 3, 4},
+                new ThinkingTestingUniqOrNotUniq().Testit(new int[] {1}, new int[] {2, 3, 4}), "");
+            Assert.AreEqual(new int[] {1, 2, 3, 4},
+                new ThinkingTestingUniqOrNotUniq().Testit(new int[] {1, 2, 3}, new int[] {4}), "");
+            Assert.AreEqual(new int[] {1, 1, 2, 2},
+                new ThinkingTestingUniqOrNotUniq().Testit(new int[] {1, 2}, new int[] {1, 2}), "");
+            //click "Submit" try more testcase...
+        }
+    }
+}
