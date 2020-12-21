57eb925859c2d455a5f7bbf195bc54a012a2ba29
a-kozhanov <a.kozhanov@outlook.com>
2020-12-21 09:40:38 +0300

    Add solution "Binary scORe" 7 kyu

---
+++ CodeWars/7kyu/BinaryScORe.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/56cafdabc8cfcc3ad4000a2b/csharp
+
+7 kyu
+Binary scORe
+
+Objective
+Given a number n we will define its scORe to be 0 | 1 | 2 | 3 | ... | n, where | is the bitwise OR operator.
+
+Write a function that takes n and finds it's scORe.
+
+n	scORe n
+0	0
+1	1
+49	63
+1000000	1048575
+Any feedback would be much appreciated
+*/
+
+using System;
+using System.Numerics;
+
+namespace CodeWars
+{
+    public class BinaryScORe
+    {
+        public static BigInteger Score(BigInteger n)
+        {
+            // for (BigInteger i = 1; i < n; i <<= 1)
+            // {
+            //     n |= i;
+            // }
+            // return n;
+
+            //return n > 0 ? BigInteger.Pow(new BigInteger(2), (int) BigInteger.Log(n, 2) + 1) - 1 : n;
+            return BigInteger.Pow(2, (int) Math.Ceiling(BigInteger.Log(n + 1, 2))) - 1;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BinaryScOReTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BinaryScOReTests
+    {
+        private static void Tester(BigInteger inp, BigInteger exp)
+        {
+            Assert.AreEqual(exp, BinaryScORe.Score(inp));
+        }
+
+        [Test]
+        public void ExampleTests()
+        {
+            Tester(BigInteger.Parse("0"), BigInteger.Parse("0"));
+            Tester(BigInteger.Parse("1"), BigInteger.Parse("1"));
+            Tester(BigInteger.Parse("49"), BigInteger.Parse("63"));
+            Tester(BigInteger.Parse("1000000"), BigInteger.Parse("1048575"));
+        }
+    }
+}
