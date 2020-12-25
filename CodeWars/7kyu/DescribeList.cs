80f81c13c2708c47b107781cc8c10d1e3be4d3ba
a-kozhanov <a.kozhanov@outlook.com>
2020-12-25 16:27:23 +0300

    Add solution "Describe a list" 7 kyu

---
+++ CodeWars/7kyu/DescribeList.cs
@@ -0,0 +1,24 @@
+﻿/*
+https://www.codewars.com/kata/57a4a3e653ba3346bc000810/csharp
+
+7 kyu
+Describe a list
+
+Write function describeList which returns "empty" if the list is empty or "singleton" if it contains only one element or "longer"" if more.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class DescribeList
+    {
+        public static string describeList(List<int> list)
+        {
+            //return list.Count == 0 ? "empty" : list.Count == 1 ? "singleton" : "longer";
+            //return list.Count == 0 ? "empty" : list.Count > 1 ? "longer" : "singleton";
+            return list.Count switch {0 => "empty", 1 => "singleton", _ => "longer"};
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/DescribeListTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DescribeListTests
+    {
+        List<int> empty = new List<int>();
+        List<int> one = new List<int>() {1};
+
+        List<int> moreThan = new List<int>() {2, 3};
+
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual("empty", DescribeList.describeList(empty));
+            Assert.AreEqual("singleton", DescribeList.describeList(one));
+            Assert.AreEqual("longer", DescribeList.describeList(moreThan));
+        }
+    }
+}
