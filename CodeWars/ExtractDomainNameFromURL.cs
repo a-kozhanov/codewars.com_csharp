aea368d3e5e7b3529ebe5035cd280f70b708838b
a-kozhanov <a.kozhanov@outlook.com>
2020-12-27 15:35:54 +0300

    Add solution "Extract the domain name from a URL" 7 kyu

---
+++ CodeWars/ExtractDomainNameFromURL.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/514a024011ea4fb54200004b/
+
+5 kyu
+Extract the domain name from a URL
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+domain_name("http://github.com/carbonfive/raygun") == "github" 
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class ExtractDomainNameFromUrl
+    {
+        public static string DomainName(string url)
+        {
+            return url.Replace("http://", "")
+                .Replace("https://", "")
+                .Replace("www.", "")
+                .Split('.')[0];
+
+            //return new UriBuilder(url).Uri.Authority.Split(".").Select(x => (x.Length, x)).Max().x;
+        }
+    }
+}
---
+++ CodeWarsTests/ExtractDomainNameFromURLTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExtractDomainNameFromUrlTests
+    {
+        // Test.assertEquals(domainName("http://google.com"), "google");
+        // Test.assertEquals(domainName("http://google.co.jp"), "google");
+        // Test.assertEquals(domainName("www.xakep.ru"), "xakep");
+        // Test.assertEquals(domainName("https://youtube.com"), "youtube");
+
+        [TestCase("google", "http://google.com")]
+        [TestCase("google", "http://google.co.jp")]
+        [TestCase("xakep", "www.xakep.ru")]
+        [TestCase("youtube", "https://youtube.com")]
+        public void BasicTests(string domain, string url)
+        {
+            Assert.AreEqual(domain, ExtractDomainNameFromUrl.DomainName(url));
+        }
+    }
+}
