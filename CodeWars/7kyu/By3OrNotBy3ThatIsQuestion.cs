2c9bf71d0d76b83c12c4b17360377f94e4e629ce
a-kozhanov <a.kozhanov@outlook.com>
2020-12-28 10:19:27 +0300

    Add solution "By 3, or not by 3? That is the question . . ." 7 kyu

---
+++ CodeWars/7kyu/By3OrNotBy3ThatIsQuestion.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/59f7fc109f0e86d705000043/train/csharp
+
+7 kyu
+By 3, or not by 3? That is the question . . .
+
+A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. 
+If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+You can expect all test case arguments to be strings representing values greater than 0.
+
+Example:
+"123"      -> true
+"8409"     -> true
+"100853"   -> false
+"33333333" -> true
+"7"        -> false
+Try to avoid using the % (modulo) operator.
+*/
+
+
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace CodeWars
+{
+    public class By3OrNotBy3ThatIsQuestion
+    {
+        public static bool DivisibleByThree(string n)
+        {
+            // return n.Select(x => int.Parse(x.ToString())).Sum() % 3 == 0;
+            // return n.Sum(d => int.Parse(d.ToString())) % 3 == 0;
+            // return n.Aggregate(0, (x, y) => (x + y) % 3) == 0;
+            // return n.Select(Convert.ToInt32).Sum() % 3 == 0;
+            // return BigInteger.Parse(n) % 3 == 0;
+            // return n.Sum(c => c - '0') % 3 == 0;
+            // return n.Sum(e => e - 48) % 3 == 0;
+            // return n.Sum(x => x - 48) % 3 == 0;
+            return n.Sum(x => x) % 3 == 0;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/By3OrNotBy3ThatIsQuestionTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class By3OrNotBy3ThatIsQuestionTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(By3OrNotBy3ThatIsQuestion.DivisibleByThree("1891009"), Is.EqualTo(false));
+            Assert.That(By3OrNotBy3ThatIsQuestion.DivisibleByThree("00009"), Is.EqualTo(true));
+            Assert.That(By3OrNotBy3ThatIsQuestion.DivisibleByThree("57"), Is.EqualTo(true));
+        }
+    }
+}
