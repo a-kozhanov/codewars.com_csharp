3ce3cac443f5a49bf52888302427a51463c4e9c1
a-kozhanov <a.kozhanov@outlook.com>
2020-12-29 11:09:54 +0300

    Add solution "Digital cypher vol 2" 7 kyu

---
+++ CodeWarsTests/DigitalCypherVol2Tests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DigitalCypherVol2Tests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("scout", DigitalCypherVol2.Decode(new int[] {20, 12, 18, 30, 21}, 1939));
+
+            Assert.AreEqual("masterpiece",
+                DigitalCypherVol2.Decode(new int[] {14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8}, 1939));
+        }
+    }
+}
