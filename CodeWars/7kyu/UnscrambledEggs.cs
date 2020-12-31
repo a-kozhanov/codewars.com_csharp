f39a07379ec0736755ff81d1e411dc777fdd7d1d
a-kozhanov <a.kozhanov@outlook.com>
2020-12-31 14:37:38 +0300

    Added solution "Unscrambled eggs" 7 kyu

---
+++ CodeWars/7kyu/UnscrambledEggs.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7/csharp
+
+7 kyu
+Unscrambled eggs
+
+The string given to your function has had an "egg" inserted directly after each consonant. 
+You need to return the string before it became eggcoded.
+
+Example
+unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//             "B---eg---in---n---er---"
+
+Kata is supposed to be for beginners to practice regular expressions, so commenting would be appreciated.
+*/
+
+namespace CodeWars
+{
+    public class UnscrambledEggs
+    {
+        public static string UnscrambleEggs(string word)
+        {
+            return word.Replace("egg", "");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/UnscrambledEggsTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class UnscrambledEggsTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("code here", UnscrambledEggs.UnscrambleEggs("ceggodegge heggeregge"));
+            Assert.AreEqual("FUN KATA", UnscrambledEggs.UnscrambleEggs("FeggUNegg KeggATeggA"));
+        }
+    }
+}
