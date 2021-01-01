70bf95c37e1c0a4675794f0a1989e61b2fb00f4e
a-kozhanov <a.kozhanov@outlook.com>
2021-01-02 02:25:08 +0300

    Added solution "Anagram Detection" 7 kyu

---
+++ CodeWars/7kyu/AnagramDetection.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/529eef7a9194e0cbc1000255/csharp
+
+7 kyu
+Anagram Detection
+
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+"Buckethead" is an anagram of "DeathCubeK"
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class AnagramDetection
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            return (test + original).ToLower().Aggregate(0, (acc, next) => acc ^ next) == 0;
+            //return test.ToLower().OrderBy(c => c).SequenceEqual(original.ToLower().OrderBy(c => c));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/AnagramDetectionTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AnagramDetectionTests
+    {
+        [Test]
+        [TestCase("foefet", "toffee", ExpectedResult = true)]
+        [TestCase("Buckethead", "DeathCubeK", ExpectedResult = true)]
+        [TestCase("Twoo", "Woot", ExpectedResult = true)]
+        [TestCase("apple", "pale", ExpectedResult = false)]
+        public static bool FixedTest(string test, string original)
+        {
+            return AnagramDetection.IsAnagram(test, original);
+        }
+    }
+}
