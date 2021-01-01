a79550f77c6ff66cd0e283ac521fe0f17feed462
a-kozhanov <a.kozhanov@outlook.com>
2021-01-01 17:26:02 +0300

    Added solution "Unlucky Days" 7 kyu

---
+++ CodeWars/7kyu/UnluckyDays.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/56eb0be52caf798c630013c0/csharp
+
+7 kyu
+Unlucky Days
+
+Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+Find the number of Friday 13th in the given year.
+
+Input: Year as an integer.
+Output: Number of Black Fridays in the year as an integer.
+
+Examples:
+unluckyDays(2015) == 3
+unluckyDays(1986) == 1
+Note: In Ruby years will start from 1593.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataUnluckyDays
+    {
+        public static int UnluckyDays(int year)
+        {
+            // int friday = 0;
+            // for (int i = 1; i <= 12; i++)
+            // {
+            //     if (new DateTime(year, i, 13).DayOfWeek == DayOfWeek.Friday) friday++;
+            // }
+            // return friday;
+
+            //return Enumerable.Range(1, 12).Sum(m => new DateTime(year, m, 13).DayOfWeek == DayOfWeek.Friday ? 1 : 0);
+            return Enumerable.Range(1, 12).Count(m => new DateTime(year, m, 13).DayOfWeek == DayOfWeek.Friday);
+            //return Enumerable.Range(1, 12).Count(m => new DateTime(year, m, 13).DayOfWeek == (DayOfWeek) 5);
+            //return Enumerable.Range(1, 12).Count(m => (int) new DateTime(year, m, 13).DayOfWeek == 5);
+            //return Enumerable.Range(1, 12).Count(m => new DateTime(year, m, 8).DayOfWeek == 0);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/UnluckyDaysTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataUnluckyDaysTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(1, KataUnluckyDays.UnluckyDays(1586));
+            Assert.AreEqual(3, KataUnluckyDays.UnluckyDays(1001));
+            Assert.AreEqual(2, KataUnluckyDays.UnluckyDays(2819));
+            Assert.AreEqual(2, KataUnluckyDays.UnluckyDays(2792));
+            Assert.AreEqual(2, KataUnluckyDays.UnluckyDays(2723));
+            Assert.AreEqual(1, KataUnluckyDays.UnluckyDays(1909));
+            Assert.AreEqual(2, KataUnluckyDays.UnluckyDays(1812));
+            Assert.AreEqual(2, KataUnluckyDays.UnluckyDays(1618));
+            Assert.AreEqual(1, KataUnluckyDays.UnluckyDays(2132));
+            Assert.AreEqual(3, KataUnluckyDays.UnluckyDays(2065));
+        }
+    }
+}
