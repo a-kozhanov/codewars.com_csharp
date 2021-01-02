201106e85a5566029a3cf7a7692a9f020076491b
a-kozhanov <a.kozhanov@outlook.com>
2021-01-02 20:18:17 +0300

    Updated solution "Welcome!" 8 kyu

--- CodeWars/8kyu/Welcome.cs
+++ CodeWars/8kyu/Welcome.cs
@@ -45,27 +45,48 @@ namespace CodeWars
     {
         public static string Greet(string language)
         {
-            Console.WriteLine($"language: {language}");
-            switch (language)
+            return language switch
             {
-                case "czech": return "Vitejte";
-                case "danish": return "Velkomst";
-                case "dutch": return "Welkom";
-                case "estonian": return "Tere tulemast";
-                case "finnish": return "Tervetuloa";
-                case "flemish": return "Welgekomen";
-                case "french": return "Bienvenue";
-                case "german": return "Willkommen";
-                case "irish": return "Failte";
-                case "italian": return "Benvenuto";
-                case "latvian": return "Gaidits";
-                case "lithuanian": return "Laukiamas";
-                case "polish": return "Witamy";
-                case "spanish": return "Bienvenido";
-                case "swedish": return "Valkommen";
-                case "welsh": return "Croeso";
-                default: return "Welcome";
-            }
+                "czech" => "Vitejte",
+                "danish" => "Velkomst",
+                "dutch" => "Welkom",
+                "estonian" => "Tere tulemast",
+                "finnish" => "Tervetuloa",
+                "flemish" => "Welgekomen",
+                "french" => "Bienvenue",
+                "german" => "Willkommen",
+                "irish" => "Failte",
+                "italian" => "Benvenuto",
+                "latvian" => "Gaidits",
+                "lithuanian" => "Laukiamas",
+                "polish" => "Witamy",
+                "spanish" => "Bienvenido",
+                "swedish" => "Valkommen",
+                "welsh" => "Croeso",
+                _ => "Welcome"
+            };
+
+            // Console.WriteLine($"language: {language}");
+            // switch (language)
+            // {
+            //     case "czech": return "Vitejte";
+            //     case "danish": return "Velkomst";
+            //     case "dutch": return "Welkom";
+            //     case "estonian": return "Tere tulemast";
+            //     case "finnish": return "Tervetuloa";
+            //     case "flemish": return "Welgekomen";
+            //     case "french": return "Bienvenue";
+            //     case "german": return "Willkommen";
+            //     case "irish": return "Failte";
+            //     case "italian": return "Benvenuto";
+            //     case "latvian": return "Gaidits";
+            //     case "lithuanian": return "Laukiamas";
+            //     case "polish": return "Witamy";
+            //     case "spanish": return "Bienvenido";
+            //     case "swedish": return "Valkommen";
+            //     case "welsh": return "Croeso";
+            //     default: return "Welcome";
+            // }
         }
 
 
@@ -96,8 +117,6 @@ namespace CodeWars
         //    {"swedish", "Valkommen"},
         //    {"welsh", "Croeso"}
         //};
-
-
 
 
         //public static string Greet(string language)
@@ -162,7 +181,5 @@ namespace CodeWars
         //    return translate.ContainsKey(language) ?
         //        translate[language] : "Welcome";
         //}
-
-
     }
 }
