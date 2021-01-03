1216b3dfc8259adbc225863c3eaab72906c71bdb
a-kozhanov <a.kozhanov@outlook.com>
2021-01-03 03:07:16 +0300

    Added solution "Calculate Meal Total" 7 kyu

---
+++ CodeWars/7kyu/CalculateMealTotal.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/58545549b45c01ccab00058c/csharp
+
+7 kyu
+Calculate Meal Total
+
+Create a function that returns the total of a meal including tip and tax. You should not tip on the tax.
+You will be given the subtotal, the tax as a percentage and the tip as a percentage. 
+Please round your result to two decimal places.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class CalculateMealTotal
+    {
+        public static double CalculateTotal(double subtotal, int tax, int tip)
+        {
+            //return Math.Round(subtotal + subtotal * (tax + tip) * 0.01, 2);
+            //return Math.Round((1 + 0.01 * tax + 0.01 * tip) * subtotal, 2);
+            return Math.Round(subtotal * (1 + (tax + tip) / 100.0), 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CalculateMealTotalTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CalculateMealTotalTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(5.75, CalculateMealTotal.CalculateTotal(5.00, 5, 10));
+            Assert.AreEqual(45.10, CalculateMealTotal.CalculateTotal(36.97, 7, 15));
+        }
+    }
+}
