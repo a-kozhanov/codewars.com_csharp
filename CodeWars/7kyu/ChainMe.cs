5d665d66e7b1502840ac445f2d90e164f50d5404
a-kozhanov <a.kozhanov@outlook.com>
2021-01-04 10:18:00 +0300

    Added solution "Chain me" 7 kyu

---
+++ CodeWars/7kyu/ChainMe.cs
@@ -0,0 +1,50 @@
+﻿/*
+https://www.codewars.com/kata/54fb853b2c8785dd5e000957/csharp
+
+7 kyu
+Chain me
+
+Write a generic function chainer
+Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for ruby).
+
+The input for each function is the output of the previous function (except the first function, which takes the starting value as it's input). 
+Return the final value after execution is complete.
+
+double input = 2;
+
+public static double add(double x) {
+  return x + 1;
+}
+
+public static double mul(double x) {
+   return x * 30;
+}
+Kata.Chain( input , new[] { (Func<double, double>)add, mul });
+//=> returns 90
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ChainMe
+    {
+        public static double Chain(double input, Func<double, double>[] fs)
+        {
+            // foreach (var f in fs)
+            //     input = f(input);
+            // return input;
+
+            // Array.ForEach(fs, f => { input = f(input); });
+            // return input;
+
+            // fs.ToList().ForEach(f => input = f(input));
+            // return input;
+
+            // return !fs.Any() ? input : Chain(fs[0](input), fs.Skip(1).ToArray());
+
+            return fs.Aggregate(input, (current, f) => f(current));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ChainMeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ChainMeTests
+    {
+        public static double add(double x)
+        {
+            return x + 10;
+        }
+
+        public static double mul(double x)
+        {
+            return x * 30;
+        }
+
+        [Test]
+        public static void ExampleTest()
+        {
+            Assert.AreEqual(ChainMe.Chain(2, new[] {(Func<double, double>) add, mul}), 360, "Incorrect Value for '2'");
+        }
+    }
+}
