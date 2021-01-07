92c127ca10c36253b98677bbe7a80580a3857d6e
a-kozhanov <a.kozhanov@outlook.com>
2021-01-07 20:23:05 +0300

    Added solution "Discover The Original Price" 7 kyu

---
+++ CodeWars/7kyu/DiscoverOriginalPrice.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/552564a82142d701f5001228/csharp
+
+7 kyu
+Discover The Original Price
+
+We need to write some code to return the original price of a product, t
+he return type must be of type decimal and the number must be rounded to two decimal places.
+
+We will be given the sale price (discounted price), and the sale percentage, 
+our job is to figure out the original price.
+
+For example:
+Given an item at $75 sale price after applying a 25% discount, 
+the function should return the original price of that item before applying the sale percentage, 
+which is ($100.00) of course, rounded to two decimal places.
+
+DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 
+25 is the sale percentage and 100 is the original price
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataDiscoverOriginalPrice
+    {
+        public static decimal DiscoverOriginalPrice(decimal discountedPrice, decimal salePercentage)
+        {
+            //return decimal.Round(discountedPrice * 100 / (100 - salePercentage), 2);
+            //return Math.Round(100 * discountedPrice / (100 - salePercentage), 2);
+            return Math.Round(discountedPrice / (1 - salePercentage / 100), 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/DiscoverOriginalPriceTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DiscoverOriginalPriceTests
+    {
+        [Test]
+        public void GivenDiscount75Rate25Returns100()
+        {
+            Assert.AreEqual(100.00M, KataDiscoverOriginalPrice.DiscoverOriginalPrice(75M, 25M));
+        }
+    }
+}
