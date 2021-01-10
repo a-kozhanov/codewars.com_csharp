13967458ef10d03ab1088446da71b38373288af5
a-kozhanov <a.kozhanov@outlook.com>
2021-01-10 15:11:49 +0300

    Added solution "Alan Partridge I - Partridge Watch" 7 kyu

---
+++ CodeWars/7kyu/AlanPartridgeIPartridgeWatch.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/5808c8eff0ed4210de000008/csharp
+
+7 kyu
+Alan Partridge I - Partridge Watch
+
+To celebrate today's launch of my Hero's new book: Alan Partridge: 
+Nomad, We have a new series of kata arranged around the great man himself.
+
+Given an array of terms, if any of those terms relate to Alan Partridge, return Mine's a Pint!
+
+The number of ! after the t should be determined by the number of Alan related terms you find in the provided array (x). 
+The related terms are:
+Partridge
+PearTree
+Chat
+Dan
+Toblerone
+Lynn
+AlphaPapa
+Nomad
+
+If you don't find any related terms, return 'Lynn, I've pierced my foot on a spike!!'
+
+All Hail King Partridge
+
+Other katas in this series:
+Alan Partridge II - Apple Turnover
+Alan Partridge III - London
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class AlanPartridgeIPartridgeWatch
+    {
+        public static string Part(string[] x)
+        {
+            // var count = x.Count(s =>
+            //     Array.Exists(new[] {"Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad"},
+            //         m => m == s));
+            // return count > 0 ? "Mine's a Pint" + new string('!', count) : "Lynn, I've pierced my foot on a spike!!";
+
+            // string[] terms = {"Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad"};
+            // var count = x.Count(s => terms.Contains(s));
+            // return count > 0 ? "Mine's a Pint" + new string('!', count) : "Lynn, I've pierced my foot on a spike!!";
+
+            var count = x.Count(s =>
+                new[] {"Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad"}.Contains(s));
+            return count > 0 ? "Mine's a Pint" + new string('!', count) : "Lynn, I've pierced my foot on a spike!!";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/AlanPartridgeIPartridgeWatchTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AlanPartridgeIPartridgeWatchTests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual("Mine's a Pint!",
+                AlanPartridgeIPartridgeWatch.Part(new String[] {"Grouse", "Partridge", "Pheasant"}));
+
+            Assert.AreEqual("Mine's a Pint!!!!!!!!",
+                AlanPartridgeIPartridgeWatch.Part(new String[]
+                {
+                    "Grouse", "Partridge", "Pheasant", "Goose", "Starling", "Robin", "Thrush", "Emu", "PearTree",
+                    "Chat", "Dan", "Square", "Toblerone", "Lynn", "AlphaPapa", "BMW", "Graham", "Tool", "Nomad",
+                    "Finger", "Hamster"
+                }));
+
+            Assert.AreEqual("Lynn, I've pierced my foot on a spike!!",
+                AlanPartridgeIPartridgeWatch.Part(new string[] {"Pheasant", "Goose", "Starling", "Robin"}));
+        }
+    }
+}
