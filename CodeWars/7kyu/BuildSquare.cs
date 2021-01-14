d81f448ffbb6109ad3b5d54c4e258ce74b734d77
a-kozhanov <a.kozhanov@outlook.com>
2021-01-14 11:39:32 +0300

    Added solution "Build a square" 7 kyu

---
+++ CodeWars/7kyu/BuildSquare.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/csharp
+
+7 kyu
+Build a square
+
+I will give you an integer. Give me back a shape that is as long and wide as the integer. 
+The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class BuildSquare
+    {
+        public static string GenerateShape(int n)
+        {
+            //return string.Concat(Enumerable.Repeat((string.Concat(Enumerable.Repeat("+", n)) + '\n'), n)).TrimEnd('\n');
+            //return string.Join("\n", new string[n].Select(_ => new string('+', n)));
+            //return string.Join("\n", Enumerable.Repeat(new string('+', n), n));
+            //return string.Join("\n", Enumerable.Repeat("".PadRight(n, '+'), n));
+            return string.Join("\n", Enumerable.Repeat("".PadLeft(n, '+'), n));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BuildSquareTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BuildSquareTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("", BuildSquare.GenerateShape(0));
+            Assert.AreEqual("+", BuildSquare.GenerateShape(1));
+            Assert.AreEqual("++\n++", BuildSquare.GenerateShape(2));
+            Assert.AreEqual("+++\n+++\n+++", BuildSquare.GenerateShape(3));
+        }
+    }
+}
