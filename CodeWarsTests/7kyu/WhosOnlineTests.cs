75e88bbcc4995e6eab8c533a6554b87fa9069522
a-kozhanov <a.kozhanov@outlook.com>
2021-01-17 15:29:54 +0300

    Added solution "You have a group chat application, but who is online!?" 7 kyu

---
+++ CodeWarsTests/7kyu/WhosOnlineTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WhosOnlineTests
+    {
+        // [Test, Description("Example test one of each")]
+        // public void SoloTest()
+        // {
+        //     User[] friends = new User[]
+        //     {
+        //         new User("David", UserStatus.Online, 10),
+        //         new User("Lucy", UserStatus.Offline, 22),
+        //         new User("Bob", UserStatus.Online, 104)
+        //     };
+        //     var expected = new Dictionary<UserStatus, IEnumerable<string>>
+        //     {
+        //         {UserStatus.Online, new[] {"David"}},
+        //         {UserStatus.Offline, new[] {"Lucy"}},
+        //         {UserStatus.Away, new[] {"Bob"}}
+        //     };
+        //     Assert.That(Kata.WhosOnline(friends), Is.EqualTo(expected));
+        // }
+        //
+        // [Test, Description("Example test no one online")]
+        // public void NoOnlineTest()
+        // {
+        //     User[] friends = new User[]
+        //     {
+        //         new User("Lucy", UserStatus.Offline, 22),
+        //         new User("Bob", UserStatus.Online, 104)
+        //     };
+        //     var expected = new Dictionary<UserStatus, IEnumerable<string>>
+        //     {
+        //         {UserStatus.Offline, new[] {"Lucy"}},
+        //         {UserStatus.Away, new[] {"Bob"}}
+        //     };
+        //     Assert.That(Kata.WhosOnline(friends), Is.EqualTo(expected));
+    }
+}
