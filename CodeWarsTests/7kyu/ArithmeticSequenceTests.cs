03799b3f64493740ca0bf6ea9620c2a938a1be40
a-kozhanov <a.kozhanov@outlook.com>
2021-01-20 10:19:46 +0300

    Added solution "Arithmetic Sequence!" 7 kyu

---
+++ CodeWarsTests/7kyu/ArithmeticSequenceTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ArithmeticSequenceTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(1, 2, 3).Returns(7);
+                yield return new TestCaseData(2, 2, 2).Returns(6);
+                yield return new TestCaseData(-50, 10, 20).Returns(150);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(int first, int n, int c) => ArithmeticSequence.Nthterm(first, n, c);
+    }
+}
