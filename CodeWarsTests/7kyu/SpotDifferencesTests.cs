b39cfad17d808fd221e49b4b80e20f5c51ffdf1d
a-kozhanov <a.kozhanov@outlook.com>
2021-01-21 09:46:00 +0300

    Added solution "Spot the Differences" 7 kyu

---
+++ CodeWarsTests/7kyu/SpotDifferencesTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SpotDifferencesTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("abcdefg", "abcqetg").Returns(new List<int> {3, 5});
+                yield return new TestCaseData("Hello World!", "hello world.").Returns(new List<int> {0, 6, 11});
+                yield return new TestCaseData("FixedGrey", "FixedGrey").Returns(new List<int>());
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public List<int> Test(string s1, string s2) => SpotDifferences.Spot(s1, s2);
+    }
+}
