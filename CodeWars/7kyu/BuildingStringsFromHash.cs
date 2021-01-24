4a1947f13e33dde8b1a82287f82ce661b26acaf8
a-kozhanov <a.kozhanov@outlook.com>
2021-01-24 15:14:18 +0300

    Added solution "Building Strings From a Hash" 7 kyu

---
+++ CodeWars/7kyu/BuildingStringsFromHash.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/51c7d8268a35b6b8b40002f2/csharp
+
+7 kyu
+Building Strings From a Hash
+
+Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+Example:
+Kata.StringifyDict(new Dictionary<char, int> {{'a', 1}, {'b', 2}}) => "a = 1,b = 2";
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class BuildingStringsFromHash
+    {
+        public static string StringifyDict<TKey, TValue>(Dictionary<TKey, TValue> hash)
+        {
+            return string.Join(",", hash.Select(x => $"{x.Key} = {x.Value}"));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BuildingStringsFromHashTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BuildingStringsFromHashTests
+    {
+        [Test, Description("Basic Tests")]
+        public void Test()
+        {
+            Assert.AreEqual("a = 1,b = 2",
+                BuildingStringsFromHash.StringifyDict(new Dictionary<char, int> {{'a', 1}, {'b', 2}}));
+
+            Assert.AreEqual("b = 1,c = 2,e = 3",
+                BuildingStringsFromHash.StringifyDict(new Dictionary<char, int> {{'b', 1}, {'c', 2}, {'e', 3}}));
+
+            Assert.AreEqual("", BuildingStringsFromHash.StringifyDict(new Dictionary<char, int>()));
+        }
+    }
+}
