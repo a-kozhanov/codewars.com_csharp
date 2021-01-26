0f846df05d2208ff1734df063362bbeb50fb7ff5
a-kozhanov <a.kozhanov@outlook.com>
2021-01-26 09:47:18 +0300

    Added solution "Simple Fun #88: Sort By Height" 7 kyu

---
+++ CodeWars/7kyu/SimpleFun88SortByHeight.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/589577f0d1b93ae32a000001/csharp
+
+7 kyu
+Simple Fun #88: Sort By Height
+
+Task
+Some people are standing in a row in a park. There are trees between them which cannot be moved.
+
+Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+
+Example
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+
+[-1, 150, 160, 170, -1, -1, 180, 190].
+
+Input/Output
+[input] integer array a
+
+If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
+
+Constraints:
+
+5 ≤ a.length ≤ 30,
+
+-1 ≤ a[i] ≤ 200.
+
+[output] an integer array
+
+Sorted array a with all the trees untouched.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun88SortByHeight
+    {
+        public int[] SortByHeight(int[] a)
+        {
+            var row = new Queue<int>(a.Where(i => i != -1).OrderBy(i => i));
+            return a.Select(i => i == -1 ? -1 : row.Dequeue()).ToArray();
+
+            // using var p = a.Where(i => i != -1).OrderBy(i => i).GetEnumerator();
+            // return a.Select(n => n == -1 ? -1 : p.MoveNext() ? p.Current : -1).ToArray();
+
+            // var row = a.Where(i => i != -1).OrderBy(i => i).ToList();
+            // for (var i = 0; i < a.Length; i++)
+            //     if (a[i] == -1)
+            //         row.Insert(i, -1);
+            // return row.ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun88SortByHeightTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun88SortByHeightTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun88SortByHeight();
+
+            Assert.AreEqual(new int[] {-1, 150, 160, 170, -1, -1, 180, 190},
+                kata.SortByHeight(new int[] {-1, 150, 190, 170, -1, -1, 160, 180}));
+
+            Assert.AreEqual(new int[] {-1, -1, -1, -1, -1}, kata.SortByHeight(new int[] {-1, -1, -1, -1, -1}));
+
+            Assert.AreEqual(new int[] {2, 2, 4, 9, 11, 16}, kata.SortByHeight(new int[] {4, 2, 9, 11, 2, 16}));
+        }
+    }
+}
