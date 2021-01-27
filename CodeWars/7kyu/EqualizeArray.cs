f265b6a486159b51e1c6a2989b32e431f56977ee
a-kozhanov <a.kozhanov@outlook.com>
2021-01-27 11:09:44 +0300

    Added solution "Equalize the array!" 7 kyu

---
+++ CodeWars/7kyu/EqualizeArray.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/580a1a4af195dbc9ed00006c/csharp
+
+7 kyu
+Equalize the array!
+
+No description!!!
+
+Input :: [10,20,25,0]
+Output :: ["+0", "+10", "+15", "-10"]
+
+Show some love, rank and upvote!
+*/
+
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class EqualizeArray
+    {
+        public static string[] Equalize(int[] array)
+        {
+            //return array.Select(i => (i - array[0]).ToString("+#;-#;+0")).ToArray();
+            //return array.Select(i => (i - array[0]).ToString("+0;-#")).ToArray();
+            return array.Select(i => $"{i - array[0]:+0;-#}").ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/EqualizeArrayTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EqualizeArrayTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new[] {"+0", "+10", "+15", "-10"}, EqualizeArray.Equalize(new[] {20, 30, 35, 10}));
+            Assert.AreEqual(new string[] { }, EqualizeArray.Equalize(new int[] { }));
+            Assert.AreEqual(new[] {"+0", "+2", "+14", "+40", "-10", "+5", "+10"},
+                EqualizeArray.Equalize(new[] {10, 12, 24, 50, 0, 15, 20}));
+        }
+    }
+}
