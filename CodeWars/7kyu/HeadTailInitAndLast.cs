431834679432c4caad01e3c3597bb92118608d3a
a-kozhanov <a.kozhanov@outlook.com>
2021-01-28 16:47:10 +0300

    Added solution "Head, Tail, Init and Last" 7 kyu

---
+++ CodeWars/7kyu/HeadTailInitAndLast.cs
@@ -0,0 +1,72 @@
+﻿/*
+https://www.codewars.com/kata/54592a5052756d5c5d0009c3/csharp
+
+7 kyu
+Head, Tail, Init and Last
+
+Haskell has some useful functions for dealing with lists:
+
+$ ghci
+GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+λ head [1,2,3,4,5]
+1
+λ tail [1,2,3,4,5]
+[2,3,4,5]
+λ init [1,2,3,4,5]
+[1,2,3,4]
+λ last [1,2,3,4,5]
+5
+Your job is to implement these functions in your given language. 
+Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+| HEAD | <----------- TAIL ------------> |
+[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+| <----------- INIT ------------> | LAST |
+
+head [x] = x
+tail [x] = []
+init [x] = []
+last [x] = x
+Here's how I expect the functions to be called in your language:
+
+new List<int> {1, 2, 3, 4, 5}.Head() => 1
+new List<int> {1, 2, 3, 4, 5}.Tail() => new List<int> {2, 3, 4, 5}
+Most tests consist of 100 randomly generated arrays, each with four tests, one for each operation. 
+There are 400 tests overall. No empty arrays will be given. Haskell has QuickCheck tests
+
+PLEASE NOTE: C#'s Last function shall be called Last_ to prevent name clashes.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class HeadTailInitAndLast
+    {
+        public static TSource Head<TSource>(this List<TSource> list)
+        {
+            return list[0];
+            // return list.First();
+        }
+
+        public static List<TSource> Tail<TSource>(this List<TSource> list)
+        {
+            return list.Skip(1).ToList();
+            //return list.GetRange(1, list.Count - 1);
+        }
+
+        public static List<TSource> Init<TSource>(this List<TSource> list)
+        {
+            return list.Take(list.Count - 1).ToList();
+            //return list.GetRange(0, list.Count - 1);
+        }
+
+        public static TSource Last_<TSource>(this List<TSource> list)
+        {
+            return list.Last();
+            //return list[^1];
+            //return list[list.Count - 1];
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HeadTailInitAndLastTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HeadTailInitAndLastTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(5, new List<int> {5, 1}.Head());
+            Assert.AreEqual(new List<int> {2, 3}, new List<int> {1, 2, 3}.Tail());
+            Assert.AreEqual(new List<int> {1, 5, 7}, new List<int> {1, 5, 7, 9}.Init());
+            Assert.AreEqual(2, new List<int> {7, 2}.Last_());
+        }
+    }
+}
