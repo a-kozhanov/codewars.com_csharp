4ce73a25be154dcc04cae6e62b46833913b69db2
a-kozhanov <a.kozhanov@outlook.com>
2021-01-29 10:58:09 +0300

    Added solution "Sort and Transform" 7 kyu

---
+++ CodeWars/7kyu/SortTransform.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/57cc847e58a06b1492000264/csharp
+
+7 kyu
+Sort and Transform
+
+Given an array of numbers, return a string made up of four parts:
+
+a) a four character 'word', made up of the characters derived from the first two and last two numbers in the array. 
+order should be as read left to right (first, second, second last, last),
+
+b) the same as above, post sorting the array into ascending order,
+
+c) the same as above, post sorting the array into descending order,
+
+d) the same as above, post converting the array into ASCII characters and sorting alphabetically.
+
+The four parts should form a single string, each part separated by a hyphen: '-'
+
+example format of solution: 'asdf-tyui-ujng-wedg'
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataSortTransform
+    {
+        public static string SortTransform(int[] arr)
+        {
+            string Tfrm(IEnumerable<int> a) => string.Concat(a.Select(x => (char) x)).Remove(2, a.Count() - 4);
+            return string.Format("{0}-{1}-{2}-{1}", Tfrm(arr), Tfrm(arr.OrderBy(x => x)), Tfrm(arr.OrderBy(x => -x)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SortTransformTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SortTransformTests
+    {
+        [Test]
+        [TestCase(new[] {121, 122, 123, 124, 125, 120, 122, 132}, "yzz-xy}-}yx-xy}")]
+        [TestCase(new[] {111, 112, 113, 114, 115, 113, 114, 110}, "oprn-nors-sron-nors")]
+        [TestCase(new[] {51, 62, 73, 84, 95, 100, 99, 126}, "3>c~-3>d~-~d>3-3>d~")]
+        public void BasicTest(int[] arr, string expected)
+        {
+            Assert.That(KataSortTransform.SortTransform(arr), Is.EqualTo(expected));
+        }
+    }
+}
