65423744be3cbc5b2d24ef8507c9c12e1f04d929
a-kozhanov <a.kozhanov@outlook.com>
2021-01-31 15:35:58 +0300

    Added solution "Target Date" 7 kyu

---
+++ CodeWars/7kyu/TargetDate.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/569218bc919ccba77000000b/csharp
+
+7 kyu
+Target Date
+
+You have an amount of money a0 > 0 and you deposit it with a constant interest rate of p% > 0 per year on the 1st of January 2016. 
+You want to have an amount a >= a0.
+
+Task:
+The function date_nb_days (or dateNbDays...) with parameters a0, a, p will return, as a string, 
+the date on which you have just reached a.
+
+Example:
+date_nb_days(100, 101, 0.98) --> "2017-01-01" (366 days)
+date_nb_days(100, 150, 2.00) --> "2035-12-26" (7299 days)
+
+Notes:
+The return format of the date is "YYYY-MM-DD"
+The deposit is always on the "2016-01-01"
+If p% is the rate for a year take the rate for a day to be p divided by 36000 ; 
+banks consider that there are 360 days in a year.
+You have: a0 > 0, p% > 0, a >= a0
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class TargetDate
+    {
+        public static String DateNbDays(double a0, double a, double p)
+        {
+            //return (new DateTime(2016, 1, 1) + TimeSpan.FromDays(Math.Log(a / a0, 1 + p / 36000) + 1)).ToString("yyyy-MM-dd");
+            //return new DateTime(2016, 01, 01).AddDays(Math.Ceiling(Math.Log(a / a0, 1 + p / 36000))).ToString("yyyy-MM-dd");
+            //return new DateTime(2016, 1, 1).AddDays(Math.Ceiling(Math.Log(a / a0, 1 + p / 36000))).ToString("yyyy-MM-dd");
+            //return $"{new DateTime(2016, 1, 1).AddDays(Math.Ceiling(Math.Log(a / a0, 1 + p / 36000))):yyyy-MM-dd}";
+            //return new DateTime(2016, 1, 2).AddDays((int) Math.Log(a / a0, 1 + p / 36000)).ToString("yyyy-MM-dd");
+
+            return $"{new DateTime(2016, 1, 2).AddDays((int) Math.Log(a / a0, 1 + p / 36000)):yyyy-MM-dd}";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/TargetDateTests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TargetDateTests
+    {
+        private static void testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test] 
+        public static void test1()
+        {
+            testing(TargetDate.DateNbDays(4281, 5087, 2), "2024-07-03");
+            testing(TargetDate.DateNbDays(4620, 5188, 2), "2021-09-19");
+            testing(TargetDate.DateNbDays(9999, 11427, 6), "2018-03-13");
+            testing(TargetDate.DateNbDays(3525, 4822, 3), "2026-04-18");
+            testing(TargetDate.DateNbDays(5923, 6465, 6), "2017-06-10");
+            testing(TargetDate.DateNbDays(4254, 4761, 8), "2017-05-22");
+            testing(TargetDate.DateNbDays(1244, 2566, 4), "2033-11-04");
+            testing(TargetDate.DateNbDays(6328, 7517, 5), "2019-05-25");
+            testing(TargetDate.DateNbDays(2920, 3834, 2), "2029-06-03");
+            testing(TargetDate.DateNbDays(7792, 8987, 4), "2019-07-09");
+        }
+    }
+}
