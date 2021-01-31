bdf9aec26b22f8c7193a3c9e1ee5db954e31efea
a-kozhanov <a.kozhanov@outlook.com>
2021-02-01 01:11:32 +0300

    Added solution "Going to the cinema" 7 kyu

---
+++ CodeWarsTests/7kyu/GoingToCinemaTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GoingToCinemaTests
+    {
+        private static void testing(long actual, long expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Fixed Tests: Movie");
+            testing(GoingToCinema.Movie(500, 15, 0.9), 43);
+            testing(GoingToCinema.Movie(100, 10, 0.95), 24);
+        }
+    }
+}
