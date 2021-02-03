3c27f1f7a5fb219414f96b52d28a91dfe23ae093
a-kozhanov <a.kozhanov@outlook.com>
2021-02-03 18:31:38 +0300

    Added solution "Leap Years" 7 kyu

---
+++ CodeWars/7kyu/LeapYears.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/526c7363236867513f0005ca/csharp
+
+7 kyu
+Leap Years
+
+In this kata you should simply determine, whether a given year is a leap year or not. 
+In case you don't know the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years
+Additional Notes:
+
+Only valid years (positive integers) will be tested, so you don't have to validate them
+Examples can be found in the test fixture.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class LeapYears
+    {
+        public static bool IsLeapYear(int year)
+        {
+            // if (year % 400 == 0) return true;
+            // if (year % 100 == 0) return false;
+            // return year % 4 == 0;
+
+            //return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+            //return year % 4 == 0 && (year % 400 == 0 || year % 100 != 0);
+            //return year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
+            //return year % 400 == 0 || year % 100 > 0 && year % 4 == 0;
+            //return year % 4 == 0 == year % 100 > 0 || year % 400 == 0;
+            return DateTime.IsLeapYear(year);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/LeapYearsTests.cs
@@ -0,0 +1,39 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class LeapYearsTests
+    {
+        [Test]
+        public static void Year1234()
+        {
+            Assert.AreEqual(false, LeapYears.IsLeapYear(1234), "Year 1234");
+        }
+
+        [Test]
+        public static void Year1984()
+        {
+            Assert.AreEqual(true, LeapYears.IsLeapYear(1984), "Year 1984");
+        }
+
+        [Test]
+        public static void Year2000()
+        {
+            Assert.AreEqual(true, LeapYears.IsLeapYear(2000), "Year 2000");
+        }
+
+        [Test]
+        public static void Year2010()
+        {
+            Assert.AreEqual(false, LeapYears.IsLeapYear(2010), "Year 2010");
+        }
+
+        [Test]
+        public static void Year2013()
+        {
+            Assert.AreEqual(false, LeapYears.IsLeapYear(2013), "Year 2013");
+        }
+    }
+}
