12da9ca8cdaca98cde1724cb293fb65ba673983f
a-kozhanov <a.kozhanov@outlook.com>
2021-02-06 00:10:00 +0300

    Added solution "Turkish Numbers, 0-99" 7 kyu

---
+++ CodeWarsTests/7kyu/TurkishNumbers099Tests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TurkishNumbers099Tests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            int[] exampleInputs = new int[] {1, 13, 27, 38, 77, 94};
+            string[] exampleOutputs = new string[]
+                {"bir", "on üç", "yirmi yedi", "otuz sekiz", "yetmiş yedi", "doksan dört"};
+
+            for (int i = 0; i < exampleInputs.Length; i++)
+                Assert.AreEqual(exampleOutputs[i], TurkishNumbers099.GetTurkishNumber(exampleInputs[i]));
+        }
+    }
+}
