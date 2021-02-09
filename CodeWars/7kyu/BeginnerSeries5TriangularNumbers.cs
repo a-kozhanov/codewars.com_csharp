4015d5823f7d4fcad1a25f8e4f3a6f2e5677fdd3
a-kozhanov <a.kozhanov@outlook.com>
2021-02-09 17:00:17 +0300

    Added solution "Beginner Series #5 Triangular Numbers" 7 kyu

---
+++ CodeWars/7kyu/BeginnerSeries5TriangularNumbers.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/56d0a591c6c8b466ca00118b/csharp
+
+7 kyu
+Beginner Series #5 Triangular Numbers
+
+Triangular number is the amount of points that can fill equilateral triangle.
+
+Example: the number 6 is a triangular number because all sides of a triangle has the same amount of points.
+
+alt text
+
+Hint!
+T(n) = n * (n + 1) / 2,
+n - is the size of one side.
+T(n) - is the triangular number.
+Given a number 'T' from interval [1; 2147483646], find if it is triangular number or not.
+
+Appreciate the feedback!
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class BeginnerSeries5TriangularNumbers
+    {
+        public bool isTriangular(int T)
+        {
+            return Math.Sqrt(T * 8 + 1) % 1 == 0;
+            // return (Math.Sqrt(2 * T + .25) - .5) % 1 == 0;
+            // return (Math.Sqrt(8 * T + 1) - 1) / 2 % 1 == 0;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BeginnerSeries5TriangularNumbersTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BeginnerSeries5TriangularNumbersTests
+    {
+        BeginnerSeries5TriangularNumbers triangular = new BeginnerSeries5TriangularNumbers();
+
+        [Test]
+        public void TriangularTests()
+        {
+            Assert.AreEqual(true, triangular.isTriangular(6));
+        }
+    }
+}
