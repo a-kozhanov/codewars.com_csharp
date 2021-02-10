03e4c17eb8591435d5d86b7ffaf68ef16c544f0a
a-kozhanov <a.kozhanov@outlook.com>
2021-02-10 16:22:17 +0300

    Added solution "Steer the Ship" 7 kyu

---
+++ CodeWarsTests/7kyu/SteerShipTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SteerShipTests
+    {
+        [Test]
+        public void ExampleTestCases()
+        {
+            Assert.AreEqual("R: 45", SteerShip.Steer(1, 1));
+            Assert.AreEqual("R: 90", SteerShip.Steer(1, 0));
+            Assert.AreEqual("L: 30", SteerShip.Steer(-1, Math.Sqrt(3)));
+
+            Assert.IsTrue(SteerShip.Steer(0, 1) == "R: 0" || SteerShip.Steer(0, 1) == "L: 0");
+            Assert.IsTrue(SteerShip.Steer(0, -1) == "R: 180" || SteerShip.Steer(0, -1) == "L: 180");
+        }
+    }
+}
