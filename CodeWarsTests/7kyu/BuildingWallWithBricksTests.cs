1da8efe413584e2eee4f3196c31b3a59bca301bd
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-02-16 00:20:56 +0300

    Added solution "Building a wall with bricks" 7 kyu

---
+++ CodeWarsTests/7kyu/BuildingWallWithBricksTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BuildingWallWithBricksTests
+    {
+        [Test]
+        public void SimpleTests()
+        {
+            BuildingWallWithBricks brick = new BuildingWallWithBricks();
+            var case1 = brick.CalculateBricksCount(180, 45);
+            var case2 = brick.CalculateBricksCount(60, 5);
+            Assert.AreEqual("21L6M6S", case1);
+            Assert.AreEqual("1L", case2);
+        }
+    }
+}
