68aa30b68d9ef901091c475b9e866d9266a5d6e6
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-02-17 00:41:09 +0300

    Added solution "Calculate the Harmonic Conjugated Point of a Triplet of Aligned Points" 7 kyu

---
+++ CodeWarsTests/7kyu/CalculateHarmonicTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CalculateHarmonicTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Assert.AreEqual(9.3333, CalculateHarmonic.HarmPoints(6, 10, 11));
+            Assert.AreEqual(7.1429, CalculateHarmonic.HarmPoints(2, 10, 20));
+        }
+    }
+}
