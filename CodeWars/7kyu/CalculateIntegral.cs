460f1fd6187b85b50b0ad64d122ca2c41ceff676
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-02-18 11:38:53 +0300

    Added solution "Calculate Integral" 7 kyu

---
+++ CodeWars/7kyu/CalculateIntegral.cs
@@ -0,0 +1,41 @@
+﻿/*
+https://www.codewars.com/kata/55d32bff0cc60b2e7f000173/csharp
+
+7 kyu
+Calculate Integral
+
+##What are you dealing with...
+
+You're a clever programmer and too lazy to calculate integrals by your own!
+So you decide to write a method which will do your calculations.
+
+You got a linear function y = m * x + b and this is the code that is already existing:
+
+public class LinFunc
+{
+  public double M { get; set; }
+  public double B { get; set; }
+
+  public double GetY(double x)
+  {
+    // not implemented
+  }
+
+  public double CalcIntegral(int a, int b)
+  {
+    // not implemented
+  }
+}
+##TODO Here it is up to you! Implement the missing code for these two methods GetY(double x) and CalcIntegral(int from, int to).
+*/
+
+namespace CodeWars
+{
+    public class CalculateIntegral
+    {
+        public double M { get; set; }
+        public double B { get; set; }
+        public double GetY(double x) => M * x + B;
+        public double CalcIntegral(int from, int to) => GetY((to + from) / 2d) * (to - from);
+    }
+}
---
+++ CodeWarsTests/7kyu/CalculateIntegralTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CalculateIntegralTests
+    {
+        [Test]
+        [TestCase(0, 1, 0, 1, ExpectedResult = 1)]
+        [TestCase(0, 1, 0, 10, ExpectedResult = 10)]
+        [TestCase(1, 0, 0, 4, ExpectedResult = 8)]
+        [TestCase(2, 0, 2, 4, ExpectedResult = 12)]
+        public static double FixedTest(double m, double b, int from, int to)
+        {
+            CalculateIntegral f = new CalculateIntegral();
+            f.M = m;
+            f.B = b;
+            return f.CalcIntegral(from, to);
+        }
+    }
+}
