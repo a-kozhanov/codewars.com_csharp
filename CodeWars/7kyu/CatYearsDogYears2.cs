bf935bbcbdec91c3f3f299a7f590d0c0056dc8c0
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-02-20 10:01:22 +0300

    Added solution "Cat Years, Dog Years (2)" 7 kyu

---
+++ CodeWars/7kyu/CatYearsDogYears2.cs
@@ -0,0 +1,56 @@
+﻿/*
+https://www.codewars.com/kata/5a6d3bd238f80014a2000187/csharp
+
+7 kyu
+Cat Years, Dog Years (2)
+
+This is related to my other Kata about cats and dogs.
+
+Kata Task
+I have a cat and a dog which I got as kitten / puppy.
+I forget when that was, but I do know their current ages as catYears and dogYears.
+Find how long I have owned each of my pets and return as a list [ownedCat, ownedDog]
+
+NOTES:
+
+Results are truncated whole numbers of "human" years
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+References
+
+http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class CatYearsDogYears2
+    {
+        public static (int, int) OwnedCatAndDog(int catYears, int dogYears)
+        {
+            // var cat = catYears >= 15 ? 1 : 0;
+            // cat += catYears - 15 >= 9 ? 1 : 0;
+            // cat += catYears - 15 - 9 >= 4 ? (catYears - 15 - 9) / 4 : 0;
+            // var dog = dogYears >= 15 ? 1 : 0;
+            // dog += dogYears - 15 >= 9 ? 1 : 0;
+            // dog += dogYears - 15 - 9 >= 5 ? (dogYears - 15 - 9) / 5 : 0;
+            // return (cat, dog);
+
+            // var ownedCat = catYears < 15 ? 0 : catYears < 24 ? 1 : (catYears - 24) / 4 + 2;
+            // var ownedDog = dogYears < 15 ? 0 : dogYears < 24 ? 1 : (dogYears - 24) / 5 + 2;
+            // return (ownedCat, ownedDog);
+
+            return (catYears < 15 ? 0 : catYears < 24 ? 1 : (catYears - 24) / 4 + 2,
+                dogYears < 15 ? 0 : dogYears < 24 ? 1 : (dogYears - 24) / 5 + 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CatYearsDogYears2Tests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CatYearsDogYears2Tests
+    {
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual((1, 1), CatYearsDogYears2.OwnedCatAndDog(15, 15));
+        }
+
+        [Test]
+        public void Two()
+        {
+            Assert.AreEqual((2, 2), CatYearsDogYears2.OwnedCatAndDog(24, 24));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Assert.AreEqual((10, 10), CatYearsDogYears2.OwnedCatAndDog(56, 64));
+        }
+    }
+}
