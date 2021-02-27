24e861264b5417c68ffdb569138ad9cc07cd2299
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-02-28 00:28:29 +0300

    Added solution "Collatz Conjecture Length" 7 kyu

---
+++ CodeWarsTests/7kyu/CollatzConjectureLengthTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CollatzConjectureLengthTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(8, CollatzConjectureLength.Collatz(20));
+            Assert.AreEqual(18, CollatzConjectureLength.Collatz(15));
+        }
+    }
+}
