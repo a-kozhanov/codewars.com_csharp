8b1907f975ae76b5d63b6b7d618a3a9cfed3e0a1
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-02-27 14:36:07 +0300

    Added solution "Circle area inside square" 7 kyu

---
+++ CodeWars/7kyu/CircleAreaInsideSquare.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/5899aa695401a83a5c0000c4/csharp
+
+7 kyu
+Circle area inside square
+
+Circle area inside square
+Turn an area of a square in to an area of a circle that fits perfectly inside the square.
+
+Example
+
+You get the blue+red area and need to calculate the red area.
+
+Your function gets a number which represents the size of the square and should return the size of the circle. 
+The tests are rounded by 8 decimals to make sure multiple types of solutions work.
+
+squareAreaToCircle(16); //should return size of the circle
+You don't have to worry about error handling or negative number input: size >= 0.
+
+This kata might be simpler than you expect, but good luck!
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class CircleAreaInsideSquare
+    {
+        public static double SquareAreaToCircle(double size)
+        {
+            return Math.PI * size / 4;
+            //return size * 0.785398163397448;
+            //return Math.Pow(0.5 * Math.Sqrt(size), 2) * Math.PI;
+            //return 3.1415926535897932384626433832795 * size / 4;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CircleAreaInsideSquareTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CircleAreaInsideSquareTests
+    {
+        [Test]
+        public void Test9()
+        {
+            var result = CircleAreaInsideSquare.SquareAreaToCircle(9);
+            Assert.AreEqual(result.GetType(), typeof(double), "should a be double");
+            Assert.AreEqual(Math.Round(result, 8), Math.Round(7.0685834705770345d, 8));
+        }
+
+        [Test]
+        public void Test20()
+        {
+            var result = CircleAreaInsideSquare.SquareAreaToCircle(20);
+            Assert.AreEqual(Math.Round(result, 8), Math.Round(15.707963267948969d, 8));
+        }
+    }
+}
