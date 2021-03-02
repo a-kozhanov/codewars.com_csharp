56b5d42a4f774b06398a7006aab0099840a08ac6
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-02 09:39:09 +0300

    Added solution "Convert a linked list to a string" 7 kyu

---
+++ CodeWarsTests/7kyu/ConvertLinkedListToStringTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertLinkedListToStringTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("1 -> 2 -> 3 -> null",
+                ConvertLinkedListToString.Stringify(new ConvertLinkedListToString.Node(1,
+                    new ConvertLinkedListToString.Node(2, new ConvertLinkedListToString.Node(3)))));
+            
+            Assert.AreEqual("0 -> 1 -> 4 -> 9 -> 16 -> null",
+                ConvertLinkedListToString.Stringify(new ConvertLinkedListToString.Node(0,
+                    new ConvertLinkedListToString.Node(1,
+                        new ConvertLinkedListToString.Node(4,
+                            new ConvertLinkedListToString.Node(9, new ConvertLinkedListToString.Node(16)))))));
+            Assert.AreEqual("null", ConvertLinkedListToString.Stringify(null));
+        }
+    }
+}
