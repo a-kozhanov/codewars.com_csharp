5382d82f13ca525901baba86cbd8c4b3ac066288
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-03 10:36:01 +0300

    Added solution "Counting power sets" 7 kyu

---
+++ CodeWarsTests/7kyu/CountingPowerSetsTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CountingPowerSetsTests
+    {
+        [Test, Description("Sample tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(1, CountingPowerSets.Powers(new int[] { }));
+            Assert.AreEqual(2, CountingPowerSets.Powers(new int[] {1}));
+            Assert.AreEqual(4, CountingPowerSets.Powers(new int[] {1, 2}));
+            Assert.AreEqual(16, CountingPowerSets.Powers(new int[] {1, 2, 3, 4}));
+        }
+    }
+}
