e6d24489547fbd4d36fd5ff3ee30c75800bff9c9
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-04 10:19:46 +0300

    Added solution "Weird words" 7 kyu

---
+++ CodeWars/7kyu/WeirdWords.cs
@@ -0,0 +1,40 @@
+﻿/*
+https://www.codewars.com/kata/57b2020eb69bfcbf64000375/csharp
+
+7 kyu
+Weird words
+
+In this kata you will have to change every letter in a given string to the next letter in the alphabet. 
+You will write a function nextLetter (Javascript) / next_letter (Python, Ruby) to do this. 
+The function will take a single parameter s (string).
+
+Examples:
+
+"Hello" --> "Ifmmp"
+
+"What is your name?" --> "Xibu jt zpvs obnf?"
+
+"zoo" --> "app"
+
+"zzZAaa" --> "aaABbb"
+Note: spaces and special characters should remain the same. 
+Capital letters should transfer in the same way but remain capitilized.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class WeirdWords
+    {
+        public static string NextLetter(string str)
+        {
+            //return string.Concat(str.Select(c => char.IsLetter(c) ? (char) (c + (c % 32 == 26 ? -25 : 1)) : c));
+            return string.Concat(str.Select(c => char.IsLetter(c) ? (char) (c + ("zZ".Contains(c) ? -25 : 1)) : c));
+            //return string.Concat(str.Select(c => char.IsLetter(c) ? (char) (c + (char.ToLower(c) == 'z' ? -25 : 1)) : c));
+            //return string.Concat(str.Select(c => char.IsLetter(c) ? "zZ".Contains(c) ? (char) (c - 25) : (char) (c + 1) : c));
+            //return string.Concat(str.Select(c => char.IsLetter(c) ? char.IsLetter((char) (c + 1)) ? (char) (c + 1) : (char) (c - 25) : c));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/WeirdWordsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WeirdWordsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Nz Obnf Jt App", WeirdWords.NextLetter("My Name Is Zoo"));
+            Assert.AreEqual("Xibu jt zpvs obnf", WeirdWords.NextLetter("What is your name"));
+            Assert.AreEqual("aPp", WeirdWords.NextLetter("zOo"));
+        }
+    }
+}
