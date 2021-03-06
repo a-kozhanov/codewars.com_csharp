2ad05ed5acf3cddf0b9ceb636fffc3ec26bdba67
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-07 00:28:56 +0300

    Added solution "Describe the shape" 7 kyu

---
+++ CodeWars/7kyu/DescribeShape.cs
@@ -0,0 +1,38 @@
+﻿/*
+https://www.codewars.com/kata/59a1ea8b70e25ef8e3002992/csharp
+
+7 kyu
+Describe the shape
+
+You will be given the number of angles of a shape with equal sides and angles, 
+and you need to return the number of its sides, and the measure of the interior angles.
+
+Should the number be equal or less than 2, return:
+
+"this will be a line segment or a dot"
+Otherwise return the result in the following format:
+
+"This shape has s sides and each angle measures d"
+(replace s with number of sides and d with the measure of the interior angles).
+
+Angle measure should be floored to the nearest integer.
+
+Number of sides will be tested from 0 to 180.
+
+Have Fun!
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class DescribeShape
+    {
+        public static string DescribeTheShape(int angles)
+        {
+            return angles > 2
+                ? $"This shape has {angles} sides and each angle measures {(angles - 2) * 180 / angles}"
+                : "this will be a line segment or a dot";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/DescribeShapeTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DescribeShapeTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(6).Returns("This shape has 6 sides and each angle measures 120");
+                yield return new TestCaseData(3).Returns("This shape has 3 sides and each angle measures 60");
+                yield return new TestCaseData(8).Returns("This shape has 8 sides and each angle measures 135");
+                yield return new TestCaseData(90).Returns("This shape has 90 sides and each angle measures 176");
+                yield return new TestCaseData(2).Returns("this will be a line segment or a dot");
+                yield return new TestCaseData(1).Returns("this will be a line segment or a dot");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(int angles) => DescribeShape.DescribeTheShape(angles);
+    }
+}
