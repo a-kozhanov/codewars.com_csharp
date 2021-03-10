5825c5bb3bf1bb602085097bf251151332157831
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-10 11:20:16 +0300

    Added solution "Chinese Zodiac" 7 kyu

---
+++ CodeWarsTests/7kyu/ChineseZodiacTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ChineseZodiacTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Wood Snake", KataChineseZodiac.ChineseZodiac(1965));
+            Assert.AreEqual("Earth Tiger", KataChineseZodiac.ChineseZodiac(1938));
+            Assert.AreEqual("Earth Tiger", KataChineseZodiac.ChineseZodiac(1998));
+            Assert.AreEqual("Fire Monkey", KataChineseZodiac.ChineseZodiac(2016));
+            Assert.AreEqual("Wood Rat", KataChineseZodiac.ChineseZodiac(1924));
+            Assert.AreEqual("Earth Monkey", KataChineseZodiac.ChineseZodiac(1968));
+            Assert.AreEqual("Water Dog", KataChineseZodiac.ChineseZodiac(2162));
+        }
+    }
+}
