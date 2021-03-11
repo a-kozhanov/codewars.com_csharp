768b0e5227f66ac1cbf002ec0ffa8b063c7e6479
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-11 10:18:53 +0300

    Added solution "draw me a chessboard" 7 kyu

---
+++ CodeWars/7kyu/DrawMeChessboard.cs
@@ -0,0 +1,63 @@
+﻿/*
+https://www.codewars.com/kata/56242b89689c35449b000059/csharp
+
+7 kyu
+draw me a chessboard
+
+A grid is a perfect starting point for many games (Chess, battleships, Candy Crush!).
+
+Making a digital chessboard I think is an interesting way of visualising how loops can work together.
+
+Your task is to write a function that takes two integers rows and columns and returns a chessboard pattern as a two dimensional array.
+
+So chessBoard(6,4) should return an array like this:
+
+[
+    ["O","X","O","X"],
+    ["X","O","X","O"],
+    ["O","X","O","X"],
+    ["X","O","X","O"],
+    ["O","X","O","X"],
+    ["X","O","X","O"]
+]
+And chessBoard(3,7) should return this:
+
+[
+    ["O","X","O","X","O","X","O"],
+    ["X","O","X","O","X","O","X"],
+    ["O","X","O","X","O","X","O"]
+]
+The white spaces should be represented by an: 'O'
+
+and the black an: 'X'
+
+The first row should always start with a white space 'O'
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class DrawMeChessboard
+    {
+        public static char[][] ChessBoard(int row, int columns)
+        {
+            return new int[row].Select((_, i) => new int[columns].Select((_, j) => (i + j) % 2 == 0 ? 'O' : 'X').ToArray()).ToArray();
+            //return new int[row].Select((_, i) => new int[columns].Select((_, j) => new[] {'O', 'X'}[(i + j) % 2]).ToArray()).ToArray();
+
+
+            // return Enumerable.Range(0, row)
+            //     .Select(i => Enumerable.Range(0, columns).Select(j => (i & 1) == (j & 1) ? 'O' : 'X').ToArray())
+            //     .ToArray();
+
+            // return Enumerable.Range(0, row)
+            //     .Select(x => Enumerable.Range(0, columns).Select(y => (y + x) % 2 == 0 ? 'O' : 'X').ToArray())s
+            //     .ToArray();
+
+
+            // return Enumerable.Range(0, row)
+            //     .Select(r => Enumerable.Range(0, columns).Select(c => (r + c) % 2 == 0 ? 'O' : 'X').ToArray())
+            //     .ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/DrawMeChessboardTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DrawMeChessboardTests
+    {
+        private Random rand = new Random();
+
+        [Test]
+        public void FormatTests()
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                var rows = rand.Next(1, 21);
+                var columns = rand.Next(1, 21);
+
+                Assert.AreEqual(rows, DrawMeChessboard.ChessBoard(rows, columns).Length,
+                    "make sure the board has the correct number of rows");
+                Assert.AreEqual(columns, DrawMeChessboard.ChessBoard(rows, columns)[0].Length,
+                    "make sure the board has the correct number of columns");
+            }
+        }
+
+        [Test]
+        public void AlternatingContentTests()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                var rows = rand.Next(2, 12);
+                var columns = rand.Next(2, 12);
+
+                Console.WriteLine(rows + " - rows, " + columns + " - columns");
+
+                var board = DrawMeChessboard.ChessBoard(rows, columns);
+
+                Assert.AreEqual(rows, board.Length, "make sure the board has the correct number of rows");
+
+                for (var j = 0; j < rows; j += 2)
+                {
+                    Assert.AreEqual('O', board[j][0]);
+                    if (j + 1 < rows)
+                    {
+                        Assert.AreEqual('X', board[j + 1][0]);
+                    }
+                }
+            }
+        }
+    }
+}
