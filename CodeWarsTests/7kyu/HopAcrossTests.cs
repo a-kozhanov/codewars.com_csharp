73b92f25c27354f907cc552f7bee84bf8af24803
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-12 19:48:06 +0300

    Added solution "Hop Across" 7 kyu

---
+++ CodeWarsTests/7kyu/HopAcrossTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataHopAcrossTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(KataHopAcross.HopAcross(new List<int> {1}), Is.EqualTo(2));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {2}), Is.EqualTo(2));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {1, 1}), Is.EqualTo(4));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {2, 1}), Is.EqualTo(3));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {2, 1, 1}), Is.EqualTo(5));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {1, 2, 1, 2}), Is.EqualTo(5));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {1, 2, 5, 1}), Is.EqualTo(5));
+            Assert.That(KataHopAcross.HopAcross(new List<int> {2, 2, 3, 1, 1, 2, 1}), Is.EqualTo(7));
+        }
+    }
+}
