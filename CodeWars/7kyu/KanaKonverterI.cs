89136821e9cfaea87300e9a72bef523f3868589b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-15 09:55:03 +0300

    Added solution "KanaKonverter I" 7 kyu

---
+++ CodeWars/7kyu/KanaKonverterI.cs
@@ -0,0 +1,53 @@
+﻿/*
+https://www.codewars.com/kata/574e96deff5b0ab00a000672/csharp
+
+7 kyu
+KanaKonverter I
+
+Write a simple converter for vowels.
+
+      Romaji:    a/A e/E i/I u/U o/O
+      Hiragana:  あ  え   い  う   お 
+      Katakana:  ア  エ   イ  ウ   オ
+Expect the above characters as input. There won't be invalid input.
+
+Rules:
+
+If there is no input return an empty string. 
+Do not change romaji. (a->a, A->A ... NOT A->a )
+Change Kana to lower case romaji. (あ->a ... NOT あ->A)
+Input can be mixed, but output should be consistent. (aAあア -> aAaa, for "romaji")
+Example 1:
+
+Input: "aeiou" 
+Output:"hiragana"
+
+Expected output: "あえいおう"
+Example 2:
+
+Input: "AえイOう"
+Output:"romaji"
+
+Expected output: "AeiOu"
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KanaKonverterI
+    {
+        public static string vowels(string input, string output)
+        {
+            var dict = new Dictionary<string, string>
+            {
+                ["romaji"] = "aeiouAEIOU",
+                ["hiragana"] = "あえいおうあえいおう",
+                ["katakana"] = "アエイオウアエイオウ"
+            };
+            var all = string.Concat(dict.Values);
+            return string.Concat(input.Select(c => dict[output][all.IndexOf(c) % 10]));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/KanaKonverterITests.cs
@@ -0,0 +1,81 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KanaKonverterITests
+    {
+        [Test]
+        public void romajiToHira()
+        {
+            Assert.AreEqual("あえいおう", KanaKonverterI.vowels("aeiou", "hiragana"));
+        }
+
+        [Test]
+        public void romajiToKata()
+        {
+            Assert.AreEqual("アエイオウ", KanaKonverterI.vowels("aeiou", "katakana"));
+        }
+
+        [Test]
+        public void romajiToRomaji()
+        {
+            Assert.AreEqual("aeiou", KanaKonverterI.vowels("aeiou", "romaji"));
+        }
+
+        [Test]
+        public void hiraToHira()
+        {
+            Assert.AreEqual("あえいおう", KanaKonverterI.vowels("あえいおう", "hiragana"));
+        }
+
+        [Test]
+        public void hiraToKata()
+        {
+            Assert.AreEqual("アエイオウ", KanaKonverterI.vowels("あえいおう", "katakana"));
+        }
+
+        [Test]
+        public void hiraToRomaji()
+        {
+            Assert.AreEqual("aeiou", KanaKonverterI.vowels("あえいおう", "romaji"));
+        }
+
+        [Test]
+        public void kataToHira()
+        {
+            Assert.AreEqual("あえいおう", KanaKonverterI.vowels("アエイオウ", "hiragana"));
+        }
+
+        [Test]
+        public void kataToKata()
+        {
+            Assert.AreEqual("アエイオウ", KanaKonverterI.vowels("アエイオウ", "katakana"));
+        }
+
+        [Test]
+        public void kataToRomaji()
+        {
+            Assert.AreEqual("aeiou", KanaKonverterI.vowels("アエイオウ", "romaji"));
+        }
+
+        [Test]
+        public void noInput()
+        {
+            Assert.AreEqual("", KanaKonverterI.vowels("", "romaji"));
+        }
+
+        [Test]
+        public void emptyParameters()
+        {
+            Assert.AreEqual("", KanaKonverterI.vowels("", ""));
+        }
+
+        [Test]
+        public void Uppercase()
+        {
+            Assert.AreEqual("aAeEoOUu", KanaKonverterI.vowels("aAeEoOUu", "romaji"));
+        }
+    }
+}
