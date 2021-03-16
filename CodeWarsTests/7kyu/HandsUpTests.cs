65db5461a5cfbece765df002f60f825f964e5983
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-16 09:36:11 +0300

    Added solution "Hands Up" 7 kyu

---
+++ CodeWarsTests/7kyu/HandsUpTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HandsUpTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(new int[] {2, 1, 0}, HandsUp.GetPositions(5));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(new int[] {0, 2, 0}, HandsUp.GetPositions(33));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(new int[] {0, 0, 0}, HandsUp.GetPositions(0));
+        }
+    }
+}
