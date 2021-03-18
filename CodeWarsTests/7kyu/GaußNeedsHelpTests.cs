2ba4bacabe1ecfca843914851692173178230168
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-18 13:40:08 +0300

    Added solution "Gauß needs help! (Sums of a lot of numbers)" 7 kyu

---
+++ CodeWarsTests/7kyu/GaußNeedsHelpTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GaußNeedsHelpTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(100l).Returns(5050l);
+                yield return new TestCaseData(300l).Returns(45150l);
+                yield return new TestCaseData(50000l).Returns(1250025000l);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public long? Test(long n) => GaußNeedsHelp.RangeSum(n);
+    }
+}
