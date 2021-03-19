3aa4a26ed31bea8658f85b4c254a9ca5e9a68bee
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-19 09:40:11 +0300

    Added solution "Bob's Short Forms" 7 kyu

---
+++ CodeWars/7kyu/BobsShortForms.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/570cbe88f616a8f4f50011ac/csharp
+
+7 kyu
+Bob's Short Forms
+
+Bob is a theoretical coder - he doesn't write code, but comes up with theories, formulas and algorithm ideas. 
+You are his secretary, and he has tasked you with writing the code for his newest project - a method for making the short form of a word. 
+Write a function shortForm(C# ShortForm, Python short_form) that takes a string and returns it converted into short form using the rule: 
+Remove all vowels, except for those that are the first or last letter. 
+Do not count 'y' as a vowel, and ignore case. 
+Also note, the string given will not have any spaces; only one word, and only Roman letters.
+
+Example:
+
+shortForm("assault");
+short_form("assault")
+ShortForm("assault");
+// should return "asslt"
+
+
+Also, FYI: I got all the words with no vowels from
+https://en.wikipedia.org/wiki/English_words_without_vowels
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class BobsShortForms
+    {
+        public string ShortForm(string str)
+        {
+            // return Regex.Replace(str, "(?<!^)([aouei])(?<!$)", string.Empty, RegexOptions.IgnoreCase);
+            // return Regex.Replace(str, @"(?<=[^\^])[aeuio](?=[^$])", "", RegexOptions.IgnoreCase);
+            // return Regex.Replace(str, @"(?<!\A)[aeiou](?!\z)", "", RegexOptions.IgnoreCase);
+            // return Regex.Replace(str, @"(?<!^)[aeiouAEIOU](?!$)", "");
+            // return Regex.Replace(str, @"(?i)(?<=.)[euioa](?=.)", "");
+            // return Regex.Replace(str, "(?<!^)[aeouiAEOUI](?!$)", "");
+            // return Regex.Replace(str, @"(?i)\B([aouei])\B", "");
+            return Regex.Replace(str, @"(?i)\B[aeuoi]\B", "");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BobsShortFormsTests.cs
@@ -0,0 +1,43 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BobsShortFormsTests
+    {
+        [Test]
+        public void TestCase()
+        {
+            var kata = new BobsShortForms();
+
+            //Should return word without vowels
+            Assert.AreEqual("typhd", kata.ShortForm("typhoid"));
+            Assert.AreEqual("fre", kata.ShortForm("fire"));
+            Assert.AreEqual("dstry", kata.ShortForm("destroy"));
+            Assert.AreEqual("kta", kata.ShortForm("kata"));
+            Assert.AreEqual("cdwrs", kata.ShortForm("codewars"));
+
+            //Should ignore vowels at beginning or end of word
+            Assert.AreEqual("assrt", kata.ShortForm("assert"));
+            Assert.AreEqual("insne", kata.ShortForm("insane"));
+            Assert.AreEqual("nce", kata.ShortForm("nice"));
+            Assert.AreEqual("amzng", kata.ShortForm("amazing"));
+            Assert.AreEqual("incrrgble", kata.ShortForm("incorrigible"));
+
+            //Should be case-insenstive
+            Assert.AreEqual("HllO", kata.ShortForm("HeEllO"));
+            Assert.AreEqual("inCRdBLE", kata.ShortForm("inCRediBLE"));
+            Assert.AreEqual("IMpsSblE", kata.ShortForm("IMpOsSiblE"));
+            Assert.AreEqual("UnnTNtNl", kata.ShortForm("UnInTENtiONAl"));
+            Assert.AreEqual("AWSme", kata.ShortForm("AWESOme"));
+
+            //Should support input with no vowels
+            Assert.AreEqual("rhythm", kata.ShortForm("rhythm"));
+            Assert.AreEqual("hymn", kata.ShortForm("hymn"));
+            Assert.AreEqual("lynx", kata.ShortForm("lynx"));
+            Assert.AreEqual("nymph", kata.ShortForm("nymph"));
+            Assert.AreEqual("pygmy", kata.ShortForm("pygmy"));
+        }
+    }
+}
