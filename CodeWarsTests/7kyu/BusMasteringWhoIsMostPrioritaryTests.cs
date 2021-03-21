b4fb7ef16654f34b98820954b43fd71d9566e05a
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-21 03:27:14 +0300

    Added solution "Bus mastering - Who is the most prioritary?" 7 kyu

---
+++ CodeWarsTests/7kyu/BusMasteringWhoIsMostPrioritaryTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BusMasteringWhoIsMostPrioritaryTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("001000000", BusMasteringWhoIsMostPrioritary.arbitrate("001000101", 9));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("000000100", BusMasteringWhoIsMostPrioritary.arbitrate("000000101", 9));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("0000", BusMasteringWhoIsMostPrioritary.arbitrate("0000", 4));
+        }
+    }
+}
