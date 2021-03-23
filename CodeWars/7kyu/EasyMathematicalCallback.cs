0445d4a1e020cdc2bedcfcf2c26d6c3cd7134a94
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-23 19:21:48 +0300

    Added solution "Easy mathematical callback" 7 kyu

---
+++ CodeWars/7kyu/EasyMathematicalCallback.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/54b7c8d2cd7f51a839000ebf/csharp
+
+7 kyu
+Easy mathematical callback
+
+Task
+Write the processArray function, which takes an array and a callback function as parameters. 
+The callback function can be, for example, a mathematical function that will be applied on each element of this array. 
+Optionally, also write tests similar to the examples below.
+
+Examples
+Array [4, 8, 2, 7, 5] after processing with function
+
+Kata.ProcessArray(new int[] {4, 8, 2, 7, 5}, (Func<int, int>)((v) => v * 2));
+will be [ 8, 16, 4, 14, 10 ].
+
+Array [7, 8, 9, 1, 2] after processing with function
+
+Kata.ProcessArray(new int[] {7, 8, 9, 1, 2}, (Func<int, int>)((v) => v + 5));
+will be [ 12, 13, 14, 6, 7 ].
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class EasyMathematicalCallback
+    {
+        public static int[] ProcessArray(int[] arr, Func<int, int> callback)
+        {
+            return arr.Select(callback).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/EasyMathematicalCallbackTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EasyMathematicalCallbackTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] {4, 8, 2, 7, 5}, (Func<int, int>) ((v) => v * 2)).Returns(
+                    new int[] {8, 16, 4, 14, 10});
+                yield return new TestCaseData(new int[] {7, 8, 9, 1, 2}, (Func<int, int>) ((v) => v + 5)).Returns(
+                    new int[] {12, 13, 14, 6, 7});
+                yield return new TestCaseData(new int[] {-1, 1, 2, 3, 4, 5}, (Func<int, int>) ((v) => v * v * v))
+                    .Returns(new int[] {-1, 1, 8, 27, 64, 125});
+                yield return new TestCaseData(new int[] { }, (Func<int, int>) ((v) => v + 1)).Returns(new int[] { });
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int[] Test(int[] arr, Func<int, int> callback) => EasyMathematicalCallback.ProcessArray(arr, callback);
+    }
+}
