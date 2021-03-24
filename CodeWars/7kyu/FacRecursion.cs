7a555a93433ce6bde56d49caefa5756f3136ebd0
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-24 10:13:19 +0300

    Added solution "Fac Recursion (Pest control - find the bugs and fix them)" 7 kyu

---
+++ CodeWars/7kyu/FacRecursion.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/57fa92b25c9910e7bc0001df/csharp
+
+7 kyu
+Fac Recursion (Pest control - find the bugs and fix them)
+
+The code is a recursive attempt in calculating the result of a factorial function.
+I.e. the result of 3! (Factorial of 3) is 3x2x1 = 6.
+In case of 0! the result is 1 and in case of a negative factorial the result should be 0.
+
+The goal is to find the bugs in the sourcecode and fix them (play a little pest control).
+*/
+
+namespace CodeWars
+{
+    public class KataFacRecursion
+    {
+        // public int FacCalculation(int n)
+        // {
+        //     return n < 0 ? 0 : n == 0 ? 1 : n * FacCalculation(n - 1);
+        // }
+
+        public int FacCalculation(int startValue)
+        {
+            return startValue < 0 ? 0 :
+                startValue == 1 || startValue == 0 ? 1 : FacCalculation(startValue - 1) * startValue;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FacRecursionTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FacRecursionTests
+    {
+        [Test]
+        public void KataFacCalculationTest()
+        {
+            KataFacRecursion fac = new KataFacRecursion();
+
+            Assert.AreEqual(1, fac.FacCalculation(1));
+
+            Assert.AreEqual(6, fac.FacCalculation(3));
+        }
+    }
+}
