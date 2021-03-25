0f7621db3643a1960f2302f59115afc59738ca4c
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-25 11:07:46 +0300

    Added solution "Execute me nTimes" 7 kyu

---
+++ CodeWars/7kyu/ExecuteMeNTimes.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/5b2b4836b6989d207700005b/csharp
+
+7 kyu
+Execute me nTimes
+
+Fix a bug in provided method which should execute a passed action n (possibly hundreds) times. 
+The method is timing out and not completing in time.
+*/
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class ExecuteMeNTimes
+    {
+        public void Execute(Action a, int nTimes)
+        {
+            Parallel.For(0, nTimes, i => a());
+            //Parallel.Invoke(Enumerable.Repeat(a, nTimes).ToArray());
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ExecuteMeNTimesTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExecuteMeNTimesTests
+    {
+        private readonly object monitor = new object();
+
+        [Test]
+        public void SampleTests()
+        {
+            ExecuteMeNTimes w = new ExecuteMeNTimes();
+            Random rand = new Random();
+            int counter = 20;
+            int executionCounter = 0;
+
+            Action a = () =>
+            {
+                Console.Write('.');
+                Thread.Sleep(1000);
+                lock (monitor)
+                {
+                    executionCounter++;
+                }
+            };
+
+            w.Execute(a, 20);
+
+            if (counter != executionCounter)
+                Console.WriteLine($"Aciton was executed {executionCounter} times instead of {counter} times");
+
+            Assert.IsTrue(counter == executionCounter);
+        }
+    }
+}
