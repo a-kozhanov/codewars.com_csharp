155935635d983d0d1356ad856627478fb411cbed
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-27 13:34:49 +0300

    Added solution "Find factors of a number" 7 kyu

---
+++ CodeWars/7kyu/FindFactorsOfNumber.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/564fa92d1639fbefae00009d/csharp
+
+7 kyu
+Find factors of a number
+
+Create a function that takes a number and finds the factors of it, listing them in descending order in an array.
+If the parameter is not an integer or less than 1, return -1. In C# return an empty array.
+For Example: factors(54) should return [54, 27, 18, 9, 6, 3, 2, 1]
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindFactorsOfNumber
+    {
+        public static int[] Factors(int num)
+        {
+            return num < 1
+                ? new int[0]
+                : Enumerable.Range(1, num).Where(i => num % i == 0).OrderBy(i => -i).ToArray();
+
+            // return num < 1
+            //     ? new int[0]
+            //     : Enumerable.Range(1, num).Where(i => num % i == 0).OrderByDescending(i => i).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FindFactorsOfNumberTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindFactorsOfNumberTests
+    {
+        [Test]
+        [TestCase(54, ExpectedResult = new int[] {54, 27, 18, 9, 6, 3, 2, 1})]
+        [TestCase(9, ExpectedResult = new int[] {9, 3, 1})]
+        public static int[] FixedTest(int num)
+        {
+            return FindFactorsOfNumber.Factors(num);
+        }
+    }
+}
