6b7d10c9ede8ec59cfbb42d5feea19247b539141
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-03-29 23:28:20 +0300

    Added solution "Hells Kitchen" 7 kyu

---
+++ CodeWars/7kyu/HellsKitchen.cs
@@ -0,0 +1,83 @@
+﻿/*
+https://www.codewars.com/kata/57d1f36705c186d018000813/csharp
+
+7 kyu
+Hells Kitchen
+
+Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+
+Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+
+Rules:
+Obviously the words should be Caps, Every word should end with '!!!!', 
+Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class HellsKitchen
+    {
+        public static string Gordon(string a)
+        {
+            return Regex.Replace(Regex.Replace(a.ToUpper(), @"[EIOU]", "*"), @"\s|$", "!!!!$0").Replace("A", "@");
+            //return Regex.Replace(Regex.Replace(a.ToUpper(), @"\b(?<=\w)", "!!!!"), @"[AEIOU]", t => "" + t == "A" ? "@" : "*");
+
+            //return string.Join(" ", a.Split(' ').Select(x => Regex.Replace(x.ToUpper().Replace('A', '@'), "[IEOU]", "*") + "!!!!"));
+
+            //return Regex.Replace(Regex.Replace(a.ToUpper().Replace("A", "@"), @"[EIOU]", "*"), @"\s|$", "!!!!$0");
+
+            // return Regex.Replace(Regex.Replace(a.ToUpper().Replace(" ", "!!!! "), @"[Aa]", "@") + "!!!!", @"[AEIOU]", "*");
+
+
+            // return string.Join(" ", a.Split(" ").Select(t1 =>
+            //     string.Concat(t1.ToUpper().Select(t2 => t2 == 'A' ? '@' : "EIOU".Contains(t2) ? '*' : t2)) + "!!!!"));
+
+
+            // return string
+            //     .Concat(a.Select(c => "eouiu".Contains(c) ? "*" : c == ' ' ? "!!!! " : c == 'a' ? "@" : $"{c}"))
+            //     .ToUpper() + "!!!!";
+
+            // return Regex.Replace(Regex.Replace(a.ToUpper(), @"\b(?<=\w)", "!!!!"), @"[AEIOU]",
+            //     t => "" + t == "A" ? "@" : "*");
+
+
+            // return string.Join(" ",
+            //     a.ToUpper().Replace('A', '@').Split(' ')
+            //         .Select(item => Regex.Replace(item, @"[eiou]", "*", RegexOptions.IgnoreCase))
+            //         .Select(str => str + "!!!!"));
+
+
+            // return Regex.Replace(
+            //     a
+            //         .ToUpper()
+            //         .Replace(" ", "!!!! ")
+            //         .Replace("A", "@"),
+            //     "[EIOU]",
+            //     "*") + "!!!!";
+
+
+            // return a.ToUpper().Replace("A", "@").Replace("E", "*").Replace("I", "*").Replace("O", "*")
+            //     .Replace("U", "*").Replace(" ", "!!!! ") + "!!!!";
+
+
+            // var arr = a.Split().Select(x => char.ToUpper(x[0]) + x[1..])
+            //     .Select(x => x.Replace('a', '@').Replace('A', '@'));
+            //Select(x => (char.ToUpper(x[0]) + x[1..] + "!!!!"))
+
+
+            // var arr = a.Split().Select(x => string.Concat((x.ToUpper() + "!!!!").Select(c =>
+            // {
+            //     if ("aA".Contains(c)) return '@';
+            //     if ("eiouEIOU".Contains(c)) return '*';
+            //     return c;
+            // })));
+            //
+            // return string.Join(" ", arr);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HellsKitchenTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HellsKitchenTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!", HellsKitchen.Gordon("What feck damn cake"));
+            Assert.AreEqual("@R*!!!! Y**!!!! ST*!!!! P*D!!!!", HellsKitchen.Gordon("are you stu pid"));
+            Assert.AreEqual("*!!!! @M!!!! @!!!! CH*F!!!!", HellsKitchen.Gordon("i am a chef"));
+        }
+    }
+}
