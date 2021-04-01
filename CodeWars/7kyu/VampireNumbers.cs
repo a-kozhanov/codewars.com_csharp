fd408f9f2dffa728ba186a09618df72a3dc3a4c2
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-01 10:35:29 +0300

    Added solution "Vampire Numbers" 7 kyu

---
+++ CodeWars/7kyu/VampireNumbers.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/54d418bd099d650fa000032d/csharp
+
+7 kyu
+Vampire Numbers
+
+Vampire Numbers
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class VampireNumbers
+    {
+        public static bool vampire_test(long x, long y)
+        {
+            //return string.Concat($"{x}{y}".OrderBy(c => c)).Equals(string.Concat((x * y).ToString().OrderBy(c => c)));
+            //return string.Concat($"{x}{y}".OrderBy(c => c)) == string.Concat($"{x * y}".OrderBy(c => c));
+            //return (x * y).ToString().OrderBy(c => c).SequenceEqual((x.ToString() + y).OrderBy(c => c));
+            //return Enumerable.SequenceEqual($"{x}{y}".OrderBy(c => c), $"{x * y}".OrderBy(c => c));
+            //return $"{x * y}".OrderBy(c => c).SequenceEqual($"{x}{y}".OrderBy(c => c));
+            return $"{x}{y}".OrderBy(c => c).SequenceEqual($"{x * y}".OrderBy(c => c));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/VampireNumbersTests.cs
@@ -0,0 +1,43 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class VampireNumbersTests
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual(true, VampireNumbers.vampire_test(21, 6), "Basic: 21 * 6 = 126 should return True");
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual(true, VampireNumbers.vampire_test(204, 615),
+                "Basic: 204 * 615 = 125460 should return True");
+        }
+
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual(true, VampireNumbers.vampire_test(30, -51),
+                "One Negative: 30 * -51 = -1530 should return True");
+        }
+
+        [Test]
+        public void test4()
+        {
+            Assert.AreEqual(false, VampireNumbers.vampire_test(-246, -510),
+                "Double Negatives: -246 * -510 = 125460 should return False (The negative signs aren't present on the product)");
+        }
+
+        [Test]
+        public void test5()
+        {
+            Assert.AreEqual(true, VampireNumbers.vampire_test(210, 600),
+                "Trailing Zeroes: 210 * 600 = 126000 should return True");
+        }
+    }
+}
