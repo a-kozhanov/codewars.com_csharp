d55734ea8006d53651953e081c49bc2c1efdb8ad
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-03 00:27:07 +0300

    Added solution "Triple Shiftian Numbers" 7 kyu

---
+++ CodeWars/7kyu/TripleShiftianNumbers.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/56b7526481290c2ff1000c75/csharp
+
+7 kyu
+Triple Shiftian Numbers
+
+Much cooler than your run-of-the-mill Fibonacci numbers, the Triple Shiftian are so defined: T[n] = 4 * T[n-1] - 5 * T[n-2] + 3 * T[n-3].
+
+You are asked to create a function which accept a base with the first 3 numbers and then returns the nth element.
+
+tripleShiftian(new int[] { 1,1,1 },25) == 1219856746
+tripleShiftian(new int[] { 1,2,3 },25) == 2052198929
+tripleShiftian(new int[] { 6,7,2 },25) == -2575238999
+tripleShiftian(new int[] { 3,2,1 },35) == 23471258855679
+tripleShiftian(new int[] { 1,9,2 },2) ==  2
+Note: this is meant to be an interview quiz, so the description is scarce in detail on purpose
+
+Special thanks to the first person I met in person here in London just because of CW and that assisted me during the creation of this kata ;)
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class TripleShiftianNumbers
+    {
+        public static long TripleShiftian(int[] baseValues, int n)
+        {
+            var values = baseValues.Select(i => (long) i).ToList();
+            for (var i = 2; i <= n; i++)
+            {
+                values.Add(4 * values[i] - 5 * values[i - 1] + 3 * values[i - 2]);
+            }
+
+            return values[n];
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/TripleShiftianNumbersTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TripleShiftianNumbersTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(1219856746L, TripleShiftianNumbers.TripleShiftian(new int[] {1, 1, 1}, 25));
+            Assert.AreEqual(2052198929L, TripleShiftianNumbers.TripleShiftian(new int[] {1, 2, 3}, 25));
+            Assert.AreEqual(2827228055L, TripleShiftianNumbers.TripleShiftian(new int[] {3, 2, 1}, 25));
+            Assert.AreEqual(-2575238999L, TripleShiftianNumbers.TripleShiftian(new int[] {6, 7, 2}, 25));
+            Assert.AreEqual(10127083068293L, TripleShiftianNumbers.TripleShiftian(new int[] {1, 1, 1}, 35));
+            Assert.AreEqual(17037073417493L, TripleShiftianNumbers.TripleShiftian(new int[] {1, 2, 3}, 35));
+            Assert.AreEqual(23471258855679L, TripleShiftianNumbers.TripleShiftian(new int[] {3, 2, 1}, 35));
+            Assert.AreEqual(-21379280887590L, TripleShiftianNumbers.TripleShiftian(new int[] {6, 7, 2}, 35));
+            Assert.AreEqual(3L, TripleShiftianNumbers.TripleShiftian(new int[] {3, 2, 1}, 3));
+            Assert.AreEqual(2L, TripleShiftianNumbers.TripleShiftian(new int[] {6, 7, 2}, 2));
+        }
+    }
+}
