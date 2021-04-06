0f8c8574cd143c92eef393d129f3b1b7c33b78bf
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-06 23:28:51 +0300

    Added solution "Array of twins" 7 kyu

---
+++ CodeWars/7kyu/ArrayOfTwins.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/58fdc60ed94f25a863000092/csharp
+
+7 kyu
+Array of twins
+
+You are given an array of numbers. 
+Check if the array contains of pairs, i.e every number occurs exactly two times. 
+If true, return true, otherwise false. Your solution should not modify the input array.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ArrayOfTwins
+    {
+        public static bool Twins(int[] arr)
+        {
+            return arr.GroupBy(i => i).All(x => x.Count() == 2);
+            //return arr.Distinct().All(x => arr.Count(i => i == x) == 2);
+            //return arr.Count(x => arr.Count(i => i == x) == 2) == arr.Length;
+            //return arr.GroupBy(x => x).Count(x => x.Count() == 2) == arr.Length / 2;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ArrayOfTwinsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ArrayOfTwinsTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {new int[] {1, 2, 3, 1, 2, 3}}).Returns(true);
+                yield return new TestCaseData(new[] {new int[] {33, 5, 17, 5}}).Returns(false);
+                yield return new TestCaseData(new[] {new int[] {2, 16, 2, 17, 2, 16}}).Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(int[] arr) => ArrayOfTwins.Twins(arr);
+    }
+}
