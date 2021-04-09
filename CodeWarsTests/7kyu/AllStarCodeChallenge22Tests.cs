27fcc77178b6644e74c9d5109205de6b298b0fcc
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-10 00:42:58 +0300

    Added solution "All Star Code Challenge #22" 7 kyu

---
+++ CodeWarsTests/7kyu/AllStarCodeChallenge22Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AllStarCodeChallenge22Tests
+    {
+        private static void Act(int seconds, string expected)
+        {
+            var actual = AllStarCodeChallenge22.ToTime(seconds);
+            Console.WriteLine($"   Input: seconds = {seconds}\nExpected: {expected}\n  Actual: {actual}\n");
+            Assert.AreEqual(expected, actual, $"Input: seconds = {seconds}");
+        }
+
+        [Test(Description = "Fixed Tests")]
+        public void FixedTests()
+        {
+            Act(3600, "1 hour(s) and 0 minute(s)");
+            Act(3601, "1 hour(s) and 0 minute(s)");
+            Act(3500, "0 hour(s) and 58 minute(s)");
+            Act(323500, "89 hour(s) and 51 minute(s)");
+            Act(0, "0 hour(s) and 0 minute(s)");
+        }
+    }
+}
