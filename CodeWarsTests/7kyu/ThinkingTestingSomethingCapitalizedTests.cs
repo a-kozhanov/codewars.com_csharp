391bc1c05286dd0e734dddb15b6a2dcdd54823c7
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-13 10:23:57 +0300

    Added solution "Thinking & Testing : Something capitalized" 7 kyu

---
+++ CodeWarsTests/7kyu/ThinkingTestingSomethingCapitalizedTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkingTestingSomethingCapitalizedTEsts
+    {
+        [Test]
+        public void TestCase()
+        {
+            //return s.ToUpper() ?
+            Assert.AreEqual("", new ThinkingTestingSomethingCapitalized().Testit(""), "");
+            Assert.AreEqual("A", new ThinkingTestingSomethingCapitalized().Testit("a"), "");
+            Assert.AreEqual("B", new ThinkingTestingSomethingCapitalized().Testit("b"), "");
+            Assert.AreEqual("A A", new ThinkingTestingSomethingCapitalized().Testit("a a"), "");
+            Assert.AreEqual("A B", new ThinkingTestingSomethingCapitalized().Testit("a b"), "");
+            Assert.AreEqual("A B C", new ThinkingTestingSomethingCapitalized().Testit("a b c"), "");
+            //click "Submit" try more testcase...
+        }
+    }
+}
