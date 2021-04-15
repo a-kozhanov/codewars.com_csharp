95412a4912f4d55beb1cdba844790fdd980401ee
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-15 10:16:23 +0300

    Added solution "Everything Is Even" 7 kyu

---
+++ CodeWars/7kyu/EverythingIsEven.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/59c0cdbd5fb133c82b00007d/csharp
+
+7 kyu
+Everything Is Even
+
+Here at Conformity Inc. we only like even things. 
+We need you to put a stop to anything that does not conform to our strict even only policy.
+
+Write a function that will convert any number to its nearest even number. 
+When the number is halfway between two even numbers it should round away from zero.
+
+Examples
+
+1 => 2
+
+0.5 => 0
+
+-1 => -2
+
+-0.5 => 0
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class EverythingIsEven
+    {
+        public static int EnsureEven(double number)
+        {
+            return (int) Math.Round(number / 2, MidpointRounding.AwayFromZero) * 2;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/EverythingIsEvenTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EverythingIsEvenTests
+    {
+        [Test]
+        public void SimpleTests()
+        {
+            Assert.AreEqual(2, EverythingIsEven.EnsureEven(1));
+            Assert.AreEqual(2, EverythingIsEven.EnsureEven(1.356));
+        }
+    }
+}
