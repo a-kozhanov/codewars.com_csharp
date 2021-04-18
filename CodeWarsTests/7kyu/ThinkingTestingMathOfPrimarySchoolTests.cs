6c2f49630f67138124dd8de66498773e61c854d2
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-19 00:05:13 +0300

    Added solution "Thinking & Testing : Math of Primary School" 7 kyu

---
+++ CodeWarsTests/7kyu/ThinkingTestingMathOfPrimarySchoolTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkingTestingMathOfPrimarySchoolTests
+    {
+        [Test]
+        public void TestCase()
+        {
+            var kata = new ThinkingTestingMathOfPrimarySchool();
+            //Add? subtract? multiply? divide? Who and who?
+            Assert.AreEqual(0, kata.Testit(new int[] {0, 0, 0, 0}), "");
+            Assert.AreEqual(0, kata.Testit(new int[] {0, 0, 0, 1}), "");
+            Assert.AreEqual(0, kata.Testit(new int[] {0, 0, 1, 1}), "");
+            Assert.AreEqual(0, kata.Testit(new int[] {0, 1, 0, 1}), "");
+            Assert.AreEqual(1, kata.Testit(new int[] {0, 1, 1, 0}), "");
+            Assert.AreEqual(1, kata.Testit(new int[] {1, 0, 0, 1}), "");
+            //click "Submit" try more testcase...
+        }
+    }
+}
