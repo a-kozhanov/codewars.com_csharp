e9ae2633e6fdcd2f09fa81ef2147cbc66a9a6757
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-21 10:11:38 +0300

    Added solution "Bubblesort Once" 7 kyu

---
+++ CodeWarsTests/7kyu/BubblesortOnceTest.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataBubblesortOnceTest
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            // Example test case from description
+
+            var expected = new int[] {7, 5, 3, 1, 2, 4, 6, 8, 9};
+            var actual = new int[] {9, 7, 5, 3, 1, 2, 4, 6, 8};
+            Assert.AreEqual(expected, KataBubblesortOnce.BubbleSortOnce(actual));
+        }
+    }
+}
