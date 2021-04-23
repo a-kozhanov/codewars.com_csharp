c93677830a0527dc4ec1539978c8e8c90970fff9
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-24 00:02:48 +0300

    Added solution "Comfortable words" 7 kyu

---
+++ CodeWars/7kyu/ComfortableWords.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/56684677dc75e3de2500002b/csharp
+
+7 kyu
+Comfortable words
+
+A comfortable word is a word which you can type always alternating the hand you type with (assuming you type using a QWERTY keyboard and use fingers as shown in the image below).
+
+That being said, create a function which receives a word and returns true/True if it's a comfortable word and false/False otherwise.
+
+The word will always be a string consisting of only ascii letters from a to z.
+
+To avoid problems with image availability, here's the lists of letters for each hand:
+
+Left: q, w, e, r, t, a, s, d, f, g, z, x, c, v, b
+Right: y, u, i, o, p, h, j, k, l, n, m
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataComfortableWords
+    {
+        public bool ComfortableWord(string word)
+        {
+            var s = string.Concat(word.Select(c => "yuiophjklnm".Contains(c) ? "R" : "L"));
+            return !(s.Contains("LL") || s.Contains("RR"));
+
+
+            // return !Regex.IsMatch(Regex.Replace(word, @"[qwertasdfgzxcvb]", " "), @"\s{2}|\w{2}");
+            // return word.Select((c, i) => "qwertasdfgzxcvb".Contains(c) ? i % 2 : (i + 1) % 2).Distinct().Count() == 1;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ComfortableWordsTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ComfortableWordsTests
+    {
+        private KataComfortableWords kata = new KataComfortableWords();
+
+        [Test]
+        public void ConfortableWords()
+        {
+            Assert.AreEqual(true, kata.ComfortableWord("yams"), "yams is a confortable word");
+            Assert.AreEqual(true, kata.ComfortableWord("odor"), "odor is a confortable word");
+            Assert.AreEqual(true, kata.ComfortableWord("their"), "their is a confortable word");
+        }
+
+        [Test]
+        public void NotConfortableWords()
+        {
+            Assert.AreEqual(false, kata.ComfortableWord("leisure"), "leisure is NOT a confortable word");
+            Assert.AreEqual(false, kata.ComfortableWord("touts"), "touts is NOT a confortable word");
+            Assert.AreEqual(false, kata.ComfortableWord("test"), "test is NOT a confortable word");
+        }
+    }
+}
