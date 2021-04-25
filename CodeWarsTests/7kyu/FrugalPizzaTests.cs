3b4c2cc9fadda4bf62b2ad9bf84cd2d28d13c818
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-25 15:13:27 +0300

    Added solution "Frugal Pizza" 7 kyu

---
+++ CodeWarsTests/7kyu/FrugalPizzaTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FrugalPizzaTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(0.11, FrugalPizza.PizzaPrice(7, 4.30));
+            Assert.AreEqual(0.09, FrugalPizza.PizzaPrice(9, 5.95));
+            Assert.AreEqual(0.07, FrugalPizza.PizzaPrice(12, 7.90));
+        }
+    }
+}
