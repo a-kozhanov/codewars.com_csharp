1034cb16dec865a4a0848982243333d6cce9c8a6
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-04-29 09:14:48 +0300

    Added solution "Nth Root of a Number" 7 kyu

---
+++ CodeWarsTests/7kyu/NthRootOfNumberTests.cs
@@ -0,0 +1,44 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NthRootOfNumberTests
+    {
+        [Test]
+        public static void Test1()
+        {
+            double actual = NthRootOfNumber.FindRoot(4, 2);
+            Assert.AreEqual(2, actual, 1e-9);
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            double actual = NthRootOfNumber.FindRoot(8, 3);
+            Assert.AreEqual(2, actual, 1e-9);
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            double actual = NthRootOfNumber.FindRoot(256, 4);
+            Assert.AreEqual(4, actual, 1e-9);
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            double actual = NthRootOfNumber.FindRoot(9, 2);
+            Assert.AreEqual(3, actual, 1e-9);
+        }
+
+        [Test]
+        public static void Test5()
+        {
+            double actual = NthRootOfNumber.FindRoot(6.25, 2);
+            Assert.AreEqual(2.5, actual, 1e-9);
+        }
+    }
+}
