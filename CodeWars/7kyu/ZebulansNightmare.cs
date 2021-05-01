cb43a866a1e82c5ef982e44682381bc4d59b045f
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-01 21:40:29 +0300

    Added solution "Zebulan's Nightmare" 7 kyu

---
+++ CodeWars/7kyu/ZebulansNightmare.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/570fd7ad34e6130455001835/csharp
+
+7 kyu
+Zebulan's Nightmare
+
+Zebulan has worked hard to write all his python code in strict compliance to PEP8 rules. 
+In this kata, you are a mischievous hacker that has set out to sabotage all his good code.
+
+Your job is to take PEP8 compatible function names and convert them to camelCase. For example:
+
+zebulansNightmare('camel_case') == 'camelCase'
+zebulansNightmare('zebulans_nightmare') == 'zebulansNightmare'
+zebulansNightmare('get_string') == 'getString'
+zebulansNightmare('convert_to_uppercase') == 'convertToUppercase'
+zebulansNightmare('main') == 'main'
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataZebulansNightmare
+    {
+        public static string ZebulansNightmare(string functionName)
+        {
+            return Regex.Replace(functionName, "_.", m => $"{char.ToUpper(m.Value[1])}");
+            // return Regex.Replace(functionName, "_.", m => "" + char.ToUpper(m.Value[1]));
+            // return Regex.Replace(functionName, @"_\w", m => (m.Value[1] + "").ToUpper());_(\\w)
+            // return Regex.Replace(functionName, "_(\\w)", m => m.ToString().ToUpper()[1..]);
+            // return Regex.Replace(functionName, "_(\\w)", m => m.ToString().ToUpper().Substring(1));
+            // return Regex.Replace(functionName, @"_[a-z]", m => m.ToString().TrimStart('_').ToUpper());
+            // return string.Concat(functionName.Split('_').Select((x, i) => i > 0 ? char.ToUpper(x[0]) + x[1..] : x));
+            // return string.Concat(functionName.Split('_').Select((x, i) => i == 0 ? x : char.ToUpper(x[0]) + x.Substring(1)));
+            // return string.Concat(functionName.ToLower().Split('_').Select((s, i) => i == 0 ? s : char.ToUpper(s[0]) + s.Substring(1)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ZebulansNightmareTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataZebulansNightmareTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("camelCase", KataZebulansNightmare.ZebulansNightmare("camel_case"));
+            Assert.AreEqual("markAsIssue", KataZebulansNightmare.ZebulansNightmare("mark_as_issue"));
+            Assert.AreEqual("copyPastePep8", KataZebulansNightmare.ZebulansNightmare("copy_paste_pep8"));
+            Assert.AreEqual("gotoNextKata", KataZebulansNightmare.ZebulansNightmare("goto_next_kata"));
+            Assert.AreEqual("repeat", KataZebulansNightmare.ZebulansNightmare("repeat"));
+            Assert.AreEqual("trollingIsFun", KataZebulansNightmare.ZebulansNightmare("trolling_is_fun"));
+            Assert.AreEqual("why", KataZebulansNightmare.ZebulansNightmare("why"));
+            Assert.AreEqual("123AbcDef", KataZebulansNightmare.ZebulansNightmare("123_abc_def"));
+        }
+    }
+}
