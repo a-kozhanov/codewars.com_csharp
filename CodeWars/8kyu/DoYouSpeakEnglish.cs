0af7368680bea9d48cb2173770092dddeccd8f9a
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-05 10:23:54 +0300

    Added solution "Do you speak "English"?" 7 kyu

---
+++ CodeWars/8kyu/DoYouSpeakEnglish.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/csharp
+
+8 kyu
+Do you speak "English"?
+
+Given a string of arbitrary length with any ascii characters. 
+Write a function to determine whether the string contains the whole word "English".
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+Return value as boolean values, true for the string to contains "English", false for it does not.
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class DoYouSpeakEnglish
+    {
+        public static bool SpeakEnglish(string sentence)
+        {
+            return Regex.IsMatch(sentence, "(?i)english");
+            // return sentence.ToLower().Contains("english");
+            // return new Regex("(?i)English").IsMatch(sentence);
+            // return sentence.ToLower().IndexOf("english") != -1;
+            // return Regex.Match(sentence, "(?i)english").Success;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/DoYouSpeakEnglishTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DoYouSpeakEnglishTests
+    {
+        [TestCase("hello world", false)]
+        [TestCase("egnlish", false)]
+        [TestCase("", false)]
+        [TestCase("english", true)]
+        [TestCase("1234english ;k", true)]
+        [TestCase("I speak English", true)]
+        [TestCase("ABC123DOREME", false)]
+        [TestCase("eNgliSh", true)]
+        [TestCase("1234#$%%eNglish ;k9", true)]
+        [TestCase("spanish, english, french", true)]
+        [TestCase("englishENGLISHEnGlISHENglisH", true)]
+        [TestCase("HeLlO tHeR3 enGLiSh MuffIN", true)]
+        public void SampleTests(string str, bool expected)
+        {
+            var actual = DoYouSpeakEnglish.SpeakEnglish(str);
+            Assert.AreEqual(expected, actual, $"Input: \"{str}\"");
+        }
+    }
+}
