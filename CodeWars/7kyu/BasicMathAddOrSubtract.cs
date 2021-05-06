5891bfc0812e8f9081e0b731ba08754cbba1a861
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-06 12:37:39 +0300

    Added solution "Basic Math (Add or Subtract)" 7 kyu

---
+++ CodeWars/7kyu/BasicMathAddOrSubtract.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/5809b62808ad92e31b000031/csharp
+
+7 kyu
+Basic Math (Add or Subtract)
+
+In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+Note: the input will not be empty.
+
+Examples
+"1plus2plus3plus4"  --> "10"
+"1plus2plus3minus4" -->  "2"
+*/
+
+using System;
+using System.Data;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class BasicMathAddOrSubtract
+    {
+        public static string Calculate(string str)
+        {
+            return str.Replace("plus", " ").Replace("minus", " -").Split().Sum(int.Parse).ToString();
+            // return str.Replace("plus", " +").Replace("minus", " -").Split().Sum(int.Parse).ToString();
+            // return new DataTable().Compute(str.Replace("plus", "+").Replace("minus", "-"), null).ToString();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BasicMathAddOrSubtractTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BasicMathAddOrSubtractTests
+    {
+        [Test(Description = "Basic (fixed) tests")]
+        public void BasicTests()
+        {
+            Test("10", "1plus2plus3plus4");
+            Test("-8", "1minus2minus3minus4");
+            Test("2", "1plus2plus3minus4");
+            Test("-2", "1minus2plus3minus4");
+            Test("-1", "1plus2minus3plus4minus5");
+        }
+
+        private void Test(string solution, string input)
+        {
+            Assert.AreEqual(solution, BasicMathAddOrSubtract.Calculate(input),
+                $"Incorrect answer for input \"{input}\"");
+        }
+    }
+}
