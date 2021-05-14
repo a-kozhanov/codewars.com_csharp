d52f672b15cd05f9095a7dc62cdc1df833e5d0e3
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-14 20:50:26 +0300

    Added solution "Failed Filter - Bug Fixing #3" 7 kyu

---
+++ CodeWars/7kyu/FailedFilterBugFixing3.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/55c606e6babfc5b2c500007c/csharp
+
+7 kyu
+Failed Filter - Bug Fixing #3
+
+Failed Filter - Bug Fixing #3
+Oh no, Timmy's filter doesn't seem to be working? 
+Your task is to fix the FilterNumber function to remove all the numbers from the string.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class FailedFilterBugFixing3
+    {
+        public static string FilterNumbers(string str)
+        {
+            return string.Concat(str.Where(c => !char.IsNumber(c)));
+            // return string.Concat(str.ToCharArray().Where(c => !"0123456789".Contains(c)));
+            // return new string(str.ToCharArray().Where(c => c < '0' || c > '9').ToArray());
+            // return new string(str.Where(c => Char.IsLetter(c) || c == '-').ToArray());
+            // return string.Concat(str.ToCharArray().Where(c => !char.IsDigit(c)));
+            // return new string(str.Where(c => !Char.IsDigit(c)).ToArray());
+            // return string.Concat(str.Where(c => !char.IsNumber(c)));
+            // return string.Concat(str.Where(c => !char.IsDigit(c)));
+            // return Regex.Replace(str, "[0-9]", "");
+            // return Regex.Replace(str, @"\d", "");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FailedFilterBugFixing3Tests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FailedFilterBugFixing3Tests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            Assert.AreEqual("test", FailedFilterBugFixing3.FilterNumbers("test123"));
+            Assert.AreEqual("abc", FailedFilterBugFixing3.FilterNumbers("a1b2c3"));
+            Assert.AreEqual("aabbccdd", FailedFilterBugFixing3.FilterNumbers("aa1bb2cc3dd"));
+        }
+    }
+}
