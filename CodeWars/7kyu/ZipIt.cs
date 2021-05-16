03d6722aa845442a0fbf99c593f98282aada0dbd
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-16 22:20:44 +0300

    Added solution "Zip it!" 7 kyu

---
+++ CodeWars/7kyu/ZipIt.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/56aaf25213edd3a88a000002/csharp
+
+7 kyu
+Zip it!
+
+Write
+
+that merges the corresponding elements of two sequences using a specified selector function fn (a block in Ruby)
+
+For example:
+
+var a = new object[] { 1, 2, 3, 4, 5 };
+var b = new object[] { 'a','b' };
+a.ZipIt(b, (c, d) => c + "" + d); --> new object[] { '1a', '2b' }
+
+var a = new object[] { 1, 2, 3, 4, 5 };
+var b = new object[] {'a','b','c','d','e'};
+a.ZipIt(b, (c, d) => (int)c + (int)(char)d)); --> new object[] { 98, 100, 102, 104, 106 }
+if arrays have different lengths, go up to the minimum length and then stop.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class KataZipIt
+    {
+        public static object[] ZipIt(this object[] a, object[] b, Func<object, object, object> fn)
+        {
+            return a.Zip(b, fn).ToArray();
+            // return Enumerable.Zip(a, b, fn).ToArray();
+            // return a.Zip(b).Select(x => fn(x.First, x.Second)).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ZipItTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars
+{
+    public class KataZipItTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var a = new object[] {1, 2, 3, 4, 5};
+            var b = new object[] {'a', 'b', 'c', 'd', 'e'};
+
+            Assert.AreEqual(new object[] {"1a", "2b", "3c", "4d", "5e"}, a.ZipIt(b, (c, d) => c + "" + d));
+            Assert.AreEqual(new object[] {"a1", "b2", "c3", "d4", "e5"}, b.ZipIt(a, (c, d) => c + "" + d));
+            Assert.AreEqual(new object[] {"a97", "b196", "c297", "d400", "e505"},
+                b.ZipIt(a.ZipIt(b, (c, d) => ((int) c) * ((int) (char) d)), (c, d) => c + "" + d));
+        }
+    }
+}
