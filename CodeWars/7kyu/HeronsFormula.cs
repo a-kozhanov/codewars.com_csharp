c0b90b468e4984112c0d029048393758da32804e
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-17 09:34:50 +0300

    Added solution "Heron's formula" 7 kyu

---
+++ CodeWars/7kyu/HeronsFormula.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/57aa218e72292d98d500240f/csharp
+
+7 kyu
+Heron's formula
+
+Write function heron which calculates the area of a triangle with sides a, b, and c.
+
+Heron's formula: sqrt (s * (s - a) * (s - b) * (s - c)), where s = (a + b + c) / 2. Output should have 2 digits precision.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class HeronsFormula
+    {
+        public static double heron(double a, double b, double c)
+        {
+            var s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            // return Math.Sqrt(4 * a * a * b * b - Math.Pow(a * a + b * b - c * c, 2)) / 4;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HeronsFormulaTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HeronsFormulaTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(6.0, HeronsFormula.heron(3, 4, 5));
+        }
+    }
+}
