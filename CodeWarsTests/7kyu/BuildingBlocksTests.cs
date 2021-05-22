839f1fc3de7b42ccc07ba65c6ebc41bc2480df6d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-22 11:21:51 +0300

    Added solution "Building blocks" 7 kyu

---
+++ CodeWarsTests/7kyu/BuildingBlocksTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BuildingBlocksTests
+    {
+        [Test]
+        public static void ExampleTest()
+        {
+            BuildingBlocks.Block b = new BuildingBlocks.Block(new int[] {2, 2, 2});
+            Assert.AreEqual(2, b.GetWidth());
+            Assert.AreEqual(2, b.GetLength());
+            Assert.AreEqual(2, b.GetHeight());
+            Assert.AreEqual(8, b.GetVolume());
+            Assert.AreEqual(24, b.GetSurfaceArea());
+        }
+    }
+}
