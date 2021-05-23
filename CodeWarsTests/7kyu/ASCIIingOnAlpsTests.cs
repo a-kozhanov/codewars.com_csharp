8eb14b6b93f9fdf3b3fe51ffe8a163b9ff8e16a3
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-24 00:03:17 +0300

    Added solution "ASCII-ing on the Alps" 7 kyu

---
+++ CodeWarsTests/7kyu/ASCIIingOnAlpsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ASCIIingOnAlpsTests
+    {
+        [Test]
+        public void SampleDecimal()
+        {
+            string result = ASCIIingOnAlps.DetermineCool(10, "ThisIsADecimal");
+            Console.WriteLine(result);
+            Assert.AreEqual("1348", result);
+        }
+    }
+}
