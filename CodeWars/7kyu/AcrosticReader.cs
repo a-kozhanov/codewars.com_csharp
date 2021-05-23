8fefba4207fcf1993560afd6aaf4910eadaf5749
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-23 11:20:16 +0300

    Added solution "Acrostic reader" 7 kyu

---
+++ CodeWars/7kyu/AcrosticReader.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/59b843b58bcb7766660000f6/csharp
+
+7 kyu
+Acrostic reader
+
+An acrostic is a text in which the first letter of each line spells out a word. 
+It is also a quick and cheap way of writing a poem for somebody, as exemplified below :
+
+Write a program that reads an acrostic to identify the "hidden" word. Specifically, 
+your program will receive a list of words (reprensenting an acrostic) and will need to return a string corresponding to the word that is spelled out by taking the first letter of each word in the acrostic.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class AcrosticReader
+    {
+        public static string ReadOut(string[] acrostic)
+        {
+            // return acrostic.Aggregate("", (a, b) => a + b.First());
+            // return string.Concat(acrostic.Select(v => v[0]));
+            // return acrostic.Aggregate("", (a, b) => a + b[0]);
+            return string.Concat(acrostic.Select(s => s[0]));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/AcrosticReaderTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class AcrosticReaderTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("JACK", AcrosticReader.ReadOut(new string[] {"Jolly", "Amazing", "Courteous", "Keen"}));
+            Assert.AreEqual("MEG", AcrosticReader.ReadOut(new string[] {"Marvelous", "Excellent", "Gifted"}));
+        }
+    }
+}
