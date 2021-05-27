7cc58a267bef134a39750f3c260974568b875532
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-27 09:58:19 +0300

    Added solution "Reverse list" 7 kyu

---
+++ CodeWars/7kyu/ReverseList.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/57a04da9e298a7ee43000111/csharp
+
+7 kyu
+Reverse list
+
+Write reverseList function that simply reverses lists.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ReverseList
+    {
+        public static int[] reverseList(int[] list)
+        {
+            // Array.Reverse(list);
+            // return list;
+
+            return list.Reverse().ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ReverseListTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReverseListTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(new int[0], ReverseList.reverseList(new int[0]));
+            Assert.AreEqual(new int[] {3, 2, 1}, ReverseList.reverseList(new int[] {1, 2, 3}));
+        }
+    }
+}
