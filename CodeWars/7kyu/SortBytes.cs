b9f0b8eadb57aff39c830aa2e4478ea1be58fad3
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-30 22:12:04 +0300

    Added solution "Sort the Bytes" 7 kyu

---
+++ CodeWars/7kyu/SortBytes.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/6076d4edc7bf5d0041b31dcf/csharp
+
+7 kyu
+Sort the Bytes
+
+Task
+Implement a function that takes an unsigned 32 bit integer as input and sorts its bytes in descending order, 
+returning the resulting (unsigned 32 bit) integer.
+
+An alternative way to state the problem is as follows: The number given as input is made up of four bytes. 
+Reorder these bytes so that the resulting (unsigned 32 bit) integer is as large as possible.
+
+Example
+Input: 3735928559
+  
+3735928559 is 0xdeadbeef in hexadecimal representation and 11011110 10101101 10111110 11101111
+in binary representation.
+  
+After sorting the bytes in descending order the resulting unsigned 32 bit integer is 4024352429 
+in decimal representation, which is 0xefdebead in hexadecimal and 11101111 11011110 10111110 10101101
+in binary.
+
+Output should be: 4024352429
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataSortBytes
+    {
+        public static uint SortBytes(uint number)
+        {
+            return BitConverter.ToUInt32(BitConverter.GetBytes(number).OrderBy(b => b).ToArray());
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SortBytesTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataSortBytesTests
+    {
+        [TestCase(0U, 0U)]
+        [TestCase(1U, 16777216U)]
+        [TestCase(2U, 33554432U)]
+        [TestCase(4294967295U, 4294967295U)]
+        [TestCase(3735928559U, 4024352429U)]
+        [TestCase(255U, 4278190080U)]
+        [TestCase(305419896U, 2018915346U)]
+        [TestCase(19088743U, 1732584193U)]
+        [TestCase(555885348U, 606282273U)]
+        [TestCase(43839U, 2873032704U)]
+        [TestCase(723893U, 3037399808U)]
+        [TestCase(3567U, 4010606592U)]
+        public void SampleTests(uint number, uint expected)
+        {
+            var actual = KataSortBytes.SortBytes(number);
+            Assert.AreEqual(expected, actual, $"For number: {number}");
+        }
+    }
+}
