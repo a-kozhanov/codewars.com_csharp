f5437b2ecf02748c4c0930013d5680149c1c0d1b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-05-31 09:48:49 +0300

    Added solution "How many twos?" 7 kyu

---
+++ CodeWars/7kyu/HowManyTwos.cs
@@ -0,0 +1,50 @@
+﻿/*
+https://www.codewars.com/kata/56aed5db9d5cb55de000001c/csharp
+
+7 kyu
+How many twos?
+
+Write a function that returns the number of '2's in the factorization of a number.
+For example,
+
+TwoCount(24)
+should return 3, since the factorization of 24 is 2^3 x 3
+
+TwoCount(17280)
+should return 7, since the factorization of 17280 is 2^7 x 5 x 3^3
+The number passed to two_count (twoCount) will always be a positive integer greater than or equal to 1.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class HowManyTwos
+    {
+        public static int TwoCount(int n)
+        {
+            return (int) Math.Log(n & -n, 2);
+
+
+            // int count;
+            // for (count = 0; n > 1 && n % 2 == 0; count++) n /= 2;
+            // return count;
+
+
+            // var res = 0;
+            // for (; n % 2 == 0; n /= 2)
+            //     res++;
+            // return res;
+
+
+            // var result = 0;
+            // while (n % 2 == 0)
+            // {
+            //     n /= 2;
+            //     result++;
+            // }
+            //
+            // return result;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HowManyTwosTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HowManyTwosTests
+    {
+        [Test]
+        [TestCase(24, ExpectedResult = 3)]
+        [TestCase(17280, ExpectedResult = 7)]
+        [TestCase(222222222, ExpectedResult = 1)]
+        [TestCase(256, ExpectedResult = 8)]
+        [TestCase(1, ExpectedResult = 0)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(7, ExpectedResult = 0)]
+        [TestCase(84934656, ExpectedResult = 20)]
+        public static int FixedTest(int num)
+        {
+            return HowManyTwos.TwoCount(num);
+        }
+    }
+}
