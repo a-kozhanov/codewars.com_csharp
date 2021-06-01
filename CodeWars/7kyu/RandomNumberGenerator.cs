4719d4afbd18e00f7639602d73cca15f347b2f89
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-01 20:04:45 +0300

    Added solution "Random Number Generator" 7 kyu

---
+++ CodeWars/7kyu/RandomNumberGenerator.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/5653935c09106259c000001a/csharp
+
+7 kyu
+Random Number Generator
+
+##Generating Random Number
+
+Peter wants to create a class that generates a random number, and here is the class he has developed so far:
+
+public class Kata
+    {
+        private Random _random = new Random();
+
+        public int GenerateRandomNumber()
+        {
+            return _random.Next(0, 1000);
+        }
+    }
+When he instantiates the class, GenerateRandomNumber method always generates the same number.
+
+Kata kata1 = new Kata();
+Kata kata2 = new Kata();
+Help him to modify the class, so the method generates a different number for each instance.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataRandomNumberGenerator
+    {
+        public int GenerateRandomNumber() => new Random().Next(0, 1000);
+    }
+}
---
+++ CodeWarsTests/7kyu/RandomNumberGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataRandomNumberGeneratorTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            KataRandomNumberGenerator kata1 = new KataRandomNumberGenerator();
+            KataRandomNumberGenerator kata2 = new KataRandomNumberGenerator();
+            Assert.AreNotEqual(kata1.GenerateRandomNumber(), kata2.GenerateRandomNumber());
+        }
+    }
+}
