20cf44485125a70df2f7e0312b54ea9386a27c7e
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-03 09:55:41 +0300

    Added solution "Get initials from person name" 7 kyu

---
+++ CodeWars/7kyu/GetInitialsFromPersonName.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/57b56af6b69bfcffb80000d7/csharp
+
+7 kyu
+Get initials from person name
+
+Write function toInitials returs initials for a given person name. E.g: "Bill Gates" -> "B. G."
+Note: initials should be separated with a space.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class GetInitialsFromPersonName
+    {
+        public static string toInitials(string name)
+        {
+            return string.Join(" ", name.Split().Select(s => s[0] + "."));
+            // return Regex.Replace(name, @"(\S)\S+\b", "$1.");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GetInitialsFromPersonNameTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GetInitialsFromPersonNameTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(GetInitialsFromPersonName.toInitials("Kerry King"), "K. K.");
+            Assert.AreEqual(GetInitialsFromPersonName.toInitials("Tom Araya"), "T. A.");
+        }
+    }
+}
