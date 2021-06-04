8ef5ea669990363511bed8b22f347e7e4eceb541
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-04 10:20:10 +0300

    Added solution "Fibonacci" 7 kyu

---
+++ CodeWars/7kyu/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿/*
+https://www.codewars.com/kata/57a1d5ef7cb1f3db590002af/csharp
+
+7 kyu
+Fibonacci
+
+Create function fib that returns n'th element of Fibonacci sequence (classic programming task).
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class Fibonacci
+    {
+        public static int fib(int n)
+        {
+            return n < 2 ? n : fib(n - 1) + fib(n - 2);
+            // return n > 1 ? fib(n - 1) + fib(n - 2) : n;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FibonacciTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void MyTest()
+            {
+                Assert.AreEqual(5, Fibonacci.fib(5));
+                Assert.AreEqual(55, Fibonacci.fib(10));
+            }
+        }
+    }
+}
