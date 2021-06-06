0c402735c7aa6fa567d32d49ea763e112cfd4d25
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-07 00:28:18 +0300

    Added solution "Thinkful - Object Drills: Quarks" 7 kyu

---
+++ CodeWarsTests/7kyu/ThinkfulObjectDrillsQuarksTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ThinkfulObjectDrillsQuarksTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            ThinkfulObjectDrillsQuarks.Quark q1 = new ThinkfulObjectDrillsQuarks.Quark("red", "up");
+            ThinkfulObjectDrillsQuarks.Quark q2 = new ThinkfulObjectDrillsQuarks.Quark("blue", "strange");
+            Assert.AreEqual("red", q1.Color);
+            Assert.AreEqual("strange", q2.Flavor);
+            Assert.AreEqual(1 / 3d, q2.BaryonNumber);
+
+            q1.Interact(q2);
+            Assert.AreEqual("blue", q1.Color);
+        }
+    }
+}
