a6edd60011a9c74b5ba26e4f6fb50e72a20c62d2
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-06 09:18:17 +0300

    Added solution "Reverse the bits in an integer" 7 kyu

---
+++ CodeWars/7kyu/ReverseBitsInAnInteger.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/5959ec605595565f5c00002b/csharp
+
+7 kyu
+Reverse the bits in an integer
+
+Write a function that reverses the bits in an integer.
+For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+You can assume that the number is not negative.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class ReverseBitsInAnInteger
+    {
+        public static long ReverseBits(long n)
+        {
+            // return Convert.ToInt64(new string(Convert.ToString(n, 2).Reverse().ToArray()), 2);
+            return Convert.ToInt64(string.Concat(Convert.ToString(n, 2).Reverse()), 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ReverseBitsInAnIntegerTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ReverseBitsInAnIntegerTests
+    {
+        [Test(Description = "Sample tests")]
+        public void SampleTests()
+        {
+            Assert.AreEqual(267, ReverseBitsInAnInteger.ReverseBits(417));
+            Assert.AreEqual(417, ReverseBitsInAnInteger.ReverseBits(267));
+            Assert.AreEqual(0, ReverseBitsInAnInteger.ReverseBits(0));
+            Assert.AreEqual(1087, ReverseBitsInAnInteger.ReverseBits(2017));
+            Assert.AreEqual(1023, ReverseBitsInAnInteger.ReverseBits(1023));
+            Assert.AreEqual(1, ReverseBitsInAnInteger.ReverseBits(1024));
+            Assert.AreEqual(long.MaxValue, ReverseBitsInAnInteger.ReverseBits(long.MaxValue));
+        }
+    }
+}
