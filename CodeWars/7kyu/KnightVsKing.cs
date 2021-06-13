f010052d81c460512bca4f30d1499566b87583d8
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-13 10:16:11 +0300

    Added solution "Knight vs King" 7 kyu

---
+++ CodeWars/7kyu/KnightVsKing.cs
@@ -0,0 +1,53 @@
+﻿/*
+https://www.codewars.com/kata/564e1d90c41a8423230000bc/csharp
+
+7 kyu
+Knight vs King
+
+Knight vs King
+If you are not familiar with chess game you can learn more Here .
+
+Here is the chess board:
+
+alt text
+
+You put a Knight and a king in the board.
+
+Complete the method that tell us which one can capture the other one.
+
+public static string KnightVsKing(object[] knightPosition, object[]kingPosition)
+        {
+        // Three possible outputs are "Knight", "King" and "None".
+        }
+You are provided with two object array; each contains an integer (first item) and a string (second item) to show the position of the pieces in the chess board.
+
+object[] kingPosition = {4, "C"};
+object[] knightPosition = {6, "D"};
+Check the test cases and Happy coding :)
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataKnightVsKing
+    {
+        public static string KnightVsKing(object[] knightPosition, object[] kingPosition)
+        {
+            var x = (int) knightPosition[0] - (int) kingPosition[0];
+            var y = ((string) knightPosition[1])[0] - ((string) kingPosition[1])[0];
+            var d = x * x + y * y;
+            return d < 3 ? "King" : d == 5 ? "Knight" : "None";
+
+            // var x = Math.Abs((int) knightPosition[0] - (int) kingPosition[0]);
+            // var y = Math.Abs(((string) knightPosition[1])[0] - ((string) kingPosition[1])[0]);
+            // var d = x + y;
+            // return d > 3 ? "None" : d == 3 ? "Knight" : "King";
+
+            // var x = Math.Abs((int) knightPosition[0] - (int) kingPosition[0]);
+            // var y = Math.Abs(((string) knightPosition[1])[0] - ((string) kingPosition[1])[0]);
+            // var d = x * x + y * y;
+            // return d == 5 ? "Knight" : d <= 2 ? "King" : "None";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/KnightVsRookTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataKnightVsRookTests
+    {
+        [Test]
+        public void KnightTest()
+        {
+            object[] rookPosition = {4, "C"};
+            object[] knightPosition = {6, "D"};
+            StringAssert.AreEqualIgnoringCase("Knight", KataKnightVsRook.KnightVsRook(knightPosition, rookPosition));
+        }
+
+        [Test]
+        public void RookTest()
+        {
+            object[] rookPosition = {2, "G"};
+            object[] knightPosition = {2, "B"};
+            StringAssert.AreEqualIgnoringCase("Rook", KataKnightVsRook.KnightVsRook(knightPosition, rookPosition));
+        }
+
+        [Test]
+        public void NoneTest()
+        {
+            object[] rookPosition = {2, "F"};
+            object[] knightPosition = {7, "B"};
+            StringAssert.AreEqualIgnoringCase("None", KataKnightVsRook.KnightVsRook(knightPosition, rookPosition));
+        }
+    }
+}
