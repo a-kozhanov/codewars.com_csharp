5e70e779056d3052179136421c63edd9d11583b0
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-14 11:05:36 +0300

    Added solution "Knight vs Rook" 7 kyu

---
+++ CodeWars/7kyu/KnightVsRook.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/564e22e30e686c7f7e00005d/csharp
+
+7 kyu
+Knight vs Rook
+
+Knight vs Rook
+If you are not familiar with chess game you can learn more Here .
+
+Here is the chess board:
+
+alt text
+
+You put a Knight and a Rook in the board.
+
+Complete the method that tell us which one can capture the other one.
+
+public static string KnightVsRook(object[] knightPosition, object[]rookPosition)
+        {
+        // Three possible outputs are "Knight", "Rook" and "None".
+        }
+You are provided with two object array; each contains an integer (first item) and a string (second item) to show the position of the pieces in the chess board.
+
+object[] rookPosition = {4, "C"};
+object[] knightPosition = {6, "D"};
+Check the test cases and Happy coding :)
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public static class KataKnightVsRook
+    {
+        public static string KnightVsRook(object[] knightPosition, object[] rookPosition)
+        {
+            var x = Math.Abs((int) knightPosition[0] - (int) rookPosition[0]);
+            var y = Math.Abs(((string) knightPosition[1])[0] - ((string) rookPosition[1])[0]);
+            return x == 0 || y == 0 ? "Rook" : x + y == 3 ? "Knight" : "None";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/KnightVsKingTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataKnightVsKingTests
+    {
+        [Test]
+        public void KnightTest()
+        {
+            object[] kingPosition = {4, "C"};
+            object[] knightPosition = {6, "D"};
+            StringAssert.AreEqualIgnoringCase("Knight", KataKnightVsKing.KnightVsKing(knightPosition, kingPosition));
+        }
+
+        [Test]
+        public void KingTest()
+        {
+            object[] kingPosition = {7, "B"};
+            object[] knightPosition = {6, "C"};
+            StringAssert.AreEqualIgnoringCase("King", KataKnightVsKing.KnightVsKing(knightPosition, kingPosition));
+        }
+
+        [Test]
+        public void NoneTest()
+        {
+            object[] kingPosition = {2, "F"};
+            object[] knightPosition = {6, "B"};
+            StringAssert.AreEqualIgnoringCase("None", KataKnightVsKing.KnightVsKing(knightPosition, kingPosition));
+        }
+    }
+}
