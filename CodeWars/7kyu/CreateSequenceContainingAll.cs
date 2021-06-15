bf8f7936d1a16da345df76add80ff845c62f45e4
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-15 18:00:12 +0300

    Added solution "Create sequence containing all allowed characters" 7 kyu

---
+++ CodeWars/7kyu/CreateSequenceContainingAll.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/56523ec6fc8ac62677000005/csharp
+
+7 kyu
+Create sequence containing all allowed characters
+
+Define createSequence(regex) function that returns a string of all characters (in ASCII order) matching specified regular expression one-character criterion.
+
+Examples
+let digits = /[0-9]/;
+// createSequence(digits) === '0123456789'
+
+let hexadecimal = /[0-9A-F]/;
+// createSequence(hexadecimal) === '0123456789ABCDEF'
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class CreateSequenceContainingAll
+    {
+        public string CreateSequence(Regex regex)
+        {
+            //return string.Concat(Enumerable.Range(0, 128).Select(x => $"{(char) x}").Where((Func<string, bool>)regex.IsMatch));
+            //return string.Concat(Enumerable.Range(0, 128).Select(char.ConvertFromUtf32).Where((Func<string, bool>)regex.IsMatch));
+            //return string.Concat(regex.Matches(string.Concat(Enumerable.Range(0, 128).Select(x => $"{(char) x}"))));
+            //return string.Concat(Enumerable.Range(0, 128).Select(i => $"{(char) i}").Where(x => regex.IsMatch(x)));
+            //return string.Concat(Enumerable.Range(0, 128).Select(i => regex.Match(new string((char) i, 1)).Value));
+            return string.Concat(Enumerable.Range(0, 128).Select(i => regex.Match($"{(char) i}")));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CreateSequenceContainingAllTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CreateSequenceContainingAllTests
+    {
+        [Test]
+        public void _0_BasicTests()
+        {
+            var kata = new CreateSequenceContainingAll();
+
+            Assert.AreEqual("abc", kata.CreateSequence(new Regex(@"[a-c]")));
+            Assert.AreEqual("0123456789ABCDEF", kata.CreateSequence(new Regex(@"[0-9A-F]")));
+        }
+    }
+}
