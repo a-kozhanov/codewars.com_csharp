ebef341fde8ab7e55b8c0f152d148cee1e76f5f4
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-16 23:41:36 +0300

    Added solution "Differential Averaging" 7 kyu

---
+++ CodeWarsTests/7kyu/DifferentialAveragingTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DifferentialAveragingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(1, DifferentialAveraging.AddToAverage(0, 0, 1));
+            Assert.AreEqual(1.5, DifferentialAveraging.AddToAverage(1, 1, 2));
+            Assert.AreEqual(2, DifferentialAveraging.AddToAverage(1.5, 2, 3));
+
+            Assert.AreEqual(123, DifferentialAveraging.AddToAverage(0, 0, 123));
+            Assert.AreEqual(289.5, DifferentialAveraging.AddToAverage(123, 1, 456));
+            Assert.AreEqual(456, DifferentialAveraging.AddToAverage(289.5, 2, 789));
+        }
+    }
+}
