5595290b01cbb508b9650bedcc0a09895b5160b8
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-21 22:45:10 +0300

    Added solution "Regexp Basics - is it a eight bit signed number?" 7 kyu

---
+++ CodeWars/7kyu/RegexpBasicsIsItEightBitSignedNumber.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/567ed73340895395c100002e/csharp
+
+7 kyu
+Regexp Basics - is it a eight bit signed number?
+
+Implement String.eight_bit_signed_number? (Ruby), String.eightBitSignedNumber() (Python), 
+eight_bit_signed_number() (JS) or StringUtils.isSignedEightBitNumber(String) (Java) which should return true/True 
+if given object is a number representable by 8 bit signed integer (-128 to -1 or 0 to 127), 
+false/False otherwise.
+
+It should only accept numbers in canonical representation, so no leading +, extra 0s, spaces etc.
+*/
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public static class RegexpBasicsIsItEightBitSignedNumber
+    {
+        public static bool SignedEightBitNumber(this string s)
+        {
+            // return Regex.IsMatch(s, @"^(0|(\-128)|(\-?((12[0-7])|(1[0-1][0-9])|([1-9][0-9]{0,1}))))\z");
+            return Regex.IsMatch(s, @"^(-?([1-9][0-9]?|1[01][0-9]|12[0-7])|-128|0)(?!\n)$");
+            // return Enumerable.Range(-128, 256).Any(i => i.ToString() == s);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/RegexpBasicsIsItEightBitSignedNumberTests.cs
@@ -0,0 +1,32 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RegexpBasicsIsItEightBitSignedNumberTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.IsFalse("".SignedEightBitNumber());
+            Assert.IsTrue("0".SignedEightBitNumber());
+            Assert.IsFalse("00".SignedEightBitNumber());
+            Assert.IsFalse("-0".SignedEightBitNumber());
+            Assert.IsTrue("55".SignedEightBitNumber());
+            Assert.IsTrue("-23".SignedEightBitNumber());
+            Assert.IsFalse("042".SignedEightBitNumber());
+            Assert.IsTrue("127".SignedEightBitNumber());
+            Assert.IsFalse("128".SignedEightBitNumber());
+            Assert.IsFalse("999".SignedEightBitNumber());
+            Assert.IsTrue("-128".SignedEightBitNumber());
+            Assert.IsFalse("-129".SignedEightBitNumber());
+            Assert.IsFalse("-999".SignedEightBitNumber());
+            Assert.IsFalse("1 ".SignedEightBitNumber());
+            Assert.IsFalse(" 1".SignedEightBitNumber());
+            Assert.IsFalse("1\n2".SignedEightBitNumber());
+            Assert.IsFalse("+1".SignedEightBitNumber());
+            Assert.IsFalse("--1".SignedEightBitNumber());
+        }
+    }
+}
