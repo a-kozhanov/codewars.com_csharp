6d2dde3608da65752282767a65a25c6720659998
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-22 20:00:06 +0300

    Added solution "Enumerable Magic #5- True for Just One?" 7 kyu

---
+++ CodeWarsTests/7kyu/EnumerableMagic5TrueForJustOneTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EnumerableMagic5TrueForJustOneTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(true, EnumerableMagic5TrueForJustOne.One(new int[] {1, 2, 3, 4, 5}, v => v < 2));
+            Assert.AreEqual(false, EnumerableMagic5TrueForJustOne.One(new int[] {1, 2, 3, 4, 5}, v => v % 2 != 0));
+            Assert.AreEqual(false, EnumerableMagic5TrueForJustOne.One(new int[] {1, 2, 3, 4, 5}, v => v > 5));
+        }
+    }
+}
