04d1774a062c5e6c992caea69ea51c585cac5f1a
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-23 11:27:46 +0300

    Added solution "Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product" 7 kyu

---
+++ CodeWars/7kyu/ExclamationMarksSeries13.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/57fb142297e0860073000064/csharp
+
+7 kyu
+Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product
+
+Description:
+Count the number of exclamation marks and question marks, return the product.
+
+Examples
+Product("") == 0
+product("!") == 0
+Product("!ab? ?") == 2
+Product("!!") == 0
+Product("!??") == 2
+Product("!???") == 3
+Product("!!!??") == 6
+Product("!!!???") == 9
+Product("!???!!") == 9
+Product("!????!!!?") == 20
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ExclamationMarksSeries13
+    {
+        public static int Product(string str)
+        {
+            // return (str.Length - str.Replace("!", "").Length) * (str.Length - str.Replace("?", "").Length);
+            return str.Count(c => c == '!') * str.Count(c => c == '?');
+            // return Regex.Matches(str,"!").Count * Regex.Matches(str,@"\?").Count;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ExclamationMarksSeries13Tests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExclamationMarksSeries13Tests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(0, ExclamationMarksSeries13.Product(""));
+            Assert.AreEqual(1, ExclamationMarksSeries13.Product("!?"));
+            Assert.AreEqual(2, ExclamationMarksSeries13.Product("!??"));
+            Assert.AreEqual(2, ExclamationMarksSeries13.Product("!!?"));
+            Assert.AreEqual(6, ExclamationMarksSeries13.Product("!!???"));
+            Assert.AreEqual(6, ExclamationMarksSeries13.Product("!!!??"));
+            Assert.AreEqual(4, ExclamationMarksSeries13.Product("!!??"));
+            Assert.AreEqual(4, ExclamationMarksSeries13.Product("!????"));
+            Assert.AreEqual(4, ExclamationMarksSeries13.Product("!!!!?"));
+            Assert.AreEqual(5, ExclamationMarksSeries13.Product("!?????"));
+            Assert.AreEqual(20, ExclamationMarksSeries13.Product("!!????!!?"));
+            Assert.AreEqual(49, ExclamationMarksSeries13.Product("!!???????!!!!!"));
+            Assert.AreEqual(165, ExclamationMarksSeries13.Product("!!???????????!!!!!?!?!?!?!"));
+            Assert.AreEqual(4, ExclamationMarksSeries13.Product("!!aabbcc??"));
+            Assert.AreEqual(4, ExclamationMarksSeries13.Product("! !aa bb   c c??   "));
+        }
+    }
+}
