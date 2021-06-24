3fcf8888ac036e75222b123717c140142efe3b69
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-24 17:11:34 +0300

    Added solution "Extending C# Objects: Calculate Word Scores With a Method" 7 kyu

---
+++ CodeWarsTests/7kyu/ExtendingCObjectsCalculateWordScoresWithMethodTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExtendingCObjectsCalculateWordScoresWithMethodTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(6, "ABC".Score());
+            Assert.AreEqual(96, "Knowledge".Score());
+            Assert.AreEqual(98, "Hard Work".Score());
+            Assert.AreEqual(0, ".,<>?".Score());
+            Assert.AreEqual(0, "".Score());
+        }
+    }
+}
