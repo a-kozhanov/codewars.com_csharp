ea9ad96304b23125d9cf16754171d23b7b5da6ce
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-25 10:05:40 +0300

    Added solution "Find the Squares" 7 kyu

---
+++ CodeWars/7kyu/FindSquares.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/60908bc1d5811f0025474291/csharp
+
+7 kyu
+Find the Squares
+
+Problem
+You are to write a function that takes an odd integer which is the difference between two consecutive perfect squares and find the squares
+
+Input Format
+num : Odd Integer 0<n<1000000
+
+Output Format
+Answer: String containing the bigger and smaller squares in one line like "Max-Min"
+
+Examples
+findSquares(9)  //returns '25-16'
+findSquares(5)  // returns '9-4' 
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class FindSquares
+    {
+        public static string findSquares(int n)
+        {
+            long a = (n + 1) / 2;
+            long b = a - 1;
+            return $"{a * a}-{b * b}";
+
+            // return $"{Math.Pow((n + 1) / 2, 2)}-{Math.Pow((n - 1) / 2, 2)}";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FindSquaresTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindSquaresTests
+    {
+        [Test]
+        public void testBasicNumbers()
+        {
+            Assert.AreEqual(FindSquares.findSquares(9), "25-16");
+        }
+
+        [Test]
+        public void testSmallerNumbers()
+        {
+            Assert.AreEqual(FindSquares.findSquares(1), "1-0");
+        }
+
+        [Test]
+        public void testBiggerNumbers()
+        {
+            Assert.AreEqual(FindSquares.findSquares(1887), "891136-889249");
+            Assert.AreEqual(FindSquares.findSquares(99991), "2499600016-2499500025");
+        }
+    }
+}
