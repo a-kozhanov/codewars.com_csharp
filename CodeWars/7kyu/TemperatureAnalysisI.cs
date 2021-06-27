51010f5236846fa76d748900c76518c6e0dd157a
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-28 00:11:26 +0300

    Added solution "Temperature analysis I" 7 kyu

---
+++ CodeWars/7kyu/TemperatureAnalysisI.cs
@@ -0,0 +1,26 @@
+﻿/*
+https://www.codewars.com/kata/588e0f11b7b4a5b373000041/csharp
+
+7 kyu
+Temperature analysis I
+
+You were given a string of integer temperature values. 
+Create a function lowest_temp(t) and return the lowest value or None/null/Nothing if the string is empty.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class TemperatureAnalysisI
+    {
+        public static int? LowestTemperature(string t)
+        {
+            // return string.IsNullOrEmpty(t) ? (int?) null : Array.ConvertAll(t.Split(), Convert.ToInt32).Min();
+            // return t.Length > 0 ? t.Split().Select(int.Parse).Min() : new int?();
+            // return t != "" ? t.Split().Min(int.Parse) : (int?) null;
+            return t.Any() ? t.Split().Min(int.Parse) : (int?) null;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/TemperatureAnalysisITests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TemperatureAnalysisITests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(-8, TemperatureAnalysisI.LowestTemperature("-1 50 -4 20 22 -7 0 10 -8"));
+            Assert.AreEqual(10, TemperatureAnalysisI.LowestTemperature("10 50 12 20 22 13 20 100 18"));
+            Assert.AreEqual(-1, TemperatureAnalysisI.LowestTemperature("-1 50 16 20 22 1 0 10 34"));
+            Assert.AreEqual(-2, TemperatureAnalysisI.LowestTemperature("-2 3 -1 12 45 14"));
+            Assert.AreEqual(null, TemperatureAnalysisI.LowestTemperature(""));
+        }
+    }
+}
