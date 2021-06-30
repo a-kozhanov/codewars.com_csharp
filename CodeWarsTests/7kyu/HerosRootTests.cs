5a09b41410464e63676f180774afc669dfdbae5b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-06-30 09:57:10 +0300

    Added solution "Hero's root" 7 kyu

---
+++ CodeWarsTests/7kyu/HerosRootTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HerosRootTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Assert.AreEqual(4, HerosRoot.IntRac(25, 1));
+            Assert.AreEqual(3, HerosRoot.IntRac(125348, 300));
+        }
+    }
+}
