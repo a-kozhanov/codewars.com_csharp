7dc206c1b1cd900a56959146368c475073babb5c
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-06 18:55:32 +0300

    Added solution "Basics 04: Rotate Matrix" 7 kyu

---
+++ CodeWars/7kyu/Basics04RotateMatrix.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/56b5dd1702a30326ce000b02/csharp
+
+7 kyu
+Basics 04: Rotate Matrix
+
+Your task is to rotate a matrix 90 degree to the left. 
+The matrix is an array of integers with dimension n,m. 
+So this kata checks some basics, it's not too difficult.
+
+There's nothing more to explain, no tricks, no "bad cases";-). Perhaps you take a look at the testcases...
+
+One easy example:
+
+Input: {{-1, 4, 5},
+        { 2, 3, 4}}
+
+Output: {{ 5, 4},
+         { 4, 3},
+         {-1, 2}}
+First there are some static tests, later on random tests too...
+
+
+Hope you have fun:-)!
+*/
+
+namespace CodeWars
+{
+    public class Basics04RotateMatrix
+    {
+        public int[,] rotateMatrixLeft(int[,] matrix)
+        {
+            var w = matrix.GetLength(0);
+            var h = matrix.GetLength(1);
+            var result = new int[h, w];
+
+            for (var i = 0; i < w; i++)
+            for (var j = 0; j < h; j++)
+                result[h - j - 1, i] = matrix[i, j];
+
+            return result;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/Basics04RotateMatrixTests.cs
@@ -0,0 +1,75 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class Basics04RotateMatrixTests
+    {
+        [Test]
+        public void smile67KataTest_withoutRandom1()
+        {
+            int[,] oldMatrix = new int[4, 4]
+            {
+                {-1, 4, 3, 4},
+                {5, 6, 7, 8},
+                {2, 5, 1, 7},
+                {3, 14, -5, 3}
+            };
+            int[,] newMatrix = new int[4, 4]
+            {
+                {4, 8, 7, 3},
+                {3, 7, 1, -5},
+                {4, 6, 5, 14},
+                {-1, 5, 2, 3}
+            };
+            Assert.AreEqual(newMatrix, new Basics04RotateMatrix().rotateMatrixLeft(oldMatrix));
+        }
+
+        [Test]
+        public void smile67KataTest_withoutRandom2()
+        {
+            int[,] oldMatrix = new int[1, 1]
+                {{-1}};
+            int[,] newMatrix = new int[1, 1]
+                {{-1}};
+            Assert.AreEqual(newMatrix, new Basics04RotateMatrix().rotateMatrixLeft(oldMatrix));
+        }
+
+        [Test]
+        public void smile67KataTest_withoutRandom3()
+        {
+            int[,] oldMatrix = new int[2, 4]
+            {
+                {-1, 4, 3, 4},
+                {5, 6, 7, 8}
+            };
+            int[,] newMatrix = new int[4, 2]
+            {
+                {4, 8},
+                {3, 7},
+                {4, 6},
+                {-1, 5}
+            };
+            Assert.AreEqual(newMatrix, new Basics04RotateMatrix().rotateMatrixLeft(oldMatrix));
+        }
+
+        [Test]
+        public void smile67KataTest_withoutRandom4()
+        {
+            int[,] oldMatrix = new int[4, 2]
+            {
+                {-1, 4},
+                {5, 6},
+                {2, 5},
+                {-5, 3}
+            };
+            int[,] newMatrix = new int[2, 4]
+            {
+                {4, 6, 5, 3},
+                {-1, 5, 2, -5}
+            };
+            Assert.AreEqual(newMatrix, new Basics04RotateMatrix().rotateMatrixLeft(oldMatrix));
+        }
+    }
+}
