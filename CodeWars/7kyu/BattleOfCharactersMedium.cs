cdc4f26633373d5ddc1d60453e3b43abad5c766d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-07 21:17:20 +0300

    Added solution "Battle of the characters (Medium)" 7 kyu

---
+++ CodeWars/7kyu/BattleOfCharactersMedium.cs
@@ -0,0 +1,40 @@
+﻿/*
+https://www.codewars.com/kata/595e9f258b763bc2d2000032/csharp
+
+7 kyu
+Battle of the characters (Medium)
+
+Description:
+Groups of characters decided to make a battle. Help them to figure out what group is more powerful. 
+Create a function that will accept 2 variables and return the one who's stronger.
+
+Rules:
+Each character has its own power:
+  A = 1, B = 2, ... Y = 25, Z = 26
+  a = 0.5, b = 1, ... y = 12.5, z = 13
+Only alphabetical characters can and will participate in a battle.
+Only two groups to a fight.
+Group whose total power (a + B + c + ...) is bigger wins.
+If the powers are equal, it's a tie.
+Examples:
+  battle("One", "Two"); // => "Two"`
+  battle("ONE", "NEO"); // => "Tie!"
+Related kata:
+Battle of the characters (Easy)
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class BattleOfCharactersMedium
+    {
+        public static string Battle(string x, string y)
+        {
+            double Score(string s) => s.Sum(c => char.IsLower(c) ? (c - 96) / 2d : c - 64);
+            double scoreX = Score(x);
+            double scoreY = Score(y);
+            return scoreX == scoreY ? "Tie!" : scoreX > scoreY ? x : y;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/BattleOfCharactersMediumTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BattleOfCharactersMediumTests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual("Two", BattleOfCharactersMedium.Battle("One", "Two"));
+            Assert.AreEqual("One", BattleOfCharactersMedium.Battle("One", "Neo"));
+            Assert.AreEqual("Tie!", BattleOfCharactersMedium.Battle("One", "neO"));
+            Assert.AreEqual("Tie!", BattleOfCharactersMedium.Battle("Foo", "BAR"));
+            Assert.AreEqual("Four", BattleOfCharactersMedium.Battle("Four", "Five"));
+        }
+    }
+}
