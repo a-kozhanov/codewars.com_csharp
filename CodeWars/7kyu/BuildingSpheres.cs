498d173d6b69943c1659ba811147bcc38723c90b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-11 17:56:34 +0300

    Added solution "Building Spheres" 7 kyu

---
+++ CodeWars/7kyu/BuildingSpheres.cs
@@ -0,0 +1,53 @@
+﻿/*
+https://www.codewars.com/kata/55c1d030da313ed05100005d/csharp
+
+7 kyu
+Building Spheres
+
+Now that we have a Block let's move on to something slightly more complex a Sphere.
+
+#Arguments for the constructor
+
+radius -> integer
+mass -> integer
+#Methods to be defined
+
+GetRadius()       =>  radius of the Sphere
+GetMass()         =>  mass of the Sphere
+GetVolume()       =>  volume of the Sphere (a double rounded to 5 place after the decimal)
+GetSurfaceArea()  =>  surface area of the Sphere (a double rounded to 5 place after the decimal)
+GetDensity()      =>  density of the Sphere (a double rounded to 5 place after the decimal)
+##Example
+
+Sphere ball = new Sphere(2,50)
+ball.GetRadius() ->       2
+ball.GetMass() ->         50
+ball.GetVolume() ->       33.51032
+ball.GetSurfaceArea() ->  50.26548
+ball.GetDensity() ->      1.49208
+Any feedback would be much appreciated
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class BuildingSpheres
+    {
+        private readonly int _radius;
+        private readonly int _mass;
+
+        // public Sphere(int radius, int mass)
+        public BuildingSpheres(int radius, int mass)
+        {
+            _radius = radius;
+            _mass = mass;
+        }
+
+        public int GetRadius() => _radius;
+        public int GetMass() => _mass;
+        public double GetVolume() => Math.Round(Math.PI * Math.Pow(GetRadius(), 3) * 4 / 3, 5);
+        public double GetSurfaceArea() => Math.Round(Math.PI * Math.Pow(GetRadius(), 2) * 4, 5);
+        public double GetDensity() => Math.Round(GetMass() / GetVolume(), 5);
+    }
+}
---
+++ CodeWarsTests/7kyu/BuildingSpheresTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BuildingSpheresTests
+    {
+        [Test]
+        public static void ExampleTest()
+        {
+            BuildingSpheres ball = new BuildingSpheres(2, 50);
+            Assert.AreEqual(2, ball.GetRadius(), "Check radius");
+            Assert.AreEqual(50, ball.GetMass(), "Check mass");
+            Assert.AreEqual(33.51032, ball.GetVolume(), "Check volume");
+            Assert.AreEqual(50.26548, ball.GetSurfaceArea(), "Check area");
+            Assert.AreEqual(1.49208, ball.GetDensity(), "Check density");
+        }
+    }
+}
