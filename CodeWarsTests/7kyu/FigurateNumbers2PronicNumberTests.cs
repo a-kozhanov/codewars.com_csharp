805961d1b596ac0f4545f92d5fbfd723274915cf
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-15 12:29:18 +0300

    Added solution "Figurate Numbers #2 - Pronic Number" 7 kyu

---
+++ CodeWarsTests/7kyu/FigurateNumbers2PronicNumberTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FigurateNumbers2PronicNumberTests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            Assert.AreEqual(true, FigurateNumbers2PronicNumber.IsPronic(0));
+            Assert.AreEqual(false, FigurateNumbers2PronicNumber.IsPronic(1));
+            Assert.AreEqual(true, FigurateNumbers2PronicNumber.IsPronic(2));
+            Assert.AreEqual(false, FigurateNumbers2PronicNumber.IsPronic(3));
+            Assert.AreEqual(false, FigurateNumbers2PronicNumber.IsPronic(4));
+            Assert.AreEqual(false, FigurateNumbers2PronicNumber.IsPronic(5));
+            Assert.AreEqual(true, FigurateNumbers2PronicNumber.IsPronic(6));
+            Assert.AreEqual(false, FigurateNumbers2PronicNumber.IsPronic(-3));
+            Assert.AreEqual(false, FigurateNumbers2PronicNumber.IsPronic(-27));
+        }
+    }
+}
