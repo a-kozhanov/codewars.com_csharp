f96a75da9d76ce9300d2514cdc7a7ab87658a32d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-17 20:30:08 +0300

    Added solution "Finding Remainder Without Using '%' Operator" 7 kyu

---
+++ CodeWars/7kyu/FindingRemainderWithoutUsingOperator.cs
@@ -0,0 +1,38 @@
+﻿/*
+https://www.codewars.com/kata/564f458b4d75e24fc9000041/csharp
+
+7 kyu
+Finding Remainder Without Using '%' Operator
+
+Task
+Write a method remainder which takes two integer arguments, dividend and divisor, 
+and returns the remainder when dividend is divided by divisor. 
+Do NOT use the modulus operator (%) to calculate the remainder!
+
+Assumption
+Dividend will always be greater than or equal to divisor.
+
+Notes
+Make sure that the implemented remainder function works exactly the same as the Modulus operator (%).
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class FindingRemainderWithoutUsingOperator
+    {
+        public static int Remainder(int D, int d)
+        {
+            // return D % d;
+            // return D - d * (D / d);
+
+            // while (D >= d)
+            //     D -= d;
+            // return D;
+
+            Math.DivRem(D, d, out var result);
+            return result;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FindingRemainderWithoutUsingOperatorTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindingRemainderWithoutUsingOperatorTests
+    {
+        [Test]
+        [TestCase(3, 2, ExpectedResult = 1)]
+        [TestCase(19, 2, ExpectedResult = 1)]
+        [TestCase(10, 2, ExpectedResult = 0)]
+        [TestCase(34, 7, ExpectedResult = 6)]
+        [TestCase(27, 5, ExpectedResult = 2)]
+        public static int FixedTest(int D, int d)
+        {
+            return FindingRemainderWithoutUsingOperator.Remainder(D, d);
+        }
+    }
+}
