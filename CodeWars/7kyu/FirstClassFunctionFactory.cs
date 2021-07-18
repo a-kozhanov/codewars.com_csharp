19771d0ccf3b33e9aeb8405206fde8c605e479f5
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-18 22:37:40 +0300

    Added solution "First-Class Function Factory" 7 kyu

---
+++ CodeWars/7kyu/FirstClassFunctionFactory.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/563f879ecbb8fcab31000041/csharp
+
+7 kyu
+First-Class Function Factory
+
+Write a function, factory, that takes a number as its parameter and returns another function.
+
+The returned function should take an array of numbers as its parameter, 
+and return an array of those numbers multiplied by the number that was passed into the first function.
+
+In the example below, 5 is the number passed into the first function. 
+So it returns a function that takes an array and multiplies all elements in it by five.
+
+Translations and comments (and upvotes) welcome!
+
+Example
+Func<int[],int[]> fives = FunctionFactory.factory(5);    // returns a function - fives
+int[] myArray = new int[]{1, 2, 3};
+fives(myArray);                  //returns [5, 10, 15];
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FirstClassFunctionFactory
+    {
+        public static Func<int[], int[]> factory(int x)
+        {
+            return a => a.Select(y => y * x).ToArray();
+            // return a => Array.ConvertAll(a, n => n * x);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FirstClassFunctionFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FirstClassFunctionFactoryTests
+    {
+        private static void testing(int[] expected, int[] actual)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void BasicTest()
+        {
+            Func<int[], int[]> threes = FirstClassFunctionFactory.factory(3);
+            int[] myArr = new int[] {1, 2, 3};
+            testing(threes(myArr), new int[] {3, 6, 9});
+
+            Func<int[], int[]> fives = FirstClassFunctionFactory.factory(5);
+            testing(fives(myArr), new int[] {5, 10, 15});
+        }
+    }
+}
