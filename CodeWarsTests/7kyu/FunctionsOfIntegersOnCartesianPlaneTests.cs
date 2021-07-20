0586884844512bed7408bd2a93a2e7ee90782059
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-20 22:55:55 +0300

    Added solution "Functions of Integers on Cartesian Plane" 7 kyu

---
+++ CodeWarsTests/7kyu/FunctionsOfIntegersOnCartesianPlaneTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FunctionsOfIntegersOnCartesianPlaneTests
+    {
+        Random randomGenerator = new Random();
+
+        [Test]
+        public void Test01()
+        {
+            Assert.AreEqual(55, FunctionsOfIntegersOnCartesianPlane.sumin(5));
+        }
+
+        [Test]
+        public void Test04()
+        {
+            Assert.AreEqual(372, FunctionsOfIntegersOnCartesianPlane.sumax(8));
+        }
+
+        [Test]
+        public void Test05()
+        {
+            Assert.AreEqual(576, FunctionsOfIntegersOnCartesianPlane.sumsum(8));
+        }
+    }
+}
